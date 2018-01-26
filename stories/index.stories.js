c83ce8e6377744a6f9feca296683af84574be715
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-26 11:18:36 -0500

    remove storybook add-on notes
    - removed storybook add-on notes from repo
    - removed site logo story using the notes add-on

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { withNotes } from '@storybook/addon-notes';
 import siteLogoDocs from '../src/components/atoms/media/site-logo/SiteLogo.md'
 import siteLogoData from '../src/components/atoms/media/site-logo/SiteLogo.json'
 
@@ -15,8 +14,9 @@ import SiteLogo from '../src/components/atoms/media/site-logo/SiteLogo';
 import Footer from '../src/components/organisms/Footer';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
 storiesOf('Atoms/Media/Site Logo', module)
-  .add('Site Logo', withNotes(siteLogoDocs)(() => <SiteLogo data={siteLogoData} />));
+  .add('Site Logo', () => <SiteLogo data={siteLogoData} />);
 
 storiesOf('Footer', module)
   .add('Footer', () => <Footer />);
