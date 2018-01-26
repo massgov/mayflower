d72dc254ee520faea950ed6a1cb8f15125f98979
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-26 11:21:11 -0500

    update syntax per eslint

--- src/components/atoms/media/site-logo/SiteLogo.js
+++ src/components/atoms/media/site-logo/SiteLogo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from '@massds/mayflower/images/pilot-logo.png';
 
-const SiteLogo = ({data}) => {
-  const {url} = data;
+const SiteLogo = ({ data }) => {
+  const { url } = data;
   return (
     <div className="ma__site-logo">
       <a href={url.domain ? url.domain : '' + '/'} title="Mass.gov home page">
