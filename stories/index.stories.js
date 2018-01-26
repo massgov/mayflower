1a56bb18e71d41af415ecb3973a61d2beb44ac0f
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-26 11:12:47 -0500

    install info and knob storybook add ons
    - installed info add-on
    - installed knobs add-on
    - added test component story using the info and knob add-ons

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,6 +3,17 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+
+
+const stories = storiesOf('Storybook Knobs', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+
+//storiesOf('Storybook Knobs', module).addDecorator(withKnobs);
 
 import '@massds/mayflower/css/index-generated.css';
 import '@massds/mayflower/css/base-theme-generated.css';
@@ -11,8 +22,10 @@ import { Welcome } from '@storybook/react/demo';
 import Button from '../src/components/atoms/Button';
 import Footer from '../src/components/organisms/Footer';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-storiesOf('Atoms/Button', module)
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
+
+storiesOf('Atoms/Button', module).addDecorator(withKnobs)
   .add('Button', () => <Button>Submit</Button>)
   .add('Button with info', () => <Button info="Testing info">Submit</Button>)
   .add('Button with text', () => <Button text="Example text"></Button>)
@@ -21,7 +34,26 @@ storiesOf('Atoms/Button', module)
   .add('Button with theme', () => <Button theme="secondary">Submit</Button>)
   .add('Button with href', () => <Button href="http://www.google.com">Click></Button>)
   .add('Button as quarternary', () => <Button theme='quaternary'></Button>)
-  .add('Button as secondary', () => <Button theme='secondary'></Button>);
+  .add('Button as secondary', () => <Button theme='secondary'></Button>)
+  .add('Button Test', 
+    withInfo(`
+      Testing this... 
+    
+      ~~~js
+      <Button>Click Here</Button>
+      ~~~
+    
+    `)(() => {
+    	const theme = text('theme','quaternary');
+    	const type = text('type','submit');
+    	const outline = boolean('outline', false);
+    	const size = text('size','small');
+    	const info = text('info','information')
+    	return(<Button theme={theme} type={type} size={size} info={info}></Button>)
+    })
+  );
+
+
 
 storiesOf('Organisms', module)
   .add('Footer', () => <Footer />);
