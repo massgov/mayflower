f9beef8752c61eb19b9e9e5ba2aad97bb09761aa
Matt Davis <matt.davis@mediacurrent.com>
2018-01-26 08:32:26 -0800

    Moving button into buttons subdir

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,11 +7,11 @@ import '@massds/mayflower/css/index-generated.css';
 import '@massds/mayflower/css/base-theme-generated.css';
 
 import { Welcome } from '@storybook/react/demo';
-import Button from '../src/components/atoms/Button';
+import Button from '../src/components/atoms/buttons/Button';
 import Footer from '../src/components/organisms/Footer';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-storiesOf('Atoms/Button', module)
+storiesOf('Atoms/Buttons/Button', module)
   .add('Button', () => <Button>Submit</Button>)
   .add('Button with text', () => <Button text="Example text" />)
   .add('Button with size', () => <Button size="small">Submit</Button>)
