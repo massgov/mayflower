37c8b96018b0e44e274d04a5faf9e233011d1f78
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-26 09:06:38 -0500

    Revert "add props to site logo"
    This reverts commit d5f5884d99fcafe408f84ec85e161416391a330f.

--- src/components/atoms/media/site-logo/SiteLogo.js
+++ src/components/atoms/media/site-logo/SiteLogo.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import logo from '@massds/mayflower/images/pilot-logo.png';
-import PropTypes from "prop-types";
 
-const SiteLogo = ({ title, alt, width, height, src, data}) => {
-  const { url } = data;
+const SiteLogo = ({data}) => {
+  const {url} = data;
   return (
     <div className="ma__site-logo">
-      <a href={url.domain ? url.domain : '' + '/'} title={title}>
-        <img src={src} alt={alt} width={width} height={height}/>
+      <a href={url.domain ? url.domain : '' + '/'} title="Mass.gov home page">
+        <img src={logo} alt="Mass.gov" width="164" height="75"/>
       </a>
     </div>
   )
 };
-
-SiteLogo.propTypes = {
-	title: PropTypes.string.isRequired,
-	alt: PropTypes.string.isRequired,
-	width: PropTypes.string.isRequired,
-	height: PropTypes.string.isRequired,
-	src: PropTypes.string.isRequired
-}
-
-SiteLogo.defaultProps = {
-	title: "Mass.gov home page",
-	alt: "Mass.gov",
-	width: "164",
-	height: "75",
-	src: "/static/media/pilot-logo.b57e29e9.png"
-}
 
 export default SiteLogo;
