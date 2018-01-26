c0a0bca26c40df52308bcdb8e93cda4a8f748470
Ian Sholtys <ian.sholtys@mediacurrent.com>
2018-01-26 16:32:49 -0500

    DP-7594 Resolved warnings and errors

--- src/components/atoms/buttons/Button.stories.js
+++ src/components/atoms/buttons/Button.stories.js
@@ -5,12 +5,12 @@ import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
 storiesOf('Atoms/Buttons/Button', module)
-  .add('Button', () => <Button>Submit</Button>)
-  .add('Button with info', () => <Button info="Testing info">Submit</Button>)
-  .add('Button with text', () => <Button text="Example text"></Button>)
-  .add('Button with type', () => <Button type="submit">Submit</Button>)
-  .add('Button with size', () => <Button size="small">Submit</Button>)
-  .add('Button with theme', () => <Button theme="secondary">Submit</Button>)
-  .add('Button with href', () => <Button href="http://www.google.com">Click></Button>)
-  .add('Button as quarternary', () => <Button theme='quaternary'></Button>)
-  .add('Button as secondary', () => <Button theme='secondary'></Button>);
+  .add('Button', () => <Button />)
+  .add('Button with info', () => <Button info="Testing info" />)
+  .add('Button with text', () => <Button text="Example text" />)
+  .add('Button with type', () => <Button type="submit" />)
+  .add('Button with size', () => <Button size="small" />)
+  .add('Button with theme', () => <Button theme="secondary" />)
+  .add('Button with href', () => <Button href="http://www.google.com" text="Click >" />)
+  .add('Button as quarternary', () => <Button theme="quaternary" />)
+  .add('Button as secondary', () => <Button theme="secondary" />);
