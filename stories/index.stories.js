2a60e5a98bb5a071fc0373467aa988a0da4c9f3e
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-25 21:16:21 -0500

    add quart and sec button, edit button size to string val
    - added quaternary button story
    - added secondary button story
    - fixed size prop as expects string val small

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -17,8 +17,11 @@ storiesOf('Atoms/Button', module)
   .add('Button with info', () => <Button info="Testing info">Submit</Button>)
   .add('Button with text', () => <Button text="Example text"></Button>)
   .add('Button with type', () => <Button type="submit">Submit</Button>)
-  .add('Button with size', () => <Button size="74">Submit</Button>)
+  .add('Button with size', () => <Button size="small">Submit</Button>)
   .add('Button with theme', () => <Button theme="secondary">Submit</Button>)
-  .add('Button with href', () => <Button href="http://www.google.com">Click></Button>);
+  .add('Button with href', () => <Button href="http://www.google.com">Click></Button>)
+  .add('Button as quarternary', () => <Button theme='quaternary'></Button>)
+  .add('Button as secondary', () => <Button theme='secondary'></Button>);
+
 storiesOf('Organisms', module)
   .add('Footer', () => <Footer />);
