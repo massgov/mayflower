ff679bf0ab9ab874093cfecf2bcc2ac995e66cc3
Matt Davis <matt.davis@mediacurrent.com>
2018-01-26 07:56:25 -0800

    Fewer stories for button

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -14,14 +14,10 @@ import Footer from '../src/components/organisms/Footer';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 storiesOf('Atoms/Button', module)
   .add('Button', () => <Button>Submit</Button>)
-  .add('Button with info', () => <Button info="Testing info">Submit</Button>)
   .add('Button with text', () => <Button text="Example text"></Button>)
-  .add('Button with type', () => <Button type="submit">Submit</Button>)
   .add('Button with size', () => <Button size="small">Submit</Button>)
   .add('Button with theme', () => <Button theme="secondary">Submit</Button>)
-  .add('Button with href', () => <Button href="http://www.google.com">Click></Button>)
-  .add('Button as quarternary', () => <Button theme='quaternary'></Button>)
-  .add('Button as secondary', () => <Button theme='secondary'></Button>);
+  .add('Button with href', () => <Button href="http://www.google.com">Click></Button>);
 
 storiesOf('Organisms', module)
   .add('Footer', () => <Footer />);
