200827fbea485c0858ddf69da6daaaee106b8c93
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-26 15:46:37 -0500

    Revert "Revert "Merge branch 'DP-7569-button-component-MD' of https://github.com/massgov/mayflower-react into DP-7569-button-component-MD""
    This reverts commit df6354e145c394ebdcfff6520ef8d17bb4f29ca9.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,20 +5,20 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { Welcome } from '@storybook/react/demo';
 
 import siteLogoDocs from '../src/components/atoms/media/site-logo/SiteLogo.md';
 import siteLogoData from '../src/components/atoms/media/site-logo/SiteLogo.json';
 
-
 import '@massds/mayflower/css/index-generated.css';
 import '@massds/mayflower/css/base-theme-generated.css';
 
-import { Welcome } from '@storybook/react/demo';
-import Button from '../src/components/atoms/Button';
+import SiteLogo from '../src/components/atoms/media/site-logo/SiteLogo';
+import Button from '../src/components/atoms/buttons/Button';
 import Footer from '../src/components/organisms/Footer';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-storiesOf('Atoms/Button', module)
+storiesOf('Atoms/Buttons/Button', module)
   .add('Button', () => <Button>Submit</Button>)
   .add('Button with info', () => <Button info="Testing info">Submit</Button>)
   .add('Button with text', () => <Button text="Example text"></Button>)
@@ -28,6 +28,9 @@ storiesOf('Atoms/Button', module)
   .add('Button with href', () => <Button href="http://www.google.com">Click></Button>)
   .add('Button as quarternary', () => <Button theme='quaternary'></Button>)
   .add('Button as secondary', () => <Button theme='secondary'></Button>);
+
+storiesOf('Atoms/Media/Site Logo', module)
+  .add('Site Logo', () => <SiteLogo data={siteLogoData} />);
 
 storiesOf('Organisms', module)
   .add('Footer', () => <Footer />);
