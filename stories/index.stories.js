64d474a970ee6f6b6649f29858372129f0d6ab8a
Steven Murray <steven.murray@mediacurrent.com>
2018-01-26 11:50:03 -0500

    DP-7607: Adding info, knobs add-ons.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { linkTo } from '@storybook/addon-links';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+
 
 import '@massds/mayflower/css/index-generated.css';
 import '@massds/mayflower/css/base-theme-generated.css';
