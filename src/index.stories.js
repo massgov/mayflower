c6eaf3d888072d29000602965c6da6a8ca635705
Ian Sholtys <ian.sholtys@mediacurrent.com>
2018-01-26 19:01:34 -0500

    DP-7583 Port CompHeading to React

--- src/index.stories.js
+++ src/index.stories.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 import { Welcome } from '@storybook/react/demo';
 
 import '@massds/mayflower/css/index-generated.css';
