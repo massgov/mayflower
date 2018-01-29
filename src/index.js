2bb13044a692adb5fd151fd112bc49499a0abe03
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-29 16:52:16 -0500

    update index to include reference to daterange and selectbox components.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ export { SocialIcon } from './components/SocialIcon';
 export { Button } from './components/atoms/buttons/Button';
 export { SiteLogo } from './components/atoms/media/site-logo/SiteLogo';
 export { InputDate } from './components/atoms/forms/InputDate';
+export { SelectBox } from './components/atoms/forms/SelectBox';
 export { LatLonGlobe } from './components/atoms/icons/LatLonGlobe/LatLonGlobe';
 
 // @molecules
 export { FooterLinks } from './components/molecules/FooterLinks';
 export { SocialLinks } from './components/molecules/SocialLinks';
+export { DateRange } from './components/molecules/DateRange/DateRange'
 
 // @organisms
 export { Footer } from './components/organisms/footer/Footer';
