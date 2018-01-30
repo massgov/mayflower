03b97061cbe88a760ae740a97b3ac33b1391e18c
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-30 12:26:58 -0500

    Revert "rename search icon jsx file to match current folder structure format"
    This reverts commit 8012399d17b28f12dfbbd86220f0f1f3fa28a76b.

--- src/components/atoms/icons/SvgSearch/SvgSearch.stories.js
+++ src/components/atoms/icons/SvgSearch/SvgSearch.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import SvgSearch from './SvgSearch';
+import SvgSearch from './index';
 
 storiesOf('Atoms/Icons', module)
   .add('SVG Search', () => <SvgSearch />);
