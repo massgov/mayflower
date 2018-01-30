8880b684d9e191012c682846bfe2b82df4d7dab4
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-30 14:20:03 -0500

    wip orgselector

--- src/components/molecules/OrgSelector/OrgSelector.js
+++ src/components/molecules/OrgSelector/OrgSelector.js
@@ -8,30 +8,26 @@ class OrgSelector extends React.Component {
     super(props)
     this.state = {
     }
+    this.renderOrgData = this.renderOrgData.bind(this)   
   }
 
   componentDidMount() {
   }
 
+  renderOrgData (selectedIndex, selected, selectedValue) {
+    if(selectedValue){
+      OrgInfo(this.props.organizations[selectedIndex])
+    }
+  }
+
   render () {
-
-    const options = this.props.organizations.map(function(option) {
-      var org = {}
-      org['value'] = option.value;
-      org['text'] = option.jobTitle;
-      return org
-    })
-
-    console.log(this.props.organizations)
 
     return(
       <section className="ma__org-selector js-org-selector">
-        <SelectBox options={options} label={this.props.selectBox.label}></SelectBox>
+        <SelectBox options={this.props.selectBox.options} label={this.props.selectBox.label} onChangeCallback={this.renderOrgData}></SelectBox>
         <div className="ma__org-selector__org-info js-org-info"></div>
-        <script>
-          
-        </script>
-      </section>
+          <OrgInfo></OrgInfo>
+        </section>
     );
 
   };
@@ -64,8 +60,9 @@ OrgSelector.propTypes = {
 };
 
 OrgSelector.defaultProps = {
-  selectBox: {label: 'State organization', required: false},
+  selectBox: {label: 'State organization', required: false, options: [{value: '', text: 'All Organizations'},{value: 'attorney-general-office', text: 'Attorney General\'s Office'},{value: 'governors-office', text: 'Governor\'s Office'}]},
   organizations: [
+  { value: '' },
   { value: 'attorney-general-office',
     image: { href: '#', alt: 'Maura Healey', src: '../../assets/images/placeholder/100x100.png', height: '100', width: '100', },
     name: { href: '#', text: 'Maura Healey', },
@@ -80,6 +77,56 @@ OrgSelector.defaultProps = {
     message: 'The Governor is the chief lawyer and law enforcement officer of the Commonwealth of Massachusetts. In addition, her office is an advocate and resource for the Commonwealth and its residents.',
     moreLink: { href: '#', text: 'more information', info: 'learn more about the Organization', },
   }]
+}
+
+const OrgInfo = (props) => {
+  if(props) {
+    const org = props
+    console.log(org)
+    return (
+        <section className="ma__org-info">
+          <div className="ma__org-info__header">
+            { org.image.src && (
+                <div className="ma__org-info__image">
+                  <a href={org.image.href}>
+                    <img
+                      alt={org.image.alt} 
+                      src={org.image.src } 
+                      height={org.image.height} 
+                      width={org.image.width} />
+                  </a>
+                </div> 
+            )}
+            <div className="ma__org-info__title">
+              <h2 className="ma__org-info__name">
+                <span className="ma__decorative-link">
+                  <a href={org.name.href}>{org.name.text}&nbsp;<svg aria-hidden="true" id="SvgjsSvg1000" xmlns="http://www.w3.org/2000/svg" version="1.1" width="16" height="18" viewBox="0 0 16 18"><defs id="SvgjsDefs1001"></defs><path id="SvgjsPath1007" d="M983.721 1887.28L983.721 1887.28L986.423 1890L986.423 1890L986.423 1890L983.721 1892.72L983.721 1892.72L978.318 1898.17L975.617 1895.45L979.115 1891.92L971.443 1891.92L971.443 1888.0700000000002L979.103 1888.0700000000002L975.617 1884.5500000000002L978.318 1881.8300000000002Z " transform="matrix(1,0,0,1,-971,-1881)"></path></svg></a>
+                </span>
+              </h2>
+              { org.jobTitle && (
+                  <h3 className="ma__org-info__job-title">
+                    {org.jobTitle}
+                  </h3>
+              )}
+            </div>
+          </div>
+          <div className="ma__org-info__content">
+            <div className="ma__rich-text">
+              {org.message}
+            </div>
+            { org.moreLink.href && (
+                <div className="ma__org-info__link">
+                  <span className="ma__decorative-link">
+                    <a 
+                      href={org.moreLink.href}
+                      aria-label={org.moreLink.info}>{org.moreLink.text}&nbsp;<svg aria-hidden="true" id="SvgjsSvg1000" xmlns="http://www.w3.org/2000/svg" version="1.1" width="16" height="18" viewBox="0 0 16 18"><defs id="SvgjsDefs1001"></defs><path id="SvgjsPath1007" d="M983.721 1887.28L983.721 1887.28L986.423 1890L986.423 1890L986.423 1890L983.721 1892.72L983.721 1892.72L978.318 1898.17L975.617 1895.45L979.115 1891.92L971.443 1891.92L971.443 1888.0700000000002L979.103 1888.0700000000002L975.617 1884.5500000000002L978.318 1881.8300000000002Z " transform="matrix(1,0,0,1,-971,-1881)"></path></svg></a>
+                  </span>
+                </div>
+            )}
+        </div>
+        </section>
+      )
+  } else { return (<div></div>)}
 }
 
 export default OrgSelector;
