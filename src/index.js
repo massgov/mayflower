337deadce8d2be03663cfccb6adcd8fe2b192be8
avrilpearl <sienna.avril.pearl@gmail.com>
2018-01-30 12:32:51 -0500

    update src/index to reference standardized component folder structure/file name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export { SvgArrow } from './components/atoms/icons/SvgArrow/index';
 export { Button } from './components/atoms/buttons/Button';
 export { SiteLogo } from './components/atoms/media/site-logo/SiteLogo';
 export { LatLonGlobe } from './components/atoms/icons/LatLonGlobe/LatLonGlobe';
-export { SvgSearch } from './components/atoms/icons/SvgSearch';
+export { SvgSearch } from './components/atoms/icons/index';
 export { SvgArrowBent } from './components/atoms/icons/SvgArrowBent/index';
 
 // @molecules
