6772fe9006cc8f6f08811802602f817665b4e1ae
Jes Constantine <jesconstantine@gmail.com>
2018-01-31 10:02:04 -0500

    Configure PressFilter knobs
    @todo create "options" arrays to import in story files

--- src/components/organisms/byAuthor/PressFilters/PressFilters.stories.js
+++ src/components/organisms/byAuthor/PressFilters/PressFilters.stories.js
@@ -12,7 +12,7 @@ storiesOf('Organisms/By-Author/PressFilters', module).addDecorator(withKnobs)
       
       ### Description
       
-      This is the standard date range pattern
+      This is the standard press filters pattern
     
       ~~~js
       <PressFilters></PressFilters>
@@ -20,6 +20,18 @@ storiesOf('Organisms/By-Author/PressFilters', module).addDecorator(withKnobs)
     
     `)(() => {
 
+          // @todo define somewhere so that we can use them whenever we implement this pattern
+          const headingLevels = {
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6
+          };
+
+
+          // @todo define somewhere so that we can use them whenever we implement this pattern
           const colorOptions = {
             "": "grey (default)",
             green: "green"
@@ -28,44 +40,56 @@ storiesOf('Organisms/By-Author/PressFilters', module).addDecorator(withKnobs)
           const action = text('Action','#');
           const coloredHeading = {
             coloredHeading: {
-              title:  text('title', "Filter Results"),
-              color:  select('color', colorOptions),
+              text:  text('Colored Heading Text', "Filter Results"),
+              color:  select('Colored Heading Color', colorOptions, ""),
+              level: select('Colored Heading Level', headingLevels, 2)
             }
           };
 
+          // @todo define somewhere so that we can use them whenever we implement this pattern
           const topicOptions = [{text: "Select", value: ""},{text: "Topic 1", value: "topic1",}, {text: "Topic 2", value: "topic2"},{text: "Topic 3", value: "topic3"}];
 
           const topic = {
-            label: text('label', "Filter by Topic"),
-            id: text('id', "topic"),
-            options: object('topics', topicOptions)
+            label: text('Topic Filter label', "Filter by Topic"),
+            id: "topic",
+            options: object('Topic Filter options', topicOptions)
           };
 
+          // @todo define somewhere so that we can use them whenever we implement this pattern
           const pressTypeOptions = [{text: "Select", value: ""},{text: "Press Release", value: "press-release",}, {text: "Press Statement", value: "press-statement"},{text: "News Article", value: "news-article"}, {text: "Blog Post", value: "blog-poast"},{text: "Speech", value: "speech"}];
 
+          // @todo define somewhere so that we can use them whenever we implement this pattern
           const pressType = {
-            label: text('label', "Filter by Announcement Type"),
-            id: text('id', "announcement-type"),
-            options: object('news type', pressTypeOptions)
+            label: text('Press Type label', "Filter by Announcement Type"),
+            id: "announcement-type",
+            options: object('Press Type Options', pressTypeOptions)
           };
 
+          // If we want to augment the default for the orgSelector uncomment and populate below.
           // const orgSelector = {}
 
+          // @todo define somewhere so that we can use them whenever we implement this pattern
           const defaultStartDate = {labelText: 'Select a start date', required: false, id: 'start-date', name: 'start-date', placeholder: 'mm/dd/yyyy', restrict: 'max'};
           const defaultEndDate = {labelText: 'Select an end date', required: false, id: 'end-date', name: 'end-date', placeholder: 'today', restrict: 'max'};
 
           const dateRange = {
-            label: text('label', 'Date range'),
-            startDate: object('startDate', defaultStartDate),
-            endDate: object('endDate', defaultEndDate)
-          }
+            label: text('Date Range label', 'Date range'),
+            startDate: object('Date Range startDate', defaultStartDate),
+            endDate: object('Date Range endDate', defaultEndDate)
+          };
+
+          // @todo define somewhere so that we can use them whenever we implement this pattern
+          const buttonThemeOptions = {'': 'primary (default)', secondary: 'secondary', quaternary: 'quaternary'};
+          const buttonSizeOptions = { '': 'default', small: 'small' };
+          const buttonTypeOptions = {"": "no button type", submit: 'submit', reset: 'reset', button: 'button'};
+
           const submitButton = {
-            text: text("text", "Submit"),
-            type: text("type", "submit"),
-            size: text("size", "small"),
-            theme: text('theme',""),
-            outline: boolean('outline', false)
-          }
+            text: text("Button text", "Submit"),
+            type: select("Button type", buttonTypeOptions, "submit"),
+            size: select("Button size", buttonSizeOptions, "small"),
+            theme: select("Button theme", buttonThemeOptions, ""),
+            outline: boolean('Button Outline', false)
+          };
 
           return(<PressFilters action={action} coloredHeading={coloredHeading} topic={topic} pressType={pressType} dateRange={dateRange} submitButton={submitButton}/>)
         })
