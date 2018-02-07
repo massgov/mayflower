4d96f4e50707801bd63c5f11fbcf3e33754cafa6
avrilpearl <sienna.avril.pearl@gmail.com>
2018-02-07 10:47:54 -0500

    change pagination component to function

--- src/components/molecules/Pagination/index.js
+++ src/components/molecules/Pagination/index.js
@@ -2,28 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-class Pagination extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // selectedOrg: {}
-    };
-    // this.setSelectedOrgState = this.setSelectedOrgState.bind(this);
-  }
-  // { pagination.prev.disabled ? disabled : '' }>
-  render() {
-    const pagination = this.props;
-    return(
-      <div className="ma__pagination js-pagination">
-        <div className="ma__pagination__container">
-          <button
-            className="ma__pagination__prev js-pagination-prev"
-            type="button"
-            disabled={pagination.prev.disabled}
-          >
-            {pagination.prev.text}
-          </button>
-          { pagination.pages.map(page => (
+const Pagination = pagination => (
+  <div className="ma__pagination js-pagination">
+    <div className="ma__pagination__container">
+      <button
+        className="ma__pagination__prev js-pagination-prev"
+        type="button"
+        disabled={pagination.prev.disabled}
+      >
+        {pagination.prev.text}
+      </button>
+      { pagination.pages.map(page => (
             page.text === 'spacer' ?
               <span className="ma__pagination__spacer">&hellip;</span> :
               <button
@@ -34,18 +23,16 @@ class Pagination extends React.Component {
                 {page.text}
               </button>
           ))}
-          <button
-            className="ma__pagination__next js-pagination-next"
-            type="button"
-            disabled={pagination.next.disabled}
-          >
-            {pagination.next.text}
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+      <button
+        className="ma__pagination__next js-pagination-next"
+        type="button"
+        disabled={pagination.next.disabled}
+      >
+        {pagination.next.text}
+      </button>
+    </div>
+  </div>
+);
 
 Pagination.propTypes = {
   next: PropTypes.shape({
