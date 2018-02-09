8500b394b97b09c3acdbcdaaa53306d6fdafdfae
avrilpearl <sienna.avril.pearl@gmail.com>
2018-02-09 12:16:07 -0500

    add keyindex to sortresults

--- src/components/molecules/SortResults/index.js
+++ src/components/molecules/SortResults/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonSort from '../../atoms/buttons/ButtonSort';
+import keyIndex from 'react-key-index';
 
-const SortResults = (sortResults) => (
-  <div className="ma__sort-results js-sort-results">
+const SortResults = (sortResults) => {
+  this.listIndex = 1;
+  const keyButtonSort = keyIndex(sortResults.sortButtons, 1);
+  console.log(keyButtonSort)
+  return (
+    <div className="ma__sort-results js-sort-results">
     {sortResults.label &&
         (<span className="ma__sort-results__label">{sortResults.label}</span>)
       }
-    {sortResults.sortButtons.map((buttonSort) => (<ButtonSort {...buttonSort} />))}
+    {keyButtonSort.map((buttonSort) => (<ButtonSort {...buttonSort} />))}
   </div>
-);
+    )
+};
 
 SortResults.propTypes = {
   /** The label text of the sort buttons */
   label: PropTypes.string,
   /** An array of sort button objects */
-  sortButtons: PropTypes.arrayOf(PropTypes.instanceOf(ButtonSort)).isRequired
+  //sortButtons: PropTypes.shape(ButtonSort.propTypes).isRequired
+  sortButtons: PropTypes.arrayOf(PropTypes.shape(ButtonSort.propTypes)).isRequired
 };
 
 export default SortResults;
