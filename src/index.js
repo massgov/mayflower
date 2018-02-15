c62663cd4040f432d0743e7579383fcf07a33544
iansholtys <ian.m.sholtys@gmail.com>
2018-02-15 15:08:07 -0500

    Added missing component exports (#52)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@
 // @atoms/@buttons
 export Button from './components/atoms/buttons/Button';
 export ButtonSearch from './components/atoms/buttons/ButtonSearch';
+export ButtonSort from './components/atoms/buttons/ButtonSort';
 // @atoms/@forms
 export InputDate from './components/atoms/forms/InputDate';
 export InputText from './components/atoms/forms/InputText';
@@ -14,8 +15,8 @@ export SelectBox from './components/atoms/forms/SelectBox';
 // @atoms/@headings
 export ColoredHeading from './components/atoms/headings/ColoredHeading';
 export CompHeading from './components/atoms/headings/CompHeading';
-export SidebarHeading from './components/atoms/headings/SidebarHeading';
 export Heading from './components/atoms/headings/Heading';
+export SidebarHeading from './components/atoms/headings/SidebarHeading';
 // @atoms/@icons
 export LatLonGlobe from './components/atoms/icons/LatLonGlobe/LatLonGlobe';
 export SvgArrow from './components/atoms/icons/SvgArrow';
@@ -24,22 +25,30 @@ export SvgSearch from './components/atoms/icons/SvgSearch';
 // @atoms/@links
 export DecorativeLink from './components/atoms/links/DecorativeLink';
 // @atoms/@lists
+export OrderedList from './components/atoms/lists/OrderedList';
 export UnorderedList from './components/atoms/lists/UnorderedList';
 // @atoms/@media
 export SiteLogo from './components/atoms/media/SiteLogo';
+// @atoms/@table
+export Table from './components/atoms/table/Table';
 // @atoms/@text
 export Paragraph from './components/atoms/text/Paragraph';
 // @molecules
-export MainNav from './components/molecules/MainNav';
+export CalloutLink from './components/molecules/CalloutLink';
 export DateRange from './components/molecules/DateRange';
 export FooterLinks from './components/molecules/FooterLinks';
+export HeaderSearch from './components/molecules/HeaderSearch';
+export MainNav from './components/molecules/MainNav';
 export OrgSelector from './components/molecules/OrgSelector';
+export Pagination from './components/molecules/Pagination';
 export PressTeaser from './components/molecules/PressTeaser';
+export ResultsHeading from './components/molecules/ResultsHeading';
 export SearchBannerForm from './components/molecules/SearchBannerForm';
 export SocialLinks from './components/molecules/SocialLinks';
+export SortResults from './components/molecules/SortResults';
 
 // @organisms
 export Footer from './components/organisms/Footer';
-export RichText from './components/organisms/RichText';
 export PressFilters from './components/organisms/PressFilters';
+export RichText from './components/organisms/RichText';
 export UtilityPanel from './components/organisms/UtilityPanel';
