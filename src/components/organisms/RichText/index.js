41c00949178666f5a7071bb11fd0ee2ee0a47696
Steven Murray <smurrayatwork@gmail.com>
2018-03-20 20:30:13 -0400

    Adds support for Object.entries in RichText. (#123)
    * Adds support for Object.entries in RichText.
    
    * Updated check to use object entries shim.

--- src/components/organisms/RichText/index.js
+++ src/components/organisms/RichText/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import entries from 'object.entries';
 // import child components
 import DecorativeLink from '../../atoms/links/DecorativeLink/index';
 import CompHeading from '../../atoms/headings/CompHeading/index';
@@ -9,6 +9,9 @@ import Paragraph from '../../atoms/text/Paragraph';
 import UnorderedList from '../../atoms/lists/UnorderedList';
 
 const RichText = (props) => {
+  if (!Object.entries) {
+    entries.shim();
+  }
   const headerIndent = props.headerIndent ? 'js-ma-outline-indent' : '';
   const anchorLinks = props.anchorLinks ? 'js-ma-insert-heading-anchors' : '';
   const children = React.Children.toArray(props.children);
