a9e91d254fa10fd4dfe5ba5d93ac4d6d7783cbba
Steven Murray <smurrayatwork@gmail.com>
2018-05-15 16:05:05 -0400

    Removes un-needed grouping tags from chevron. (#182)

--- src/components/atoms/icons/SvgChevron/index.js
+++ src/components/atoms/icons/SvgChevron/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 const SvgChevron = () => (
   <svg width="59" height="38" viewBox="0 0 59 38" xmlns="http://www.w3.org/2000/svg">
-    <g id="Page-1" fill="none" fillRule="evenodd">
-      <g id="Icons" transform="translate(-842 -1931)" fill="#000">
-        <g id="24---Down-(Solid)" transform="translate(842 1931)">
-          <path
-            d="M29.414,37.657 L0.344,8.586 L8.828,0.102 L29.414,20.686 L50,0.1 L58.484,8.585 L29.414,37.657"
-            id="Down"
-          />
-        </g>
-      </g>
-    </g>
+    <path
+      d="M29.414,37.657 L0.344,8.586 L8.828,0.102 L29.414,20.686 L50,0.1 L58.484,8.585 L29.414,37.657"
+      id="Down"
+    />
   </svg>
 );
 
