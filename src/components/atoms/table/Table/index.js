1e625f737e4e1ce087730b56166f5373980a903a
Sienna Svob <sienna.avril.pearl@gmail.com>
2018-06-22 13:08:40 -0400

    Update Table Atom Component  (#132)
    * wip table
    
    * update table atom to consume and render true data
    
    * Bug fixes.
    
    * Fixes bug causing row/col span to not display properly.
    
    * Updated rol/col span logic.
    
    * Wraps up ticket work.
    
    * Updates markdown.

--- src/components/atoms/table/Table/index.js
+++ src/components/atoms/table/Table/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Table = (props) => {
-  const raw = {
-    __html: props.text
-  };
+const Table = (table) => {
+  let tableWideClass = '';
+  let tableHeaderRow = [];
+  if (table.head && table.head.rows) {
+    tableHeaderRow = table.head.rows[table.head.rows.length - 1];
+    if (tableHeaderRow.cells.length > 3) {
+      tableWideClass = ' ma__table--wide';
+    }
+  }
   return(
-    <p dangerouslySetInnerHTML={raw} />
+    <table className={`ma__table${tableWideClass}`}>
+      {table.head && table.head.rows &&
+        <thead>
+          { table.head.rows.map((row, rowIndex) => (
+            <tr key={row.key || `thead-row${rowIndex}`}>
+              { row.cells.map((cell, cellIndex) => (
+                <th
+                  key={cell.key || `thead-row${rowIndex}-cell${cellIndex}`}
+                  scope="col"
+                  colSpan={cell.colspan || null}
+                  rowSpan={cell.rowspan || null}
+                >{cell.text}
+                </th>
+            ))}
+            </tr>
+          ))}
+        </thead>
+      }
+      { table.bodies.map((body, bodyIndex) => (
+        <tbody key={body.key || `tbody${bodyIndex}`}>
+          { body.rows.map((row, rowIndex) => {
+            const tableIndexOffset = row.rowSpanOffset ? 0 : 1;
+            const defaultRowKey = `tbody${bodyIndex}-row${rowIndex}`;
+            return(
+              <tr key={row.key || defaultRowKey} className={row.rowSpanOffset ? 'is-offset' : null}>
+                { row.cells.map((cell, cellIndex) => {
+                  let dataLabel = null;
+                  if (tableHeaderRow.length > 0) {
+                    // Mayflower twig loops started at 1 - add one to index.
+                    dataLabel = tableHeaderRow.cells[(cellIndex + 1) - tableIndexOffset].text;
+                  }
+                  const defaultCellKey = `${defaultRowKey}-cell${cellIndex}`;
+                  return(cell.heading ? (
+                    <th
+                      key={cell.key || defaultCellKey}
+                      data-label={dataLabel}
+                      scope="row"
+                      colSpan={cell.colspan || null}
+                      rowSpan={cell.rowspan || null}
+                    >{cell.text}
+                    </th>
+                  ) : (
+                    <td
+                      key={cell.key || defaultCellKey}
+                      data-label={dataLabel}
+                      colSpan={cell.colspan || null}
+                      rowSpan={cell.rowspan || null}
+                    >
+                      {cell.text}
+                    </td>
+                  ));
+              })}
+              </tr>);
+          })}
+        </tbody>
+        ))}
+    </table>
   );
 };
 
@@ -18,8 +79,12 @@ Table.propTypes = {
         heading: PropTypes.bool.isRequired,
         colspan: PropTypes.string,
         rowspan: PropTypes.string,
-        text: PropTypes.string.isRequired
-      }))
+        text: PropTypes.string.isRequired,
+        // Optional key for cell in case the table is dynamic.
+        key: PropTypes.string
+      })),
+      // Optional key for row in case the table is dynamic.
+      key: PropTypes.string
     }))
   }),
   bodies: PropTypes.arrayOf(PropTypes.shape({
@@ -29,9 +94,14 @@ Table.propTypes = {
         heading: PropTypes.bool.isRequired,
         colspan: PropTypes.string,
         rowspan: PropTypes.string,
-        text: PropTypes.string.isRequired
-      }))
-    }))
+        text: PropTypes.string.isRequired,
+        // Optional key for cell in case the table is dynamic.
+        key: PropTypes.string
+      })),
+      // Optional key for row in case the table is dynamic.
+      key: PropTypes.string
+    })),
+    key: PropTypes.string
   }))
 };
 
