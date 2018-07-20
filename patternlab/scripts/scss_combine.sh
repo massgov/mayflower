4b757a00d086bb834f582a32b3af3fce261ed876
Minghua Sun <clairesunstudio@gmail.com>
2018-07-20 15:05:32 -0400

    run script on atoms

--- patternlab/scripts/scss_combine.sh
+++ patternlab/scripts/scss_combine.sh
@@ -1,12 +1,12 @@
 # cd into patternlab folder
 root=$(pwd)
 topdir=$root/"styleguide/source/assets/scss"
-dir1="01-atoms"
-dir2="06-theme/01-atoms"
+dir1="01-atoms/global"
+dir2="06-theme/"$dir1
 
 for f in $topdir/$dir1/*.scss
 do
-    outf=$topdir/test/`basename $f .scss`.scss
+    outf=$topdir/$dir1/`basename $f .scss`.scss
     cp $f $outf
     
     [ -f $topdir/$dir2/`basename $f` ] && {
