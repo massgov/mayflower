6c98ec67140a0494b3715b5c675b36a63e6eb2d7
Tim Dickens <tim.dickens@mediacurrent.com>
2018-07-20 08:38:35 -0400

    DP-8725 Updating responsive tables for reported issues.

--- patternlab/styleguide/source/assets/js/modules/responsiveTable.js
+++ patternlab/styleguide/source/assets/js/modules/responsiveTable.js
@@ -1,9 +1,6 @@
 import throttle from "../helpers/throttle.js";
 
-export default function (window,document,$,undefined) {
-
-  let responsiveTables = [];
-  let $window = $(window);
+export default function (window, document, $, undefined) {
 
   // Responsive table HTML structure
   // <div class="ma__table--responsive">
@@ -12,6 +9,8 @@ export default function (window,document,$,undefined) {
   //   </div>
   // </div>
 
+  const $window = $(window);
+  let responsiveTables = [];
 
   function setWidths(rt) {
     rt.$table
@@ -23,34 +22,35 @@ export default function (window,document,$,undefined) {
           .width($(this).width());
       });
 
-    // Set width of sticky table head
+    // Set width of sticky table head.
     rt.$stickyHeader.width(rt.$table.width());
 
   }
 
   function calcAllowance($table, $stickyHeader) {
     var a = 0;
-    // Calculate allowance
+    // Calculate allowance.
     $table.find("tbody tr:lt(3)").each(function () {
       a += $(this).height();
     });
 
-    // Set fail safe limit (last three row might be too tall)
-    // Set arbitrary limit at 0.25 of viewport height, or you can use an arbitrary pixel value
+    // Set fail safe limit (last three row might be too tall).
+    // Set arbitrary limit at 0.25 of viewport height,
+    // or you can use an arbitrary pixel value.
     if (a > $window.height() * 0.25) {
       a = $window.height() * 0.25;
     }
 
-    // Add the height of sticky header
+    // Add the height of sticky header.
     a += $stickyHeader.height();
     return a;
   }
 
   function positionStickyHeader(rt) {
-    // Return value of calculated allowance
+    // Return value of calculated allowance.
     let allowance = calcAllowance(rt.$table, rt.$stickyHeader);
-    let visibleParams = getVisibleParams(rt.$table[0]);
-    // Position sticky header based on viewport scrollTop
+    let visibleParams = getVisibleParams(rt.$root[0]);
+    // Position sticky header based on viewport scrollTop.
     if (
       $window.scrollTop() > rt.$table.offset().top &&
       $window.scrollTop() < rt.$table.offset().top + rt.$table.outerHeight() - allowance
@@ -67,14 +67,15 @@ export default function (window,document,$,undefined) {
         additionalOffset += $(".js-scroll-anchors").height();
       }
 
-      // When top of viewport is in the table itself
+      // When top of viewport is in the table itself.
       rt.$stickyHeader.css({
         opacity: 1,
         top: $window.scrollTop() - rt.$table.offset().top + additionalOffset
       });
 
-    } else {
-      // When top of viewport is above or below table
+    }
+    else {
+      // When top of viewport is above or below table.
       rt.$stickyHeader.css({
         opacity: 0,
         top: 0
@@ -86,15 +87,20 @@ export default function (window,document,$,undefined) {
     let canScrollHorizontally = rt.$table.width() > rt.$table.parent().width();
     rt.$root.toggleClass('has-horizontal-scroll', canScrollHorizontally);
 
-    if (canScrollHorizontally && visibleParams.bottomOutOfView && !visibleParams.entirelyOutOfView && scrolledBottom - rt.$table.offset().top > 100) {
+    if (
+        canScrollHorizontally &&
+        visibleParams.bottomOutOfView &&
+        !visibleParams.entirelyOutOfView &&
+        scrolledBottom - rt.$table.offset().top > 100
+      ) {
       rt.$root.find(".ma__table__horizontal-nav").css({
         bottom: (tableBottom - scrolledBottom) + 65
       });
-    } else {
+    }
+    else {
       rt.$root.find(".ma__table__horizontal-nav").css({ bottom: 0 });
     }
   }
-
 
   function initializeTable(element) {
     let $table = $(element).find('table');
@@ -104,7 +110,7 @@ export default function (window,document,$,undefined) {
       let $thead = $table.find("thead").clone();
       $table.after('<table class="ma__table sticky-thead" />');
 
-      // Add class, remove margins, reset width and wrap table
+      // Add class, remove margins, reset width and wrap table.
       $table
       .addClass("sticky-enabled")
       .css({
@@ -134,6 +140,7 @@ export default function (window,document,$,undefined) {
     let topOutOfView = elementTop < pageTop;
     let bottomOutOfView = elementBottom > pageBottom;
     let entirelyOutOfView = pageTop > elementBottom || pageBottom < elementTop;
+
     return {
       topOutOfView: topOutOfView,
       bottomOutOfView: bottomOutOfView,
@@ -141,22 +148,27 @@ export default function (window,document,$,undefined) {
     };
   }
 
-
   function recalcScrollbar(rt) {
     let containerWidth = rt.$table.parent().width();
     let tableWidth = rt.$table.width();
-    let visiblePercentage = (containerWidth/ tableWidth) * 100;
-    let leftVisiblePercentage = Math.abs( (rt.$table.offset().left - rt.$table.parent().offset().left) / tableWidth) * 100;
+    let visiblePercentage = (containerWidth / tableWidth) * 100;
+    let leftVisiblePercentage = Math.abs((rt.$table.offset().left - rt.$table.parent().offset().left) / tableWidth) * 100;
+    let calcOperator;
 
-    rt.$root.find(".ma__scroll-indicator__button").width(`calc(${visiblePercentage}% + 2px)`);
+    if (leftVisiblePercentage == 0) {
+      calcOperator = '-';
+    }
+    else {
+      calcOperator = '+';
+    }
+
     rt.$root.find(".ma__scroll-indicator__button").css({
-      left: `calc(${leftVisiblePercentage}% - 2px)`
+      left: `calc(${leftVisiblePercentage}% ${calcOperator} 2px)`,
+      width: `calc(${visiblePercentage}%)`
     });
   }
 
-
-  // apply scroll-based classes
-
+  // Apply scroll-based classes.
   function applyScrollClasses(rt) {
     let visibleParams = getVisibleParams(rt.$root[0]);
 
@@ -171,7 +183,8 @@ export default function (window,document,$,undefined) {
 
     if (!visibleParams.entirelyOutOfView) {
       $(".ma__floating-action").hide();
-    } else {
+    }
+    else {
       $(".ma__floating-action").show();
     }
   }
@@ -205,18 +218,6 @@ export default function (window,document,$,undefined) {
     });
   }
 
-  $('.js-ma-responsive-table').each((i, el) => initializeTable(el));
-
-
-  $window.on("resize", handleWindowResize);
-
-  $window.on("scroll", handleScroll);
-
-
-
-  var lastScrollAt = Date.now()
-    , scrollTimeout;
-
   function scrollStartStop() {
     var $this = $(this);
 
@@ -235,82 +236,117 @@ export default function (window,document,$,undefined) {
     }, 100);
   }
 
-  $(document).on('scroll', scrollStartStop);
-
-
-  // fire on horizontal scroll of container as well
-  $(".ma__table--responsive__wrapper").on("scroll", throttle(handleScroll, 100));
-
-  $(".ma__table__horizontal-nav__left").click(function() {
-    let $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
-
-    $scrollContainer.animate({
-      scrollLeft: ($scrollContainer.scrollLeft() - 200) < 0 ? 0 : ($scrollContainer.scrollLeft() - 200)
-      }, 250);
-  });
-
-  $(".ma__table__horizontal-nav__right").click(function() {
-    let $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
-    $scrollContainer.animate({
-      scrollLeft: $scrollContainer.scrollLeft() + 200
-    }, 250);
-  });
-
-
-  function handleScrollerInteraction(e) {
-    let $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
-
-    let initialPosition = {
-      x: e.pageX,
-      y: e.pageY
-    };
-    console.log('initial position', initialPosition);
-
-    function handleMouseUp() {
-      // remove listeners
-      $('body').off('mousemove', handleMouseMove).off('mouseup', handleMouseUp);
-    }
-    function handleMouseMove(e) {
-      let newPosition = { x: e.pageX, y: e.pageY };
-
-      $scrollContainer.scrollLeft($scrollContainer.scrollLeft() + newPosition.x - initialPosition.x);
-    }
-
-    // Attach to body so you don't get a disconnected handler if you drag off the bar
-    $('body').on("mouseup", handleMouseUp);
-    $('body').on('mousemove', handleMouseMove);
-
-  }
-
-
+  // Scroller click handler.
   function handleScrollerClick(e) {
-    let $scrollContainer = $(this)
-      .parents(".js-ma-responsive-table")
-      .find(".ma__table--responsive__wrapper");
+    const $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
     let posX = $(this).position().left;
     let midpoint = $(e.target).offset().left + $(e.target).width() / 2;
     let clickpoint = e.pageX - posX;
 
     if (clickpoint < midpoint ) {
-      // scroll left
+      // Scroll left.
       $scrollContainer.animate({
         scrollLeft: $scrollContainer.scrollLeft() - Math.abs(midpoint - clickpoint)
       }, 250);
-    } else {
-      // scroll right
+    }
+    else {
+      // Scroll right.
       $scrollContainer.animate({
         scrollLeft: $scrollContainer.scrollLeft() + Math.abs(midpoint - clickpoint)
       }, 250);
     }
   }
 
-  $(".ma__scroll-indicator__button").on("mousedown", handleScrollerInteraction);
-  // deaden clicking on the scroll button, handle clicking in the trough
-  $(".ma__scroll-indicator__button").on("click", e => e.stopPropagation() );
-  $(".ma__scroll-indicator").on('click', handleScrollerClick);
+  function handleScrollerInteraction(e) {
+    const $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
 
+    let initialPosition = {
+      x: e.pageX,
+      y: e.pageY
+    };
+
+    function handleMouseUp() {
+      // Remove listeners.
+      $('body')
+        .off('mousemove', handleMouseMove)
+        .off('mouseup', handleMouseUp);
+    }
+
+    function handleMouseMove(e) {
+      let newPosition = {
+        x: e.pageX,
+        y: e.pageY
+      };
+
+      $scrollContainer.scrollLeft($scrollContainer.scrollLeft() + newPosition.x - initialPosition.x);
+    }
+
+    // Attach to body so you don't get a
+    // disconnected handler if you drag off the bar.
+    $('body')
+      .on("mouseup", handleMouseUp)
+      .on('mousemove', handleMouseMove);
+
+  }
+
+  // Scrollbar event handlers.
+  function scrollbarEventHandlers() {
+    const amountToScroll = 200;
+    // @todo Cache `$scrollContainer` to avoid multiple lookups.
+
+    // Scrollbar left arrow.
+    $(".ma__table__horizontal-nav__left").click(function() {
+      // On click of left arrow element, animate the movement of the scrollbar
+      // to the left by the integer amount defined in `amountToScroll`.
+      const $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
+      const currentScrollLeft = $scrollContainer.scrollLeft();
+
+      $scrollContainer.animate({
+        scrollLeft: (currentScrollLeft - amountToScroll) < 0 ? 0 : (currentScrollLeft - amountToScroll)
+      }, 250);
+    });
+
+    // Scrollbar right arrow.
+    $(".ma__table__horizontal-nav__right").click(function() {
+      // On click of left arrow element, animate the movement of the scrollbar
+      // to the left by the integer amount defined in `amountToScroll`.
+      const $scrollContainer = $(this).parents(".js-ma-responsive-table").find(".ma__table--responsive__wrapper");
+      $scrollContainer.animate({
+        scrollLeft: $scrollContainer.scrollLeft() + amountToScroll
+      }, 250);
+    });
+
+    // Scroll indicator element.
+    $(".ma__scroll-indicator").on('click', handleScrollerClick);
+
+    $(".ma__scroll-indicator__button")
+      .on("mousedown", handleScrollerInteraction)
+      // Deaden clicking on the scroll button
+      // in order to handle click on parent.
+      .on("click", e => e.stopPropagation());
+
+  }
+
+  let lastScrollAt = Date.now();
+  let scrollTimeout;
+
+  // fire on horizontal scroll of container as well.
+  $(".ma__table--responsive__wrapper").on("scroll", throttle(handleScroll, 100));
+
+  $('.js-ma-responsive-table').each((i, el) => initializeTable(el));
+
+  // Setup resize events.
+  $window.on("resize", handleWindowResize);
+
+  // @todo - Is this needed to run on :attach?
   handleWindowResize();
 
+  $window.on("scroll", handleScroll);
 
-}(window,document,jQuery);
+  // @todo - Clean this up to be cleaner.
+  $(document).on('scroll', scrollStartStop);
 
+  // Setup scrollbar handlers.
+  scrollbarEventHandlers();
+
+}(window, document, jQuery);
