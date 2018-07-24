cb82691e5f6f35a55abce183de9d4459eda70768
avrilpearl <sienna.avril.pearl@gmail.com>
2018-07-23 21:59:45 -0400

    update collapse animation

--- react/src/components/animations/Collapse/index.js
+++ react/src/components/animations/Collapse/index.js
@@ -9,54 +9,16 @@ import Transition, {
   EXITING
 } from 'react-transition-group/Transition';
 
-//import capitalize from './utils/capitalize';
-//import createChainedFunction from './utils/createChainedFunction';
-
-
 function capitalize(string) {
   return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
 }
 
-/**
- * Safe chained function
- *
- * Will only create a new function if needed,
- * otherwise will pass back existing functions or null.
- *
- * @param {function} functions to chain
- * @returns {function|null}
- */
-function createChainedFunction(...funcs) {
-  return funcs.filter(f => f != null).reduce((acc, f) => {
-    if (typeof f !== 'function') {
-      throw new Error(
-        'Invalid Argument Type, must only provide functions, undefined, or null.'
-      );
-    }
-
-    if (acc === null) {
-      return f;
-    }
-
-    return function chainedFunction(...args) {
-      acc.apply(this, args);
-      f.apply(this, args);
-    };
-  }, null);
-}
-
-const MARGINS = {
-  height: ['marginTop', 'marginBottom'],
-  width: ['marginLeft', 'marginRight']
-};
-
-// reading a dimension prop will cause the browser to recalculate,
-// which will let our animations work
-function triggerBrowserReflow(node) {
-  node.offsetHeight; // eslint-disable-line no-unused-expressions
-}
-
 function getDimensionValue(dimension, elem) {
+  const MARGINS = {
+    height: ['marginTop', 'marginBottom'],
+    width: ['marginLeft', 'marginRight']
+  };
+
   let value = elem[`offset${capitalize(dimension)}`];
   let margins = MARGINS[dimension];
 
@@ -67,142 +29,55 @@ function getDimensionValue(dimension, elem) {
   );
 }
 
-const collapseStyles = {
-  [EXITED]: 'collapsed',
-  [EXITING]: 'expanding',
-  [ENTERING]: 'collapsing',
-  [ENTERED]: 'expanded'
-};
-
-const propTypes = {
-  /**
-   * Show the component; triggers the expand or collapse animation
-   */
-  in: PropTypes.bool,
-
-  /**
-   * Wait until the first "enter" transition to mount the component (add it to the DOM)
-   */
-  mountOnEnter: PropTypes.bool,
-
-  /**
-   * Unmount the component (remove it from the DOM) when it is collapsed
-   */
-  unmountOnExit: PropTypes.bool,
-
-  /**
-   * Run the expand animation when the component mounts, if it is initially
-   * shown
-   */
-  appear: PropTypes.bool,
-
-  /**
-   * Duration of the collapse animation in milliseconds, to ensure that
-   * finishing callbacks are fired even if the original browser transition end
-   * events are canceled
-   */
-  timeout: PropTypes.number,
-
-  /**
-   * Callback fired before the component expands
-   */
-  onEnter: PropTypes.func,
-  /**
-   * Callback fired after the component starts to expand
-   */
-  onEntering: PropTypes.func,
-  /**
-   * Callback fired after the component has expanded
-   */
-  onEntered: PropTypes.func,
-  /**
-   * Callback fired before the component collapses
-   */
-  onExit: PropTypes.func,
-  /**
-   * Callback fired after the component starts to collapse
-   */
-  onExiting: PropTypes.func,
-  /**
-   * Callback fired after the component has collapsed
-   */
-  onExited: PropTypes.func,
-
-  /**
-   * The dimension used when collapsing, or a function that returns the
-   * dimension
-   *
-   * _Note: Bootstrap only partially supports 'width'!
-   * You will need to supply your own CSS animation for the `.width` CSS class._
-   */
-  dimension: PropTypes.oneOfType([
-    PropTypes.oneOf(['height', 'width']),
-    PropTypes.func
-  ]),
-
-  /**
-   * Function that returns the height or width of the animating DOM node
-   *
-   * Allows for providing some custom logic for how much the Collapse component
-   * should animate in its specified dimension. Called with the current
-   * dimension prop value and the DOM node.
-   */
-  getDimensionValue: PropTypes.func,
-
-  /**
-   * ARIA role of collapsible element
-   */
-  role: PropTypes.string
-};
-
-const defaultProps = {
-  in: false,
-  timeout: 300,
-  mountOnEnter: false,
-  unmountOnExit: false,
-  appear: false,
-
-  dimension: 'height',
-  getDimensionValue
-};
 
 class Collapse extends React.Component {
-  getDimension() {
-    return typeof this.props.dimension === 'function'
-      ? this.props.dimension()
-      : this.props.dimension;
+  constructor(props) {
+    super(props);
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleEntering = this.handleEntering.bind(this);
+    this.handleEntered = this.handleEntered.bind(this);
+    this.handleExit = this.handleExit.bind(this);
+    this.handleExiting = this.handleExiting.bind(this);
   }
 
-  // for testing
+  getDimension() {
+    return(
+      typeof this.props.dimension === 'function' ? this.props.dimension() : this.props.dimension
+    );
+  }
+
+  // Get initial dimension
   _getScrollDimensionValue(elem, dimension) {
     return `${elem[`scroll${capitalize(dimension)}`]}px`;
   }
 
-  /* -- Expanding -- */
-  handleEnter = elem => {
+  // Reading a dimension prop will cause the browser to recalculate, letting our animations work
+  triggerBrowserReflow(node) {
+    node.offsetHeight;
+  }
+
+  // Expanding
+  handleEnter(elem) {
     elem.style[this.getDimension()] = '0';
   };
 
-  handleEntering = elem => {
+  handleEntering(elem) {
     const dimension = this.getDimension();
     elem.style[dimension] = this._getScrollDimensionValue(elem, dimension);
   };
 
-  handleEntered = elem => {
+  handleEntered(elem) {
     elem.style[this.getDimension()] = null;
   };
 
-  /* -- Collapsing -- */
-  handleExit = elem => {
+  // Collapsing
+  handleExit(elem) {
     const dimension = this.getDimension();
-    elem.style[dimension] = `${this.props.getDimensionValue(
-      dimension,
-      elem
-    )}px`;
-    triggerBrowserReflow(elem);
+    elem.style[dimension] = `${this.props.getDimensionValue(dimension,elem)}px`;
+    this.triggerBrowserReflow(elem);
   };
 
-  handleExiting = elem => {
+  handleExiting(elem) {
     elem.style[this.getDimension()] = '0';
   };
 
@@ -218,27 +93,25 @@ class Collapse extends React.Component {
       ...props
     } = this.props;
 
+    const collapseStyles = {
+      [EXITED]: 'collapsed',
+      [EXITING]: 'expanding',
+      [ENTERING]: 'collapsing',
+      [ENTERED]: 'expanded'
+    };
+
     delete props.dimension;
     delete props.getDimensionValue;
-
-    const handleEnter = createChainedFunction(this.handleEnter, onEnter);
-    const handleEntering = createChainedFunction(
-      this.handleEntering,
-      onEntering
-    );
-    const handleEntered = createChainedFunction(this.handleEntered, onEntered);
-    const handleExit = createChainedFunction(this.handleExit, onExit);
-    const handleExiting = createChainedFunction(this.handleExiting, onExiting);
 
     return (
       <Transition
         {...props}
         aria-expanded={props.role ? props.in : null}
-        onEnter={handleEnter}
-        onEntering={handleEntering}
-        onEntered={handleEntered}
-        onExit={handleExit}
-        onExiting={handleExiting}
+        onEnter={this.handleEnter}
+        onEntering={this.handleEntering}
+        onEntered={this.handleEntered}
+        onExit={this.handleExit}
+        onExiting={this.handleExiting}
       >
         {(state, innerProps) =>
           React.cloneElement(children, {
@@ -256,7 +129,49 @@ class Collapse extends React.Component {
   }
 }
 
-Collapse.propTypes = propTypes;
-Collapse.defaultProps = defaultProps;
+Collapse.propTypes = {
+  /** Show the component; triggers the expand or collapse animation */
+  in: PropTypes.bool,
+  /** Wait until the first "enter" transition to mount the component (add it to the DOM) */
+  mountOnEnter: PropTypes.bool,
+  /** Unmount the component (remove it from the DOM) when it is collapsed */
+  unmountOnExit: PropTypes.bool,
+  /** Run the expand animation when the component mounts, if it is initially shown */
+  appear: PropTypes.bool,
+  /** Duration of the collapse animation in milliseconds */
+  timeout: PropTypes.number,
+  /** Callback fired before the component expands */
+  onEnter: PropTypes.func,
+  /** Callback fired after the component starts to expand */
+  onEntering: PropTypes.func,
+  /** Callback fired after the component has expanded */
+  onEntered: PropTypes.func,
+  /** Callback fired before the component collapses */
+  onExit: PropTypes.func,
+  /** Callback fired after the component starts to collapse */
+  onExiting: PropTypes.func,
+  /** Callback fired after the component has collapsed */
+  onExited: PropTypes.func,
+  /** The dimension used when collapsing, or a function that returns the dimension */
+  dimension: PropTypes.oneOfType([
+    PropTypes.oneOf(['height', 'width']),
+    PropTypes.func
+  ]),
+  /** Function that returns the height or width of the animating DOM node Allows for providing some custom
+   * logic for how much the Collapse component should animate in its specified dimension. Called with
+   * the current dimension prop value and the DOM node. */
+  getDimensionValue: PropTypes.func,
+  /** ARIA role of collapsible element */
+  role: PropTypes.string
+};
+Collapse.defaultProps = {
+  in: false,
+  timeout: 300,
+  mountOnEnter: false,
+  unmountOnExit: false,
+  appear: false,
+  dimension: 'height',
+  getDimensionValue
+};
 
 export default Collapse;
