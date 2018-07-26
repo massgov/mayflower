0116b4399fed2a8c07c9e83b52685d4974776772
Rob Bayliss <rob@lastcallmedia.com>
2018-07-26 18:17:33 -0400

    Lock in good screenshots

--- patternlab/backstopjs/scripts/onReady.js
+++ patternlab/backstopjs/scripts/onReady.js
@@ -44,22 +44,20 @@ module.exports = async function(page, scenario, vp) {
     });
 
     await page.evaluate(async function () {
-        // Disable jQuery animation.
-        // It will complete immediately.
+        // Disable jQuery animation for any future calls.
         jQuery.fx.off = true;
+        // Immediately complete any in-progress animations.
+        jQuery(':animated').finish();
     });
 
-    // // Finally, wait for ajax to complete - this is to give alerts
-    // // time to finish rendering. This can take a while, especially
-    // // in local environments.
+    // Finally, wait for ajax to complete - this is to give alerts
+    // time to finish rendering. This can take a while, especially
+    // in local environments.
     await page.waitForFunction('jQuery.active == 0');
 
-
     // Add a slight delay.  This covers up some of the jitter caused
-    // by weird network conditions, slow javascript, etc. Ultimately,
-    // we should work to reduce this number, as it represents layout
-    // thrashing that is perceptible to users.
-    //
-    // This delay can be adjusted
+    // by weird network conditions, slow javascript, etc. We should
+    // work to reduce this number, since it represents instability
+    // in our styling.
     await page.waitFor(300);
 }
