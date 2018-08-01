5bcdf4a8226f2986fd4d42734a9f0ef5d93c7d38
Rob Bayliss <rob@lastcallmedia.com>
2018-08-01 17:50:47 -0400

    DP-9854: Update babelify dependency

--- patternlab/styleguide/source/assets/js/helpers/slugify.js
+++ patternlab/styleguide/source/assets/js/helpers/slugify.js
@@ -1,15 +1,10 @@
-module.exports = function(window, document, undefined){
-  "use strict";
+"use strict";
 
-  function slugify(text) {
+export default function slugify(text) {
     return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
-  }
-
-  return slugify;
-
-}(window, document);
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
+}
