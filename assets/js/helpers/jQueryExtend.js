5e987650ba4e7ddd74edabc8a8386d1d9d905449
Minghua Sun <clairesunstudio@gmail.com>
2018-08-03 12:38:47 -0400

    remove js and data from root

--- assets/js/helpers/jQueryExtend.js
+++
@@ -1,9 +0,0 @@
-export default (function (window,document,$,undefined) {
-  $.extend($.expr[':'], {
-    // jQuery find all focusable elements
-    // see: https://coderwall.com/p/jqsanw/jquery-find-every-focusable-elements
-    focusable: function(el, index, selector){
-      return $(el).is('a, button, :input, [tabindex]');
-    }
-  });
-})(window,document,jQuery);
--- assets/js/modules/responsiveVideo.js
+++
@@ -1,5 +0,0 @@
-export default (function (window,document,$,undefined) {
-
-  $('.js-ma-responsive-video').fitVids();
-
-})(window,document,jQuery);
