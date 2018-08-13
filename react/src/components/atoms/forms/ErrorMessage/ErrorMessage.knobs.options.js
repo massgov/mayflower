36de4f860fb669d98a6c4a900551c1bde7b556f9
Minghua Sun <clairesunstudio@gmail.com>
2018-08-13 16:29:23 -0400

    fix

--- react/src/components/atoms/forms/ErrorMessage/ErrorMessage.knobs.options.js
+++ react/src/components/atoms/forms/ErrorMessage/ErrorMessage.knobs.options.js
@@ -1,5 +1,5 @@
 export default {
   inputId: 'GUID2738572489',
-  message: 'Error message for the input located above',
+  message: 'Error message for the input located below',
   status: ['error', 'success']
 };
