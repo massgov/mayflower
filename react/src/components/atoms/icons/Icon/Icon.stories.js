ac18b079bd934007b47b2bc76386d42a8e121cd7
Steven Murray <steven.murray@mediacurrent.com>
2018-08-15 17:20:35 -0400

    Dynamically populate Icon story.

--- react/src/components/atoms/icons/Icon/Icon.stories.js
+++ react/src/components/atoms/icons/Icon/Icon.stories.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs/react';
+import path from 'path';
 
 import Icon from './index';
 
+function getAssets() {
+  const req = require.context('./assets', true, /\.*.svg$/);
+  const options = {};
+  req.keys().forEach((filename) => {
+    const key = path.basename(filename, '.svg');
+    options[key] = path.basename(filename);
+  });
+  return options;
+}
+
 storiesOf('atoms/icons', module).addDecorator(withKnobs)
   .add('Icon', () => {
+    const assets = getAssets();
     const options = {
       '': 'Choose',
-      arrow: 'arrow.svg',
-      latlonglobe: 'latlonglobe.svg',
-      arrowbent: 'arrowbent.svg'
+      ...assets
     };
     const input = select('Icon.filename', options, '');
     return(<Icon name={input} />);
