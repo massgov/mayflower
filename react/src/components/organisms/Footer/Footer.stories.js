f124f92911e2e7a4e11bade7bb1f0d21668f0529
Minghua Sun <clairesunstudio@gmail.com>
2018-08-17 14:50:25 -0400

    add footertext props

--- react/src/components/organisms/Footer/Footer.stories.js
+++ react/src/components/organisms/Footer/Footer.stories.js
@@ -16,7 +16,14 @@ storiesOf('organisms/Footer', module).addDecorator(withKnobs)
     const props = {
       footerLinks: object('footer.footerLinksData', FooterLinksData),
       socialLinks: object('footer.socialLinksData', SocialLinksData),
-      privacyPolicy: text('footer.privacyPolicy', '')
+      footerText: object('footer.footerText', {
+        copyright: '2018 Commonwealth of Massachusetts.',
+        description: 'Mass.gov® is a registered service mark of the Commonwealth of Massachusetts.',
+        privacyPolicy: {
+          text: 'Mass.gov Privacy Policy',
+          url: 'https://www.mass.gov/privacypolicy'
+        }
+      })
     };
     return(<Footer {...props} />);
   }))
