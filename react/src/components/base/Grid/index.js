4298a14dfd5126f678ad11d5128369b9564a9e6c
Minghua Sun <clairesunstudio@gmail.com>
2018-09-06 11:48:49 -0400

    wip

--- react/src/components/base/Grid/index.js
+++ react/src/components/base/Grid/index.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 const Grid = (grid) => {
   return(
-    <div className="ma__grid-row">
-      <div className="ma__grid-col-1">col-1</div>
-      <div className="ma__grid-col-2">col-2</div>
-      <div className="ma__grid-col-3">col-3</div>
-      <div className="ma__grid-col-6">col-6</div>
-    </div>
+    <React.Fragment>
+      <div className="ma__grid-row">
+        <div className="ma__grid-col-1">col-1</div>
+        <div className="ma__grid-col-2">col-2</div>
+        <div className="ma__grid-col-3">col-3</div>
+        <div className="ma__grid-col-6">col-6</div>
+      </div>
+      <div className="ma__grid-row">
+        <div className="ma__grid-col-4 ma__grid-col-md-2">col-4</div>
+        <div className="ma__grid-col-4 ma__grid-col-md-2">col-4</div>
+        <div className="ma__grid-col-4 ma__grid-col-md-2">col-4</div>
+      </div>
+    </React.Fragment>
   );
 };
 
