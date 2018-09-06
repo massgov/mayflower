cf7f46f47a064f5a2c30fba1ea397d0efd8bb7b8
Steven Murray <steven.murray@mediacurrent.com>
2018-09-06 15:06:40 -0400

    WIP

--- react/src/components/molecules/GeneralTeaser/GeneralTeaser.stories.js
+++ react/src/components/molecules/GeneralTeaser/GeneralTeaser.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text, select, boolean, date, number, array, object } from '@storybook/addon-knobs/react';
+import { withKnobs, text, select, boolean, number, object } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
 import GeneralTeaser from './index';
 import Paragraph from '../../atoms/text/Paragraph';
@@ -11,12 +11,12 @@ storiesOf('molecules', module).addDecorator(withKnobs)
   .add('GeneralTeaser', withInfo(`<div>${GeneralTeaserDocs}</div>`)(() => {
     const props = {
       image: {
-        src: text('GeneralTeaser.image.src', ''),
-        alt: text('GeneralTeaser.image.alt', '')
+        src: text('GeneralTeaser.image.src', 'https://mayflower.digital.mass.gov/assets/images/placeholder/800x400.png'),
+        alt: text('GeneralTeaser.image.alt', 'alt text')
       },
       eyebrow: text('GeneralTeaser.eyebrow', 'press-release'),
       title: {
-        info: text('GeneralTeaser.title.info', ''),
+        info: text('GeneralTeaser.title.info', 'Title info here'),
         text: text('GeneralTeaser.title.text', 'Lorem ipsum dolor sit amet'),
         href: text('GeneralTeaser.title.href', '#'),
         showFileIcon: boolean('GeneralTeaser.title.showFileIcon', false)
@@ -48,6 +48,7 @@ storiesOf('molecules', module).addDecorator(withKnobs)
       secondaryInfo: []
     };
     for (let i = 1; i < 4; i++) {
+      // ContactGroup items array.
       const primaryItem = {
         type: select(`GeneralTeaser.primaryInfo.items.${i}.type`, {
           phone: 'phone',
@@ -55,12 +56,13 @@ storiesOf('molecules', module).addDecorator(withKnobs)
           email: 'email',
           address: 'address'
         }, 'phone'),
-        label: text(`GeneralTeaser.primaryInfo.items.${i}.label`, ''),
-        address: text(`GeneralTeaser.primaryInfo.items.${i}.address`, ''),
+        label: text(`GeneralTeaser.primaryInfo.items.${i}.label`, `Primary Info Label ${i}:`),
+        address: text(`GeneralTeaser.primaryInfo.items.${i}.address`, '123 Imaginary Lane'),
         link: object(`GeneralTeaser.primaryInfo.items.${i}.link`, { ...props.title, text: `PrimaryInfo Item Link ${i}` }),
-        details: text(`GeneralTeaser.primaryInfo.items.${i}.details`, '')
+        details: text(`GeneralTeaser.primaryInfo.items.${i}.details`, `Primary Info details ${i}`)
       };
-      const subLink = { ...props.title, text: `Sublink ${i}` };
+      // DecorativeLink props for subLinks.
+      const subLink = { ...props.title, text: `Sublink ${i}`, key: `GeneralTeaser.subLinks.${i}` };
       const secondary = {
         icon: select(`GeneralTeaser.secondaryInfo.${i}.icon`, {
           SvgMarker: 'SvgMarker (Address Icon)',
@@ -69,11 +71,50 @@ storiesOf('molecules', module).addDecorator(withKnobs)
           SvgFax: 'SvgFax (FaxIcon)'
         }, 'SvgMarker')
       };
+      // DecorativeLink props for secondaryInfo's linkedTitle.
       const secondaryLink = { ...props.title, text: `SecondaryInfo Link ${i}` };
       secondary.linkedTitle = object(`GeneralTeaser.secondaryInfo.${i}.linkedTitle`, secondaryLink);
       props.primaryInfo.items.push(primaryItem);
       props.secondaryInfo.push(secondary);
       props.subLinks.push(object(`GeneralTeaser.subLinks.${i}`, subLink));
+    }
+    // If you want to make scenarios for each function, use the value of the backstop query param.
+    if (window.location.search.indexOf('backstop') > -1) {
+      const teasers = [];
+      // Add default view from story.
+      teasers.push(props);
+      // Tests file icon next to url title.
+      const addUrlFileIconTests = () => {
+        const typeProps = { ...props.title };
+        typeProps.showFileIcon = true;
+        const knownTypes = ['pdf', 'docx', 'xlxs', 'generic'];
+        // Make a teaser for each icon to ensure it displays.
+        knownTypes.forEach((type) => {
+          typeProps.href = `index.${type}`;
+          teasers.push({ title: { ...typeProps } });
+        });
+      };
+      // Tests file icon next to sublink url title.
+      const addSubLinkUrlFileIconTests = () => {
+        const typeProps = { ...props.title };
+        typeProps.showFileIcon = true;
+        const knownTypes = ['pdf', 'docx', 'xlxs', 'generic'];
+        // Make a teaser for each icon to ensure it displays.
+        knownTypes.forEach((type, index) => {
+          typeProps.href = `index.${type}`;
+          const key = `${props.subLinks[0].key}.${index}`;
+          teasers.push({ title: props.title, subLinks: [{ key, ...typeProps }] });
+        });
+      };
+      // Generate teasers for backstop.
+      const makeTeasers = () => teasers.map((value, index) => {
+        const key = `GeneralTeaser.${index}`;
+        const backstopProps = { key, ...value };
+        return<GeneralTeaser {...backstopProps} />;
+      });
+      addUrlFileIconTests();
+      addSubLinkUrlFileIconTests();
+      return makeTeasers();
     }
     return(
       <GeneralTeaser {...props} />
