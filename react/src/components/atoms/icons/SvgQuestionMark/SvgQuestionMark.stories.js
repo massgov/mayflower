66219354593f46e1212c47d9530f83d92c18ccb1
Mark Casias <mark.casias@mediacurrent.com>
2018-09-14 13:18:01 -0600

    DP-10057: Removing questionmark component

--- react/src/components/atoms/icons/SvgQuestionMark/SvgQuestionMark.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-
-import SvgQuestionMark from './index';
-
-storiesOf('atoms/icons', module)
-  .add('SvgQuestionMark', withInfo()(() => <SvgQuestionMark />));
