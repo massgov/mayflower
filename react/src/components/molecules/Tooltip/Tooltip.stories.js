c5c3237e14381c9a913c0f9d911600e7b86ba15b
Mark Casias <mark.casias@mediacurrent.com>
2018-09-14 13:18:19 -0600

    DP-10057: replacing question mark icon

--- react/src/components/molecules/Tooltip/Tooltip.stories.js
+++ react/src/components/molecules/Tooltip/Tooltip.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 
-import SvgQuestionMark from '../../atoms/icons/SvgQuestionMark';
+import Icon from '../../atoms/icons/Icon';
 
 import Tooltip from './index';
 
@@ -15,7 +15,7 @@ storiesOf('molecules', module)
       closeText: 'Close',
       message: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod.',
       controlId: 'tooltip1',
-      openIcon: <SvgQuestionMark />,
+      openIcon: <Icon name="questionmark" svgHeight={20} svgWidth={20} />,
       info: 'Description on link for screen',
       title: 'Title',
       level: 2
