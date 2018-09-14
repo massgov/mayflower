ef0146efc1d4603971d908227e7f93381cc156ad
Minghua Sun <clairesunstudio@gmail.com>
2018-09-14 12:39:17 -0400

    export icon asset list

--- react/src/components/atoms/icons/Icon/Icon.stories.js
+++ react/src/components/atoms/icons/Icon/Icon.stories.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select, text } from '@storybook/addon-knobs/react';
-
+import { assets, svgOptions } from './Icon.knob.options';
 import Icon from './index';
 
 storiesOf('atoms/icons', module).addDecorator(withKnobs)
   .add('Icon', () => {
     // This needs to be dynamic somehow.
-    const assets = {
-      arrow: 'arrow.svg',
-      arrowbent: 'arrowbent.svg',
-      building: 'building.svg',
-      chevron: 'chevron.svg',
-      circlechevron: 'circlechevron.svg',
-      fax: 'fax.svg',
-      inputerror: 'inputerror.svg',
-      inputsuccess: 'inputsuccess.svg',
-      laptop: 'laptop.svg',
-      latlonglobe: 'latlonglobe.svg',
-      login: 'login.svg',
-      marker: 'marker.svg',
-      opennow: 'opennow.svg',
-      phone: 'phone.svg',
-      search: 'search.svg',
-      wheelchair: 'wheelchair.svg',
-      xlxs: 'xlxs.svg',
-      docx: 'docx.svg',
-      pdf: 'pdf.svg',
-      generic: 'generic.svg'
-    };
-    const options = {
-      '': 'Choose',
-      ...assets
-    };
-    const name = select('Icon.name', options, '');
+
+    const name = select('Icon.name', svgOptions, '');
     const svgWidth = text('Icon.svgWidth', 40);
     const svgHeight = text('Icon.svgHeight', 40);
     const title = text('Icon.title', 'Icon Title Here');
