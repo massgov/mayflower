c30436fd23c907618bf359a6acd41daa5a4f540e
Peter Mallett <peter.mallett@mediacurrent.com>
2018-09-19 10:46:26 -0600

    DP-10392 re-simplify build/run commands, commit assets symlink

--- react/scripts/assets-clean.js
+++
@@ -1,8 +0,0 @@
-const fse = require('fs-extra');
-
-fse.remove('src/assets', (err) => {
-  /* eslint-disable no-console */
-  if (err) return console.error(err);
-  console.log('build assets cleaned.');
-  return 0;
-});
