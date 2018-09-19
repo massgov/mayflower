5d276dd1d090b5ed769f8971810baaae17fc90b8
Sienna Svob <sienna.avril.pearl@gmail.com>
2018-09-19 17:10:05 -0400

    Refactor button variants and classnames (#236)
    * refactor button class names, create defined usage of quaternary,secondary,tertiary,primary buttons
    
    * up pl button md
    
    * add changelog
    
    * fix issue with large button
    
    * add disabled version
    
    * add cursor not allowed

--- react/src/components/atoms/buttons/Button/Button.stories.js
+++ react/src/components/atoms/buttons/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, boolean, select } from '@storybook/addon-knobs/react';
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from './index';
@@ -11,14 +11,14 @@ import buttonOptions from './Button.knobs.options';
 
 storiesOf('atoms/buttons', module).addDecorator(withKnobs)
   .add('Button', withInfo(`<div>${ButtonDocs}</div>`)(() => {
-    const defaultOutline = false;
     const props = {
+      usage: select('button.usage', buttonOptions.usage),
       theme: select('button.theme', buttonOptions.theme),
       type: select('button.type', buttonOptions.type),
       size: select('button.size', buttonOptions.size),
-      outline: boolean('button.outline', defaultOutline),
       info: text('button.info', 'this will be the tooltip text on hover'),
-      text: text('button.text', 'button'),
+      disabled: boolean('button.disabled', false),
+      text: text('button.text', 'Button'),
       href: text('button.href', ''),
       onClick: action('button clicked')
     };
