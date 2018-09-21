4f4b5fa456122923986a42538e50cde76f21285a
Minghua Sun <clairesunstudio@gmail.com>
2018-09-21 10:55:22 -0400

    create a different story for adding post input filter

--- react/src/components/organisms/SearchBanner/SearchBanner.stories.js
+++ react/src/components/organisms/SearchBanner/SearchBanner.stories.js
@@ -25,6 +25,125 @@ storiesOf('organisms', module).addDecorator(withKnobs)
     const hideDateRange = boolean('filterBox.hideDateRange', false);
     const DesktopHidden = boolean('SearchBanner.filterDesktopHidden', false);
     const withTabs = boolean('HeaderSearch.withTabs', true);
+    const props = {
+      searchBox: {
+        placeholder: text('HeaderSearch.placeholder', 'Search Mass.gov'),
+        buttonSearch: {
+          onClick: (e) => {
+            action('SearchBanner searchBox.buttonSearch clicked')(e);
+            e.preventDefault();
+          },
+          ariaLabel: text('HeaderSearch.buttonSearch.ariaLabel', 'Search'),
+          text: text('HeaderSearch.buttonSearch.text', 'Search')
+        },
+        onSubmit: action('SearchBanner searchBox.onSubmit'),
+        onChange: action('SearchBanner searchBox.onChange'),
+        defaultText: text('HeaderSearch.defaultText', '')
+      }
+
+    };
+    if (withTabs) {
+      props.tabs = {
+        tabs: object('tabs', tabsOptions.tabValues),
+        handleClick: action('SearchBanner tab clicked'),
+        selectedTab: select('tabs.selectedTab', tabsOptions.tabValues.map((tab) => tab.value), 'all')
+      };
+    }
+    if (withOrgDropdown) {
+      props.searchBox.orgDropdown = {
+        dropdownButton: object('searchBox.orgDropdown.dropdownButton', {
+          text: ('All Organizations'),
+          capitalized: true
+        }),
+        inputText: object('searchBox.orgDropdown.inputText', {
+          boxed: true,
+          label: null,
+          placeholder: 'Search an organization...',
+          id: 'org-typeahead',
+          options,
+          selected: '',
+          onChange: action('SearchBanner searchBox.orgDropdown.onChange')
+        })
+      };
+    }
+    if (withFilterBox) {
+      props.toggleButtonOnClick = action('SearchBanner toggleButtonOnClick');
+      props.filterBoxExpanded = boolean('SearchBanner.filterBoxExpanded', true);
+      props.filterDesktopHidden = DesktopHidden;
+      props.filterToggleText = text('SearchBanner.filterBoxText', 'More Filters');
+      props.filterBox = {
+        filterDesktopHidden: DesktopHidden,
+        active: boolean('filterBox.active', true),
+        action: text('filterBox.action', '#'),
+        topic: (hideTopic ? undefined : {
+          label: text('filterBox.topic.label', 'Filter by Topic'),
+          stackLabel: boolean('filterBox.topic.stackLabel', true),
+          id: 'topic',
+          options: object('filterBox.topic.options', selectBoxOptions.options.topics),
+          required: boolean('filterBox.topic.required', true)
+        }),
+        organization: {
+          label: text('filterBox.organization.label', 'State organization'),
+          id: text('filterBox.organization.id', 'state-organization'),
+          options: object('filterBox.organization.options', inputOptions.options.orgSelector),
+          selected: select(
+            'filterBox.organization.defaultSelected',
+            [''].concat(inputOptions.options.orgSelector.map((option) => option.text)),
+            ''
+          ),
+          placeholder: text('filterBox.organization.placeholder', 'All Organizations'),
+          onChange: action('filterBox.organization typeahead onChange')
+        },
+        pressType: (hideType ? undefined : {
+          typeAhead: {
+            label: text('filterBox.pressType.label', 'Filter by Type'),
+            id: text('filterBox.pressType.id', 'press-type'),
+            options: object('filterBox.pressType.options', inputOptions.options.pressTypes),
+            selected: select(
+              'filterBox.pressType.defaultSelected',
+              [''].concat(inputOptions.options.pressTypes.map((option) => option.text)),
+              ''
+            ),
+            placeholder: text('filterBox.pressType.placeholder', 'All Types'),
+            onChange: action('SearchBanner filterBox.pressType.typeAhead.onChange')
+          }
+        }),
+        dateRange: (hideDateRange ? undefined : {
+          label: text('filterBox.dateRange.label', 'Date range'),
+          startDate: object('filterBox.dateRange.startDate', filterBoxSharedProps.startDate),
+          endDate: object('filterBox.dateRange.endDate', filterBoxSharedProps.endDate)
+        }),
+        submitButton: {
+          text: text('filterBox.submitButton.text', 'Submit'),
+          type: select('filterBox.submitButton.type', buttonOptions.type, 'submit'),
+          size: select('filterBox.submitButton.size', buttonOptions.size),
+          theme: select('filterBox.submitButton.theme', buttonOptions.theme, ''),
+          outline: boolean('filterBox.submitButton.outline', false),
+          onClick: action('SearchBanner filterBox.submitButton.onClick')
+        },
+        clearButton: {
+          text: text('filterBox.clearButton.text', 'Clear all filters'),
+          info: text('filterBox.clearButton.aria-label', 'Clear all filters'),
+          onClearCallback: action('SearchBanner filterBox.clearButton.onClearCallback')
+        }
+      };
+      if (props.filterBox.dateRange) {
+        props.filterBox.dateRange.startDate.defaultDate = new Date(props.filterBox.dateRange.startDate.defaultDate);
+        props.filterBox.dateRange.endDate.defaultDate = new Date(props.filterBox.dateRange.endDate.defaultDate);
+      }
+    }
+    return(<SearchBanner {...props} />);
+  }))
+  // second story
+  .add('SearchBanner with postInputFilter', withInfo('<div></div>')(() => {
+    const options = inputOptions.options.orgSelector;
+    const withOrgDropdown = boolean('HeaderSearch.withOrgDropdown', false);
+    const withFilterBox = boolean('HeaderSearch.withFilterBox', true);
+    const hideTopic = boolean('filterBox.hideTopic', true);
+    const hideType = boolean('filterBox.hideType', false);
+    const hideDateRange = boolean('filterBox.hideDateRange', false);
+    const DesktopHidden = boolean('SearchBanner.filterDesktopHidden', false);
+    const withTabs = boolean('HeaderSearch.withTabs', false);
     const selectBoxProps = {
       label: text('selectBox.label', ''),
       stackLabel: boolean('selectBox.stackLabel', true),
