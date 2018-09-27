a02bc887e3aee85569745f52a4e418143e126618
Mark Casias <mark.casias@mediacurrent.com>
2018-09-27 14:23:19 -0600

    DP-10057: Adding knobs and options to story.

---
+++ react/src/components/molecules/Tooltip/Tooltip.knob.options.js
@@ -0,0 +1,12 @@
+export default {
+  openText: 'Want more information?',
+  closeText: 'Close',
+  message: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod.',
+  controlId: 'tooltip1',
+  info: 'Description on link for screen',
+  title: 'Title',
+  level: {
+    2: 'h2',
+    3: 'h3'
+  }
+}
--- react/src/components/molecules/Tooltip/Tooltip.stories.js
+++ react/src/components/molecules/Tooltip/Tooltip.stories.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { withKnobs, select, text, boolean } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
-
-
-import Icon from '../../atoms/icons/Icon';
+import tooltipOptions from './Tooltip.knob.options';
 
 import Tooltip from './index';
 
 storiesOf('molecules', module)
-  .add('Tooltip', withInfo('<div></div>')(() => {
+  .addDecorator(withKnobs)
+  .add('Tooltip', withInfo('<div>Tooltip</div>')(() => {
     const props = {
-      openText: 'Want more information?',
-      closeText: 'Close',
-      message: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod.',
-      controlId: 'tooltip1',
-      openIcon: <Icon name="questionmark" svgHeight={20} svgWidth={20} />,
-      info: 'Description on link for screen',
-      title: 'Title',
-      level: 2
+      openText: text('Tooltip.openText', tooltipOptions.openText),
+      closeText: text('Tooltip.closeText', tooltipOptions.closeText),
+      message: text('Tooltip.message', tooltipOptions.message),
+      controlId: text('Tooltip.controlId', tooltipOptions.controlId),
+      openIcon: boolean('Tooltip.openIcon', true),
+      info: text('Tooltip.info', tooltipOptions.info),
+      title: text('Tooltip.title', tooltipOptions.title),
+      level: select('Tooltip.level', tooltipOptions.level)
     };
     return(<Tooltip {...props} />);
   }));
