66d0e682854b479ff8e2729bbc753430b5bc29cd
Minghua Sun <clairesunstudio@gmail.com>
2018-10-03 17:47:05 -0400

    add real data

--- react/src/components/datavis/Table/Table.stories.js
+++ react/src/components/datavis/Table/Table.stories.js
@@ -5,11 +5,14 @@ import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs/react';
 import { action } from '@storybook/addon-actions';
 
+import countyData from './county.data.json';
+//import townData from './town.data.json';
+
 import Table from './index';
 
 storiesOf('dataviz', module).addDecorator(withKnobs)
   .add('Table', withInfo(`<div></div>`)(() => {
     return(
-      <Table />
+      <Table data={countyData}/>
     );
   }));
--- react/src/components/datavis/Table/index.js
+++ react/src/components/datavis/Table/index.js
@@ -3,30 +3,17 @@ import ReactTable from 'react-table';
 
 import '../../../../node_modules/react-table/react-table.css';
 
-const Table = () => {
-    const data = [{
-      name: 'Tanner Linsley',
-      age: 26,
-      friend: {
-        name: 'Jason Maurer',
-        age: 23,
-      }
-    }]
-   
+const Table = (props) => {
+    const { data } = props
     const columns = [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
+      Header: 'County',
+      accessor: 'COUNTY' 
+    },{
+      Header: 'Trips started',
+      accessor: 'ORIGIN_TRIPS' // String-based value accessors!
     }, {
-      Header: 'Age',
-      accessor: 'age',
-      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    }, {
-      id: 'friendName', // Required because our accessor is not a string
-      Header: 'Friend Name',
-      accessor: d => d.friend.name // Custom value accessors!
-    }, {
-      Header: props => <span>Friend Age</span>, // Custom header components!
-      accessor: 'friend.age'
+      Header: 'Trips started per person',
+      accessor: 'ORIGIN_TRIPS_PER_PERSON'
     }]
     
     return(
