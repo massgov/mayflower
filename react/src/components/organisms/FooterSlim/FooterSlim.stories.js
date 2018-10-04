9c20a359aa80a440e0f097e7ebe6f132a3ee6252
Mark Casias <mark.casias@mediacurrent.com>
2018-10-04 07:16:22 -0600

    DP-10440: starting props conversion

--- react/src/components/organisms/FooterSlim/FooterSlim.stories.js
+++ react/src/components/organisms/FooterSlim/FooterSlim.stories.js
@@ -8,5 +8,15 @@ import FooterSlim from './index';
 storiesOf('organisms', module)
   .addDecorator(withKnobs)
   .add('FooterSlim', withInfo('{ FooterReadme }')(() => {
-    return(<FooterSlim />);
+    const props = {
+      FooterSlimTitle: "Massachusetts Executive Office of Eductation (EDU)",
+      FooterSlimDescription: "The Department of Early Education and Care'&apos;'s mission is to support the healthy growth and development of all children by providing high quality programs and resources for families",
+      FooterSlimAddress: "51 Sleeper St. 4th Floor, Boston, MA 02210",
+      FooterSlimPhone: "(617) 988-6600",
+      FooterSlimLink: {
+        href: "#",
+        title: "EEC Official Website"
+      }
+    }
+    return(<FooterSlim {...props} />);
   }));
--- react/src/components/organisms/FooterSlim/index.js
+++ react/src/components/organisms/FooterSlim/index.js
@@ -1,46 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
 import Icon from '../../atoms/icons/Icon'
 import SiteLogo from '../../atoms/media/SiteLogo';
 
 import './style.css';
 
-export default () => (
+
+const FooterSlim = (props) => {
+  const year = '';
+  return (
   <footer className="ma__footer_slim" id="footer">
     <div className="ma__footer_slim--container ma__container">
       <SiteLogo />
       <div className="ma__footer_slim--container-inner">
         <section className="ma__footer_slim--info">
-          <h3>Massachusetts Executive Office of Eductation (EDU)</h3>
-          <p>
-            The Department of Early Education and Care&apos;s mission is to
-            support the healthy growth and development of all children by
-            providing high quality programs and resources for families
-          </p>
+          <h3>{ props.FooterSlimTitle }</h3>
+          <p>{ props.FooterSlimDescription }</p>
           <p className="ma__footer_slim--copyright">
-            &copy; 2018 Commonwealth of Massachusetts
+            &copy; {moment().year()} Commonwealth of Massachusetts
           </p>
         </section>
         <section className="ma__footer_slim--details">
           <div className="ma__footer_slim--links">
+            {/* todo: */}
             <a href="#">Lead Agencies Policies</a>
             <a href="#">Child Care Licensing Procedures</a>
           </div>
           <div className="ma__footer_slim--contact">
-            <p>
-              <Icon name="marker" svgWidth={20} svgHeight={20} />
-              <span>51 Sleeper St. 4th Floor, Boston, MA 02210</span>
-            </p>
-            <p>
-              <Icon name="phone" svgWidth={20} svgHeight={20} />
-              <span>(617) 988-6600</span>
-            </p>
+            {props.FooterSlimAddress ? (<p><Icon name="marker" svgWidth={20} svgHeight={20} /><span>props.FooterSlimAddress}</span></p>) : ''}
+            {props.FooterSlimPhone ? (<p><Icon name="phone" svgWidth={20} svgHeight={20} /><span>{props.FooterSlimPhone}</span></p>) : ''}
             <p>
               <Icon name="laptop" svgWidth={20} svgHeight={20} />
-              <a href="#">EEC Official Website</a>
+              <a href={props.FooterSlimLink.href}>{props.FooterSlimLink.title}</a>
             </p>
           </div>
         </section>
       </div>
     </div>
   </footer>
-);
+)};
+
+FooterSlim.propTypes = {
+  FooterSlimTitle: PropTypes.text,
+  FooterSlimDescription: PropTypes.text,
+  FooterSlimAddress: PropTypes.text,
+  FooterSlimPhone: PropTypes.text,
+  FooterSlimLink: PropTypes.shape({
+    href: PropTypes.text,
+    title: PropTypes.title
+  })
+};
+
+export default FooterSlim;
