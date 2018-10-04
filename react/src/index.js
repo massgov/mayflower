77ad9874943ab72c0a41212f06e016347c9bb5b7
Minghua Sun <clairesunstudio@gmail.com>
2018-10-04 13:56:38 -0400

    add changelog and export

--- react/src/index.js
+++ react/src/index.js
@@ -90,3 +90,6 @@ export Error500 from './components/pages/Error500';
 
 //@animations
 export Collapse from './components/animations/Collapse';
+
+//@dataviz
+export DataTable from './components/datavis/DataTable';
