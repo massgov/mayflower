02dae4957f5670c6d83e3e0876088c0510509c87
avrilpearl <sienna.avril.pearl@gmail.com>
2018-10-04 10:13:47 -0400

    fix lint errors

--- react/src/components/organisms/FooterSlim/FooterSlim.stories.js
+++ react/src/components/organisms/FooterSlim/FooterSlim.stories.js
@@ -10,20 +10,20 @@ storiesOf('organisms', module)
   .addDecorator(withKnobs)
   .add('FooterSlim', withInfo({ FooterSlimReadme })(() => {
     const props = {
-      title: text('footerSlim.title','Massachusetts Executive Office of Eductation (EDU)'),
-      description: text('footerSlim.description',"The Department of Early Education and Care'&apos;'s mission is to support the healthy growth and development of all children by providing high quality programs and resources for families"),
+      title: text('footerSlim.title', 'Massachusetts Executive Office of Eductation (EDU)'),
+      description: text('footerSlim.description', "The Department of Early Education and Care'&apos;'s mission is to support the healthy growth and development of all children by providing high quality programs and resources for families"),
       links: object('footer.footerLinksData', [
-        { href: '#', title: 'Lead Agencies Policies'},
-        { href: '#', title: 'Child Care Licensing Procedures'}
+        { href: '#', title: 'Lead Agencies Policies' },
+        { href: '#', title: 'Child Care Licensing Procedures' }
       ]),
       contact: {
-        address: text('footerSlim.contact.address','51 Sleeper St. 4th Floor, Boston, MA 02210'),
-        phone: text('footerSlim.contact.phone','(617) 988-6600'),
+        address: text('footerSlim.contact.address', '51 Sleeper St. 4th Floor, Boston, MA 02210'),
+        phone: text('footerSlim.contact.phone', '(617) 988-6600'),
         online: {
-          href: text('footerSlim.contact.online.href','#'),
-          title: text('footerSlim.contact.online.title','EEC Official Website')
+          href: text('footerSlim.contact.online.href', '#'),
+          title: text('footerSlim.contact.online.title', 'EEC Official Website')
         }
       }
-    }
+    };
     return(<FooterSlim {...props} />);
   }));
--- react/src/components/organisms/FooterSlim/index.js
+++ react/src/components/organisms/FooterSlim/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
-import Icon from '../../atoms/icons/Icon'
+import Icon from '../../atoms/icons/Icon';
 import SiteLogo from '../../atoms/media/SiteLogo';
 
 import './style.css';
 
 
-const FooterSlim = (props) => {
-  props.links.map((link, linkIndex) => {
-    console.log(link.href)
-  })
-  return (
+const FooterSlim = (props) => (
   <footer className="ma__footer_slim" id="footer">
     <div className="ma__footer_slim--container ma__container">
       <SiteLogo />
@@ -25,13 +21,13 @@ const FooterSlim = (props) => {
           </p>
         </section>
         <section className="ma__footer_slim--details">
-        {props.links &&
-          <div className="ma__footer_slim--links">
-            {props.links.map((link, linkIndex) => {
-              return <a href={link.href}>{link.title}</a>
-            })}
-          </div>
-        }
+          {props.links &&
+            <div className="ma__footer_slim--links">
+              {props.links.map((link, linkIndex) => (
+                <a href={link.href} key={`footslimlinks-${linkIndex}`}>{link.title}</a>
+              ))}
+            </div>
+          }
           <div className="ma__footer_slim--contact">
             {props.contact.address &&
               <p>
@@ -56,7 +52,7 @@ const FooterSlim = (props) => {
       </div>
     </div>
   </footer>
-)};
+);
 
 FooterSlim.propTypes = {
   /** The main title to be displayed in the footer */
@@ -64,12 +60,10 @@ FooterSlim.propTypes = {
   /** A short description */
   description: PropTypes.string.isRequired,
   /** Additional links for key information */
-  links: PropTypes.arrayOf(
-    PropTypes.shape({
-      href: PropTypes.string,
-      title: PropTypes.string,
-    })
-  ),
+  links: PropTypes.arrayOf(PropTypes.shape({
+    href: PropTypes.string,
+    title: PropTypes.string
+  })),
   /** Contact details for the responsible authority */
   contact: PropTypes.shape({
     address: PropTypes.string,
