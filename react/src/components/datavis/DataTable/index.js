f1b331d11ea8d491985aa3ca0ab8899f1b82a6f2
Minghua Sun <clairesunstudio@gmail.com>
2018-10-05 11:47:08 -0400

    add custom styles and clean up code

--- react/src/components/datavis/DataTable/index.js
+++ react/src/components/datavis/DataTable/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactTable from 'react-table';
 
 import '../../../../node_modules/react-table/react-table.css';
+import './styles.css';
 
-const DataTable = (props) => {
-    return(
-      <ReactTable {...props} />
-    )
-}
+const DataTable = (props) => <ReactTable {...props} />;
 
 export default DataTable;
