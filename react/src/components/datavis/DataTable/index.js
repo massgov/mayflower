adf82c6b33c816ad0b818475fad9f330e30d3bcd
Minghua Sun <clairesunstudio@gmail.com>
2018-10-09 10:03:11 -0400

    update import relative path

--- react/src/components/datavis/DataTable/index.js
+++ react/src/components/datavis/DataTable/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTable from 'react-table';
 
-import '../../../../node_modules/react-table/react-table.css';
+import 'react-table/react-table.css';
 import './styles.css';
 
 const DataTable = (props) => <ReactTable {...props} />;
