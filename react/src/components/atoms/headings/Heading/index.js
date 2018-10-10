9adb7cd7d69c3b9d19ee2967fec78d3b01ef0aef
Minghua Sun <clairesunstudio@gmail.com>
2018-10-10 13:58:07 -0400

    remove imports

--- react/src/components/atoms/headings/Heading/index.js
+++ react/src/components/atoms/headings/Heading/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './style.css';
 
 const Heading = (props) => {
   const Element = `h${props.level}`;
--- react/src/components/atoms/text/Paragraph/index.js
+++ react/src/components/atoms/text/Paragraph/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './style.css';
 
 const Paragraph = (paragraph) => (
   <p dangerouslySetInnerHTML={{ __html: paragraph.text }} />
