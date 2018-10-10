cdf3c4a77617ef9f69596d6891213bad2063ed6d
Mark Casias <mark.casias@mediacurrent.com>
2018-10-10 11:17:55 -0600

    DP-10693: Adjusting Link molecule for optional icon

--- react/src/components/molecules/Link/index.js
+++ react/src/components/molecules/Link/index.js
@@ -3,25 +3,29 @@ import PropTypes from 'prop-types';
 
 import { Icon } from '../../../index';
 
-const Link = (props) => (
+const Link = (props) => {
+  const icon = props.icon ? (<Icon name={props.icon} svgWidth={13.2} svgHeight={13.2} />) : '';
+  return (
   <a
     href={props.href}
     className="js-clickable-link"
     title={props.info}
-  >{props.text}&nbsp;<Icon name="arrow" svgWidth={13.2} svgHeight={13.2} />
+  >{props.text}&nbsp;{icon}
   </a>
-);
+)};
 
 Link.propTypes = {
   href: PropTypes.string,
   info: PropTypes.string,
-  text: PropTypes.string
+  text: PropTypes.string,
+  icon: PropTypes.string
 };
 
 Link.defaultProps = {
   href: '#',
   info: '',
-  text: 'Lorem ipsum dolor sit amet'
+  text: 'Lorem ipsum dolor sit amet',
+  icon: 'arrow'
 };
 
 export default Link;
