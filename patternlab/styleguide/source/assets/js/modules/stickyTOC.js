35d508c95878c1ab8fb9dad1746aa6cc3029355a
Nathan James <nathan.james@mediacurrent.com>
2018-10-10 12:06:26 -0500

    Update stickyTOC for simpler integration and maintanence

--- patternlab/styleguide/source/assets/js/modules/stickyTOC.js
+++ patternlab/styleguide/source/assets/js/modules/stickyTOC.js
@@ -1,109 +1,120 @@
 export default (function (window,document,$,undefined) {
 
-  $('.pre-content .ma__sticky-toc').each(function() {
-    const $toc = $('.ma__sticky-toc'),
-          $tocContent = $('.ma__sticky-toc__links'),
-          $tocSections = $('.page-content, .ma__stacked-row-section__container .ma__stacked-row-section__title, .ma__stacked-row .ma__stacked-row__title').find('h2:visible, .ma__comp-heading:visible'),
-          lastHeading = $tocSections.last().text(),
-          tocSectionCount = $tocSections.length,
-          $tocColumn = $('.ma__sticky-toc__column'),
-          $mobileToggle = $('.ma__sticky-toc__toggle-link'),
-          $tocToggle = $('.stickyTOC-open'),
-          $tocFooter = $('.ma__sticky-toc__footer'),
-          $stickyToc = $('.ma__sticky-toc__current-section'),
-          minSectionsToShow = $toc.data('min-to-show'),
-          feedbackButton = $('body').find('.ma__fixed-feedback-button');
+  const tocs = Array.from(document.getElementsByClassName("ma__sticky-toc"));
+  tocs.forEach((toc) => {
+    // The container for the links in the table of contents.
+    const tocContent = toc.querySelector(".ma__sticky-toc__links");
+    // The parent that should be checked for sections.
+    const tocParent = toc.closest(toc.dataset.parent);
+    // The destinations for each table of content link.
+    const tocSections = tocParent.querySelectorAll(toc.dataset.sections);
+    // The number of sections / links.
+    const tocSectionCount = tocSections.length;
+    // The text of the last heading.
+    const lastHeading = tocSections[tocSectionCount - 1].textContent;
+    // Another wroapper around the links, probably originally to put the links in two columns.
+    const tocColumn = toc.querySelector(".ma__sticky-toc__column");
+    // Container in the sticky header to hold the current sections header.
+    const stickyToc = toc.querySelector(".ma__sticky-toc__current-section");
+    // The minimum number of sections required on the page to display the table of contents.
+    const minSectionsToShow = toc.dataset["min-to-show"];
+    // The overlay div that shows when the stuck menu is shown.
+    const stuckOverlay = toc.querySelector(".ma__sticky-toc__overlay");
+    // The menu that slides out after the sticky menu is clicked.
+    let stuckMenu;
+    
 
     // Remove wrapper if not enough links.
     if (minSectionsToShow && (tocSectionCount < minSectionsToShow) || !minSectionsToShow && tocSectionCount < 3 ) {
-      $toc.remove();
+      toc.parentElement.removeChild(toc);
     }
     else {
       // To set an overflow rule for jumpy IE wrapping
-      $('html').addClass('stickyTOC');
+      document.firstElementChild.classList.add('stickyTOC');
     }
     // Show expander when more than 10 links.
-    if (tocSectionCount <= 10) {
-      if ($(window).width() > 480 ) {
-        $('.ma__sticky-toc__footer').toggle();
-      }
+    if (tocSectionCount <= 10 && window.innerWidth > 480 ) {
+      toc.querySelector(".ma__sticky-toc__footer").style.display = 'none';
     }
 
     // Use headers to fill TOC.
-    $tocSections.each(function() {
-      let $section = $(this);
-      let sectionId = $section.attr('id');
-      let sectionTitle = $section.text();
+    Array.from(tocSections).forEach((section) => {
+      let sectionId = section.id;
+      let sectionTitle = section.textContent;
       if (!sectionId) {
         sectionId = sectionTitle.replace(/\s+/g, '-').toLowerCase();
-        $section.attr('id', sectionId);
       }
-      let $tocLink = '<div class="ma__sticky-toc__link"><svg xmlns=\"http://www.w3.org/2000/svg\" aria-hidden=\"true\" width=\"35\" height=\"35\" viewBox=\"0 0 35 35\"><path class=\"st0\" d=\"M17.5 35C7.8 35 0 27.2 0 17.5 0 7.8 7.8 0 17.5 0 27.2 0 35 7.8 35 17.5 35 27.2 27.2 35 17.5 35zM16 9l-3 2.9 5.1 5.1L13 22.1l3 2.9 8-8L16 9z\"/></svg><a href="#'+sectionId+'" >'+sectionTitle+'</a></div>';
-      let dest = '<span class="sticky-toc-jump-target" id="' + sectionId + '"></span>';
+      const tocLink = document.createElement('div');
+      tocLink.className = "ma__sticky-toc__link";
+      tocLink.innerHTML = `<svg xmlns=\"http://www.w3.org/2000/svg\" aria-hidden=\"true\" width=\"35\" height=\"35\" viewBox=\"0 0 35 35\"><path class=\"st0\" d=\"M17.5 35C7.8 35 0 27.2 0 17.5 0 7.8 7.8 0 17.5 0 27.2 0 35 7.8 35 17.5 35 27.2 27.2 35 17.5 35zM16 9l-3 2.9 5.1 5.1L13 22.1l3 2.9 8-8L16 9z\"/></svg><a href="#${sectionId}" >${sectionTitle}</a>`;
+      const dest = document.createElement('span');
+      dest.className = "sticky-toc-jump-target";
+      dest.id = sectionId;
 
-      $section.removeAttr('id');
-      $section.parent().prepend(dest);
-      $('.ma__sticky-toc__column').append($tocLink);
+      section.id = "";
+      section.parentElement.prepend(dest);
+      tocColumn.appendChild(tocLink);
     });
+
+    tocParent.addEventListener("click", (e) => {
+      if (e.target.matches(".ma__sticky-toc__link a")) {
+        stickyToc.textContent = e.target.textContent;
+      }
+    }, true);
 
     // Toggle mobile TOC open.
-    $mobileToggle.on('click', function() {
-      $tocContent.toggleClass('is-open');
+    toc.querySelector(".ma__sticky-toc__toggle-link").addEventListener("click", () => {
+      tocContent.classList.toggle("is-open");
     });
 
-    $tocFooter.on('click', function() {
-      let $button = $(this).find('button');
-      let $buttonText = $button.text();
+    toc.querySelector(".ma__sticky-toc__footer").addEventListener("click", () => {
+      const button = this.firstChild;
 
       // Toggle hidden links open.
-      $tocContent.toggleClass('open');
-      $button.toggleClass('open');
+      tocContent.classList.toggle("open");
+      button.classList.toggle("open");
 
       // Toggle button text.
-      $button.text($buttonText === "show less" ? "show more" : "show less");
+      button.textContent = (button.textContent === "show less" ? "show more" : "show less");
     });
 
-    $(window).resize(function () {
+    window.addEventListener("resize", () => {
+      const button = toc.querySelector(".ma__sticky-toc__footer button");
       // Reset menu for each form factor on resize.
-      $('.ma__sticky-toc__link').removeAttr('style');
-      $tocContent.removeClass('open');
-      $tocFooter.find('button').removeClass('open').text('Show More');
+      tocContent.classList.remove("open");
+      button.classList.remove("open");
+      button.textContent = "show more";
     });
 
-    // Set title bar to 'Related since the scroll never makes it that far'
-    $('body').on('click', '.ma__sticky-toc__link a[text="' + lastHeading + '"]', function() {
-      $('.ma__sticky-toc__current-section').text(lastHeading);
-    });
-
-    $(window).scroll(function () {
-      const windowTop = $(window).scrollTop();
+    window.addEventListener("scroll", () => {
+      const windowTop = window.pageYOffset;
       const windowBottom = window.innerHeight;
-      const docHeight = $(document).height();
-      const stickyNavActive  = $toc.offset().top + $toc.outerHeight() - 20;
+      const docHeight = document.documentElement.scrollHeight;
+      const stickyNavActive  = toc.offsetTop + toc.offsetHeight;
 
       // Active Sticky TOC when on page TOC scrolls past.
       if (stickyNavActive > windowTop) {
-        $toc.removeClass('stuck');
-      } else {
-        $toc.addClass('stuck');
+        toc.classList.remove("stuck");
+      }
+      else {
+        toc.classList.add("stuck");
 
         if (windowTop + windowBottom === docHeight) {
-          $stickyToc.text(lastHeading);
+          stickyToc.textContent = lastHeading;
         }
         else {
           // Identify the section to show for the heading.
-          const active = $tocSections.filter((index, section) => {
-            const $target = $(section).siblings('.sticky-toc-jump-target');
-            const top = $target.offset().top;
-            // Works with Firefox.
-            return top - windowTop < 20;
+          const active = Array.from(tocSections).reverse().find((section) => {
+            const target = section.parentElement.querySelector(".sticky-toc-jump-target");
+            const top = target.getBoundingClientRect().top;
+            return top < 10;
           });
-          $stickyToc.text($(active.last()).text());
+          stickyToc.textContent = active.textContent;
         }
       }
     });
     // Back to top button
-    $(".stickyTOC-top").on('click',function(e) {
+    toc.querySelector(".stickyTOC-top").addEventListener("click", (e) => {
       e.preventDefault();
       try {
         $("html, body").stop(true,true).animate({scrollTop:0}, '750');
@@ -117,53 +128,41 @@ export default (function (window,document,$,undefined) {
     });
 
     function menuToggle() {
-      $('.ma__sticky-toc__stuck-menu').toggleClass('sticky-nav-open');
-      $('.ma__sticky-toc__overlay').toggle();
-      $('.ma__floating-action').toggle();
+      stuckMenu.classList.toggle("sticky-nav-open");
+      stuckOverlay.style.display = stuckOverlay.style.display === "block" ? "none" : "block";
     }
 
-    $tocToggle.on('click', function() {
+    toc.querySelector(".stickyTOC-open").addEventListener('click', () => {
       // Append sticky menu when first called
-      if ($('.ma__sticky-toc__stuck-menu').length == 0) {
-        $tocColumn.clone(true).addClass('ma__sticky-toc__stuck-menu').appendTo('#main-content');
+      if (typeof stuckMenu === "undefined") {
+        const menuCopy = tocColumn.cloneNode(true);
+        menuCopy.classList.add("ma__sticky-toc__stuck-menu");
+        stuckMenu = tocParent.appendChild(menuCopy);
+
+        // Close button
+        stuckMenu.querySelector(".secondary-label-close").addEventListener("click", () => {
+          menuToggle();
+        });
 
         // wait a beat so the slide in can work on first click
-        setTimeout(function(){
+        setTimeout(() => {
           menuToggle();
         }, 100);
       }
       else {
         menuToggle();
       }
-
-      hideShowFeedback();
-
     });
 
-    // Close button
-    $('.secondary-label-close').on('click', function() {
-      menuToggle();
-      hideShowFeedback();
-    });
-
-    // Close sticky menu on click off, include tools buttons
-    $('body').on('click', function(event) {
-      if(!$(event.target).closest('.ma__sticky-toc__stuck-menu').length && !$(event.target).closest('.ma__sticky-toc__tools').length) {
-      
-        if($('.ma__sticky-toc__stuck-menu').hasClass('sticky-nav-open')) {
+    // Close sticky menu on click off or link click.
+    document.body.addEventListener("click", (e) => {
+      if (!e.target.matches(".ma__sticky-toc__stuck-menu")
+        && (e.target.closest(".ma__sticky-toc__stuck-menu") === null
+        || e.target.matches(".ma__sticky-toc__stuck-menu a"))) {
+        if (typeof stuckMenu !== "undefined" && stuckMenu.matches(".sticky-nav-open")) {
           menuToggle();
-          hideShowFeedback();
         }
       }
-    });
-
-    // close menu when link clicked
-    $('body').on('click', '.ma__sticky-toc__stuck-menu a', function() {
-      menuToggle();
-    });
-
-    function hideShowFeedback() {
-      feedbackButton.toggleClass('hide-button');
-    }
+    }, true);
   });
 })(window,document,jQuery);
