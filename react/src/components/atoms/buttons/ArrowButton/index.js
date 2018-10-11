34d27a80c0c5f7ad136e345e400f3d1216f62ca7
Mark Casias <mark.casias@mediacurrent.com>
2018-10-11 09:24:11 -0600

    DP-10693: Update ArrowButton component for actions

--- react/src/components/atoms/buttons/ArrowButton/index.js
+++ react/src/components/atoms/buttons/ArrowButton/index.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
+import classNames from 'classnames';
 import './style.css';
 
 const ArrowButton = (props) => {
-  const classes = [
-    'ma__arrow-button',
-    'ma__arrow-button--' + props.direction
-  ];
+  const buttonClasses = classNames({
+    'ma__arrow-button': true,
+    [`ma__arrow-button--${props.direction}`]: props.direction,
+    'ma__arrow-button--left': !props.direction
+  });
+  const Element = props.href ? 'a' : 'button';
+  const onClickCallback = (e) => {
+    if (typeof props.onClick === 'function') {
+      e.preventDefault();
+      props.onClick(e);
+    }
+  };
 
   return (
-    <a
-      className={classes.join(' ')}
-      href={ props.href }
-      title={ props.info }>
-    </a>
+    <Element
+      className={buttonClasses}
+      onClick={(e) => onClickCallback(e)}
+      href={props.href}
+      title={props.info}
+      aria-label={props.info}
+    />
   );
 }
 
 ArrowButton.propTypes = {
-  /** Link destination */
-  href: PropTypes.string.isRequired,
-  /** Title value */
-  info: PropTypes.string.isRequired,
-  /** Direction of arrow, Left or Right */
-  direction: PropTypes.oneOf(['left','right'])
-}
+  /** Custom click handler function. */
+  onClick: PropTypes.func,
+  /** When populated with a url, this component renders a <a> vs a <button> */
+  href: PropTypes.string,
+  /** The text which renders in the standard browser tooltip on hover */
+  info: PropTypes.string,
+  /** Direction of the arrow. */
+  direction: PropTypes.oneOf(['left', 'right']),
+};
 
 ArrowButton.defaultProps = {
-  href: '#',
+  onClick: '',
+  href: '',
   info: '',
   direction: 'left'
 };
