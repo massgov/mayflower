6394f4a890115646ab6374868cc65d6f538d7724
Mark Casias <mark.casias@mediacurrent.com>
2018-10-11 09:49:43 -0600

    DP-10693: Refactor ArrowNav to accept function

--- react/src/components/molecules/ArrowNav/ArrowNav.stories.js
+++ react/src/components/molecules/ArrowNav/ArrowNav.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text, select } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
 
 import ArrowNav from './index';
 import ArrowNavReadme from './ArrowNav.md';
@@ -10,21 +11,14 @@ import { Link, ArrowButton } from '../../../index';
 storiesOf('molecules', module)
   .addDecorator(withKnobs)
   .add('ArrowNav', withInfo({ ArrowNavReadme })(() => {
-    const LinkProps = {
-      href: text('Link.href', '#'),
-      info: text('Link.info', 'link.info'),
-      text: text('Link.text', 'link.text')
-    };
-    const ButtonProps = {
-      href: text('ArrowButton.href', '#'),
-      info: text('ArrowButton.info', 'info'),
-      direction: select('ArrowButton.direction', ['left', 'right'])
-    }
     const props = {
-      title: text('ArrowNav.title', 'ArrowNav.title'),
-      label: text('ArrowNav.label', "ArrowNav.label"),
-      ArrowButton: <ArrowButton {...ButtonProps}/>,
-      Link: <Link {...LinkProps} />
+      href: text('Link.href', ''),
+      info: text('Info', 'link info'),
+      text: text('Link Text', 'Text'),
+      title: text('Title Text', 'Title'),
+      onClick: action('Clicked'),
+      direction: select('ArrowButton.direction', ['left', 'right']),
+      label: text('Label', "Label"),
     };
     return(<ArrowNav {...props} />);
   }));
--- react/src/components/molecules/ArrowNav/index.js
+++ react/src/components/molecules/ArrowNav/index.js
@@ -1,56 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {ArrowButton} from '../../../index';
 import './style.css';
 
 const ArrowNav = (props) => {
-  const sectionClasses = [
-    'ma__arrow-nav',
-    'ma__arrow-nav--' + props.ArrowButton.props.direction
-  ];
+  const sectionClasses = classNames({
+    'ma__arrow-nav': true,
+    [`ma__arrow-nav--${props.direction}`] : props.direction
+  });
+  const Element = props.href ? 'a' : 'button';
+  const onClickCallback = (e) => {
+    if (typeof props.onClick === 'function') {
+      e.preventDefault();
+      props.onClick(e);
+    }
+  };
   return (
-    <section className={sectionClasses.join(' ')}>
-      {props.ArrowButton}
+    <section className={sectionClasses}>
+      <ArrowButton {...props} />
       <h2 className="ma__arrow-nav__title" data-label={ props.label }>
         <span>{props.title}</span>
       </h2>
       <div className="ma__arrow-nav__link">
-        {props.Link}
+        <a
+          href={props.href}
+          onClick={(e) => onClickCallback(e)}
+          className="js-clickable-link"
+          title={props.info}
+        >
+          {props.text}
+        </a>
       </div>
     </section>
   )
 }
 
 ArrowNav.propTypes = {
+  /** Top label information */
   label: PropTypes.string.isRequired,
+  /** Title */
   title: PropTypes.string.isRequired,
-  ArrowButton: (props, propName, componentName) => {
-    const component = props[propName];
-    const isValid = (comp) => {
-      if (typeof comp.type === 'string') {
-        return comp.type === 'ArrowButton';
-      }
-      return comp.type.name && comp.type.name === 'ArrowButton';
-    };
-    if (!component || (component && !isValid(component))) {
-      return new Error(`Invalid prop ${propName} supplied to ${componentName}. Got: ${
-        component.type.name
-      }. Validation failed.`);
-    }
-  },
-  Link: (props, propName, componentName) => {
-    const component = props[propName];
-    const isValid = (comp) => {
-      if (typeof comp.type === 'string') {
-        return comp.type === 'Link';
-      }
-      return comp.type.name && comp.type.name === 'Link';
-    };
-    if (!component || (component && !isValid(component))) {
-      return new Error(`Invalid prop ${propName} supplied to ${componentName}. Got: ${
-        component.type.name
-      }. Validation failed.`);
-    }
-  }
+  /** Link information */
+  info: PropTypes.string,
+  /** Arrow Direction */
+  direction: PropTypes.oneOf(['left', 'right']),
+  /** Link href */
+  href: PropTypes.string,
+  /** Function */
+  onClick: PropTypes.func,
+  /** Bottom Link text */
+  text: PropTypes.string,
+}
+
+ArrowNav.defaults = {
+  label: '',
+  title: '',
+  info: '',
+  href: '',
+  onClick: '',
+  text: '',
+  direction: 'left',
 }
 
 export default ArrowNav;
