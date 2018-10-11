2e9e38b46f319c2fc404a1fd863c20d2452cb0e4
Minghua Sun <clairesunstudio@gmail.com>
2018-10-11 18:12:04 -0400

    add budget data into tabs

--- react/src/components/organisms/TabContainer/TabContainer.stories.js
+++ react/src/components/organisms/TabContainer/TabContainer.stories.js
@@ -2,35 +2,60 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, boolean } from '@storybook/addon-knobs/react';
+import shortid from '@storybook/addon-knobs/react';
 
 import TabContainer from '.';
 import Tab from './tab';
+import TabContainerData from './TabContainer.knobs.options';
 
+const props = {
+  nested: boolean('TabContainer.nested', true)
+};
 
-storiesOf('organisms', module).addDecorator(withKnobs)
+storiesOf('organisms/TabContainer', module).addDecorator(withKnobs)
   .add(
     'TabContainer',
+    withInfo()(() => (
+      <TabContainer>
+        <Tab title="Tab 1">
+          <TabContainer {...props}>
+            <Tab title="Nested Tab Here">This should support nesting like this.</Tab>
+            <Tab title="Another Nested Tab">Tabs have unique ids that are tracked locally without state.</Tab>
+          </TabContainer>
+        </Tab>
+        <Tab title="Tab 2">And this is my second tab.</Tab>
+        <Tab title="Tab 3">Last Tab!</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab">Tab Content</Tab>
+      </TabContainer>
+    ))
+  )
+  .add(
+    'TabContainer with real data',
     withInfo()(() => {
-      const props = {
-        nested: boolean('TabContainer.nested', true)
-      };
-      return(
-        <TabContainer>
-          <Tab title="Tab 1">
-            <TabContainer {...props}>
-              <Tab title="Nested Tab Here">This should support nesting like this.</Tab>
-              <Tab title="Another Nested Tab">Tabs have unique ids that are tracked locally without state.</Tab>
-            </TabContainer>
-          </Tab>
-          <Tab title="Tab 2">And this is my second tab.</Tab>
-          <Tab title="Tab 3">Last Tab!</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-          <Tab title="Tab">Tab Content</Tab>
-        </TabContainer>
-      );
+      if (TabContainerData.budgetTabs) {
+        return(
+          <TabContainer>
+            { TabContainerData.budgetTabs.map((tab, index) => (
+              <Tab key={`tab${index}`} title={tab.tab}>
+                { tab.subTabs && (
+                <TabContainer {...props}>
+                  {tab.subTabs.map((tab, index) => (
+                    <Tab key={`subTab${index}`} title={tab.tab}>{tab.content}</Tab>
+                  ))}
+                </TabContainer>
+              )
+              }
+                {tab.content}
+              </Tab>
+            ))
+            }
+          </TabContainer>
+        );
+      }
     })
   );
