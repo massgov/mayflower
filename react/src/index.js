8117038d1028c6946405ff841f41ef529abaffc2
Steven Murray <smurrayatwork@gmail.com>
2018-10-22 13:10:17 -0400

    DP-10765: Porting Table of Contents Hierarchy component. (#311)
    * WIP.
    
    * fix flashing
    
    * WIP.
    
    * Tweaks story to get rid of accordionwrapper.
    
    * Wrapping up ticket work.
    
    * Adding backstop references.
    
    * Adds markdown info.
    
    * Code review fixes.
    
    * Updates backstop.
    
    * consolidate scss
    
    * update screenshots
    
    * update story nesting and vr pngs
    
    * add missing img for vrt

--- react/src/index.js
+++ react/src/index.js
@@ -86,6 +86,7 @@ export IllustratedHeader from './components/organisms/IllustratedHeader';
 export FilterBox from './components/organisms/FilterBox';
 export SearchBanner from './components/organisms/SearchBanner';
 export AccordionWrapper from './components/organisms/AccordionWrapper';
+export TableofContents from './components/organisms/TableofContents';
 export TabContainer from './components/organisms/TabContainer';
 export Tab from './components/organisms/TabContainer/tab';
 
