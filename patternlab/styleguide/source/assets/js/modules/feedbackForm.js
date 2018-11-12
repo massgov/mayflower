c746b7a8abe342b457c840011d73e01fcbfae32a
devclair <clair.smith@mediacurrent.com>
2018-11-12 13:08:30 -0600

    DP-11136: updates to include new sections with logic

--- patternlab/styleguide/source/assets/js/modules/feedbackForm.js
+++ patternlab/styleguide/source/assets/js/modules/feedbackForm.js
@@ -3,12 +3,12 @@ export default (function (window, document, $) {
   const $feedbackForm = $('.ma__mass-feedback-form__form');
   const $fields = $('.ma__mass-feedback-form__fields');
   const $radios = $feedbackForm.find('input[type="radio"]');
-  const $noResponse = $('.ma__mass-feedback-form__form--no-response');
-  const $noResponseLink = $noResponse.find('.form--no-response');
+  const $Response = $('.ma__mass-feedback-form__form--user-response');
+  const $noResponseLink = $Response.find('.form--no-response');
   const $alertToggle = $('.ma__header-alert__hide');
 
   $noResponseLink.on('click', function () {
-    $noResponse.toggleClass('is-open');
+    $Response.toggleClass('is-open');
   });
 
   $radios.on('change', function () {
