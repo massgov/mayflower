9f911f7f07c9e683330be3a683647edc003f5a21
devclair <clair.smith@mediacurrent.com>
2018-11-13 09:04:59 -0600

    DP-11136: add basic open/close for contact form

--- patternlab/styleguide/source/assets/js/modules/feedbackForm.js
+++ patternlab/styleguide/source/assets/js/modules/feedbackForm.js
@@ -2,7 +2,8 @@ export default (function (window, document, $) {
 
   const $feedbackForm = $('.ma__mass-feedback-form__form');
   const $fields = $('.ma__mass-feedback-form__fields');
-  const $radios = $feedbackForm.find('input[type="radio"]');
+  const $loadRadios = $feedbackForm.find('.feedback-load input[type="radio"]');
+  const $contactRadios = $feedbackForm.find('.user-response__contact input[type="radio"]');
   const $Response = $('.ma__mass-feedback-form__form--user-response');
   const $noResponseLink = $Response.find('.form--no-response');
   const $alertToggle = $('.ma__header-alert__hide');
@@ -11,19 +12,34 @@ export default (function (window, document, $) {
     $Response.toggleClass('is-open');
   });
 
-  $radios.on('change', function () {
+  $loadRadios.on('change', function () {
 
-    let selected = $("input[name='foundIndicator']:checked").val();
+    let foundIndicator = $("input[name='foundIndicator']:checked").val();
 
-    if (selected === 'yes') {
+    if (foundIndicator === 'yes') {
       $fields.addClass('open positive');
       $fields.removeClass('negative');
     }
-    else if (selected === 'no') {
+    else if (foundIndicator === 'no') {
       $fields.addClass('open negative');
       $fields.removeClass('positive');
     }
   });
+
+  $contactRadios.on('change', function () {
+
+    let contactGroup = $("input[name='contactGroups']:checked").val();
+
+    if (contactGroup === 'yes') {
+      $('.ma__mass-feedback-form__contact-form').addClass('open');
+    }
+    else if (contactGroup === 'no') {
+      $('.ma__mass-feedback-form__contact-form').removeClass('open');
+    }
+  });
+
+
+
 
   $alertToggle.on('click', function (e) {
     e.preventDefault();
