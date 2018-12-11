809cfa5e440ef3f957868619f85e51d5c4667eca
Nathan James <nathan.james@mediacurrent.com>
2018-12-11 15:12:13 -0600

    Feedback form updates for Drupal integration

--- patternlab/styleguide/source/assets/js/modules/feedbackForm.js
+++ patternlab/styleguide/source/assets/js/modules/feedbackForm.js
@@ -1,70 +1,67 @@
-export default (function (window, document, $) {
-
-  const $feedbackForm = $('.ma__mass-feedback-form__form');
-  const $fields = $('.ma__mass-feedback-form__fields');
-  const $textArea = $fields.find('textarea');
-  const $contactForm = $feedbackForm.find('.ma__mass-feedback-form__contact-form');
+export default (function(window, document, $) {
+  const $feedbackForm = $(".ma__mass-feedback-form__form");
+  const $fields = $(".ma__mass-feedback-form__fields");
+  const $textArea = $fields.find("textarea");
+  const $contactForm = $feedbackForm.find(
+    ".ma__mass-feedback-form__contact-form"
+  );
   const $loadRadios = $feedbackForm.find('.feedback-load input[type="radio"]');
-  const $contactRadios = $feedbackForm.find('.user-response__contact input[type="radio"]');
-  const $Response = $('.ma__mass-feedback-form__form--user-response');
-  const $noResponseLink = $Response.find('.form--no-response');
-  const $alertToggle = $('.ma__in-page-alert__hide');
-  const $submitButton = $('.ma__mass-feedback-form__form--submit-wrapper .submitButton');
+  const $contactRadios = $feedbackForm.find(
+    '.user-response__contact input[type="radio"]'
+  );
+  const $Response = $(".ma__mass-feedback-form__form--user-response");
+  const $noResponseLink = $Response.find(".form--no-response");
+  const $alertToggle = $(".ma__in-page-alert__hide");
 
   // Open no reponse answer accordion.
-  $noResponseLink.on('click', function () {
-    $Response.toggleClass('is-open');
+  $noResponseLink.on("click", function() {
+    $Response.toggleClass("is-open");
   });
 
   // Auto size the text boxes
-  $textArea.each(function () {
+  $textArea.each(function() {
     let $el = $(this);
 
-    $el.on('keyup', function () {
-      if ($el.prop('scrollHeight') > $el.prop('clientHeight')) {
-        $el.css('height', $el.prop('scrollHeight') + "px");
-      }
-      else {
-        $el.css('height', "100%");
+    $el.on("keyup", function() {
+      if ($el.prop("scrollHeight") > $el.prop("clientHeight")) {
+        $el.css("height", $el.prop("scrollHeight") + "px");
+      } else {
+        $el.css("height", "100%");
       }
     });
   });
 
   // Open feedback form on radio button selection.
-  $loadRadios.on('change', function () {
-
+  $loadRadios.on("change", function() {
     let foundIndicator = $loadRadios.filter(":checked").val();
 
-    if (foundIndicator === 'yes') {
-      $fields.addClass('is-open positive');
-      $fields.removeClass('negative');
-    }
-    else if (foundIndicator === 'no') {
-      $fields.addClass('is-open negative');
-      $fields.removeClass('positive');
+    if (foundIndicator === "Yes") {
+      $fields.addClass("is-open positive");
+      $fields.removeClass("negative");
+    } else if (foundIndicator === "No") {
+      $fields.addClass("is-open negative");
+      $fields.removeClass("positive");
     }
   });
 
   // Open contact form on radio selection.
-  $contactRadios.on('change', function () {
-
+  $contactRadios.on("change", function() {
     let contactGroup = $contactRadios.filter(":checked").val();
+    let $submitButton = $(this)
+      .closest("form")
+      .find(".submitButton");
 
-    if (contactGroup === 'yes') {
-      $contactForm.addClass('is-open');
+    if (contactGroup === "Yes") {
+      $contactForm.addClass("is-open");
       $submitButton.val("Send");
-    }
-    else if (contactGroup === 'no') {
-      $contactForm.removeClass('is-open');
+    } else if (contactGroup === "No") {
+      $contactForm.removeClass("is-open");
       $submitButton.val("Send Feedback");
     }
-
-
   });
 
   // Close alert.
-  $alertToggle.on('click', function (e) {
+  $alertToggle.on("click", function(e) {
     e.preventDefault();
   });
-
 })(window, document, jQuery);
