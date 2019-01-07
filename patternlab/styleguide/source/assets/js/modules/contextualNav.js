aa6e3d9c711d3c12ea5fc9392f2f5cbfb8bcd6e1
devclair <clair.smith@mediacurrent.com>
2019-01-07 10:51:36 -0600

    Working menu each button opens first menu

--- patternlab/styleguide/source/assets/js/modules/contextualNav.js
+++ patternlab/styleguide/source/assets/js/modules/contextualNav.js
@@ -1,3 +1,15 @@
+const menuButton = document.querySelector(".js-header-menu-button");
+
+if (null !== menuButton) {
+  menuButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("body").classList.toggle("show-menu");
+  });
+}
+
+
+
+
 const contextualNavLinks = document.querySelectorAll(".ma__contextual-navigation--anchor");
 const currentLocation = window.location.href;
 
@@ -10,3 +22,26 @@ contextualNavLinks.forEach(function (navLink, index) {
   }
 });
 
+const contextualMenuButton = document.querySelectorAll('.ma__contextual-navigation__nav-wrapper .js-main-nav-toggle');
+
+if (null !== contextualMenuButton) {
+  contextualMenuButton.forEach(function () {
+
+    this.addEventListener('click', function () {
+
+      document.querySelector("body").classList.toggle("show-submenu");
+
+
+      let submenu = document.querySelector('.js-main-nav-content');
+      if (submenu.classList.contains('is-closed')) {
+        submenu.className += ' is-open';
+        submenu.classList.remove('is-closed');
+      } else {
+        submenu.className += ' is-closed';
+        submenu.classList.remove('is-open');
+      }
+
+    });
+  });
+
+}
