58818def18d85fa46ea7644d70d1aa8568301864
devclair <clair.smith@mediacurrent.com>
2019-01-07 18:16:40 -0600

    switch to es5 for IE11

--- patternlab/styleguide/source/assets/js/modules/contextualNav.js
+++ patternlab/styleguide/source/assets/js/modules/contextualNav.js
@@ -1,5 +1,5 @@
 // Open menu.
-const menuButton = document.querySelector(".js-header-menu-button");
+var menuButton = document.querySelector(".js-header-menu-button");
 
 if (null !== menuButton) {
   menuButton.addEventListener("click", function (event) {
@@ -9,12 +9,12 @@ if (null !== menuButton) {
 }
 
 // Remove current page from contextual nav menu.
-const contextualNavLinks = document.querySelectorAll(".ma__contextual-navigation--anchor");
-const currentLocation = window.location.href;
+var contextualNavLinks = document.querySelectorAll(".ma__contextual-navigation--anchor");
+var currentLocation = window.location.href;
 
 contextualNavLinks.forEach(function (navLink) {
 
-  let navLinkHref = navLink.getAttribute("href");
+  var navLinkHref = navLink.getAttribute("href");
 
   if (navLinkHref == currentLocation) {
     navLink.parentNode.remove();
@@ -22,18 +22,24 @@ contextualNavLinks.forEach(function (navLink) {
 });
 
 // Open submenus.
-document.querySelectorAll('.ma__contextual-navigation__nav-wrapper .ma__main-nav__top-link').forEach(button => {
-  button.addEventListener('click', () => {
-    let subMenu = button.parentNode.querySelector('.js-main-nav-content');
-    let isClosed = subMenu.classList.contains('is-closed');
+document.querySelectorAll('.ma__contextual-navigation__nav-wrapper .ma__main-nav__top-link').forEach(function (button) {
+  button.addEventListener('click', function () {
+    var menuIsOpen = document.querySelector("body").classList.contains('show-submenu');
+    var subMenu = button.parentNode.querySelector('.js-main-nav-content');
+    var isClosed = subMenu.classList.contains('is-closed');
 
-    document.querySelector("body").classList.toggle("show-submenu");
+    if (menuIsOpen) {
+      document.querySelector("body").classList.remove('show-submenu');
+    }
+    else {
+      document.querySelector("body").classList.add('show-submenu');
+    }
+
 
     if (isClosed) {
       subMenu.classList.add('is-open');
       subMenu.classList.remove('is-closed');
-    }
-    else {
+    } else {
       subMenu.classList.remove('is-open');
       subMenu.classList.add('is-closed');
     }
@@ -41,9 +47,16 @@ document.querySelectorAll('.ma__contextual-navigation__nav-wrapper .ma__main-nav
 });
 
 // Close Submenus 
-document.querySelector('.ma__contextual-navigation__nav-wrapper .js-close-sub-nav').addEventListener('click', () => {
-  const openMenu = document.querySelectorAll(".js-main-nav-content.is-open");
-  document.querySelector("body").classList.toggle("show-submenu");
+document.querySelector('.ma__contextual-navigation__nav-wrapper .js-close-sub-nav').addEventListener('click', function () {
+  var openMenu = document.querySelectorAll(".js-main-nav-content.is-open");
+  var menuIsOpen = document.querySelector("body").classList.contains('show-submenu');
+
+  if (menuIsOpen) {
+    document.querySelector("body").classList.remove('show-submenu');
+  }
+  else {
+    document.querySelector("body").classList.add('show-submenu');
+  }
 
   [].forEach.call(openMenu, function (el) {
     el.classList.remove('is-open');
