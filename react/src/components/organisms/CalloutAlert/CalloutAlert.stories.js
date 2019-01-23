5e7dec5ba0b27ae7f2ca637208cd2c3598888679
Minghua Sun <clairesunstudio@gmail.com>
2019-01-23 11:28:53 -0500

    lint fix

--- react/src/components/organisms/CalloutAlert/CalloutAlert.stories.js
+++ react/src/components/organisms/CalloutAlert/CalloutAlert.stories.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, select, boolean } from '@storybook/addon-knobs/react';
+import { withKnobs, select } from '@storybook/addon-knobs/react';
 
 import CalloutAlert from './index';
 import CalloutAlertDocs from './CalloutAlert.md';
 import { svgOptions } from '../../atoms/icons/Icon/Icon.knob.options';
 
-import Icon from '../../atoms/icons/Icon';
 import Paragraph from '../../atoms/text/Paragraph';
 import OrderedList from '../../atoms/lists/OrderedList';
 import DecorativeLink from '../../atoms/links/DecorativeLink';
