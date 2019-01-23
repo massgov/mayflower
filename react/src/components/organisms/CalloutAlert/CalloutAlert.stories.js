e0d363943dd51146689bd71b79c0e79cbf0800f8
Minghua Sun <clairesunstudio@gmail.com>
2019-01-23 11:28:15 -0500

    add docs

--- react/src/components/organisms/CalloutAlert/CalloutAlert.stories.js
+++ react/src/components/organisms/CalloutAlert/CalloutAlert.stories.js
@@ -4,14 +4,16 @@ import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs/react';
 
 import CalloutAlert from './index';
+import CalloutAlertDocs from './CalloutAlert.md';
 import { svgOptions } from '../../atoms/icons/Icon/Icon.knob.options';
 
 import Icon from '../../atoms/icons/Icon';
 import Paragraph from '../../atoms/text/Paragraph';
 import OrderedList from '../../atoms/lists/OrderedList';
+import DecorativeLink from '../../atoms/links/DecorativeLink';
 
 storiesOf('organisms', module).addDecorator(withKnobs)
-  .add('CalloutAlert', withInfo(`<div></div>`)(() => {
+  .add('CalloutAlert', withInfo(`<div>${CalloutAlertDocs}</div>`)(() => {
     const name = select('CalloutAlert.icon', svgOptions, '');
 
 
@@ -21,7 +23,8 @@ storiesOf('organisms', module).addDecorator(withKnobs)
       'c-primary-alt': 'c-primary-alt',
       'c-gray-dark': 'c-gray-dark',
       'c-error-red': 'c-error-red'
-    }
+    };
+
     const theme = select('CalloutAlert.theme', themeOptions, '');
     const calloutAlertProps = {
       icon: { name },
@@ -32,6 +35,7 @@ storiesOf('organisms', module).addDecorator(withKnobs)
       <CalloutAlert {...calloutAlertProps}>
         <Paragraph />
         <OrderedList />
+        <DecorativeLink />
       </CalloutAlert>
     );
   }));
