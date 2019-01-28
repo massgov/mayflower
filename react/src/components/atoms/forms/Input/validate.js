ba905716ad18c779062e184945ae770b2de6a6e6
Minghua Sun <clairesunstudio@gmail.com>
2019-01-28 15:40:49 -0500

    fix typo

--- react/src/components/atoms/forms/Input/validate.js
+++ react/src/components/atoms/forms/Input/validate.js
@@ -5,7 +5,7 @@ export const validNumber = (num, min, max) => {
     if (num < Number(min)) {
       return {
         showError: true,
-        errorMsg: `Please enter a valid greater than or equal to ${min}.`
+        errorMsg: `Please enter a valid number greater than or equal to ${min}.`
       };
     }
   }
@@ -13,7 +13,7 @@ export const validNumber = (num, min, max) => {
     if (num > Number(max)) {
       return {
         showError: true,
-        errorMsg: `Please enter a valid less than or equal to ${max}.`
+        errorMsg: `Please enter a valid number less than or equal to ${max}.`
       };
     }
   }
