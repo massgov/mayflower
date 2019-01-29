34ba37d536c3cdd8ddc7edff0e1f3db1b3132478
Minghua Sun <clairesunstudio@gmail.com>
2019-01-29 13:44:51 -0500

    fix float and step

---
+++ react/src/components/atoms/forms/InputPercentage/InputNumber.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs/react';
+
+import InputNumber from './index';
+import InputNumberOptions from './InputNumber.knobs.options';
+import inputNumberText from './InputNumber.md';
+
+storiesOf('atoms/forms', module).addDecorator(withKnobs)
+  .add('InputPercentage', withInfo(`<div>${inputNumberText}</div>`)(() => {
+    const inputTextOptionsWithKnobs = Object.assign(...Object.entries(InputNumberOptions).map(([k, v]) => (
+      { [k]: v() })));
+    return(
+      <InputNumber {...inputTextOptionsWithKnobs} />
+    );
+  }));
