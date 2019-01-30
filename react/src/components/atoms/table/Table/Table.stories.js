72324e65f4b4ef4c341053bf92f83542627f2222
Steven Murray <steven.murray@mediacurrent.com>
2019-01-30 12:25:39 -0500

    WIP.

--- react/src/components/atoms/table/Table/Table.stories.js
+++ react/src/components/atoms/table/Table/Table.stories.js
@@ -8,5 +8,23 @@ import Table from '.';
 import tableReadme from './Table.md';
 import tableOptions from './Table.knobs.options';
 
+tableOptions.feeTable.head.rows[0].cells.push({
+  heading: false,
+  colspan: '',
+  rowspan: '',
+  text: 'Testing'
+});
+tableOptions.feeTable.bodies.rows = tableOptions.feeTable.bodies.map((body) => {
+  return body.rows.map((row) => {
+    row.cells.push({
+      heading: false,
+      colspan: '',
+      rowspan: '',
+      text: 'testtest'
+    });
+    return row;
+  });
+});
+
 storiesOf('atoms/table', module).addDecorator(withKnobs)
   .add('Table', withInfo(`<div>${tableReadme}</div>`)(() => (<Table {...object('tableOptions.feeTable', tableOptions.feeTable)} />)));
