da20098e71e2558a06654b6ebd5974b1aeb9d60c
Steven Murray <steven.murray@mediacurrent.com>
2019-02-06 16:56:56 -0500

    Adds storybook 4.x and nwb 0.23.x

--- react/.storybook/webpack.config.js
+++ react/.storybook/webpack.config.js
@@ -1,6 +1,13 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.forEach((rule, ruleIndex) => {
+    if (rule && typeof rule !== 'string' && rule.test.toString().indexOf('js') > -1) {
+      if (defaultConfig.module.rules[ruleIndex].options) {
+        defaultConfig.module.rules[ruleIndex].options.plugins.push('@babel/plugin-proposal-export-default-from');
+      }
+    }
+  });
   defaultConfig.resolve = {
     ...defaultConfig.resolve,
      alias: {
