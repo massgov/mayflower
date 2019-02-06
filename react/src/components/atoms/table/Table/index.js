9dfd769080e64ecd97a7ebd51256285658e4993b
Steven Murray <steven.murray@mediacurrent.com>
2019-02-06 14:26:59 -0500

    Removes wrapper divs.

--- react/src/components/atoms/table/Table/index.js
+++ react/src/components/atoms/table/Table/index.js
@@ -12,79 +12,73 @@ const Table = (table) => {
       tableWideClass = ' ma__table--wide';
     }
   }
-  const responsiveClasses = classnames({
-    'ma__table--responsive__wrapper': true
-  });
   return(
-    <div className="ma__table--responsive js-ma-responsive-table">
-      <div className={responsiveClasses} role="region" id={table.id}>
-        <table className={`ma__table${tableWideClass}`}>
-          {
-            (table.head && table.head.rows) &&
-            <thead>
-              { table.head.rows.map((row, rowIndex) => (
-                <tr key={row.key || `thead-row${rowIndex}`}>
-                  { row.cells.map((cell, cellIndex) => (
-                    <th
-                      key={cell.key || `thead-row${rowIndex}-cell${cellIndex}`}
-                      scope="col"
-                      colSpan={cell.colspan || null}
-                      rowSpan={cell.rowspan || null}
-                    >{cell.text}
-                    </th>
-                ))}
-                </tr>
-              ))}
-            </thead>
-        }{table.bodies.map((body, bodyIndex) => (
-          <tbody key={body.key || `tbody${bodyIndex}`}>
-            { body.rows.map((row, rowIndex) => {
-              const tableIndexOffset = row.rowSpanOffset ? 0 : 1;
-              const defaultRowKey = `tbody${bodyIndex}-row${rowIndex}`;
-              return(
-                <tr key={row.key || defaultRowKey} className={row.rowSpanOffset ? 'is-offset' : null}>
-                  { row.cells.map((cell, cellIndex) => {
-                    let dataLabel = null;
-                    if ((cellIndex > 0) && tableHeaderRow.cells && tableHeaderRow.cells.length > 0) {
-                      // Mayflower twig loops started at 1 - add one to index.
-                      dataLabel = tableHeaderRow.cells[(cellIndex + 1) - tableIndexOffset].text;
-                    }
-                    const defaultCellKey = `${defaultRowKey}-cell${cellIndex}`;
-                    const cellClasses = classnames({
-                      'ma__table-cell': true,
-                      'no-left-pad': (!row.rowSpanOffset && !cell.heading && cellIndex === 1)
-                    });
-                    return(cell.heading ? (
-                      <th
-                        className={cellClasses}
-                        key={cell.key || defaultCellKey}
-                        data-label={dataLabel}
-                        scope="row"
-                        colSpan={cell.colspan || null}
-                        rowSpan={cell.rowspan || null}
-                      >{cell.text}
-                      </th>
-                    ) : (
-                      <td
-                        className={cellClasses}
-                        key={cell.key || defaultCellKey}
-                        data-label={dataLabel}
-                        colSpan={cell.colspan || null}
-                        rowSpan={cell.rowspan || null}
-                      >
-                        {cell.text}
-                      </td>
-                    ));
-                })}
-                </tr>
-              );
-            })
-            }
-          </tbody>))
-          }
-        </table>
-      </div>
-    </div>
+    <table id={table.id} className={`ma__table${tableWideClass}`}>
+      {
+        (table.head && table.head.rows) &&
+        <thead>
+          { table.head.rows.map((row, rowIndex) => (
+            <tr key={row.key || `thead-row${rowIndex}`}>
+              { row.cells.map((cell, cellIndex) => (
+                <th
+                  key={cell.key || `thead-row${rowIndex}-cell${cellIndex}`}
+                  scope="col"
+                  colSpan={cell.colspan || null}
+                  rowSpan={cell.rowspan || null}
+                >{cell.text}
+                </th>
+            ))}
+            </tr>
+          ))}
+        </thead>
+    }{table.bodies.map((body, bodyIndex) => (
+      <tbody key={body.key || `tbody${bodyIndex}`}>
+        { body.rows.map((row, rowIndex) => {
+          const tableIndexOffset = row.rowSpanOffset ? 0 : 1;
+          const defaultRowKey = `tbody${bodyIndex}-row${rowIndex}`;
+          return(
+            <tr key={row.key || defaultRowKey} className={row.rowSpanOffset ? 'is-offset' : null}>
+              { row.cells.map((cell, cellIndex) => {
+                let dataLabel = null;
+                if ((cellIndex > 0) && tableHeaderRow.cells && tableHeaderRow.cells.length > 0) {
+                  // Mayflower twig loops started at 1 - add one to index.
+                  dataLabel = tableHeaderRow.cells[(cellIndex + 1) - tableIndexOffset].text;
+                }
+                const defaultCellKey = `${defaultRowKey}-cell${cellIndex}`;
+                const cellClasses = classnames({
+                  'ma__table-cell': true,
+                  'no-left-pad': (!row.rowSpanOffset && !cell.heading && cellIndex === 1)
+                });
+                return(cell.heading ? (
+                  <th
+                    className={cellClasses}
+                    key={cell.key || defaultCellKey}
+                    data-label={dataLabel}
+                    scope="row"
+                    colSpan={cell.colspan || null}
+                    rowSpan={cell.rowspan || null}
+                  >{cell.text}
+                  </th>
+                ) : (
+                  <td
+                    className={cellClasses}
+                    key={cell.key || defaultCellKey}
+                    data-label={dataLabel}
+                    colSpan={cell.colspan || null}
+                    rowSpan={cell.rowspan || null}
+                  >
+                    {cell.text}
+                  </td>
+                ));
+            })}
+            </tr>
+          );
+        })
+        }
+      </tbody>
+    ))
+      }
+    </table>
   );
 };
 
