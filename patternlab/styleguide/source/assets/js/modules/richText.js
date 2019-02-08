ab9c2c98d995441e38b7c8888a24cc73c49767b9
Yukiko Gannett <yukikogannett@gmail.com>
2019-02-08 10:37:56 -0500

    DP-11507 Move richText.js functionality to Drupal (#450)
    * remove patternlab/styleguide/source/assets/js/modules/richText.js
    
    * remove richtext.js from index.js
    
    * add changlog for DP-11507
    
    * put back richText.js. remove ll.8-29 where add hooks to table.
    
    * update changelog
    
    * update table markup in patternlab/styleguide/source/_patterns/05-pages/information-details~toc-and-responsive-tables.json
    
    * fix format error
    
    * change the table template in rte content in patternlab/styleguide/source/_patterns/05-pages/information-details~toc-and-responsive-tables.json
    
    * change the table template in rte content in _patterns/05-pages/information-details~responsive-table.json
    
    * put back richText.js
    
    * add missing class setting shown in table.twig to table-responsive.twig

--- patternlab/styleguide/source/assets/js/modules/richText.js
+++ patternlab/styleguide/source/assets/js/modules/richText.js
@@ -5,29 +5,6 @@ export default (function (window, document, $) {
     let $el = $(this),
       richTextIndex = index;
 
-    const $table = $el.find("table").not("table table");
-    $table.each(function() {
-      const $this = $(this);
-      let $wrapped;
-      // If this table does not have the `ma__table--responsive` class.
-      if (!$this.closest('.ma__table--responsive').length) {
-        $this
-          .wrap('<div class="ma__table--responsive js-ma-responsive-table"><div class="ma__table--responsive__wrapper"></div></div>');
-      }
-      // At this point there is a `.ma__table--responsive` class,
-      // but does this table already posses a responsive table structure.
-      else if(!$this.closest('.ma__table--responsive').length) {
-        $this
-          .wrap('<div class="ma__table--responsive js-ma-responsive-table"><div class="ma__table--responsive__wrapper"></div></div>');
-      }
-
-      $wrapped = $this.closest(".ma__table--responsive");
-      if (!$wrapped.find("nav.ma__table__horizontal-nav").length) {
-        const $tableNav = $('<nav class="ma__table__horizontal-nav"><button class="ma__table__horizontal-nav__left" aria-controls="{{tableID}}"><span class="visually-hidden">Scroll left</span></button><div class="clip-scrollbar"><div class="ma__scroll-indicator"><div class="ma__scroll-indicator--bar" aria-controls="{{tableID}}" role="scrollbar" aria-orientation="horizontal"><div class="ma__scroll-indicator__button"></div></div></div></div><button class="ma__table__horizontal-nav__right" aria-controls="{{tableID}}"><span class="visually-hidden">Scroll right</span></button></nav>');
-        $wrapped.prepend($tableNav);
-      }
-    });
-
     // Provide css hooks to indent each child heading and its nested contents if js hook is present.
     if ($el.hasClass('js-ma-outline-indent')) {
       $el.find(':header').each(function(index,heading){
