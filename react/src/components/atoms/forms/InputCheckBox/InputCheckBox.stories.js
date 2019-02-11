8d25e6590534283efae49e1146427bf360e1e41c
Minghua Sun <clairesunstudio@gmail.com>
2019-02-11 16:33:38 -0500

    fix story

--- react/src/components/atoms/forms/InputCheckBox/InputCheckBox.stories.js
+++ react/src/components/atoms/forms/InputCheckBox/InputCheckBox.stories.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs/react';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import InputCheckBox from './index';
 import InputCheckBoxOptions from './InputCheckBox.knobs.options';
-import inputCheckBoxText from './InputCheckBox.md';
+import inputCheckBoxDocs from './InputCheckBox.md';
 
-storiesOf('atoms/forms', module).addDecorator(withKnobs)
-  .add('InputCheckBox', withInfo(`<div>${inputCheckBoxText}</div>`)(() => {
-    const inputCheckBoxOptionsWithKnobs = Object.assign(...Object.entries(InputCheckBoxOptions).map(([k, v]) => (
-      { [k]: v() })));
-    return(
-      <InputCheckBox {...inputCheckBoxOptionsWithKnobs} />
-    );
-  }));
+storiesOf('atoms/forms', module)
+  .addDecorator(withInfo)
+  .addDecorator(withKnobs({ escapeHTML: false }))
+  .add(
+    'InputCheckBox', (() => {
+      const inputCheckBoxOptionsWithKnobs = Object.assign(...Object.entries(InputCheckBoxOptions).map(([k, v]) => (
+        { [k]: v() })));
+      return(
+        <InputCheckBox {...inputCheckBoxOptionsWithKnobs} />
+      );
+    }),
+    { info: inputCheckBoxDocs }
+  );
