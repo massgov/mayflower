ff71b474198e20a91cb01f009caace8ac9767e13
Minghua Sun <clairesunstudio@gmail.com>
2019-02-12 11:55:56 -0500

    add reference image

--- react/src/components/atoms/forms/InputCheckBox/InputCheckBox.knobs.options.js
+++ react/src/components/atoms/forms/InputCheckBox/InputCheckBox.knobs.options.js
@@ -4,7 +4,7 @@ import { svgOptions } from '../../icons/Icon/Icon.knob.options';
 
 export default {
   id: () => text('InputCheckBox.id', 'input-checkbox'),
-  label: () => text('InputCheckBox.label', 'input checkbox'),
+  label: () => text('InputCheckBox.label', 'Apply to all'),
   icon: () => select('InputCheckBox.icon', svgOptions, ''),
   defaultValue: () => boolean('InputCheckBox.checked', false),
   onChange: () => action('InputCheckBox.onChange')
