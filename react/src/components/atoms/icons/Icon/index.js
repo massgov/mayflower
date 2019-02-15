a5254252542b5790a688793e1fda874a72e58adf
avrilpearl <sienna.avril.pearl@gmail.com>
2019-02-15 12:01:26 -0500

    wip - temp testing of polyfill option

--- react/src/components/atoms/icons/Icon/index.js
+++ react/src/components/atoms/icons/Icon/index.js
@@ -3,6 +3,42 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
+// This condition actually should detect if it's an Node environment
+if (typeof require.context === 'undefined') {
+  const fs = require('fs');
+  const path = require('path');
+
+  require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.svg$/) => {
+    const files = {};
+
+    function readDirectory(directory) {
+      fs.readdirSync(directory).forEach((file) => {
+        const fullPath = path.resolve(directory, file);
+
+        if (fs.statSync(fullPath).isDirectory()) {
+          if (scanSubDirectories) readDirectory(fullPath);
+
+          return;
+        }
+
+        if (!regularExpression.test(fullPath)) return;
+
+        files[fullPath] = true;
+      });
+    }
+
+    readDirectory(path.resolve(__dirname, './assets'));
+
+    function Module(file) {
+      return require(file);
+    }
+
+    Module.keys = () => Object.keys(files);
+
+    return Module;
+  };
+}
+
 // Loads all SVGs within the assets directory
 const req = require.context('!svg-sprite-loader!./assets', true, /.svg$/);
 function loadAssets(asset) {
