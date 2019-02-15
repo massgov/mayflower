6d9df8c82422282abe5a7ab7c083af91c1bac3ca
avrilpearl <sienna.avril.pearl@gmail.com>
2019-02-15 17:26:55 -0500

    adding unit test in storybook

--- react/src/components/atoms/buttons/Button/Button.stories.js
+++ react/src/components/atoms/buttons/Button/Button.stories.js
@@ -5,8 +5,9 @@ import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { specs, describe, it } from 'storybook-addon-specifications';
-import { mount } from "enzyme";
-import expect from "expect";
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import sinon from 'sinon';
 
 import Button from './index';
 import ButtonDocs from './Button.md';
@@ -29,12 +30,28 @@ storiesOf('atoms/buttons', module)
         onClick: action('button clicked')
       };
       const story = <Button {...props} />;
-      /*specs(() => describe('Hello World', function () {
-        it('Should have the Hello World label', function () {
-          let output = mount(story);
-          expect(output.text()).toContain('Button');
+      specs(() => describe('Button', () => {
+        it(`Should display text "${props.text}".`, () => {
+          const wrapper = mount(story);
+          expect(wrapper.text()).to.contain(props.text);
         });
-      }));*/
+        it('Simulate button click to test onClick callback function.', () => {
+          const callback = sinon.spy();
+          const wrapper = mount(<Button {...props} disabled={false} onClick={callback} />);
+          wrapper.find('button').simulate('click');
+          expect(callback).to.have.property('callCount', 1);
+        });
+        it('Should not be clickable in disabled state.', () => {
+          const callback = sinon.spy();
+          const wrapper = mount(<Button {...props} disabled onClick={callback} />);
+          wrapper.find('button').simulate('click');
+          expect(callback).to.have.property('callCount', 0);
+        });
+        it('Should have disabled classsname in disabled state.', () => {
+          const wrapper = mount(<Button {...props} disabled />);
+          expect(wrapper.find('button').hasClass('ma__button--disabled')).to.equal(true);
+        });
+      }));
       return(
         story
       );
