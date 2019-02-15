fc1eaba0ae924e14a5c2b79acc48bde099f42564
avrilpearl <sienna.avril.pearl@gmail.com>
2019-02-15 13:27:21 -0500

    wip

--- react/.storybook/config.js
+++ react/.storybook/config.js
@@ -1,7 +1,8 @@
 import {configure} from '@storybook/react';
+import requireContext from 'require-context.macro';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.js$/);
+const req = requireContext('../src', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
