4fb53814cf8f3ae22092e55bcd5fb9825a6cc7ac
devclair <clair.smith@mediacurrent.com>
2019-02-19 09:14:32 -0600

    DP-6198: close utility nav on esc press

--- patternlab/styleguide/source/assets/js/modules/mainNav.js
+++ patternlab/styleguide/source/assets/js/modules/mainNav.js
@@ -1,44 +1,44 @@
-export default (function (window,document,$,undefined) {
+export default (function (window, document, $, undefined) {
 
   let windowWidth = window.innerWidth;
 
-  $(window).resize(function(){
+  $(window).resize(function () {
     windowWidth = window.innerWidth;
   });
 
-  $('.js-main-nav').each(function() {
+  $('.js-main-nav').each(function () {
     let openClass = "is-open",
-        closeClass = "is-closed",
-        submenuClass = "show-submenu",
-        $parent = $(this),
-        $mainNavToggle = $parent.find('.js-main-nav-toggle'),
-        $mainNavItems = $parent.find('.js-main-nav-toggle, .js-main-nav-top-link'),
-        breakpoint = 840; // matches CSS breakpoint for Main Nav
+      closeClass = "is-closed",
+      submenuClass = "show-submenu",
+      $parent = $(this),
+      $mainNavToggle = $parent.find('.js-main-nav-toggle'),
+      $mainNavItems = $parent.find('.js-main-nav-toggle, .js-main-nav-top-link'),
+      breakpoint = 840; // matches CSS breakpoint for Main Nav
 
-    $mainNavItems.on('keydown', function(e) {
+    $mainNavItems.on('keydown', function (e) {
       // Grab all the DOM info we need...
       let $link = $(this),
-          $topLevelLinks = $parent.find('.ma__main-nav__top-link'),
-          open = $link.hasClass(openClass),
-          $openContent = $parent.find('.js-main-nav-content.' + openClass),
-          $focusedElement = $(document.activeElement),
-          menuFlipped = (windowWidth < breakpoint),
-      // relevant if open..
-          $topLevelItem = $focusedElement.parents('.ma__main-nav__item'),
-          $topLevelLink = $topLevelItem.find('.ma__main-nav__top-link'),
-          $dropdownLinks = $link.find('.ma__main-nav__subitem .ma__main-nav__link'),
-          dropdownLinksLength = $dropdownLinks.length,
-          focusIndexInDropdown = $dropdownLinks.index($focusedElement),
-      // Easy access to the key that was pressed.
-          keycode = e.keyCode,
-          action = {
-            'skip': keycode === 9,
-            'close': keycode === 27,
-            'left': keycode === 37,
-            'right': keycode === 39,
-            'up': keycode === 38,
-            'down': keycode === 40
-          };
+        $topLevelLinks = $parent.find('.ma__main-nav__top-link'),
+        open = $link.hasClass(openClass),
+        $openContent = $parent.find('.js-main-nav-content.' + openClass),
+        $focusedElement = $(document.activeElement),
+        menuFlipped = (windowWidth < breakpoint),
+        // relevant if open..
+        $topLevelItem = $focusedElement.parents('.ma__main-nav__item'),
+        $topLevelLink = $topLevelItem.find('.ma__main-nav__top-link'),
+        $dropdownLinks = $link.find('.ma__main-nav__subitem .ma__main-nav__link'),
+        dropdownLinksLength = $dropdownLinks.length,
+        focusIndexInDropdown = $dropdownLinks.index($focusedElement),
+        // Easy access to the key that was pressed.
+        keycode = e.keyCode,
+        action = {
+          'skip': keycode === 9, // tab
+          'close': keycode === 27, // esc
+          'left': keycode === 37, // left arrow
+          'right': keycode === 39, // right arrow
+          'up': keycode === 38, // up arrow
+          'down': keycode === 40 // down arrow
+        };
 
       // Default behavior is prevented for all actions except 'skip'.
       if (action.close || action.left || action.right || action.up || action.down) {
@@ -46,17 +46,17 @@ export default (function (window,document,$,undefined) {
       }
 
       // Skip out of the menu and close any currently-open submenus.
-      if(action.skip) {
+      if (action.skip) {
         hide($openContent);
         $link.removeClass(openClass);
-        $topLevelLink.attr('aria-expanded','false');
+        $topLevelLink.attr('aria-expanded', 'false');
         return;
       }
 
       // Navigate into or within a submenu. This is needed on up/down actions
       // (unless the menu is flipped and closed) and when using the right arrow
       // while the menu is flipped and submenu is closed.
-      if(((action.up || action.down) && !(menuFlipped && !open))
+      if (((action.up || action.down) && !(menuFlipped && !open))
         || (action.right && menuFlipped && !open)) {
         // Open pull down menu if necessary.
         if (!open) {
@@ -69,12 +69,12 @@ export default (function (window,document,$,undefined) {
         focusIndexInDropdown += (action.up ? -1 : 1);
         // If the menu is flipped, skip the last item in each submenu. Otherwise,
         // skip the first item. This is done by repeating the index adjustment.
-        if(menuFlipped) {
-          if(focusIndexInDropdown === dropdownLinksLength - 1) {
+        if (menuFlipped) {
+          if (focusIndexInDropdown === dropdownLinksLength - 1) {
             focusIndexInDropdown += (action.up ? -1 : 1);
           }
         } else {
-          if(focusIndexInDropdown === 0 || focusIndexInDropdown >= dropdownLinksLength) {
+          if (focusIndexInDropdown === 0 || focusIndexInDropdown >= dropdownLinksLength) {
             focusIndexInDropdown += (action.up ? -1 : 1);
           }
         }
@@ -85,20 +85,20 @@ export default (function (window,document,$,undefined) {
       }
 
       // Close menu and return focus to menubar
-      if(action.close || (menuFlipped && action.left)) {
+      if (action.close || (menuFlipped && action.left)) {
         hide($openContent);
         $link.removeClass(openClass);
-        $topLevelLink.focus().attr('aria-expanded','false');
+        $topLevelLink.focus().attr('aria-expanded', 'false');
         return;
       }
 
       // Navigate between submenus. This is needed for left/right actions in
       // normal layout, or up/down actions in flipped layout (when nav is closed).
-      if(((action.left || action.right) && !menuFlipped) ||
-         ((action.up || action.down) && menuFlipped && !open)) {
+      if (((action.left || action.right) && !menuFlipped) ||
+        ((action.up || action.down) && menuFlipped && !open)) {
         let index = $topLevelLinks.index($topLevelLink),
-            prev = action.left || action.up,
-            linkCount = $topLevelLinks.length;
+          prev = action.left || action.up,
+          linkCount = $topLevelLinks.length;
 
         // hide content
         // If menubar focus
@@ -107,7 +107,7 @@ export default (function (window,document,$,undefined) {
         // If dropdown focus
         //  - Open previous pull down menu and select first item
         hide($openContent);
-        $topLevelLink.attr('aria-expanded','false');
+        $topLevelLink.attr('aria-expanded', 'false');
         // Get previous item if left arrow, next item if right arrow.
         index += (prev ? -1 : 1);
         // Wrap around if at the end of the set of menus.
@@ -117,31 +117,31 @@ export default (function (window,document,$,undefined) {
       }
 
     });
-    $mainNavItems.on('mouseenter', function(e) {
-      $(this).children('button').attr("aria-expanded","true");
+    $mainNavItems.on('mouseenter', function (e) {
+      $(this).children('button').attr("aria-expanded", "true");
 
-      if(windowWidth > breakpoint) {
+      if (windowWidth > breakpoint) {
         let $openContent = $(this).find('.js-main-nav-content');
         show($openContent);
       }
     });
-    $mainNavItems.on('mouseleave', function(e) {
-      $(this).children('button').attr("aria-expanded","false");
+    $mainNavItems.on('mouseleave', function (e) {
+      $(this).children('button').attr("aria-expanded", "false");
 
-      if(windowWidth > breakpoint) {
+      if (windowWidth > breakpoint) {
         let $openContent = $(this).find('.js-main-nav-content');
         hide($openContent);
       }
     });
-    $mainNavToggle.children('button, a').on('click', function(e) {
+    $mainNavToggle.children('button, a').on('click', function (e) {
       let $el = $(this),
-          $elParent = $el.parent(),
-          $content = $elParent.find('.js-main-nav-content'),
-          $openContent = $parent.find('.js-main-nav-content.' + openClass),
-          isOpen = $content.hasClass(openClass);
+        $elParent = $el.parent(),
+        $content = $elParent.find('.js-main-nav-content'),
+        $openContent = $parent.find('.js-main-nav-content.' + openClass),
+        isOpen = $content.hasClass(openClass);
 
       // mobile
-      if(windowWidth <= breakpoint) {
+      if (windowWidth <= breakpoint) {
         e.preventDefault();
         // add open class to this item
         $elParent.addClass(openClass);
@@ -151,20 +151,20 @@ export default (function (window,document,$,undefined) {
         hide($openContent);
         $el.attr('aria-expanded', 'false');
 
-        if(!isOpen) {
+        if (!isOpen) {
           show($content);
           $el.attr('aria-expanded', 'true');
         }
       }
     });
 
-    $('.js-close-sub-nav').on('click', function(){
+    $('.js-close-sub-nav').on('click', function () {
       let $openContent = $parent.find('.js-main-nav-content.' + openClass);
       hide($openContent);
     });
 
     // Hide any open submenu content when the sidebar menu is closed
-    $('.js-header-menu-button').click(function() {
+    $('.js-header-menu-button').click(function () {
       let $openContent = $parent.find('.js-main-nav-content.' + openClass);
       hide($openContent);
     });
@@ -173,30 +173,30 @@ export default (function (window,document,$,undefined) {
       $('body').removeClass(submenuClass);
       $parent.find("." + openClass).removeClass(openClass);
 
-      if(windowWidth <= breakpoint) {
+      if (windowWidth <= breakpoint) {
         $content.addClass(closeClass);
       } else {
         $content
-        .stop( true, true )
-        .slideUp('fast',function() {
-          $content
-            .addClass(closeClass)
-            .slideDown(0);
-        });
+          .stop(true, true)
+          .slideUp('fast', function () {
+            $content
+              .addClass(closeClass)
+              .slideDown(0);
+          });
       }
     }
 
     function show($content) {
       $('body').addClass(submenuClass);
-      if(windowWidth <= breakpoint) {
+      if (windowWidth <= breakpoint) {
         $content
           .addClass(openClass)
           .removeClass(closeClass);
       } else {
         $content
-          .stop( true, true )
-          .delay( 200 )
-          .slideUp(0,function() {
+          .stop(true, true)
+          .delay(200)
+          .slideUp(0, function () {
             $content
               .addClass(openClass)
               .removeClass(closeClass)
@@ -208,4 +208,4 @@ export default (function (window,document,$,undefined) {
 
   });
 
-})(window,document,jQuery);
+})(window, document, jQuery);
