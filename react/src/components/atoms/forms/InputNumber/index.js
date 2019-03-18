f273aba825f92be76b10bd8685bc20fc99d4f752
Minghua Sun <clairesunstudio@gmail.com>
2019-03-18 12:32:24 -0400

    Update react/src/components/atoms/forms/InputNumber/index.js
    Co-Authored-By: smurrayatwork <smurrayatwork@gmail.com>

--- react/src/components/atoms/forms/InputNumber/index.js
+++ react/src/components/atoms/forms/InputNumber/index.js
@@ -93,7 +93,7 @@ const NumberInput = (props) => {
             }
             const updateError = displayErrorMessage(newValue);
             context.updateState({ value: newValue, ...updateError }, () => {
-              if (typeof props.onChange === 'function') {
+              if (is.fn(props.onChange)) {
                 props.onChange(e, newValue, props.id);
               }
             });
