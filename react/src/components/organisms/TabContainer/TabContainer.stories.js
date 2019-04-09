703eb5f4bccd54ec66e46af9a0c1de66741503ea
Minghua Sun <clairesunstudio@gmail.com>
2019-04-09 17:22:47 -0400

    add action to story

--- react/src/components/organisms/TabContainer/TabContainer.stories.js
+++ react/src/components/organisms/TabContainer/TabContainer.stories.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 import TabContainer from '.';
 import Tab from './tab';
 import TabContainerData from './TabContainer.knobs.options';
+
+const tabProps = {
+  handleClick: action('Tab click')
+};
 
 storiesOf('organisms/TabContainer', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
@@ -12,22 +17,23 @@ storiesOf('organisms/TabContainer', module)
     const props = {
       nested: boolean('nested', true)
     };
+
     return(
       <TabContainer>
-        <Tab title="Tab 1">
+        <Tab title="Tab 1" {...tabProps}>
           <TabContainer {...props}>
-            <Tab title="Nested Tab Here">This should support nesting like this.</Tab>
-            <Tab title="Another Nested Tab">Tabs have unique ids that are tracked locally without state.</Tab>
+            <Tab title="Nested Tab Here" {...tabProps}>This should support nesting like this.</Tab>
+            <Tab title="Another Nested Tab" {...tabProps}>Tabs have unique ids that are tracked locally without state.</Tab>
           </TabContainer>
         </Tab>
-        <Tab title="Tab 2">And this is my second tab.</Tab>
-        <Tab title="Tab 3">Last Tab!</Tab>
-        <Tab title="Tab">Tab Content</Tab>
-        <Tab title="Tab">Tab Content</Tab>
-        <Tab title="Tab">Tab Content</Tab>
-        <Tab title="Tab">Tab Content</Tab>
-        <Tab title="Tab">Tab Content</Tab>
-        <Tab title="Tab">Tab Content</Tab>
+        <Tab title="Tab 2" {...tabProps}>And this is my second tab.</Tab>
+        <Tab title="Tab 3" {...tabProps}>Last Tab!</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
+        <Tab title="Tab" {...tabProps}>Tab Content</Tab>
       </TabContainer>
     );
   }))
@@ -39,11 +45,11 @@ storiesOf('organisms/TabContainer', module)
       return(
         <TabContainer>
           { TabContainerData.budgetTabs.map((tab, index) => (
-            <Tab key={`tab${index}`} title={tab.tab}>
+            <Tab key={`tab${index}`} title={tab.tab} {...tabProps}>
               { tab.subTabs && (
                 <TabContainer {...props}>
                   {tab.subTabs.map((tab, index) => (
-                    <Tab key={`subTab${index}`} title={tab.tab}>{tab.content}</Tab>
+                    <Tab key={`subTab${index}`} title={tab.tab} {...tabProps}>{tab.content}</Tab>
                   ))}
                 </TabContainer>
               )
