355f58126df53eaab1217fd99be9806138f20e7e
Steven Murray <steven.murray@mediacurrent.com>
2019-06-11 17:30:42 -0400

    Bug fixes.

--- react/src/components/molecules/EmergencyAlert/EmergencyAlert.stories.js
+++ react/src/components/molecules/EmergencyAlert/EmergencyAlert.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
 import EmergencyAlert from './index';
-import Button from '../../atoms/buttons/Button';
+import Link from '../Link';
 
 storiesOf('molecules', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
@@ -14,10 +14,10 @@ storiesOf('molecules', module)
           'no theme': '',
           'c-primary-alt': 'c-primary-alt',
           'c-gray-dark': 'c-gray-dark'
-        }),
+        }, 'c-warning'),
         message: text('message', 'MassPort is operating with limited flights due to road closures.'),
         timeStamp: text('timeStamp', '02.15.16, 5:00 p.m'),
-        link: ({ theme }) => <Button href="https://www.mass.gov">Read full alert</Button>
+        link: ({ theme }) => <Link href="https://www.mass.gov">Read full alert</Link>
       };
       return(
         <EmergencyAlert {...props} />
