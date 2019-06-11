92566356a067386ef45111cbb8bc62c88fa36a8e
Steven Murray <steven.murray@mediacurrent.com>
2019-06-11 17:25:10 -0400

    Adds support for button to use children prop, adds theme prop to EmergencyAlert.

--- react/src/components/molecules/EmergencyAlert/EmergencyAlert.stories.js
+++ react/src/components/molecules/EmergencyAlert/EmergencyAlert.stories.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import EmergencyAlert from './index';
-import Link from '../Link';
+import Button from '../../atoms/buttons/Button';
 
 storiesOf('molecules', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
     'EmergencyAlert', (() => {
       const props = {
+        theme: select('theme', {
+          'c-warning (default)': 'c-warning',
+          'no theme': '',
+          'c-primary-alt': 'c-primary-alt',
+          'c-gray-dark': 'c-gray-dark'
+        }),
         message: text('message', 'MassPort is operating with limited flights due to road closures.'),
         timeStamp: text('timeStamp', '02.15.16, 5:00 p.m'),
-        link: () => <Link href="https://www.mass.gov"><span>Read full alert</span></Link>
+        link: ({ theme }) => <Button href="https://www.mass.gov">Read full alert</Button>
       };
       return(
         <EmergencyAlert {...props} />
--- react/src/components/molecules/EmergencyAlert/index.js
+++ react/src/components/molecules/EmergencyAlert/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import is from 'is';
 import './style.css';
 
 const EmergencyAlert = (props) => {
-  const { message, timeStamp, link } = props;
+  const { message, timeStamp, link, theme } = props;
+  const paragraphClasses = classNames({
+    'ma__emergency-alert': true,
+    [`ma__emergency-alert--${theme}`]: theme && theme.length > 0
+  });
   return(
-    <p className="ma__emergency-alert">
+    <p className={paragraphClasses}>
       <span className="ma__emergency-alert__message">
         {message}
         {(timeStamp) && (<span className="ma__emergency-alert__time-stamp">&nbsp;{timeStamp}</span>)}
       </span>
       {is.fn(link) && (
         <span className="ma__emergency-alert__link">
-          &nbsp;{link()}
+          &nbsp;{link({ theme })}
         </span>
       )}
     </p>
@@ -21,12 +26,17 @@ const EmergencyAlert = (props) => {
 };
 
 EmergencyAlert.propTypes = {
+  theme: PropTypes.oneOf(['', 'c-primary-alt', 'c-warning', 'c-gray-dark']),
   /** A message describing the event. */
   message: PropTypes.string.isRequired,
   /** A string representing the time of the event. */
   timeStamp: PropTypes.string,
   /** An optional function whose return value is a link to take the user to page with more information. */
   link: PropTypes.func
+};
+
+EmergencyAlert.defaultProps = {
+  theme: 'c-warning'
 };
 
 export default EmergencyAlert;
