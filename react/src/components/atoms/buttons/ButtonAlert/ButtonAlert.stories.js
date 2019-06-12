cee7c71f4faca9d87f35b449140a86a19fcfbc1c
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2019-06-12 11:04:16 -0400

    DP-14335 - updating story file

--- react/src/components/atoms/buttons/ButtonAlert/ButtonAlert.stories.js
+++ react/src/components/atoms/buttons/ButtonAlert/ButtonAlert.stories.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text, select, boolean, array } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import ButtonAlert from './index';
 import ButtonAlertDocs from './ButtonAlert.md';
-import ButtonAlertOptions from './ButtonAlert.knobs.options';
-
 
 storiesOf('atoms/buttons', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
