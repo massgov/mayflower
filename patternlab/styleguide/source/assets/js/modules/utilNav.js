afb0fc9975ed07d76c2278931d3ec20912620e5c
devclair <clair.smith@mediacurrent.com>
2019-06-14 14:07:12 -0500

    DP-13939: update aria attributes

--- patternlab/styleguide/source/assets/js/modules/utilNav.js
+++ patternlab/styleguide/source/assets/js/modules/utilNav.js
@@ -7,16 +7,13 @@ export default (function (window, document, $, undefined) {
     const height = $panel.height();
     const $closeButton = $panel.find('.js-close-util-nav');
 
+
     $panel.css('top', '-' + height + 'px');
 
     $closeButton.on('click', function () {
       $panel.css('top', '-' + height + 'px');
       $panel.toggleClass('is-closed');
-    });
-
-    $('.js-close-sub-nav').on('click', function () {
-      $panel.toggleClass('is-closed');
-      $panel.css('top', '-' + height + 'px');
+      $panel.attr("aria-hidden", "true");
     });
 
   });
@@ -25,17 +22,12 @@ export default (function (window, document, $, undefined) {
     const $thisButton = $(this);
     const $thisButtonId = $(this).data("panel");
     const $thisPanel = $('.js-util-nav-content[data-panel=' + $thisButtonId + ']');
-    $('body').toggleClass('show-submenu');
 
     $thisButton.on('click', function () {
       $thisPanel.removeClass('is-closed');
       $thisPanel.removeAttr('style');
-      $('body').toggleClass('show-submenu');
+      $thisPanel.attr("aria-hidden", "false");
     });
-  });
-
-  $('.js-close-sub-nav').on('click', function () {
-
   });
 
 })(window, document, jQuery);
