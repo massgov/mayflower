55cc44d38bb22facec16170b6f1be6d63cc17597
devclair <clair.smith@mediacurrent.com>
2019-06-16 15:25:33 -0500

    DP-13939: code cleanup

--- patternlab/styleguide/source/assets/js/modules/utilNav.js
+++ patternlab/styleguide/source/assets/js/modules/utilNav.js
@@ -1,41 +1,60 @@
-export default (function (window, document, $, undefined) {
-  let $panels = $('.js-util-nav-content');
-  let $utilityButtons = $('.js-util-nav-toggle');
 
-  $panels.each(function () {
-    const $panel = $(this);
-    const height = $panel.height();
-    const $closeButton = $panel.find('.js-close-util-nav');
+let $panels = $('.js-util-nav-content');
+let $utilityButtons = $('.js-util-nav-toggle');
 
+$panels.each(function () {
+  const $panel = $(this);
+  const height = $panel.height();
+  const $closeButton = $panel.find('.js-close-util-nav');
+  $panel.css('top', '-' + height + 'px');
 
-    $panel.css('top', '-' + height + 'px');
-
-    $closeButton.on('click', function () {
+  $(window).on('resized', function () {
+    if ($(window).width() > 840) {
       $panel.css('top', '-' + height + 'px');
-      $panel.toggleClass('is-closed');
-      $panel.attr("aria-hidden", "true");
-    });
-
+    }
+    else {
+      $panel.removeAttr('style')
+    }
   });
 
-  $utilityButtons.each(function () {
-    const $thisButton = $(this);
-    const $thisButtonId = $(this).data("panel");
-    const $thisPanel = $('.js-util-nav-content[data-panel=' + $thisButtonId + ']');
 
-    $thisButton.on('click', function () {
-      $thisPanel.removeClass('is-closed');
-      $thisPanel.removeAttr('style');
-      $thisPanel.attr("aria-hidden", "false");
-
-      $('body').addClass('show-submenu');
-    });
+  $closeButton.on('click', function () {
+    $panel.css('top', '-' + height + 'px');
+    $panel.toggleClass('is-closed');
+    $panel.attr("aria-hidden", "true");
   });
 
-  $('.js-close-sub-nav').on('click', function () {
-    $('.js-util-nav-content').addClass('is-closed');
-    $('.js-util-nav-content').removeAttr('style');
-    $('body').removeClass('show-submenu');
-  });
+});
 
-})(window, document, jQuery);
+$utilityButtons.each(function () {
+  const $thisButton = $(this);
+  const $thisButtonId = $(this).data("panel");
+  const $thisPanel = $('.js-util-nav-content[data-panel=' + $thisButtonId + ']');
+  const $tabSet = $thisPanel.find('.ma__utility-panel').first().find('.js-clickable-link').first();
+
+  console.log($tabSet);
+
+  $thisButton.on('click', function () {
+    $thisPanel.removeClass('is-closed');
+    $thisPanel.removeAttr('style');
+    $thisPanel.attr("aria-hidden", "false");
+
+    $('body').addClass('show-submenu');
+  });
+});
+
+$('.js-close-sub-nav').on('click', function () {
+  $('.js-util-nav-content').addClass('is-closed');
+  $('.js-util-nav-content').removeAttr('style');
+  $('body').removeClass('show-submenu');
+});
+
+// debouncer
+var resize_timeout;
+$(window).on('resize orientationchange', function () {
+  clearTimeout(resize_timeout);
+
+  resize_timeout = setTimeout(function () {
+    $(window).trigger('resized');
+  }, 250);
+});
