6059aa95649ac33979e58dcb57edbe6f4ef8d9af
Minghua Sun <clairesunstudio@gmail.com>
2019-08-26 17:41:19 -0400

    add circle auto changelog

---
+++ .circleci/dependabot.sh
@@ -0,0 +1,1 @@
+git log --pretty=oneline --abbrev-commit | head -n 1 | tee -a  changelogs/dependabot.md
---
+++ changelogs/dependabot.md
@@ -0,0 +1,1 @@
+890932109 React/Update socialLinks to use Icon and followup accessibility for SR (#712)
