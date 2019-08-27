c1e365f78391be1225ddce93d400c9b65ef9f107
minghua <clairesunstudio@gmail.com>
2019-08-27 13:32:35 -0400

    specify author

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -1,4 +1,4 @@
-git log --pretty=oneline --abbrev-commit | head -n 1 | tee -a  changelogs/dependabot.md
+git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | tee -a  changelogs/dependabot.md
 branch=$(git branch | grep \* | cut -d ' ' -f2)
 git add .
 git commit -m "Add changelog to branch"
