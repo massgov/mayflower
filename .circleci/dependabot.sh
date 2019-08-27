41a7256b9373e901666b1c69e028d3d73f8bb2d5
minghua <clairesunstudio@gmail.com>
2019-08-27 17:24:16 -0400

    fix logic

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -1,12 +1,15 @@
-git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | tee -a  changelogs/dependabot.md
 branch=$(git branch | grep \* | cut -d ' ' -f2)
-if git diff changelogs/dependabot.md; then
+lastCommit=$(git log --pretty=oneline --abbrev-commit | head -n 1)
+lastDependabot=$(git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1)
+
+if [ "$lastCommit" = "$lastDependabot" ]
+then
+  lastCommit | tee -a  changelogs/dependabot.md
   git add changelogs/dependabot.md
   git commit -m "Add changelog to branch"
   echo "Commit changelog";
   git remote add origin git@github.com:massgov/mayflower.git
   git push -u origin $branch
-  echo "Push to repo";
 else
   echo "Nothing to commit"
 fi
--- changelogs/dependabot.md
+++
@@ -1,9 +0,0 @@
-
-effe5338a update path
-b5aa5547 specify key
-7a191e1c Add changelog to branch
-a0285f72 Add changelog to branch
-54d95313 Add changelog to branch
-0193b388 Add changelog to branch
-df5b174d Add changelog to branch
-1703107dc [Security] Bump extend from 3.0.1 to 3.0.2 in /react (#580)
