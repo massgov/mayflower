e7bf34971a4c9677ece510e4e4f778b3a5fc08bd
minghua <clairesunstudio@gmail.com>
2019-08-27 13:35:10 -0400

    add condition

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -1,8 +1,12 @@
 git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | tee -a  changelogs/dependabot.md
 branch=$(git branch | grep \* | cut -d ' ' -f2)
 git add .
-git commit -m "Add changelog to branch"
-echo "Commit changelog";
-git remote add origin git@github.com:massgov/mayflower.git
-git push -u origin $branch
-echo "Push to repo";
+if git diff-index --quiet HEAD; then
+  echo "Nothing to commit"
+else
+  git commit -m "Add changelog to branch"
+  echo "Commit changelog";
+  git remote add origin git@github.com:massgov/mayflower.git
+  git push -u origin $branch
+  echo "Push to repo";
+fi
