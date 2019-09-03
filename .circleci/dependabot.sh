5a66b074081e86e2b646b05faabecc12a0140d81
minghua <clairesunstudio@gmail.com>
2019-09-03 12:45:44 -0400

    test

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -21,9 +21,9 @@ if [ "$lastCommit" ]
 then
   echo "$commitType\n- [Dependency] $lastCommit" >> changelogs/dependabot.md
   git add changelogs/dependabot.md
-  git commit -m "Add changelog to changelogs/dependabot.md"
-  echo "Commit dependabot changelog";
-  git push -u origin $branch
+  # git commit -m "Add changelog to changelogs/dependabot.md"
+  # echo "Commit dependabot changelog";
+  # git push -u origin $branch
 else
   echo "No changelog added"
 fi
--- changelogs/dependabot.md
+++ changelogs/dependabot.md
@@ -12,3 +12,5 @@ Changed\n- [Dependency] Add changelog to changelogs/dependabot.md
 Changed\n- [Dependency] Add changelog to changelogs/dependabot.md
 Changed\n- [Dependency] Add changelog to changelogs/dependabot.md
 Changed\n- [Dependency] Add changelog to changelogs/dependabot.md
+Changed
+- [Dependency]  Merge branch 'auto-dependabot-changelog' of github.com:massgov/mayflower into auto-dependabot-changelog
