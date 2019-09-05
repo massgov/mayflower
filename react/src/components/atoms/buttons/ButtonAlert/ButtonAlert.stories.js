f559411e2450166d9f32758af5f1415337adb21b
avrilpearl <sienna.avril.pearl@gmail.com>
2019-09-05 15:20:57 -0400

    update screenshots

--- react/src/components/atoms/buttons/ButtonAlert/ButtonAlert.stories.js
+++ react/src/components/atoms/buttons/ButtonAlert/ButtonAlert.stories.js
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import ButtonAlert from './index';
 import ButtonAlertDocs from './ButtonAlert.md';
+import buttonWithIconOptions from '../ButtonWithIcon/ButtonWithIcon.knobs.options';
 
 storiesOf('atoms/buttons', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
@@ -15,7 +16,8 @@ storiesOf('atoms/buttons', module)
         showText: text('showText', 'Show'),
         hideText: text('hideText', 'Hide'),
         classes: text('classes', ''),
-        isOpen: boolean('isOpen', false)
+        isOpen: boolean('isOpen', false),
+        theme: select('theme', buttonWithIconOptions.theme)
       };
       return(
         <ButtonAlert
