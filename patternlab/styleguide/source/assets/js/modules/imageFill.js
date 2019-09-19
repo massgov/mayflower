42a9c1c9564ee46aa6a27b3171ce009255664a0f
Melissa Bent <melissa.bent@mediacurrent.com>
2019-09-18 18:13:57 -0600

    DP-15262: Update Info Details sidebar handling

--- patternlab/styleguide/source/assets/js/modules/imageFill.js
+++ patternlab/styleguide/source/assets/js/modules/imageFill.js
@@ -1,6 +1,6 @@
-export default (function (window,document,$,undefined) {
+export default (function (window, document, $, undefined) {
 
-  const sidebarHeight = $('.sidebar').outerHeight( true ) + 100;
+  const sidebarHeight = $('.sidebar').length > 0 ? $('.sidebar').outerHeight(true) + 100 : 0;
   // Set height of sidebar plus 100 pixels so full width images
   // don't sit too tight under under the sidebar content.
   let debounceTimer;
@@ -9,7 +9,7 @@ export default (function (window,document,$,undefined) {
     // Define wrapper width for use.
     var wrapperWidth = $('.main-content').width();
 
-    $('.ma__figure--full, .ma__iframe--full').each(function() {
+    $('.ma__figure--full, .ma__iframe--full').each(function () {
       var $thisMedia = $(this);
 
       // Get position of image relative to container.
@@ -24,17 +24,17 @@ export default (function (window,document,$,undefined) {
     });
   }
 
-  $(window).on('load', function() {
+  $(window).on('load', function () {
     mediaWidth();
   });
 
-  $(window).resize(function() {
-    if(typeof debounceTimer === "number") {
+  $(window).resize(function () {
+    if (typeof debounceTimer === "number") {
       window.clearTimeout(debounceTimer);
     }
-    debounceTimer = window.setTimeout(function(){
+    debounceTimer = window.setTimeout(function () {
       mediaWidth();
-    },250);
+    }, 250);
   });
 
-})(window,document,jQuery);
+})(window, document, jQuery);
