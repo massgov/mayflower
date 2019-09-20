9e53247d0a6fd5c3b495b92339852aefff7b9bab
Lanny Heidbreder <lanny@75thtrombone.com>
2019-09-20 13:52:55 -0500

    DP-15349 Refactored and debugged

--- patternlab/styleguide/source/assets/js/helpers/truncateBodyText.js
+++ patternlab/styleguide/source/assets/js/helpers/truncateBodyText.js
@@ -3,36 +3,72 @@
  * Truncate body text elements to four lines if they are six lines long or more at page load, and provide skip links for unsighted users.
  */
 
-var calculateTruncation = function($el) {
+/**
+ * Calculate and store elements and dimensions for later truncation.
+ */
+var calculateTruncation = (function($, $el) {
+  // Force reflow to correctly calculate height
+  $el.get(0).getBoundingClientRect()
+
   // Find a paragraph whose line-height to gauge.
   var $p = $el.find('p');
   // If there are no paragraphs, gauge by the element itself.
   $p = $p.length ? $p : $el;
+
+  // Calculate how tall the element should be when truncated.
   const style = window.getComputedStyle($p.get(0));
   const lineHeight = parseInt(style.lineHeight);
   const paddingTop = parseInt(style.paddingTop);
+
+  // lineLimit represents the line count that *triggers* truncation, NOT
+  // the height *after* truncation.
   const lineLimit = window.matchMedia('(max-width: 910px)').matches ? 6 : 8;
 
-  $el.wrapInner('<div></div>');
+  $el.data({
+    thresholdHeight: (lineHeight * lineLimit) + paddingTop,
+    truncatedHeight: (lineHeight * (lineLimit - 2)) + paddingTop,
+    intrinsicHeight: $el.children('div').height()
+  });
 
-  $el.data('thresholdHeight', (lineHeight * lineLimit) + paddingTop);
-  $el.data('truncatedHeight', (lineHeight * (lineLimit - 2)) + paddingTop);
-  $el.data('intrinsicHeight', $el.children().height());
-
+  // Create new elements and gather focusable ones for later tabindex-ing.
   if(!$el.data('truncationPreviouslyCalculated')) {
-    const focusableSelector = '[href], button, input:not([type="hidden"]), textarea, select, [tabindex]:not([tabindex^="-"])';
-    $el.data('$button', $('<button class="ma-truncated-body-text__button">Show more</button>'));
-    $el.data('$fadeOverlay', $('<div class="ma-truncated-body-text__fade-overlay"></div>'));
-    $el.data('$focusableEls', $el.find(focusableSelector));
-    $el.data('truncationPreviouslyCalculated', true);
-  }
-}
+    $el.data({
+      $button: $('<button class="ma-truncated-body-text__button">Show more</button>'),
+      $fadeOverlay: $('<div class="ma-truncated-body-text__fade-overlay"></div>'),
+      $focusableEls: $el.find('[href], button, input:not([type="hidden"]), textarea, select, [tabindex]:not([tabindex^="-"])'),
+      truncationPreviouslyCalculated: true
+    });
 
+    $el.data('$button').data('toggleText', 'Show less');
+  }
+}).bind(undefined, jQuery);
+
+var removeTruncation = function($el) {
+    $el.removeClass('ma-truncated-body-text expanded');
+    $el.children('div').removeClass('ma-truncated-body-text__contents').css('height', 'auto');
+
+    if($el.data('truncationPreviouslyCalculated')) {
+      $el.data('$button').detach();
+      $el.data('$button').data('toggleText', 'Show less');
+      $el.data('$button').text('Show more');
+      $el.data('$fadeOverlay').detach();
+      toggleTabindexes($el.data('$focusableEls'), true)
+    }
+
+    $el.data('$button').off('.truncateBodyText');
+};
+
+/**
+ * Make elements focusable or not focusable based on expansion/collapse
+ * of content area.
+ */
 var toggleTabindexes = function($focusableEls, expanded) {
   if(expanded) {
     $focusableEls.each(function(i, el) {
       var $el = $(el);
 
+      // Restore tabindex to elements that had one;
+      // remove it alogether from ones that didn't.
       if($el.data('originalTabIndex')) {
         $el.attr('tabindex', $el.data('originalTabIndex'));
       } else {
@@ -43,6 +79,7 @@ var toggleTabindexes = function($focusableEls, expanded) {
     $focusableEls.each(function(i, el) {
       var $el = $(el);
 
+      // Save the old tabindex for later restoration.
       if($el.attr('tabindex')) {
         $el.data('originalTabIndex', $el.attr('tabindex'))
       }
@@ -50,61 +87,85 @@ var toggleTabindexes = function($focusableEls, expanded) {
       $el.attr('tabindex', '-1');
     });
   }  
-}
+};
 
-export default (function (window,document,$,$el) {
-  // Disable previous truncation.
-  $el.removeClass('ma-truncated-body-text expanded');
-  $el.find('.ma-truncated-body-text__contents').children().unwrap();
-  $el.children().removeClass('ma-truncated-body-text__contents');
-
-  if($el.data('truncationPreviouslyCalculated')) {
-    $el.data('$button').detach();
-    $el.data('$fadeOverlay').detach();
-    toggleTabindexes($el.data('$focusableEls'), true)
-  }
-
-  // Force reflow to correctly calculate height
-  $el.get(0).getBoundingClientRect()
-  
-  // Calculate new dimensions.
+/**
+ * Turn on truncation (if height requires it) based on previous calculations.
+ */
+var enableTruncation = function($el) {
   calculateTruncation($el);
 
+  // Abort if truncation is unnecessary.
   if($el.data('intrinsicHeight') < $el.data('thresholdHeight')) {
     return;
   }
 
-  $el.children().addClass('ma-truncated-body-text__contents');
+  // Restore BEM classes.  
+  $el.addClass('ma-truncated-body-text');
+  $el.children('div').addClass('ma-truncated-body-text__contents');
 
   const $button = $el.data('$button');
 
-  $el.wrapInner('<div class="ma-truncated-body-text__contents"></div>');
-  const $contents = $el.children();
+  // This is just the one .ma-truncated-body-text__contents div.
+  const $contents = $el.children('div');
 
-  $el.addClass('ma-truncated-body-text');
-  $el.prepend($el.data('$button'));
-
-  toggleTabindexes($el.data('$focusableEls'));
-
+  // Add the button at the gradient.
+  $el.prepend($button);
   $contents.append($el.data('$fadeOverlay'));
+
+
+  // Remove tabindex attributes from contents to prevent tabbing
+  // to hidden elements.
+  toggleTabindexes($el.data('$focusableEls'));
+ 
+  // The actual truncation happens here.
   $contents.css('height', $el.data('truncatedHeight') + 'px');
 
-  $button.data('toggleText', 'Show less');
-
-  $button.on('click', function() {
+  $button.on('click.truncateBodyText', function() {
     $el.toggleClass('expanded');
+
+    // Toggle height between truncated and expanded.
     $contents.css('height', $el.data($el.hasClass('expanded') ? 'intrinsicHeight' :'truncatedHeight'));
 
+    // Change button text to the opposite option.
     var oldText = $button.text().trim();
     $button.text($button.data('toggleText'));
     $button.data('toggleText', oldText);
 
+    // Reset or unset focusable state of links/buttons.
     toggleTabindexes($el.data('$focusableEls'), $el.hasClass('expanded'));
 
+    // After expansion, focus the first link or button in the content.
     if($el.hasClass('expanded')) {
       $contents.one('transitionend', function() {
         $el.data('$focusableEls').eq(0).trigger('focus');
+        $contents.get(0).scrollTop = 0;
       });
     }
   });
+};
+
+/**
+ * Initialize truncation at page load.
+ */
+var initTruncation = function($el) {
+  $el.wrapInner('<div></div>');
+
+  enableTruncation($el);
+}
+
+export default (function (window,document,$,$el) {
+  initTruncation($el);
+
+  var resizeTimeout = false;
+  var truncateResize = (function($el) {
+    removeTruncation($el);
+    enableTruncation($el);
+  }).bind(undefined, $el);
+
+  $(window).on('resize', function() {
+    window.clearTimeout(resizeTimeout);
+    resizeTimeout = window.setTimeout(truncateResize, 500);
+  });
+
 }).bind(undefined,window,document,jQuery);
--- patternlab/styleguide/source/assets/js/modules/activateBodyTextTruncation.js
+++ patternlab/styleguide/source/assets/js/modules/activateBodyTextTruncation.js
@@ -5,27 +5,9 @@ import truncateBodyText from "../helpers/truncateBodyText";
  * Initiate truncateBodyText wherever it is needed.
  */
 
-var truncate = function($textAreas) {
-  $textAreas.each(function(i, el) {
-    truncateBodyText($(el));
-  });
-}
-
 export default (function (window,document,$) {
-  var $textAreas = $('.ma__services-flexible-link-groups .page-content--intro .ma__rich-text');
-
-  if(!$textAreas.length) {
-    return;
-  }
-
-  var truncateTimeout = false;
-  var boundTruncate = truncate.bind(undefined, $textAreas)
-
-  boundTruncate();
-
-  $(window).on('resize', function() {
-    window.clearTimeout(truncateTimeout);
-    truncateTimeout = window.setTimeout(boundTruncate, 500);
+  $('.ma__services-flexible-link-groups .page-content--intro .ma__rich-text').each(function(i, el) {
+    truncateBodyText($(el));
   });
 })(window,document,jQuery);
   
