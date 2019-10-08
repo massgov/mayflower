cc75c87fbd66c9f1fef3dc4527e914b5043a6c6c
avrilpearl <sienna.avril.pearl@gmail.com>
2019-10-08 12:47:42 -0400

    Update ref image

--- react/src/components/atoms/contact/EventTime/EventTime.stories.js
+++ react/src/components/atoms/contact/EventTime/EventTime.stories.js
@@ -6,14 +6,14 @@ import { withKnobs, text, date } from '@storybook/addon-knobs';
 import EventTime from './index';
 
 const startTime = new Date('March 15, 2002 03:00:00');
-const endTime = new Date('March 15, 2002 18:00:00');
+const endTime = new Date('March 15, 2002 12:00:00');
 
 storiesOf('atoms/contact', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add('EventTime', (() => {
     const props = {
       startDate: date('startDate', startTime),
-      emdDate: date('endDate', endTime),
+      endDate: date('endDate', endTime),
       details: text('details', 'First come first serve.')
     };
     return(
