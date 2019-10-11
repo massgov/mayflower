11f3f18abfd7bcf5d6b8b19c27035d937608e300
minghua <clairesunstudio@gmail.com>
2019-10-11 16:31:42 -0400

    add notes

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -3,6 +3,8 @@ lastCommit=$(git log --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 lastCommitClean=$lastCommit
 lastDependabot=$(git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 commitType="Changed"
+# the filename that will be committed ends with the slugified version of the commit message starting from index 5 to index 20
+# e.g. if the commit message is Bump react-scripts from 1.00 to 2.00 it will be saved as dependabot-react-scripts-fr.md
 fileName=$(echo ${lastCommit:5:20} | iconv -t ascii//TRANSLIT | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9-]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z)
 
 if [[ $lastCommitClean =~ "[Security]"[[:space:]](.*) ]]; then
