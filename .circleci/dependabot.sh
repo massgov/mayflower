0b23ad64b9026cf529411c84f2a1102577cde139
minghua <clairesunstudio@gmail.com>
2019-10-11 16:31:33 -0400

    get filename from commit

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -3,6 +3,7 @@ lastCommit=$(git log --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 lastCommitClean=$lastCommit
 lastDependabot=$(git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 commitType="Changed"
+fileName=$(echo ${lastCommit:5:20} | iconv -t ascii//TRANSLIT | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9-]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z)
 
 if [[ $lastCommitClean =~ "[Security]"[[:space:]](.*) ]]; then
   lastCommitClean="${BASH_REMATCH[1]}"
@@ -18,7 +19,7 @@ fi
 
 if [ "$lastCommit" = "$lastDependabot" ]
 then
-  printf "\n$commitType\n- $lastCommitClean" > changelogs/dependabot-${branch##*/}.md
+  printf "\n$commitType\n- $lastCommitClean" > changelogs/dependabot-${fileName}.md
   git add changelogs/dependabot-${branch##*/}.md
   git commit -m "Added a new changelog to the `changelogs/` folder"
   echo "Commit dependabot changelog";
