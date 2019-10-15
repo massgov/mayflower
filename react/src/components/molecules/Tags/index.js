92626503ab31cb4c92fb501efd7b810846d86d5b
Minghua Sun <clairesunstudio@gmail.com>
2019-10-15 17:14:22 -0400

    add Tags

---
+++ react/src/components/molecules/Tags/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ButtonTag from '../../atoms/buttons/ButtonTag';
+import './style.css';
+
+class Tags extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: this.props.tags ? this.props.tags : null
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({ tags: nextProps.tags });
+  }
+  handleClearAll() {
+    if (typeof this.props.onClearCallback === 'function') {
+      this.props.onClearCallback();
+    }
+    this.setState({ tags: null });
+  }
+  handleClearThis(event) {
+    if (typeof this.props.onClearThisCallback === 'function') {
+      this.props.onClearThisCallback(event.target);
+    }
+  }
+  render() {
+    const { tags } = this.state;
+    return(
+      tags && (
+      <div className="ma__results-heading__tags">
+        { tags.map((tag, tagIndex) => (
+            <ButtonTag
+              type={tag.type}
+              value={tag.value}
+              text={tag.text}
+              handleClick={(e) => this.handleClearThis(e)}
+              key={`resultsHeading.tag.${tagIndex}`}
+            />
+           ))}
+        { tags.length > 1 && (
+        <button type="button" className="ma__results-heading__clear js-results-heading-clear" onClick={() => this.handleClearAll()}>Clear all</button>
+          )}
+      </div>)
+    );
+  }
+}
+
+Tags.propTypes = {
+  /** The tags applied to the search list
+        type: The type of tag
+        text: The text displayed by the tag (required)
+        value: The value of the tag  */
+  tags: PropTypes.arrayOf(PropTypes.shape({
+    type: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    value: PropTypes.string
+  })),
+  /** Custom onClick function that triggers when 'Clear all' button is clicked */
+  onClearCallback: PropTypes.func,
+  /** Custom onClick function that triggers when a tag is clicked */
+  onClearThisCallback: PropTypes.func
+};
+
+export default Tags;
