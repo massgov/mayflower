ab25c7b15481f3c09ee24cadf16467e9d0571c3c
Minghua Sun <clairesunstudio@gmail.com>
2019-10-15 12:07:17 -0400

    revert changes

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -1,12 +1,9 @@
 branch=$(git branch | grep \* | cut -d ' ' -f2)
 lastCommit=$(git log --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 lastCommitClean=$lastCommit
-lastDependabot=$(git log --author="clairesunstudio" --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
+lastDependabot=$(git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 commitType="Changed"
-# the filename that will be committed ends with the slugified version of the commit message starting from index 5 to index 20
-# e.g. if the commit message is Bump react-scripts from 1.00 to 2.00 it will be saved as dependabot-react-scripts-fr.md
-fileName=$(echo ${lastCommit:5:20} | iconv -t ascii//TRANSLIT | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9-]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z)
-echo $fileName
+
 if [[ $lastCommitClean =~ "[Security]"[[:space:]](.*) ]]; then
   lastCommitClean="${BASH_REMATCH[1]}"
   commitType="Security"
@@ -21,8 +18,8 @@ fi
 
 if [ "$lastCommit" = "$lastDependabot" ]
 then
-  printf "\n$commitType\n- $lastCommitClean" > changelogs/dependabot-${fileName}.md
-  git add changelogs/dependabot-${fileName}.md
+  printf "\n$commitType\n- $lastCommitClean" > changelogs/dependabot-${branch##*/}.md
+  git add changelogs/dependabot-${branch##*/}.md
   git commit -m "Added a new changelog to the `changelogs/` folder"
   echo "Commit dependabot changelog";
   git push -u origin $branch
--- changelogs/dependabot-t-bump-test-commit.md
+++
@@ -1,3 +0,0 @@
-
-Changed
-- (React) Bump test commit x.x.x to x.x.x (##)
