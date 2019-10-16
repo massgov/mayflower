f3bb7ae62686ab4208a2900eab2dbca8fd0ee067
minghua <clairesunstudio@gmail.com>
2019-10-16 10:46:55 -0400

    fix input checkbox story icon passing

--- react/src/components/atoms/forms/InputCheckBox/InputCheckBox.stories.js
+++ react/src/components/atoms/forms/InputCheckBox/InputCheckBox.stories.js
@@ -2,19 +2,37 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { text, boolean, select, array } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
 
 import InputCheckBox from './index';
-import InputCheckBoxOptions from './InputCheckBox.knobs.options';
 import inputCheckBoxDocs from './InputCheckBox.md';
+
+import Icon from '../../icons/Icon';
+import { svgOptions } from '../../icons/Icon/Icon.knob.options';
 
 storiesOf('atoms/forms', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
     'InputCheckBox', (() => {
-      const inputCheckBoxOptionsWithKnobs = Object.assign(...Object.entries(InputCheckBoxOptions).map(([k, v]) => (
-        k === 'icon' ? { icon: { name: v(), ariaHidden: true } } : { [k]: v() })));
+      const props = {
+        id: text('id', 'input-checkbox'),
+        label: text('label', 'Apply to all'),
+        defaultValue: boolean('checked', false),
+        onChange: action('onChange'),
+        disabled: boolean('disabled', false),
+        required: boolean('required', false),
+        errorMsg: text('errorMsg', 'You are required to check this box.'),
+        labelText: text('labelText', 'Checkbox Input'),
+        classes: array('classes', [])
+      };
+      const iconProps = {
+        name: select('icon.name', svgOptions, 'chevron'),
+        svgWidth: 20,
+        svgHeight: 20
+      };
       return(
-        <InputCheckBox {...inputCheckBoxOptionsWithKnobs} />
+        <InputCheckBox {...props} icon={<Icon {...iconProps} />} />
       );
     }),
     { info: inputCheckBoxDocs }
--- react/src/components/atoms/forms/InputCheckBox/index.js
+++ react/src/components/atoms/forms/InputCheckBox/index.js
@@ -14,6 +14,7 @@ const CheckBox = (props) => (
       {
         (context) => {
           const { value } = context;
+          console.log(value)
           const {
             icon, label, disabled, required, id
           } = props;
