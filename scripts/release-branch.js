68f11067b3459f2f947d02eba0e312db75269a99
Melissa Rossi <mrossi113@gmail.com>
2019-11-28 13:20:42 -0500

    final product

--- scripts/release-branch.js
+++ scripts/release-branch.js
@@ -94,37 +94,37 @@ git().add('./*');
 git().commit('changelog update and remove old changelog files');
 
 // Push the release branch to GitHub
-// git().push('origin', releaseBranch);
+git().push('origin', releaseBranch);
 
 // Create the pull request in GitHub
-// const data = JSON.stringify({
-//   title: `Release/${minor}`,
-//   body: 'xxx',
-//   head: minor,
-//   base: 'master',
-// })
-//
-// const options = {
-//   username: `massgov-bot:${process.env.DANGER_GITHUB_API_TOKEN}`,
-//   userAgent: 'https://api.github.com/repos/massgov/mayflower/',
-//   path: 'https://api.github.com/repos/massgov/mayflower/pulls',
-//   method: 'POST',
-//   headers: {
-//     'Accept': 'application/json',
-//     'Content-Type': 'application/json',
-//   }
-// }
-// const req = https.request(options, (res) => {
-//   console.log(`statusCode: ${res.statusCode}`)
-//
-//   res.on('data', (d) => {
-//     process.stdout.write(d)
-//   })
-// })
-//
-// req.on('error', (err) => {
-//   if (err) throw err;
-// })
-//
-// req.write(data)
-// req.end()
+const data = JSON.stringify({
+  title: `Release/${minor}`,
+  body: 'xxx',
+  head: minor,
+  base: 'master',
+})
+
+const options = {
+  username: `massgov-bot:${process.env.DANGER_GITHUB_API_TOKEN}`,
+  userAgent: 'https://api.github.com/repos/massgov/mayflower/',
+  path: 'https://api.github.com/repos/massgov/mayflower/pulls',
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+}
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (err) => {
+  if (err) throw err;
+})
+
+req.write(data)
+req.end()
