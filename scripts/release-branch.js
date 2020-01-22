208d4835aa1bacd8becde3ea08bc797daff82f85
Minghua Sun <clairesunstudio@gmail.com>
2020-01-22 11:18:51 -0500

    Automate the updating of the release PR and tag descriptions (#899)
    * setup test
    
    * mock data
    
    * wip
    
    * wip
    
    * wip
    
    * fix
    
    * add notes
    
    * test it here!!!!
    
    * or see the changelogs compilation here!!
    
    * uncomment everything
    
    * remove test data
    
    * Update scripts/release-branch.js
    
    * Update scripts/release-branch.js

--- scripts/release-branch.js
+++ scripts/release-branch.js
@@ -48,26 +48,48 @@ const title = `## ${minor} (${month}/${day}/${year})`;
 const directoryPath = path.resolve(__dirname, '../changelogs');
 const changelogPath = `${path.resolve(__dirname, '../')}/CHANGELOG.md`;
 
-let newLogs = [];
+
 // Read directory path and exclude the template.yml file.
 const changelogs = fs.readdirSync(directoryPath).filter(function(file) {
   return file.match(/^.*\.yml$/g) && file!== "template.yml";
 });
 
+let changeTypes = []
+let changeContents = {}
+let newLogs = [];
+/** Loop through each changelog in the changelogs directory,
+  * consolidate changelogs with the same changeTypes together
+*/
 changelogs.forEach((fileName) => {
   const content = yaml.safeLoad(fs.readFileSync(`${directoryPath}/${fileName}`, 'utf8'));
-  Object.keys(content).forEach((changeType, i) => {
-    newLogs.push(`\n### ${changeType} \n`)
-    content[changeType].forEach((change) => {
-      const newChange = `- (${change.project}) [${change.component}] ${change.issue}: ${change.description}\n`
-      newLogs.push(newChange);
-    });
+  Object.keys(content).forEach((changeType) => {
+    // If it encounters a new change type
+    if(changeTypes.indexOf(changeType) < 0) {
+      // add it to the changeType array
+      changeTypes.push(changeType);
+      // add the content of the changelog to changeContents object under the changeType key
+      changeContents[changeType] = content[changeType]
+    } else {
+      // If it's an existing change type, append the content of the changelog into the existing changeType array
+      changeContents[changeType] = [...changeContents[changeType], ...content[changeType]]
+    }
   });
 });
+
+/** Format consolidated changelogs data into a string with desired markdown syntax */
+changeTypes.forEach((changeType) => {
+  newLogs.push(`\n### ${changeType} \n`)
+  changeContents[changeType].forEach((change) => {
+    const newChange = `- (${change.project}) [${change.component}] ${change.issue}: ${change.description}\n`
+    newLogs.push(newChange);
+  });
+})
+
 
 const fd = fs.readFileSync(changelogPath).toString().split("\n");
 fd.splice(3, 0, title, newLogs.join(''));
 var allLogs = fd.join('\n');
+
 
 
 // Remove the changelog files
