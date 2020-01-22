eb8f626649beb178f892e51efda27856a3879a06
minghua <clairesunstudio@gmail.com>
2020-01-22 11:26:07 -0500

    wip

--- scripts/release-vars.js
+++ scripts/release-vars.js
@@ -6,8 +6,6 @@ const Octokit = require("@octokit/rest");
 const octokit = new Octokit({
   auth: process.env.DANGER_GITHUB_API_TOKEN
 });
-exports.octokit = octokit;
-
 
 // Find out the latest release tag and display it in the command line.
 const latest = shell.exec('git tag  | grep -E "^[0-9]" | sort -V | tail -1');
@@ -17,7 +15,10 @@ console.log(`Display the latest tag: ${latest}`);
 
 // Increment the release branch.
 const minor = semver.inc(latest.toString(), 'minor');
-exports.minor = minor;
 
 // Print out the new minor version
 console.log(`New release tag: ${minor}`);
+
+
+exports.octokit = octokit;
+exports.minor = minor;
