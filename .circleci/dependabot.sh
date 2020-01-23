200f77dc5ef4ca76e1f316958ec2e88da9797863
minghua <clairesunstudio@gmail.com>
2020-01-23 12:32:47 -0500

    wip

--- .circleci/dependabot.sh
+++ .circleci/dependabot.sh
@@ -2,23 +2,32 @@ branch=$(git branch | grep \* | cut -d ' ' -f2)
 lastCommit=$(git log --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
 lastCommitClean=$lastCommit
 lastDependabot=$(git log --author="dependabot-preview" --pretty=oneline --abbrev-commit | head -n 1 | cut -c 10-)
-commitType="Changed"
+changeType="Changed"
+project=""
 
 if [[ $lastCommitClean =~ "[Security]"[[:space:]](.*) ]]; then
   lastCommitClean="${BASH_REMATCH[1]}"
-  commitType="Security"
+  changeType="Security"
 fi
 
 if [[ $lastCommitClean =~ "/react" ]]; then
-  lastCommitClean="(React) [Dependency] $lastCommitClean"
+  lastCommitClean="(React) $lastCommitClean"
+  project="React"
 fi
 if [[ $lastCommitClean =~ "/patternlab" ]]; then
-  lastCommitClean="(Patternlab) [Dependency] $lastCommitClean"
+  lastCommitClean="(Patternlab) $lastCommitClean"
+  project="Patternlab"
 fi
 
 if [ "$lastCommit" = "$lastDependabot" ]
 then
-  printf "\n$commitType\n- $lastCommitClean" > changelogs/dependabot-${branch##*/}.md
+  printf
+    "\n$changeType:\n
+    - project: $project\n
+      component: [Dependency]
+      description: $lastCommitClean
+      issue: "
+  > changelogs/dependabot-${branch##*/}.md
   git add changelogs/dependabot-${branch##*/}.md
   git commit -m "Added a new changelog to the `changelogs/` folder"
   echo "Commit dependabot changelog";
