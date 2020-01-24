cccc1addc3397adb5218db0b3df97453c2df5268
minghua <clairesunstudio@gmail.com>
2020-01-24 14:01:21 -0500

    wip

--- scripts/release-tag.js
+++ scripts/release-tag.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const changelogPath = `${path.resolve(__dirname, '../')}/CHANGELOG.md`;
-const re = /\n##\s/
+const re = /\n##\s/;
 const newLogs = fs.readFileSync(changelogPath).toString().split(re, 2);
-const newLogsWithTitle = `\n## ${newLogs[1]}`
+const newLogsWithTitle = `\n## ${newLogs[1]}`;
 
 octokit.repos.createRelease({
   owner: 'massgov',
@@ -14,13 +14,4 @@ octokit.repos.createRelease({
   target_commitish: 'master',
   name: minor,
   body: newLogsWithTitle
-})
-
-// Remove tempLogs JSON
-fs.unlink(tempLogsPath, (err) => {
-  if (err) {
-      console.log(`failed to delete tempLogs.json: ${err.toString()}`);
-  } else {
-      console.log('successfully deleted tempLogs.json');
-  }
 });
