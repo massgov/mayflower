c8d5570376654c157077184a7ff1380dd082c688
minghua <clairesunstudio@gmail.com>
2020-01-27 12:00:07 -0500

    render colors

--- react/src/components/base/Colors/Colors.stories.js
+++ react/src/components/base/Colors/Colors.stories.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
+
+import { themeColors, utilityColors } from './colors.json';
 
 import './styles.css';
 
 const Color = ({ color, value, name }) => {
   return (
     <li style={{ width: 300, margin: 20, padding: 10 }}>
-      <h2 className="ma__sidebar-heading">{color}</h2>
+      <h3 className="ma__sidebar-heading">{color}</h3>
       <div className="sg-swatch" style={{ background: value, borderRadius: 0 }} />
       <div className="sg-info">
         <span>{value}</span>
@@ -22,14 +24,18 @@ storiesOf('base', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
     'Colors', (() => {
-      const props = {
-        name: "$c-primary",
-        color: "Bay Blue",
-        value: "#14558f"
-      };
       return(
-        <ul className="sg-colors">
-          <Color {...props} />
-        </ul>
+        <Fragment>
+          <ul className="sg-colors">
+            {
+              themeColors.map((color) => <Color {...color} />)
+            }
+          </ul>
+          <ul className="sg-colors">
+            {
+              utilityColors.map((color) => <Color {...color} />)
+            }
+          </ul>
+        </Fragment>
       );
     }));
