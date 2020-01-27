0de74db0eb87f3e3c216f365a42dc75928352e9f
minghua <clairesunstudio@gmail.com>
2020-01-27 13:42:48 -0500

    get computed colors

--- react/src/components/base/Colors/Colors.stories.js
+++ react/src/components/base/Colors/Colors.stories.js
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
@@ -10,7 +11,7 @@ import './styles.css';
 
 const Color = ({ color, value, name }) => {
   return (
-    <li style={{ width: 300, margin: 20, padding: 10 }}>
+    <li style={{ width: 300, margin: 10, padding: 10 }}>
       <h3 className="ma__sidebar-heading">{color}</h3>
       <div className="sg-swatch" style={{ background: value, borderRadius: 0 }} />
       <div className="sg-info">
@@ -23,12 +24,18 @@ const Color = ({ color, value, name }) => {
 };
 
 const GradientTile = ({ color, value, name, index }) => {
+  const colorRef = useRef(null);
+  const [rgb, setRgb] = useState('')
+  useEffect(() => {
+    const computedStyles = window.getComputedStyle(ReactDOM.findDOMNode(colorRef.current)).getPropertyValue('background-color');
+    setRgb(() => computedStyles) ;
+  });
   return (
     <li className="title tile--tint">
       <h3 className="ma__sidebar-heading">{`${color} ${index * 10} %`}</h3>
-      <div className="sg-swatch" />
+      <div className="sg-swatch" ref={colorRef} />
       <div className="sg-info">
-        <span>{value}</span>
+        <span>{rgb}</span>
         <br />
         <code style={{ fontSize: '1rem' }}>{`${name}-${index * 10}`}</code>
       </div>
@@ -43,7 +50,7 @@ for (i = 0; i < tiles; i++) {
   tilesArray.push(i);
 }
 
-storiesOf('base', module)
+storiesOf('base/colors', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
     'Colors', (() => {
@@ -71,10 +78,10 @@ storiesOf('base', module)
       );
     }))
   .add(
-    'Colors', (() => {
+    'Gradients', (() => {
       return(
         <Fragment>
-          <ul class="sg-colors">
+          <ul className="sg-colors">
             {
               tilesArray.map((i) => <GradientTile color="blue" value="#14558f" name="c-primary" index={i} />)
             }
