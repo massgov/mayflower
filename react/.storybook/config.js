a4aa72b382f35566769bdb6c31ab6bf44e7b4abe
minghua <clairesunstudio@gmail.com>
2020-01-31 11:29:06 -0500

    add welcome landing page

--- react/.storybook/config.js
+++ react/.storybook/config.js
@@ -14,7 +14,7 @@ addParameters({
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach((filename) => {
+  req.keys().sort().forEach((filename) => {
     req(filename);
   });
   addDecorator(
--- react/src/index.stories.js
+++ react/src/index.stories.js
@@ -1,3 +1,12 @@
 // @TODO come up with a welcome story
-
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import './index.css';
+
+storiesOf('101|mayflower-react ', module)
+  .add(
+    'Welcome 🎉', (() => {
+      return(<div>text</div>);
+    })
+  );
