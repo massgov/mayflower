3407c04b732e043bf45bac772cecdc44f8d09103
Minghua Sun <clairesunstudio@gmail.com>
2020-02-06 17:30:42 -0500

    React/dp 17340 add typography (#918)
    * wip
    
    * add typography style mixins
    
    * wip
    
    * move fonts styles and new classes into shared assets
    
    * render sizes
    
    * use grid
    
    * wip
    
    * wip
    
    * wip
    
    * add font family variables
    
    * display source code pro
    
    * wip
    
    * find and replace image source
    
    * find and replace image source
    
    * wip dynamic import
    
    * add symlink
    
    * wip
    
    * fix image paths in md
    
    * cleanup
    
    * add changelog
    
    * add changelog
    
    * add screenshots
    
    * fix build
    
    * Update changelogs/DP-17340.yml
    
    * address comments

---
+++ react/src/components/base/Typography/Typography.knobs.options.js
@@ -0,0 +1,14 @@
+/**
+ * Option definitions for the Button's enumerable properties (imported in stories)
+ */
+export default {
+  size: {
+    'Heading 1': 'h1',
+    'Heading 2': 'h2',
+    'Heading 3': 'h3',
+    'Heading 4': 'h4',
+    'Heading 5': 'h5',
+    'Heading 6': 'h6',
+    'Body text': 'p'
+  }
+};
