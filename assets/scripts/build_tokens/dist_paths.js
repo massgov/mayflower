b6413883e4a1c25a3de2901a59f9921fa09e145c
minghua <clairesunstudio@gmail.com>
2020-02-24 14:50:36 -0500

    allow adding folder as a path

--- assets/scripts/build_tokens/dist_paths.js
+++ assets/scripts/build_tokens/dist_paths.js
@@ -1,6 +1,9 @@
 const distPaths = [{
   src: 'colors/_mayflower-color-tokens.scss',
   dest: 'scss/_colors.scss'
+},{
+  src: 'fonts',
+  dest: 'fonts'
 }]
 
 module.exports = distPaths;
--- assets/scripts/build_tokens/index.js
+++ assets/scripts/build_tokens/index.js
@@ -7,16 +7,34 @@ distPaths.forEach((dist) => {
   const dest = path.resolve(__dirname, `../../dist/${dist.dest}`)
   // create new directory for destination if not already exists
   const destFolder = dest.split('/');
-  destFolder.pop();
+  const lastPath = destFolder.pop();
+  // Check if destination is a directory
+  const isDir = !lastPath.includes('.');
   const destFolderPath = destFolder.join('/');
   if (fs.existsSync(src)) {
     if (!fs.existsSync(destFolderPath)){
       fs.mkdirSync(destFolderPath);
     }
-    fs.copyFile(src, dest, (err) => {
-      if (err) throw err;
-      console.log(`${dist.src} was copied to dist/${dist.dest}`);
-    });
+    if (isDir) {
+      const contents = fs.readdirSync(src);
+      let i=0;
+      for (; i<contents.length;i++) {
+        const fileName = contents[i];
+        // exclude hidden files, e.g. .DS_Store
+        if (!fileName.startsWith('.') ) {
+          console.log(fileName);
+          fs.copyFile(`${src}/${fileName}`, `${dest}/${fileName}`, (err) => {
+            if (err) throw err;
+            console.log(`${dist.src}/${fileName} was copied to dist/${dist.dest}/${fileName}`);
+          });
+        }
+      }
+    } else {
+      fs.copyFile(src, dest, (err) => {
+        if (err) throw err;
+        console.log(`${dist.src} was copied to dist/${dist.dest}`);
+      });
+    }
   } else {
     console.log('source file doesn\'t exist!')
   }
