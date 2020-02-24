c1354216796a4c27698fdb782ced7deb197b4c52
minghua <clairesunstudio@gmail.com>
2020-02-24 16:10:26 -0500

    add notes

--- assets/scripts/build_tokens/dist_paths.js
+++ assets/scripts/build_tokens/dist_paths.js
@@ -1,3 +1,8 @@
+/**
+  This array is looped through in build_tokens/index.js to map the source files and the distributed files in @massds/mayflower-tokens
+  @src relative path, from the assets/ root, of a file (.ext) or a folder (no trailing slash)
+  @dest relative path, under the dist/ folder, of a file (.ext) or a folder (no trailing slash)
+*/
 const distPaths = [{
   src: 'scss/00-base/_color-tokens.scss',
   dest: 'scss/_colors.scss'
