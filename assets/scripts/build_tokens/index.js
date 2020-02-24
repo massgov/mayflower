66ebbdc29559aeda59eaeba99035848a242f2995
minghua <clairesunstudio@gmail.com>
2020-02-24 13:00:52 -0500

    create new directory for destination if not already exists

--- assets/scripts/build_tokens/index.js
+++ assets/scripts/build_tokens/index.js
@@ -5,10 +5,19 @@ const distPaths = require('./dist_paths');
 distPaths.forEach((dist) => {
   const src = path.resolve(__dirname, `../../${dist.src}`)
   const dest = path.resolve(__dirname, `../../dist/${dist.dest}`)
+  // create new directory for destination if not already exists
+  const destFolder = dest.split('/');
+  destFolder.pop();
+  const destFolderPath = destFolder.join('/');
   if (fs.existsSync(src)) {
+    if (!fs.existsSync(destFolderPath)){
+      fs.mkdirSync(destFolderPath);
+    }
     fs.copyFile(src, dest, (err) => {
       if (err) throw err;
       console.log(`${dist.src} was copied to dist/${dist.dest}`);
     });
+  } else {
+    console.log('source file doesn\'t exist!')
   }
 });
