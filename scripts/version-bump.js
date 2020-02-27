73939fd33088817dea4a0eb584558ce795684edf
minghua <clairesunstudio@gmail.com>
2020-02-27 15:23:36 -0500

    wip

--- scripts/version-bump.js
+++ scripts/version-bump.js
@@ -3,12 +3,27 @@ const fs = require('fs');
 
 const version = '0.0.0';
 const reactPath = '../react/package.json';
-const resolvedReactPath = path.resolve(__dirname, reactPath);
-
+const patternlabPath = '../patternlab/styleguide/package.json';
+const assetsPath = '../assets/package.json';
 
 const reactPkg = require(reactPath);
+const patternlabPkg = require(patternlabPath);
+const assetsPkg = require(assetsPath);
+
 reactPkg.version = version;
+patternlabPkg.version = version;
+assetsPkg.version = version;
+
+const resolvedReactPath = path.resolve(__dirname, reactPath);
+const resolvedPatternlabPath = path.resolve(__dirname, patternlabPath);
+const resolvedAssetsPath = path.resolve(__dirname, assetsPath);
 
 fs.writeFileSync(resolvedReactPath, JSON.stringify(reactPkg, null, 2), (err) => {
+  if (err) throw err;
+})
+fs.writeFileSync(resolvedPatternlabPath, JSON.stringify(patternlabPkg, null, 2), (err) => {
+  if (err) throw err;
+})
+fs.writeFileSync(resolvedAssetsPath, JSON.stringify(assetsPkg, null, 2), (err) => {
   if (err) throw err;
 })
