48bef6f4be52df2e613db1a86fb836541a137707
minghua <clairesunstudio@gmail.com>
2020-03-02 15:08:11 -0500

    log url

--- react/backstop/scripts/onReady.js
+++ react/backstop/scripts/onReady.js
@@ -1,5 +1,6 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
+  console.log('URL > ' + scenario.url);
   await require('./clickAndHoverHelper')(page, scenario);
 
   // add more ready handlers here...
