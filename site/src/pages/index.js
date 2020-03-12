3bcdd0449721b34bff8a994d9787bf24be22bf30
Minghua Sun <clairesunstudio@gmail.com>
2020-03-12 15:02:13 -0400

    add graphql data

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -8,32 +8,26 @@ import { IllustratedHeader, Button, GenTeaser } from "@massds/mayflower-react";
 
 import './index.scss';
 
-const headerProps = {
-  bgInfo: '',
-  bgImage: '',
-  category: 'Commonwealth of Massachusetts',
-  inverted: true,
-  pageHeader: {
-    title: 'A design system for the state government'
-  }
-}
-
-const layoutProps = {
-    pre: () => (
-      <IllustratedHeader {...headerProps}>
-        <span className="ma__page-banner__callout ma__page-banner__callout--primary">
-          We make it easier to build a accessible, mobile-friendly government websites with a consistent look and feel for the state of Massachusetts.
-        </span>
-        <div className="ma__button-group">
-          <Button text="Github" usage="secondary"/>
-          <Button text="Github" usage="tertiary" theme="c-gray-dark" />
-        </div>
-      </IllustratedHeader>
-    )
-};
 
 const IndexPage = ({ data }) => {
-  const { projects } = data.content
+  const { header, projects } = data.content
+  const { paragraph, buttons, ...headerProps } = header;
+  const layoutProps = {
+      pre: () => (
+        <IllustratedHeader {...headerProps}>
+          <span className="ma__page-banner__callout ma__page-banner__callout--primary">
+            {paragraph}
+          </span>
+          <div className="ma__button-group">
+            {
+              buttons.map((button) => (
+                <Button {...button}/>
+              ))
+            }
+          </div>
+        </IllustratedHeader>
+      )
+  };
   return(
     <Layout {...layoutProps}>
       <SEO title="Home" />
@@ -64,6 +58,22 @@ export default IndexPage
 export const query = graphql`
   query IndexPageQuery {
     content: dataJson {
+      header {
+        pageHeader {
+          title
+        }
+        category
+        inverted
+        paragraph
+        bgImage
+        bgInfo
+        buttons {
+          href
+          text
+          usage
+          theme
+        }
+      }
       projects {
         title {
           text
