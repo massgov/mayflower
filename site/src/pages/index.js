5985f04ab73b794545f507b66f296899092c0413
Minghua Sun <clairesunstudio@gmail.com>
2020-03-12 11:09:34 -0400

    add buttons

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import { IllustratedHeader } from "@massds/mayflower-react";
+import { IllustratedHeader, Button } from "@massds/mayflower-react";
 
 import './index.scss';
 
@@ -23,6 +23,10 @@ const layoutProps = {
         <span className="ma__page-banner__callout ma__page-banner__callout--primary">
           We make it easier to build a accessible, mobile-friendly government websites with a consistent look and feel for the state of Massachusetts.
         </span>
+        <div className="ma__button-group">
+          <Button text="Github" usage="secondary"/>
+          <Button text="Github" usage="tertiary" theme="c-gray-dark" />
+        </div>
       </IllustratedHeader>
     )
 };
