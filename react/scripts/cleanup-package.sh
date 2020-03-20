a52fd4b36fe8e3b65c629f957e92b5289fae211c
Minghua Sun <clairesunstudio@gmail.com>
2020-03-20 16:49:56 -0400

    modify prepublish hook and include cleanup script

---
+++ react/scripts/cleanup-package.sh
@@ -0,0 +1,2 @@
+rm -rf es/assets/package.json es/assets/package-lock.json es/assets/dist es/assets/scripts
+rm -rf lib/assets/package.json lib/assets/package-lock.json lib/assets/dist lib/assets/scripts
