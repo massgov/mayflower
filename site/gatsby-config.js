6d98d0f537938d95f2940e1c2168ce2578d90172
Minghua Sun <clairesunstudio@gmail.com>
2020-03-24 16:28:31 -0400

    setup env variable to handle path prefix

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+console.log(process.env.GATSBY_PATH_PREFIX)
+
 module.exports = {
-  pathPrefix: `/home`,
+  pathPrefix: process.env.GATSBY_PATH_PREFIX || '',
   siteMetadata: {
     title: `Mayflower`,
     description: `A design system for the Commonwealth of Massachusetts`,
