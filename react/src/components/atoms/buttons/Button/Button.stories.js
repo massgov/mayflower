a3f7783bda04fe913e3e0f7a7632cdd2bdae2429
Luke Ollett <ollett@gmail.com>
2020-03-31 09:39:14 +0100

    show/hide theme knob based on usage, a little clean up.

--- react/src/components/atoms/buttons/Button/Button.stories.js
+++ react/src/components/atoms/buttons/Button/Button.stories.js
@@ -12,8 +12,17 @@ storiesOf('atoms/buttons', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
     'Button', (() => {
-      const props = {
-        usage: select('usage', buttonOptions.usage),
+      let props = {
+        usage: select('usage', buttonOptions.usage)
+      };
+
+      // Tertiary and quaternary usages don't have different themes.
+      if (props.usage !== 'tertiary' && props.usage !== 'quaternary') {
+        props.theme = select('theme', buttonOptions.theme);
+      }
+
+      props = {
+        ...props,
         type: select('type', buttonOptions.type),
         size: select('size', buttonOptions.size),
         info: text('info', 'this will be the tooltip text on hover'),
@@ -23,10 +32,7 @@ storiesOf('atoms/buttons', module)
         onClick: action('button clicked'),
         classes: array('classes', [])
       };
-      // Tertiary and quaternary usages don't have different themes.
-      if (props.usage === undefined || props.usage === 'primary' || props.usage === 'secondary') {
-        props.theme = select('theme', buttonOptions.theme);
-      }
+
       return(
         <Button {...props} />
       );
