dd4c4aadfb9a881adcfe9e0415ff4ae0a59dc64a
Minghua Sun <clairesunstudio@gmail.com>
2020-04-27 18:31:38 -0400

    React/dp 18466 refactor label (#1028)
    * add label component
    
    * update style
    
    * add back optional class for patternlab
    
    * rework knobs
    
    * use Label
    
    * add legend option
    
    * add changelog
    
    * update md
    
    * style linting
    
    * lint fix
    
    * vr
    
    * lint fix
    
    * update screenshots
    
    * address comment
    
    * address comment

---
+++ react/src/components/forms/Label/Label.knobs.options.js
@@ -0,0 +1,8 @@
+export default {
+  inputId: 'GUID2738572489',
+  labelText: 'This is a Label of an input',
+  hidden: false,
+  disabled: false,
+  useLegend: false,
+  conditionText: 'optional'
+};
