67794bdab160c9edee37f71bd43a80f449cc7dc1
Steven Murray <smurrayatwork@gmail.com>
2020-05-06 12:15:44 -0400

    Update react/.storybook/main.js

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -2,7 +2,6 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
-    //'@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs',
