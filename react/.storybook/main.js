a7682b6f4c19d9db5f27d7639d0f6ed9f8366cbd
smurrayatwork <steven.murray@mediacurrent.com>
2020-05-06 12:04:46 -0400

    DP-18263: Updates react package to use latest mayflower-assets package, removes debugging in storybook config.

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -20,14 +20,11 @@ module.exports = {
     ]);
 
     // Alter the mini-css-extract-plugin to look for asset files relative to
-    // the storybook-static directory. Note that we guard this logic because
-    // the storybook development server doesn't actually use the
-    // mini-css-extract-plugin.
+    // the storybook-static directory.
     if ( configType === 'PRODUCTION') {
       const cssRule = config.module.rules.find(rule => rule.test && 'some.css'.match(rule.test));
       if(cssRule) {
         const cssFileLoader = cssRule.use.find(loader => loader.loader && loader.loader.match('mini-css-extract-plugin'));
-        console.log(cssFileLoader);
         if(cssFileLoader) {
           cssFileLoader.options.publicPath = (resourcePath, context) => {
             // publicPath is the relative path of the resource to the context
