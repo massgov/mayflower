ea6a6b2bb5374e32fd0286f5dc61598f5b9f7c6f
Minghua Sun <clairesunstudio@gmail.com>
2020-05-07 14:35:11 -0400

    create story to show more option

--- react/src/components/organisms/TeaserListing/TeaserListing.stories.js
+++ react/src/components/organisms/TeaserListing/TeaserListing.stories.js
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, object, boolean, number } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
-import { DecorativeLink, Paragraph, GenTeaser } from '../../../index';
+import { DecorativeLink, Paragraph, GenTeaser, Collapse } from '../../../index';
 import SidebarHeading from '../../atoms/headings/SidebarHeading';
 import CompHeading from '../../atoms/headings/CompHeading';
 import TeaserListing from '.';
 import TeaserListingDocs from './TeaserListing.md';
 import TeaserListingData from './TeaserListing.knob.options';
 
-storiesOf('organisms', module)
+storiesOf('organisms/TeaserListing', module)
   .addDecorator(withKnobs({ escapeHTML: false }))
   .add(
-    'TeaserListing', () => {
+    'TeaserListing with Features', () => {
       const featuresProps = {
         stacked: boolean('stacked', false, 'Features')
       };
       const itemsProps = {
         columns: number('columns', 2, 'Items')
       };
-      const othersProps = {
-        shownItems: number('TeaserListing shownItems', 4),
-        moreLabel: text('TeaserListing moreLabel', 'More'),
-        lessLabel: text('TeaserListing lessLabel', 'Less'),
-        more: object('TeaserListing more', TeaserListingData.teaserListing.more)
-      };
       return(
         <TeaserListing>
-          <CompHeading {...TeaserListingData.teaserListing.compHeading} />
-          <SidebarHeading {...TeaserListingData.teaserListing.sidebarHeading} />
-          <Paragraph {...TeaserListingData.teaserListing.description} />
+          <CompHeading {...TeaserListingData.compHeading} />
+          <Paragraph {...TeaserListingData.description} />
           <TeaserListing.Features {...featuresProps}>
             {
-              TeaserListingData.teaserListing.featuredItems.map((item, i) => (
+              TeaserListingData.featuredItems.map((item, i) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <GenTeaser key={`teaser_feature_${i}`}>
                   <GenTeaser.Image img={item.image} />
@@ -52,7 +46,7 @@ storiesOf('organisms', module)
 
           <TeaserListing.Items {...itemsProps}>
             {
-              TeaserListingData.teaserListing.items.map((item, i) => (
+              TeaserListingData.items.map((item, i) => (
                 // eslint-disable-next-line react/no-array-index-key
                 <TeaserListing.Item key={`teaser_item_${i}`}>
                   <GenTeaser>
@@ -65,6 +59,71 @@ storiesOf('organisms', module)
               ))
             }
           </TeaserListing.Items>
+        </TeaserListing>
+      );
+    },
+    { info: TeaserListingDocs }
+  )
+  .add(
+    'TeaserListing with More', () => {
+      const itemsProps = {
+        columns: number('columns', 2, 'Items')
+      };
+      const moreProps = {
+        shownItems: number('TeaserListing shownItems', 4),
+        moreLabel: text('TeaserListing moreLabel', 'More'),
+        lessLabel: text('TeaserListing lessLabel', 'Less'),
+        more: object('TeaserListing more', TeaserListingData.more)
+      };
+      const [open, setOpen] = useState(false);
+      return(
+        <TeaserListing>
+          <CompHeading {...TeaserListingData.compHeading} />
+          <Paragraph {...TeaserListingData.description} />
+          <TeaserListing.Items {...itemsProps}>
+            {
+              TeaserListingData.items.map((item, i) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <TeaserListing.Item key={`teaser_item_${i}`}>
+                  <GenTeaser>
+                    <GenTeaser.Details>
+                      <GenTeaser.Title title={item.title} />
+                      <GenTeaser.Description description={item.description} />
+                    </GenTeaser.Details>
+                  </GenTeaser>
+                </TeaserListing.Item>
+              ))
+            }
+          </TeaserListing.Items>
+          <Collapse in={open} dimension="height">
+            <div className="ma__teaser-listing__extra">
+              <TeaserListing.Items {...itemsProps}>
+                {
+                  TeaserListingData.items.map((item, i) => (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <TeaserListing.Item key={`teaser_item_${i}`}>
+                      <GenTeaser>
+                        <GenTeaser.Details>
+                          <GenTeaser.Title title={item.title} />
+                          <GenTeaser.Description description={item.description} />
+                        </GenTeaser.Details>
+                      </GenTeaser>
+                    </TeaserListing.Item>
+                  ))
+                }
+              </TeaserListing.Items>
+            </div>
+          </Collapse>
+          <button
+            className="ma__content-link ma__content-link--chevron ma__content-link__acordion-toggle js-accordion-link"
+            aria-label={(open) ? moreProps.lessLabel : moreProps.moreLabel}
+            onClick={() => setOpen(!open)}
+          >
+            {(open)
+              ? (<span className="less">{moreProps.lessLabel}</span>)
+              : (<span className="more">{moreProps.moreLabel}</span>)
+            }
+          </button>
         </TeaserListing>
       );
     },
