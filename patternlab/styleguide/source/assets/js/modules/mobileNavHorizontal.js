0c497ed3f85189c1dc310bee0d592de7176f3f71
abaghdasaryan <arthur.baghdasar@gmail.com>
2020-06-12 18:01:02 +0400

    Horizontal nav revert

---
+++ patternlab/styleguide/source/assets/js/modules/mobileNavHorizontal.js
@@ -0,0 +1,28 @@
+// ****** Menu button ******
+let menuButton = document.querySelector(".js-header-menu-button");
+let feedbackButton = document.querySelector('.ma__fixed-feedback-button');
+
+if (null !== menuButton) {
+  menuButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.querySelector("body").classList.toggle("show-menu");
+
+    // Hide feedback button when menu opens.
+    feedbackButton.classList.toggle("hide-button");
+  });
+}
+
+// ****** Main Header Search button on mobile should open the mobile menu  ******
+let searchForm = document.querySelector(".js-header-search-menu .js-header-search-form");
+
+if (null !== searchForm) {
+  searchForm.addEventListener("submit", function (event) {
+    if (window.innerWidth > 620) {
+      return;
+    }
+    event.preventDefault();
+    document.querySelector("body").classList.toggle("show-menu");
+    document.querySelector('.ma__header__nav-search .ma__header-search__input').focus();
+    feedbackButton.classList.toggle("hide-button");
+  });
+}
