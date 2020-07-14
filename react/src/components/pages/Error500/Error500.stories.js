3b445706377bb464a26171a185796e4acd3e06d8
smurrayatwork <steven.murray@mediacurrent.com>
2020-07-14 14:25:03 -0400

    Updates Error components, NarrowTemplate to support passing in custom sitelogo component.

--- react/src/components/pages/Error500/Error500.stories.js
+++ react/src/components/pages/Error500/Error500.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import logo from '@massds/mayflower-assets/static/images/stateseal.png';
+import SiteLogo from 'MayflowerReactMedia/SiteLogo';
 
 import Error500 from '.';
 import Error500Docs from './Error500.md';
@@ -7,7 +9,9 @@ import Error500Docs from './Error500.md';
 storiesOf('pages', module)
   .add(
     'Error500', (() => (
-      <Error500 />
+      <Error500
+        siteLogo={() => <SiteLogo siteName="Mass.gov" url={{ domain: '/' }} image={{ src: logo }} />}
+      />
     )),
     { info: Error500Docs }
   );
--- react/src/components/pages/Error500/index.js
+++ react/src/components/pages/Error500/index.js
@@ -5,13 +5,28 @@
  * @requires module:@massds/mayflower-assets/scss/03-organisms/error-page
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import NarrowTemplate from 'MayflowerReactTemplates/NarrowTemplate';
 import ErrorPage from 'MayflowerReactOrganisms/ErrorPage';
 
-const Error500 = () => (
-  <NarrowTemplate side="right" color="yellow">
-    <ErrorPage type="500" label="Uh Oh" title="Something went wrong" message="Sorry, we're having some technical issues. Please try refreshing the page in a few minutes" />
+const Error500 = ({ siteLogo }) => (
+  <NarrowTemplate
+    side="right"
+    color="yellow"
+    siteLogo={siteLogo}
+  >
+    <ErrorPage
+      type="500"
+      label="Uh Oh"
+      title="Something went wrong"
+      message="Sorry, we're having some technical issues. Please try refreshing the page in a few minutes"
+    />
   </NarrowTemplate>
 );
+
+Error500.propTypes = {
+  /** A function that returns a SiteLogo component for NarrowTemplate to render. */
+  siteLogo: PropTypes.func
+};
 
 export default Error500;
