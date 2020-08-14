0c6e36f4585d051ae4564ef4aafe0b211b95e65a
Minghua Sun <clairesunstudio@gmail.com>
2020-08-14 14:48:07 -0400

    mockup pfml footer

--- react/src/components/organisms/FooterSlim/FooterSlim.stories.js
+++ react/src/components/organisms/FooterSlim/FooterSlim.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, object, text, number, boolean } from '@storybook/addon-knobs';
 import logo from '@massds/mayflower-assets/static/images/stateseal.png';
+import pfmlLogo from './pfml_logo.png';
+import dfmlLogo from './dfml_logo.png';
 import FooterSlim from './index';
 import FooterSlimDocs from './FooterSlim.md';
 import SiteLogo from 'MayflowerReactMedia/SiteLogo';
@@ -51,9 +53,9 @@ storiesOf('organisms/FooterSlim', module)
           domain: text('siteLogo: url domain', 'https://www.mass.gov/', 'SiteLogo 1')
         },
         image: {
-          src: text('siteLogo: image src', logo, 'SiteLogo 1'),
+          src: text('siteLogo: image src', dfmlLogo, 'SiteLogo 1'),
           alt: text('siteLogo: image alt', 'Massachusetts state seal', 'SiteLogo 1'),
-          width: number('siteLogo: image width', 45, {}, 'SiteLogo 1'),
+          width: number('siteLogo: image width', 180, {}, 'SiteLogo 1'),
           height: number('siteLogo: image height', 45, {}, 'SiteLogo 1')
         },
         title: text('siteLogo: title', 'Mass.gov homepage', 'SiteLogo 1')
@@ -63,26 +65,22 @@ storiesOf('organisms/FooterSlim', module)
           domain: text('siteLogo: url domain', 'http://www.doe.mass.edu/', 'SiteLogo 2')
         },
         image: {
-          src: text('siteLogo: image src', 'http://www.doe.mass.edu/images/Master-Logo_695x338_color.png', 'SiteLogo 2'),
+          src: text('siteLogo: image src', pfmlLogo, 'SiteLogo 2'),
           alt: text('siteLogo: image alt', 'Massachusetts state seal', 'SiteLogo 2'),
-          width: number('siteLogo: image width', 120, {}, 'SiteLogo 2'),
+          width: number('siteLogo: image width', 180, {}, 'SiteLogo 2'),
           height: number('siteLogo: image height', 45, {}, 'SiteLogo 2')
         },
         title: text('siteLogo: title', 'Mass.gov homepage', 'SiteLogo 2')
       };
       const props = {
-        title: text('title', 'Massachusetts Executive Office of Eductation (EDU)'),
-        description: text('description', "The Department of Early Education and Care's mission is to support the healthy growth and development of all children by providing high quality programs and resources for families"),
-        links: object('links', [
-          { href: '#', title: 'Lead Agencies Policies' },
-          { href: '#', title: 'Child Care Licensing Procedures' }
-        ]),
+        title: text('title', 'Department of Family and Medical Leave (DFML) '),
+        description: text('description', "The Department of Family and Medical Leave oversees the Commonwealth’s Paid Family and Medical Leave (PFML) program. "),
         contact: {
-          address: text('contact: address', '51 Sleeper St. 4th Floor, Boston, MA 02210', 'Contact'),
-          phone: text('contact: phone', '(617) 988-6600', 'Contact'),
+          address: text('contact: address', 'xxxxxxxxx, Boston, MA 02210', 'Contact'),
+          phone: text('contact: phone', '(617) xxx-xxxx', 'Contact'),
           online: {
             href: text('contact: online href', '#', 'Contact'),
-            title: text('contact: online title', 'EEC Official Website', 'Contact')
+            title: text('contact: online title', 'Department of Family and Medical Leave', 'Contact')
           }
         },
         siteLogo: <><SiteLogo {...siteLogo1Props} /><SiteLogo {...siteLogo2Props} /></>,
