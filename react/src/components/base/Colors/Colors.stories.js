8560d2fa501bc98f0a8c963fb3c1c07112d2549c
smurrayatwork <steven.murray@mediacurrent.com>
2020-08-21 15:59:37 -0400

    WIP: Adds misc stories to CSF/MDX format.

--- react/src/components/base/Colors/Colors.stories.js
+++ react/src/components/base/Colors/Colors.stories.js
@@ -1,98 +1,100 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { StoryPage } from 'StorybookConfig/preview';
 import SidebarHeading from '../../atoms/headings/SidebarHeading';
 import { ColorSwatch, GradientSpectrum } from './index';
 import { themeColors, grayScaleColors, utilityColors, primaryColors, primaryAltColors, highLightColors } from './colors.json';
-
 
 import ColorGradientsDocs from './ColorGradients.md';
 
 import './styles.scss';
 
-storiesOf('brand/colors', module)
-  .addDecorator(withKnobs({ escapeHTML: false }))
-  .add('Colors', (() => (
-    <React.Fragment>
-      <SidebarHeading title="Mayflower Brand Colors" level={2} />
-      <ul className="sg-colors">
-        {
+export const Colors = () => (
+  <React.Fragment>
+    <SidebarHeading title="Mayflower Brand Colors" level={2} />
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        themeColors.map((color, i) => <ColorSwatch key={`themeColors${i}`} {...color} variable={color.token} />)
+      }
+    </ul>
+    <SidebarHeading title="Theme Colors Usage" level={2} />
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        primaryColors.map((color, i) => <ColorSwatch key={`primaryColors${i}`} {...color} />)
+      }
+    </ul>
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        primaryAltColors.map((color, i) => <ColorSwatch key={`primaryAltColors${i}`} {...color} />)
+      }
+    </ul>
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        highLightColors.map((color, i) => <ColorSwatch key={`highLightColors${i}`} {...color} />)
+      }
+    </ul>
+    <SidebarHeading title="Gray Scale Colors" level={2} />
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        grayScaleColors.map((color, i) => <ColorSwatch key={`grayScaleColors${i}`} {...color} />)
+      }
+    </ul>
+    <SidebarHeading title="Utility Colors" level={2} />
+    <ul className="sg-colors">
+      {
+        // eslint-disable-next-line react/no-array-index-key
+        utilityColors.map((color, i) => <ColorSwatch key={`utilityColors${i}`} {...color} />)
+      }
+    </ul>
+  </React.Fragment>
+);
+
+export const GradientsLight = () => (
+  <React.Fragment>
+    {
+      themeColors.map(({ token, name }, i) => {
+        const props = {
+          token: token.match(/\$(.*)/)[1],
+          name,
+          effect: 'tint'
+        };
+        return(
           // eslint-disable-next-line react/no-array-index-key
-          themeColors.map((color, i) => <ColorSwatch key={`themeColors${i}`} {...color} variable={color.token} />)
-        }
-      </ul>
-      <SidebarHeading title="Theme Colors Usage" level={2} />
-      <ul className="sg-colors">
-        {
+          <GradientSpectrum {...props} key={`spectrum_${token}${i}`} />
+        );
+      })
+    }
+  </React.Fragment>
+);
+GradientsLight.storyName = 'Gradients (Light)';
+
+export const GradientsDark = () => (
+  <React.Fragment>
+    {
+      themeColors.map(({ token, name }, i) => {
+        const props = {
+          token: token.match(/\$(.*)/)[1],
+          name,
+          effect: 'shade'
+        };
+        return(
           // eslint-disable-next-line react/no-array-index-key
-          primaryColors.map((color, i) => <ColorSwatch key={`primaryColors${i}`} {...color} />)
-        }
-      </ul>
-      <ul className="sg-colors">
-        {
-          // eslint-disable-next-line react/no-array-index-key
-          primaryAltColors.map((color, i) => <ColorSwatch key={`primaryAltColors${i}`} {...color} />)
-        }
-      </ul>
-      <ul className="sg-colors">
-        {
-          // eslint-disable-next-line react/no-array-index-key
-          highLightColors.map((color, i) => <ColorSwatch key={`highLightColors${i}`} {...color} />)
-        }
-      </ul>
-      <SidebarHeading title="Gray Scale Colors" level={2} />
-      <ul className="sg-colors">
-        {
-          // eslint-disable-next-line react/no-array-index-key
-          grayScaleColors.map((color, i) => <ColorSwatch key={`grayScaleColors${i}`} {...color} />)
-        }
-      </ul>
-      <SidebarHeading title="Utility Colors" level={2} />
-      <ul className="sg-colors">
-        {
-          // eslint-disable-next-line react/no-array-index-key
-          utilityColors.map((color, i) => <ColorSwatch key={`utilityColors${i}`} {...color} />)
-        }
-      </ul>
-    </React.Fragment>
-  )))
-  .add(
-    'Gradients (Light)', (() => (
-      <React.Fragment>
-        {
-          themeColors.map(({ token, name }, i) => {
-            const props = {
-              token: token.match(/\$(.*)/)[1],
-              name,
-              effect: 'tint'
-            };
-            return(
-              // eslint-disable-next-line react/no-array-index-key
-              <GradientSpectrum {...props} key={`spectrum_${token}${i}`} />
-            );
-          })
-        }
-      </React.Fragment>
-    )),
-    { info: ColorGradientsDocs }
-  )
-  .add(
-    'Gradients (Dark)', (() => (
-      <React.Fragment>
-        {
-          themeColors.map(({ token, name }, i) => {
-            const props = {
-              token: token.match(/\$(.*)/)[1],
-              name,
-              effect: 'shade'
-            };
-            return(
-              // eslint-disable-next-line react/no-array-index-key
-              <GradientSpectrum {...props} key={`spectrum_${token}${i}`} />
-            );
-          })
-        }
-      </React.Fragment>
-    )),
-    { info: ColorGradientsDocs }
-  );
+          <GradientSpectrum {...props} key={`spectrum_${token}${i}`} />
+        );
+      })
+    }
+  </React.Fragment>
+);
+GradientsDark.storyName = 'Gradients (Dark)';
+export default {
+  title: 'brand/colors',
+  parameters: {
+    docs: {
+      page: () => <StoryPage Description={ColorGradientsDocs} />
+    }
+  }
+};
--- react/src/components/base/Icon/Icon.stories.js
+++ react/src/components/base/Icon/Icon.stories.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { StoryPage } from 'StorybookConfig/preview';
-
-import { storiesOf } from '@storybook/react';
-import { withKnobs, select, text, array, boolean, color, number } from '@storybook/addon-knobs';
 import { assets, svgOptions } from './Icon.knob.options';
 import * as Icon from './index';
 import IconDisplay from './IconDisplay';
@@ -39,7 +36,7 @@ BasicIcon.argTypes = {
       type: 'select',
       options: Object.fromEntries(
         Object.entries(svgOptions).map(([key, value]) => [`Icon${key[0].toUpperCase() + key.slice(1)}`, value ? `Icon${value[0].toUpperCase() + value.slice(1)}` : value])
-        ),
+      )
     }
   },
   fill: {
@@ -50,21 +47,11 @@ BasicIcon.argTypes = {
 };
 
 export const AllIcons = (args) => {
-  const allIconProps = Object.keys(assets).map((key) => ({
-    key,
-    name: key,
-    width,
-    height,
-    title,
-    classes,
-    ariaHidden,
-    fill
-  }));
   return(
     <ul className="sg-icons">
       {
-        allIconProps.map((iconProp) => (
-          <IconDisplay {...iconProp} key={`icon_${iconProp.key}`} />
+        Object.keys(assets).map((icon) => (
+          <IconDisplay {...args} name={icon} key={`icon_${icon}`} />
         ))
       }
     </ul>
@@ -72,7 +59,6 @@ export const AllIcons = (args) => {
 };
 AllIcons.storyName = 'All Icons';
 AllIcons.args = {
-  name: 'IconAlert',
   width: 40,
   height: 50,
   title: 'Icon Title Here',
@@ -80,76 +66,10 @@ AllIcons.args = {
   ariaHidden: false,
   fill: '#000'
 };
-
-// storiesOf('brand/icons', module)
-//   .addDecorator(withKnobs({ escapeHTML: false }))
-//   .add('Icon', () => {
-//     // This needs to be dynamic somehow.
-
-//     const width = text('width', 40);
-//     const height = text('height', 40);
-//     const title = text('title', 'Icon Title Here');
-//     const classes = array('classes', ['']);
-//     const ariaHidden = boolean('ariaHidden', false);
-//     const fill = color('fill color', '#000');
-//     const props = {
-//       width,
-//       height,
-//       title,
-//       classes,
-//       ariaHidden,
-//       fill
-//     };
-//     // Capitalizes the name of each SVG icon to match
-//     // what SVGR names components.
-//     const component = select('name',
-//       Object.fromEntries(
-//         Object.entries(svgOptions).map(([key, value]) => [`Icon${key[0].toUpperCase() + key.slice(1)}`, value ? `Icon${value[0].toUpperCase() + value.slice(1)}` : value])
-//         ),
-//         'IconAlert'
-//     );
-//     const SelectedComponent = Icon[component];
-//     if (window.location.search.indexOf('backstop') > -1) {
-//       return Object.entries(assets).map(([key]) => {
-//         const backstopProps = {
-//           key,
-//           width,
-//           height,
-//           title
-//         };
-//         const BackstopIcon = Icon[`Icon${key[0].toUpperCase() + key.slice(1)}`];
-//         return<BackstopIcon {...backstopProps} />;
-//       });
-//     }
-//     return(<SelectedComponent {...props} />);
-//   });
-
-// storiesOf('brand/icons', module)
-//   .addDecorator(withKnobs({ escapeHTML: false }))
-//   .add('All Icons', () => {
-//     const width = number('width', 40);
-//     const height = number('height', 40);
-//     const title = text('title', 'Icon Title Here');
-//     const classes = array('classes', ['']);
-//     const ariaHidden = boolean('ariaHidden', false);
-//     const fill = color('fill color', '#000');
-//     const allIconProps = Object.keys(assets).map((key) => ({
-//       key,
-//       name: key,
-//       width,
-//       height,
-//       title,
-//       classes,
-//       ariaHidden,
-//       fill
-//     }));
-//     return(
-//       <ul className="sg-icons">
-//         {
-//           allIconProps.map((iconProp) => (
-//             <IconDisplay {...iconProp} key={`icon_${iconProp.key}`} />
-//           ))
-//         }
-//       </ul>
-//     );
-//   });
+AllIcons.argTypes = {
+  fill: {
+    control: {
+      type: 'color'
+    }
+  }
+};
