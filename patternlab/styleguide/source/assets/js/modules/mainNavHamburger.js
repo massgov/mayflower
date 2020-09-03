7d6e9447a1865ee7492237922a088c7f15f4fc79
Tom Fleming <tom@lastcallmedia.com>
2020-09-03 16:41:53 -0400

    Set up with correct positioning and animated scroll - DP-19782

--- patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -515,7 +515,44 @@ function openMenu() {
   menuButton.setAttribute("aria-pressed", "true");
   let alertsInterface = document.querySelector('.ma__emergency-alerts__interface');
   if (document.querySelector("body").clientWidth < 841 && alertsInterface !== null) {
-    alertsInterface.scrollIntoView();
+    let emergencyAlerts = document.querySelector(".ma__emergency-alerts");
+    let scrollOffset = emergencyAlerts.offsetHeight - (alertsInterface.offsetHeight/1.5);
+
+    if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
+
+      function customScrollTo(to, duration) {
+        var start = 0,
+            change = to - start,
+            currentTime = 0,
+            increment = 20;
+
+        var animateScroll = function(){
+          currentTime += increment;
+          var val = Math.easeInOutQuad(currentTime, start, change, duration);
+          window.scrollTo(0,val);
+
+          if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+          }
+        };
+        animateScroll();
+      }
+
+      Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+      };
+
+      customScrollTo(scrollOffset, 1000);
+    } else{
+      window.animate({
+        scrollTop: scrollOffset
+      }, 1000, function(){
+        window.clearQueue();
+      });
+    }
   }
 }
 
