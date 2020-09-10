41f2d215a43cd50435c99cf6a7065e79f36f24d5
abaghdasaryan <arthur.baghdasar@gmail.com>
2020-09-10 22:32:49 +0400

    DP-19868: Search icon issue fox for Apple devices

--- patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -519,42 +519,9 @@ function openMenu() {
   if (document.querySelector("body").clientWidth < 841 && alertsInterface !== null) {
     let emergencyAlerts = document.querySelector(".ma__emergency-alerts");
     let scrollOffset = emergencyAlerts.offsetHeight - (alertsInterface.offsetHeight/1.5);
-
-
-    if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
-      function customScrollTo(to, duration) {
-        window.addEventListener('scroll', function(e) {e.preventDefault();}, true);
-        var start = 0,
-            change = to - start,
-            currentTime = 0,
-            increment = 20;
-
-        var animateScroll = function(){
-          currentTime += increment;
-          var val = Math.easeInOutQuad(currentTime, start, change, duration);
-          window.scrollTo(0,val);
-
-          if(currentTime < duration) {
-            setTimeout(animateScroll, increment);
-          }
-        };
-        animateScroll();
-      }
-
-      Math.easeInOutQuad = function (t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-      };
-      $('html, body').animate({
-        scrollTop: scrollOffset
-      }, 250);
-    } else {
-      $('html, body').animate({
-        scrollTop: scrollOffset
-      }, 250);
-    }
+    $('html, body').animate({
+      scrollTop: scrollOffset
+    }, 250);
   }
 }
 
@@ -590,6 +557,16 @@ function jumpToSearch(e) {
   } else {
     hamburgerMenuContainer.removeAttribute("aria-hidden");
     commonOpenMenuTasks();
+
+    let alertsInterface = document.querySelector('.ma__emergency-alerts__interface');
+    if (alertsInterface !== null) {
+      let emergencyAlerts = document.querySelector(".ma__emergency-alerts");
+      let scrollOffset = emergencyAlerts.offsetHeight - (alertsInterface.offsetHeight/1.5);
+      $('html, body').animate({
+        scrollTop: scrollOffset
+      }, 250);
+    }
+
     jumpToSearchButton.setAttribute("aria-pressed", "true");
     // Set focus on the search input field.
     const osInfo = navigator.appVersion;
