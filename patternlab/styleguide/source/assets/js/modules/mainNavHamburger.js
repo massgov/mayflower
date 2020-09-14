69d9c65f6841f2babba085273d2999226d5c5d50
Tom Fleming <tom@lastcallmedia.com>
2020-09-14 15:55:19 -0400

    Swapped back in JS-only menu animation - DP-19870

--- patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -531,9 +531,16 @@ function openMenu() {
   if (alertsInterface !== null) {
     let emergencyAlerts = document.querySelector(".ma__emergency-alerts");
     let scrollOffset = emergencyAlerts.offsetHeight - (alertsInterface.offsetHeight/1.5);
-    $('html, body').animate({
-      scrollTop: scrollOffset
-    }, 250);
+    if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
+      customScrollTo(scrollOffset, 250);
+    }
+    else {
+      window.scrollTo({
+        top: scrollOffset,
+        left: 0,
+        behavior: "smooth"
+      });
+    }
   }
 }
 
@@ -579,9 +586,16 @@ function jumpToSearch(e) {
     if (alertsInterface !== null) {
       let emergencyAlerts = document.querySelector(".ma__emergency-alerts");
       let scrollOffset = emergencyAlerts.offsetHeight - (alertsInterface.offsetHeight/1.5);
-      $('html, body').animate({
-        scrollTop: scrollOffset
-      }, 250);
+      if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
+        customScrollTo(scrollOffset, 250);
+      }
+      else {
+        window.scrollTo({
+          top: scrollOffset,
+          left: 0,
+          behavior: "smooth"
+        });
+      }
     }
 
     jumpToSearchButton.setAttribute("aria-pressed", "true");
@@ -840,3 +854,28 @@ function closeSubMenu() {
     }, 700);
   }
 }
+
+function customScrollTo(to, duration) {
+  var start = 0,
+      change = to - start,
+      currentTime = 0,
+      increment = 20;
+
+  var animateScroll = function(){
+    currentTime += increment;
+    var val = Math.easeInOutQuad(currentTime, start, change, duration);
+    window.scrollTo(0,val);
+
+    if(currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+  animateScroll();
+}
+
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d/2;
+  if (t < 1) return c/2*t*t + b;
+  t--;
+  return -c/2 * (t*(t-2) - 1) + b;
+};
