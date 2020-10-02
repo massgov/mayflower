3855da6610c517c46d042df7dac8d5f1a72e24d6
Yukiko Gannett <yukikogannett@gmail.com>
2020-10-02 11:35:25 -0400

    remove unused function

--- patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -879,10 +879,3 @@ function closeSubMenu() {
     openSubMenu.classList.remove("submenu-open");
   }
 }
-
-Math.easeInOutQuad = function (t, b, c, d) {
-  t /= d/2;
-  if (t < 1) return c/2*t*t + b;
-  t--;
-  return -c/2 * (t*(t-2) - 1) + b;
-};
