458c45fa7af69f438c51e773fa6a12a330010142
Minghua Sun <clairesunstudio@gmail.com>
2020-10-02 19:20:11 -0400

    wip

--- react/src/components/base/Typography/Typography.knobs.options.js
+++ react/src/components/base/Typography/Typography.knobs.options.js
@@ -3,12 +3,12 @@
  */
 export default {
   size: {
-    'Heading 1': 'h1',
-    'Heading 2': 'h2',
-    'Heading 3': 'h3',
-    'Heading 4': 'h4',
-    'Heading 5': 'h5',
-    'Heading 6': 'h6',
+    'Heading 1': '1',
+    'Heading 2': '2',
+    'Heading 3': '3',
+    'Heading 4': '4',
+    'Heading 5': '5',
+    'Heading 6': '6',
     'Body text': 'p'
   }
 };
