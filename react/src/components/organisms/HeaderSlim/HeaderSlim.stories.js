59fdccae50a51333bd77532eaeebb1ea67bfc454
Minghua Sun <clairesunstudio@gmail.com>
2020-10-12 14:14:59 -0400

    add story

--- react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
+++ react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
@@ -3,7 +3,10 @@ import { StoryPage } from 'StorybookConfig/preview';
 
 import logo from '@massds/mayflower-assets/static/images/stateseal.png';
 import SiteLogo from 'MayflowerReactMedia/SiteLogo';
+import HeaderSearch from 'MayflowerReactMolecules/HeaderSearch';
+import Button from 'MayflowerReactButtons/Button';
 import HeaderSlim from '.';
+
 
 export const HeaderSlimExample = (args) => <HeaderSlim {...args} />;
 
@@ -24,9 +27,31 @@ HeaderSlimExample.args = {
       title="Mass.gov homepage"
     />
   ),
+  skipNav: (<a className="ma__header__skip-nav" href="#main-content">skip to main content</a>)
+};
+
+export const HeaderSlimWithNavs = (args) => <HeaderSlim {...args} />;
+
+HeaderSlimWithNavs.storyName = 'HeaderSlim with Nav';
+HeaderSlimWithNavs.args = {
+  siteLogo: (
+    <SiteLogo
+      url={{
+        domain: 'https://www.mass.gov/'
+      }}
+      image={{
+        src: logo,
+        alt: 'Massachusetts state seal',
+        width: 45,
+        height: 45
+      }}
+      siteName="Mass.gov"
+      title="Mass.gov homepage"
+    />
+  ),
   skipNav: (<a className="ma__header__skip-nav" href="#main-content">skip to main content</a>),
-  mainNav: <a href="#main-content">main Nav Link</a>,
-  utilityNav: <a href="#main-content">utility Nav Link</a>
+  mainNav: <Button usage="secondary" text="Log in"/>,
+  utilityNav: <><a href="#main-content">Nav Link 1</a><a href="#main-content">Nav Link 2</a></>
 };
 
 export default {
