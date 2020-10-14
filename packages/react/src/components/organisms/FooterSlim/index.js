47250e32569c4e38ba39ae8a3178961b51dc17db
Minghua Sun <clairesunstudio@gmail.com>
2020-10-14 12:41:52 -0400

    Update packages/react/src/components/organisms/FooterSlim/index.js
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>

--- packages/react/src/components/organisms/FooterSlim/index.js
+++ packages/react/src/components/organisms/FooterSlim/index.js
@@ -17,7 +17,7 @@ import IconPhone from 'MayflowerReactBase/Icon/IconPhone';
 import IconLaptop from 'MayflowerReactBase/Icon/IconLaptop';
 
 const FooterSlim = ({
-  title, description, siteLogo, stackedLogo = false, links = null, contact = null
+  title, description, SiteLogo, stackedLogo = false, links = null, contact = null
 }) => {
   const today = new Date();
   const year = today.getFullYear();
