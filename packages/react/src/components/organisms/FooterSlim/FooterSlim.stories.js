04aa59b1d62257f2d673791ce6c7b0963fc491c2
Minghua Sun <clairesunstudio@gmail.com>
2020-10-14 12:44:09 -0400

    Update packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>

--- packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
+++ packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
@@ -81,7 +81,7 @@ FooterSlimMultiLogos.args = {
       title: 'EEC Official Website'
     }
   },
-  siteLogo: (
+  SiteLogo: () => (
     <React.Fragment>
       <SiteLogo {...siteLogo1Props} />
       <SiteLogo {...siteLogo2Props} />
