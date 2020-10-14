b14fd743024ffdde9b933d12625b344662741098
Minghua Sun <clairesunstudio@gmail.com>
2020-10-14 15:48:05 -0400

    order stories

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,4 +1,16 @@
+const storyKindOrder = [
+  'Get Started',
+  'Design Principles',
+  'Design Tokens',
+  'Branding Components',
+  'Base Elements'
+];
 
 export const parameters = {
+  options: {
+    storySort: {
+      order: storyKindOrder
+    },
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
