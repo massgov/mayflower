1b77d324235505fffbe4a083dd36ec4a15844d8b
Minghua Sun <clairesunstudio@gmail.com>
2020-10-22 13:28:18 -0400

    React/dp 19539 footer slim multiple logos1 (#1208)
    * merge conflicts
    
    * remove obsolete file
    
    * Update packages/react/src/components/organisms/FooterSlim/index.js
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    * Update packages/react/src/components/organisms/FooterSlim/index.js
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    * Update packages/react/src/components/organisms/FooterSlim/index.js
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    * Update packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    * Update packages/react/src/components/organisms/FooterSlim/index.js
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    * update phone to be anchor link
    
    * fix default story and update siteLogo type to node
    
    * address comments
    
    * fix narrow template vrt
    
    * fix error pages vrt
    
    * approve backstop images
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>

--- packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
+++ packages/react/src/components/organisms/FooterSlim/FooterSlim.stories.js
@@ -38,6 +38,58 @@ FooterSlimExample.args = {
   siteLogo: <SiteLogo {...siteLogoProps} />
 };
 
+export const FooterSlimMultiLogos = (args) => <FooterSlim {...args} />;
+
+FooterSlimMultiLogos.storyName = 'FooterSlim with Two Logos';
+const siteLogo1Props = {
+  url: {
+    domain: 'https://www.mass.gov/'
+  },
+  image: {
+    src: logo,
+    alt: 'Massachusetts state seal',
+    width: 45,
+    height: 45
+  },
+  title: 'Mass.gov homepage'
+};
+const siteLogo2Props = {
+  url: {
+    domain: 'http://www.doe.mass.edu/'
+  },
+  image: {
+    src: 'http://www.doe.mass.edu/images/Master-Logo_695x338_color.png',
+    alt: 'Department of Eduction logo',
+    width: 120,
+    height: 45
+  },
+  title: 'Department of Education homepage'
+};
+
+FooterSlimMultiLogos.args = {
+  title: 'Massachusetts Executive Office of Eductation (EDU)',
+  description: "The Department of Early Education and Care'&apos;'s mission is to support the healthy growth and development of all children by providing high quality programs and resources for families",
+  links: [
+    { href: '#', title: 'Lead Agencies Policies' },
+    { href: '#', title: 'Child Care Licensing Procedures' }
+  ],
+  contact: {
+    address: '51 Sleeper St. 4th Floor, Boston, MA 02210',
+    phone: '(617) 988-6600',
+    online: {
+      href: '#',
+      title: 'EEC Official Website'
+    }
+  },
+  siteLogo: (
+    <React.Fragment>
+      <SiteLogo {...siteLogo1Props} />
+      <SiteLogo {...siteLogo2Props} />
+    </React.Fragment>
+  ),
+  stackedLogo: true
+};
+
 export default {
   title: 'organisms/FooterSlim',
   component: FooterSlim,
