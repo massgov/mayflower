9a060842652d9fb4d6cc85739916f80ab1897a97
Minghua Sun <clairesunstudio@gmail.com>
2020-10-22 10:40:07 -0400

    React/dp 19541 extend header slim (#1207)
    * allow passing custom skipNav
    
    * add utilityNav and mainNav render props
    
    * fix styles
    
    * add story
    
    * change story to pass in header search
    
    * fix stylelint
    
    * add changelog
    
    * make render props new components
    
    * convert SiteLogo prop
    
    * change proptype check for SiteLogo
    
    * Update react/src/components/organisms/HeaderSlim/index.js
    
    * make skipNav proptype a component instead of function
    
    * convert prop to node type
    
    * fix
    
    * lowercase props
    
    * change mainNav component to validate the proptype works properly
    
    * approve backstop images
    
    * Update packages/assets/scss/03-organisms/_header-slim.scss
    
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>
    
    Co-authored-by: smurrayatwork <steven.murray@mediacurrent.com>
    Co-authored-by: Steven Murray <smurrayatwork@gmail.com>

--- packages/react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
+++ packages/react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
@@ -2,28 +2,52 @@ import React from 'react';
 import { StoryPage } from 'StorybookConfig/preview';
 
 import logo from '@massds/mayflower-assets/static/images/stateseal.png';
-import SiteLogo from 'MayflowerReactMedia/SiteLogo';
+import SiteLogoHeader from 'MayflowerReactMedia/SiteLogo';
+import ButtonWithIcon from 'MayflowerReactButtons/ButtonWithIcon';
+import IconSearch from 'MayflowerReactBase/Icon/IconSearch';
 import HeaderSlim from '.';
+
 
 export const HeaderSlimExample = (args) => <HeaderSlim {...args} />;
 
 HeaderSlimExample.storyName = 'Default';
 HeaderSlimExample.args = {
-  siteLogo: (
-    <SiteLogo
-      url={{
-        domain: 'https://www.mass.gov/'
-      }}
-      image={{
-        src: logo,
-        alt: 'Massachusetts state seal',
-        width: 45,
-        height: 45
-      }}
-      siteName="Mass.gov"
-      title="Mass.gov homepage"
-    />
-  )
+  siteLogo: (<SiteLogoHeader
+    url={{
+      domain: 'https://www.mass.gov/'
+    }}
+    image={{
+      src: logo,
+      alt: 'Massachusetts state seal',
+      width: 45,
+      height: 45
+    }}
+    siteName="Mass.gov"
+    title="Mass.gov homepage"
+  />),
+  skipNav: <a className="ma__header__skip-nav" href="#main-content">skip to main content</a>
+};
+
+export const HeaderSlimWithNavs = (args) => <HeaderSlim {...args} />;
+
+HeaderSlimWithNavs.storyName = 'HeaderSlim with Nav';
+HeaderSlimWithNavs.args = {
+  siteLogo: (<SiteLogoHeader
+    url={{
+      domain: 'https://www.mass.gov/'
+    }}
+    image={{
+      src: logo,
+      alt: 'Massachusetts state seal',
+      width: 45,
+      height: 45
+    }}
+    siteName="Mass.gov"
+    title="Mass.gov homepage"
+  />),
+  skipNav: <a className="ma__header__skip-nav" href="#main-content">skip to main content</a>,
+  mainNav: <ButtonWithIcon icon={<IconSearch />} />,
+  utilityNav: <div><a href="#main-content">Nav Link 1</a><a href="#main-content">Nav Link 2</a></div>
 };
 
 export default {
