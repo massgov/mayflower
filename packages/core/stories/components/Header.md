e40f4f6980bd85cbfcf6fbce6d209dbd42bbf8bf
Minghua Sun <clairesunstudio@gmail.com>
2020-10-23 16:25:59 -0400

    render html and add preview story template

---
+++ packages/core/stories/components/Header.md
@@ -0,0 +1,10 @@
+### Description
+This is a pattern for the site header. The options to hide the search header and back to link are only included in the mayflower react instance of this organism.
+
+@see [@organisms/header](https://mayflower.digital.mass.gov/patternlab/?p=organisms-header)
+
+### Pattern Contains
+* Utility Nav
+* Site Logo
+* Header Search
+* Main Nav
--- packages/core/stories/components/Header.stories.js
+++ packages/core/stories/components/Header.stories.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
+import { StoryPage } from '../../.storybook/preview';
 import Header from '@massds/mayflower-react/dist/HeaderSlim';
 import SiteLogo from '@massds/mayflower-react/dist/SiteLogo';
 import logo from '@massds/mayflower-assets/static/images/stateseal.png';
 import '@massds/mayflower-assets/scss/03-organisms/_header-slim.scss';
 import '@massds/mayflower-assets/scss/01-atoms/_site-logo.scss';
+// import HeaderDocs from './Header.md';
 
 import generateTitle from '../util/generateTitle';
 
 export default {
   title: generateTitle('Header'),
   component: Header,
+  parameters: {
+    docs: {
+      page: () => <StoryPage  />
+    }
+  }
 };
 
 const Template = (args) => <Header {...args} />;
