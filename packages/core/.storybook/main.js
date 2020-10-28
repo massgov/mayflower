565083429543d8496224e9d07936bb67f8899840
Minghua Sun <clairesunstudio@gmail.com>
2020-10-28 10:48:14 -0400

    setup core storybook

---
+++ packages/core/.storybook/main.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const assets = require('@massds/mayflower-assets');
+const nodeModules = path.resolve(path.join(process.cwd(), 'node_modules'));
+const pnpmNodeModules = path.join(nodeModules, '.pnpm', 'node_modules');
+console.log(path.resolve(nodeModules, '@massds/mayflower-react/dist/styles'))
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          sourceMap: true
+        },
+        sassLoaderOptions: {
+          sourceMap: true,
+          implementation: require('sass'),
+          sassOptions: {
+            includePaths: [
+              nodeModules,
+              pnpmNodeModules,
+              path.resolve(__dirname, '../stories'),
+              path.resolve(nodeModules, '@massds/mayflower-react/dist')
+            ].concat(assets.includePaths)
+          }
+        }
+      }
+    }
+  ]
+}
---
+++ packages/core/.storybook/mayflowerTheme.js
@@ -0,0 +1,36 @@
+import { create } from '@storybook/theming/create';
+import logo from './mayflower-logo.png';
+export default create({
+  base: 'light',
+
+  // colorPrimary: 'hotpink',
+  colorSecondary: '#14558F',
+
+  // UI
+  appBg: '#F2F2F2',
+  // appContentBg: 'silver',
+  appBorderColor: '#DCDCDC',
+  appBorderRadius: 6,
+
+  // Typography
+  fontBase: '"Noto Sans VF", "Noto Sans", "Helvetica", "Arial", sans-serif',
+
+  // Text colors
+  textColor: 'black',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  barTextColor: '#707070',
+  barSelectedColor: '#14558F',
+  barBg: '#F2F2F2',
+
+  // Form colors
+  inputBg: 'white',
+  inputBorder: '#DCDCDC',
+  inputTextColor: '#141414',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Mayflower React',
+  brandUrl: 'https://github.com/massgov/mayflower',
+  brandImage: logo,
+});
