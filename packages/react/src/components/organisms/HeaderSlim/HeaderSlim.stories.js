12b70f35e08278db4d21a39d62065ada6ec8b09f
smurrayatwork <steven.murray@mediacurrent.com>
2020-11-12 14:56:33 -0500

    Updates paths for Header, Footer.

--- packages/react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
+++ packages/react/src/components/organisms/HeaderSlim/HeaderSlim.stories.js
@@ -5,7 +5,7 @@ import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
 import SiteLogoHeader from 'MayflowerReactMedia/SiteLogo';
 import ButtonWithIcon from 'MayflowerReactButtons/ButtonWithIcon';
 import IconSearch from 'MayflowerReactBase/Icon/IconSearch';
-import styles from '@massds/mayflower-assets/build/header-slim.scss';
+import styles from '@massds/mayflower-assets/build/scss/header-slim.scss';
 import HeaderSlim from '.';
 
 export const HeaderSlimExample = (args) => <HeaderSlim {...args} />;
