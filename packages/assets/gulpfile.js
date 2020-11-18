63b62250f6d7e088d44441f48d8b15d288095a7e
smurrayatwork <steven.murray@mediacurrent.com>
2020-11-18 09:37:54 -0500

    Changes casing from jQuery to jquery for assets build.

--- packages/assets/gulpfile.js
+++ packages/assets/gulpfile.js
@@ -70,7 +70,7 @@ function watchScss() {
 
 function compileHamburgerHeader() {
   return src([
-    require.resolve('jQuery/dist/jquery.min.js'),
+    require.resolve('jquery/dist/jquery.min.js'),
     './build/js/header-hamburger-vendor.js',
     '../patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js',
     '../patternlab/styleguide/source/assets/js/modules/mobileNav.js'
@@ -84,7 +84,7 @@ function compileHamburgerHeader() {
 
 function compileMiniHamburgerHeader() {
   return src([
-    require.resolve('jQuery/dist/jquery.min.js'),
+    require.resolve('jquery/dist/jquery.min.js'),
     './build/js/header-hamburger-vendor.js',
     '../patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js',
     '../patternlab/styleguide/source/assets/js/modules/mobileNav.js'
@@ -112,7 +112,7 @@ function compileMainNav() {
 
 function compileMixedHeader() {
   return src([
-    require.resolve('jQuery/dist/jquery.min.js'),
+    require.resolve('jquery/dist/jquery.min.js'),
     './build/js/header-hamburger-vendor.js',
     './js/mainNav.js',
     '../patternlab/styleguide/source/assets/js/modules/mainNavMixed.js',
@@ -130,7 +130,7 @@ function compileMixedHeader() {
 
 function compileMiniMixedHeader() {
   return src([
-    require.resolve('jQuery/dist/jquery.min.js'),
+    require.resolve('jquery/dist/jquery.min.js'),
     './build/js/header-hamburger-vendor.js',
     './js/mainNav.js',
     '../patternlab/styleguide/source/assets/js/modules/mainNavMixed.js',
