360920f6c91bacd54f84af9256292d399e226362
Minghua Sun <clairesunstudio@gmail.com>
2020-11-18 16:15:50 -0500

    format html and display state seal CDN url

--- packages/core/stories/util/renderCode.js
+++ packages/core/stories/util/renderCode.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { Story, DocsContext } from '@storybook/addon-docs/blocks';
+import prettier from 'prettier/standalone';
+import parserHtml from 'prettier/parser-html';
 
-
-export const renderReactMarkup = (component) => (
-  ReactDOMServer.renderToStaticMarkup(component)
-)
+export const renderReactMarkup = (component) => {
+  const markup = ReactDOMServer.renderToStaticMarkup(component)
+  const prettyMarkup = prettier.format(markup,
+  {
+    htmlWhitespaceSensitivity: 'ignore',
+    endOfLine: 'auto',
+    parser: 'html',
+    plugins: [parserHtml]
+  });
+  // Replaces the path to the state seal with the latest assets CDN.
+  return prettyMarkup.replace(/static\/media\/stateseal\.(.*)\.png/, 'https://unpkg.com/@massds/mayflower-assets/static/images/logo/stateseal.png');
+}
 
 // can't pass DocsContext from .mdx
 // export const StoryCode = ({ children }) => (
