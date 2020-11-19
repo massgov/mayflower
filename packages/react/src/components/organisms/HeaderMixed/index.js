c2f0391dc10af2468b99c0931e2c9302702a28c8
smurrayatwork <steven.murray@mediacurrent.com>
2020-11-19 12:31:43 -0500

    WIP

--- packages/react/src/components/organisms/HeaderMixed/index.js
+++ packages/react/src/components/organisms/HeaderMixed/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import GoogleTranslateElement from 'MayflowerReactButtons/GoogleTranslateElement';
 
 const HeaderMixed = () => {
-  let foo;
   return(
     <header className="ma__header__hamburger ma__header__mixed" id="header">
 
@@ -9,7 +10,6 @@ const HeaderMixed = () => {
 
       <nav className="ma__header__hamburger__nav" aria-label="main navigation" id="main-navigation" role="navigation">
         <div className="ma__header__hamburger-wrapper">
-
           <div className="ma__header__hamburger__button-container js-sticky-header">
             <button
               type="button"
@@ -35,11 +35,7 @@ const HeaderMixed = () => {
               <ul className="ma__utility-nav__items">
                 <li className="ma__utility-nav__item">
                   <div className="ma__utility-nav__translate">
-                    <div id="google_translate_element" />
-                    <div className="ma__utility-nav__translate-icon">
-                      <svg aria-hidden="true" focusable="false"><use xlinkHref="#45cc01d390b2119ca5cf3e30539a07e0.2" /></svg>
-                      <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}><symbol xmlns="http://www.w3.org/2000/svg" aria-hidden="true" version="1.1" viewBox="0 0 24 24" id="45cc01d390b2119ca5cf3e30539a07e0.2"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 16.057v-3.057h2.994c-.059 1.143-.212 2.24-.456 3.279-.823-.12-1.674-.188-2.538-.222zm1.957 2.162c-.499 1.33-1.159 2.497-1.957 3.456v-3.62c.666.028 1.319.081 1.957.164zm-1.957-7.219v-3.015c.868-.034 1.721-.103 2.548-.224.238 1.027.389 2.111.446 3.239h-2.994zm0-5.014v-3.661c.806.969 1.471 2.15 1.971 3.496-.642.084-1.3.137-1.971.165zm2.703-3.267c1.237.496 2.354 1.228 3.29 2.146-.642.234-1.311.442-2.019.607-.344-.992-.775-1.91-1.271-2.753zm-7.241 13.56c-.244-1.039-.398-2.136-.456-3.279h2.994v3.057c-.865.034-1.714.102-2.538.222zm2.538 1.776v3.62c-.798-.959-1.458-2.126-1.957-3.456.638-.083 1.291-.136 1.957-.164zm-2.994-7.055c.057-1.128.207-2.212.446-3.239.827.121 1.68.19 2.548.224v3.015h-2.994zm1.024-5.179c.5-1.346 1.165-2.527 1.97-3.496v3.661c-.671-.028-1.329-.081-1.97-.165zm-2.005-.35c-.708-.165-1.377-.373-2.018-.607.937-.918 2.053-1.65 3.29-2.146-.496.844-.927 1.762-1.272 2.753zm-.549 1.918c-.264 1.151-.434 2.36-.492 3.611h-3.933c.165-1.658.739-3.197 1.617-4.518.88.361 1.816.67 2.808.907zm.009 9.262c-.988.236-1.92.542-2.797.9-.89-1.328-1.471-2.879-1.637-4.551h3.934c.058 1.265.231 2.488.5 3.651zm.553 1.917c.342.976.768 1.881 1.257 2.712-1.223-.49-2.326-1.211-3.256-2.115.636-.229 1.299-.435 1.999-.597zm9.924 0c.7.163 1.362.367 1.999.597-.931.903-2.034 1.625-3.257 2.116.489-.832.915-1.737 1.258-2.713zm.553-1.917c.27-1.163.442-2.386.501-3.651h3.934c-.167 1.672-.748 3.223-1.638 4.551-.877-.358-1.81-.664-2.797-.9zm.501-5.651c-.058-1.251-.229-2.46-.492-3.611.992-.237 1.929-.546 2.809-.907.877 1.321 1.451 2.86 1.616 4.518h-3.933z" /></symbol></svg>
-                    </div>
+                    <GoogleTranslateElement id="google_translate_element_hamburger" />
                   </div>
                 </li>
                 <li className="ma__utility-nav__item">
@@ -228,7 +224,6 @@ const HeaderMixed = () => {
               </ul>
             </div>
           </div>
-
           <div className="ma__header__hamburger__nav-container" aria-hidden="true">
             <div className="ma__header__hamburger__logo ma__header__hamburger__logo--mobile">
               <div className="ma__site-logo">
@@ -658,12 +653,9 @@ const HeaderMixed = () => {
                 </ul>
               </div>
             </div>
-            {' '}
 
           </div>
         </div>
-        {' '}
-
       </nav>
 
       <div className="ma__header__container">
@@ -910,19 +902,13 @@ const HeaderMixed = () => {
                 </li>
               </ul>
             </div>
-            {' '}
-
           </div>
           <div className="ma__header__utility-nav ma__header__utility-nav--narrow">
             <div className="ma__utility-nav js-util-nav">
               <ul className="ma__utility-nav__items">
                 <li className="ma__utility-nav__item">
                   <div className="ma__utility-nav__translate">
-                    <div id="google_translate_element" />
-                    <div className="ma__utility-nav__translate-icon">
-                      <svg aria-hidden="true" focusable="false"><use xlinkHref="#45cc01d390b2119ca5cf3e30539a07e0.4" /></svg>
-                      <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}><symbol xmlns="http://www.w3.org/2000/svg" aria-hidden="true" version="1.1" viewBox="0 0 24 24" id="45cc01d390b2119ca5cf3e30539a07e0.4"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 16.057v-3.057h2.994c-.059 1.143-.212 2.24-.456 3.279-.823-.12-1.674-.188-2.538-.222zm1.957 2.162c-.499 1.33-1.159 2.497-1.957 3.456v-3.62c.666.028 1.319.081 1.957.164zm-1.957-7.219v-3.015c.868-.034 1.721-.103 2.548-.224.238 1.027.389 2.111.446 3.239h-2.994zm0-5.014v-3.661c.806.969 1.471 2.15 1.971 3.496-.642.084-1.3.137-1.971.165zm2.703-3.267c1.237.496 2.354 1.228 3.29 2.146-.642.234-1.311.442-2.019.607-.344-.992-.775-1.91-1.271-2.753zm-7.241 13.56c-.244-1.039-.398-2.136-.456-3.279h2.994v3.057c-.865.034-1.714.102-2.538.222zm2.538 1.776v3.62c-.798-.959-1.458-2.126-1.957-3.456.638-.083 1.291-.136 1.957-.164zm-2.994-7.055c.057-1.128.207-2.212.446-3.239.827.121 1.68.19 2.548.224v3.015h-2.994zm1.024-5.179c.5-1.346 1.165-2.527 1.97-3.496v3.661c-.671-.028-1.329-.081-1.97-.165zm-2.005-.35c-.708-.165-1.377-.373-2.018-.607.937-.918 2.053-1.65 3.29-2.146-.496.844-.927 1.762-1.272 2.753zm-.549 1.918c-.264 1.151-.434 2.36-.492 3.611h-3.933c.165-1.658.739-3.197 1.617-4.518.88.361 1.816.67 2.808.907zm.009 9.262c-.988.236-1.92.542-2.797.9-.89-1.328-1.471-2.879-1.637-4.551h3.934c.058 1.265.231 2.488.5 3.651zm.553 1.917c.342.976.768 1.881 1.257 2.712-1.223-.49-2.326-1.211-3.256-2.115.636-.229 1.299-.435 1.999-.597zm9.924 0c.7.163 1.362.367 1.999.597-.931.903-2.034 1.625-3.257 2.116.489-.832.915-1.737 1.258-2.713zm.553-1.917c.27-1.163.442-2.386.501-3.651h3.934c-.167 1.672-.748 3.223-1.638 4.551-.877-.358-1.81-.664-2.797-.9zm.501-5.651c-.058-1.251-.229-2.46-.492-3.611.992-.237 1.929-.546 2.809-.907.877 1.321 1.451 2.86 1.616 4.518h-3.933z" /></symbol></svg>
-                    </div>
+                    <GoogleTranslateElement />
                   </div>
                 </li>
                 <li className="ma__utility-nav__item">
@@ -1116,5 +1102,93 @@ const HeaderMixed = () => {
     </header>
   );
 };
+
+HeaderMixed.MenuBar = ({ as = 'ul', children, ...rest }) => {
+  const Component = as;
+  return(
+    <Component {...rest} role="menubar">
+      {children}
+    </Component>
+  );
+};
+HeaderMixed.Menu = ({ as = 'ul', children, MenuItem = HeaderMixed.MenuItem, ...rest }) => {
+  const Component = as;
+  return(
+    <Component {...rest} role="menu">
+      {children}
+    </Component>
+  );
+};
+
+HeaderMixed.Link = ({ as = 'a', href, children, ...rest }) => {
+  const Component = as;
+  return(
+    <Component {...rest} href={href} role="menuitem">
+      {children}
+    </Component>
+  );
+};
+
+HeaderMixed.MenuItem = ({
+  as = 'li',
+  children,
+  ...rest
+}) => {
+  const Component = as;
+  return(
+    <Component {...rest} role="none">
+      {children}
+    </Component>
+  );
+};
+
+HeaderMixed.SubMenu = ({
+  as = 'div',
+  Menu = HeaderMixed.Menu,
+  MenuItem = HeaderMixed.MenuItem,
+  children,
+  ...rest
+}) => {
+  const Component = as;
+  return(
+    <Component {...rest} role="none">
+      {children}
+    </Component>
+  );
+};
+
+HeaderMixed.NavItem = ({
+  as = 'li',
+  MenuItem = HeaderMixed.MenuItem,
+  Menu = HeaderMixed.Menu,
+  SubMenu = HeaderMixed.SubMenu,
+  children,
+  ...rest
+}) => {
+  const Component = as;
+  return(
+    <Component {...rest} role="none">
+      <button type="button" role="menuitem" id="button1" className="ma__main__hamburger-nav__top-link js-main-nav-hamburger__top-link" aria-expanded="false" aria-haspopup="true">
+        <span className="visually-hidden show-label">Show the sub topics of </span>
+        Living
+        <span className="toggle-indicator" aria-hidden="true" />
+      </button>
+      { children }
+    </Component>
+  );
+};
+HeaderMixed.Button = ({ children, ...rest }) => (
+  <button {...rest} type="button" role="menuitem" className="ma__main-nav__top-link" aria-haspopup="true" aria-expanded="false" tabIndex="0">
+    <span className="visually-hidden show-label">Show the sub topics of </span>
+    {children}
+  </button>
+);
+HeaderMixed.HamburgerButton = ({ children, ...rest }) => (
+  <button {...rest} type="button" role="menuitem" className="ma__main__hamburger-nav__top-link js-main-nav-hamburger__top-link" aria-expanded="false" aria-haspopup="true">
+    <span className="visually-hidden show-label">Show the sub topics of </span>
+    {children}
+    <span className="toggle-indicator" aria-hidden="true" />
+  </button>
+);
 
 export default HeaderMixed;
