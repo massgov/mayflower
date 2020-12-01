13027d877a5db2b3ddb0f93d7e4916fab1a5dfe5
Minghua Sun <clairesunstudio@gmail.com>
2020-12-01 17:33:13 -0500

    add place holder

--- packages/core/stories/components/Template/Template.stories.js
+++ packages/core/stories/components/Template/Template.stories.js
@@ -8,21 +8,38 @@ const { STORYBOOK_CDN_PATH } = process.env;
 
 import { attachHTML } from '../../util/renderCode';
 
-const templateSlim = (
+const template = (renderHeader, renderFooter) => (
   <div id="body-wrapper">
-    {headerStories.headerSlimmest()}
-      <main id="main-content">
-        <div className="main-content main-content--two">
-          <div className="page-content">
-            {
-              // place your content here
-            }
+    {renderHeader}
+    <main id="main-content">
+      <div className="pre-content sp--bottom">
+        <div className="ma__placeholder">
+          Pre Content
+        </div>
+      </div>
+      <div className="main-content main-content--two">
+        <div className="page-content">
+          <div className="ma__placeholder">
+            Main Content
           </div>
         </div>
-      </main>
-    {footerStories.footerSlim()}
+        <aside className="sidebar">
+          <div className="ma__placeholder">
+            Side Content
+          </div>
+        </aside>
+      </div>
+    </main>
+    <div className="ma__placeholder">
+      Post Content
+    </div>
+    {renderFooter}
   </div>
 )
+
+const templateSlim = template(headerStories.headerSlimmest(), footerStories.footerSlim())
+
+const templateFullNav = template(headerStories.headerFullNav(), footerStories.footerFullNav())
 
 const notesTemplateSlim = `
   // Link to CSS:
@@ -34,5 +51,19 @@ const notesTemplateSlim = `
   </head>
 `
 
+const notesTemplate = `
+  // Link to CSS:
+  <head>
+    <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/general.css">
+    <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/layout.css">
+    <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/header.css">
+    <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/footer.css">
+  </head>
+`
+
 export const templateBasic = () => templateSlim;
 attachHTML(templateBasic, templateSlim, notesTemplateSlim)
+
+
+export const templateFull = () => templateFullNav;
+attachHTML(templateBasic, templateFullNav, notesTemplateSlim)
