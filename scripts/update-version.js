903a8c7e67713cb54b6c937f4a1e4676791e7e73
Minghua Sun <clairesunstudio@gmail.com>
2020-12-02 17:09:53 -0500

    Revert "apply review suggestions"
    This reverts commit c55cec382c315af1ede5bcc0844cd5e215408462.

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,15 +1,16 @@
 const path = require('path');
-const fs = require('fs').promises;
+const fs = require('fs');
 
-module.exports = async function updateCoreVersion(newVer) {
+module.exports = function updateCoreVersion(newVer) {
   const filePath = `${path.resolve(__dirname, '../packages/core')}/.env`;
 
-  try {
-    const content = fs.readFileSync(filePath).toString();
-    const regex = /[0-9]+\.[0-9]+\.[0-9]+/g;
-    const newContent = content.replace(regex, newVer)
-    await fs.writeFile(filePath, newContent);
-  } catch(error) {
-    throw error;
-  }
+  const content = fs.readFileSync(filePath).toString();
+
+
+  const regex = /[0-9]+\.[0-9]+\.[0-9]+/g;
+  const newContent = content.replace(regex, newVer)
+
+  fs.writeFileSync(filePath, newContent, (err) => {
+    if (err) throw err;
+  })
 }
