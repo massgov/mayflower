0e247da7aff11aeb6e8892e608e2ffb083437a53
tkoleary <terrenceoleary@gmail.com>
2020-12-14 11:49:32 -0600

    initial fixes remove svg

---
+++ packages/assets/font-compression-scripts.py
@@ -0,0 +1,41 @@
+pyftsubset\
+  NotoSans.ttf \
+  --output-file="NotoSans.woff" \
+  --flavor=woff \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
+
+pyftsubset\
+  NotoSansItalic.ttf \
+  --output-file="NotoSansItalic.woff" \
+  --flavor=woff \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
+
+pyftsubset\
+  NotoSans-VF.ttf \
+  --output-file="NotoSans-VF.woff" \
+  --flavor=woff \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
+
+pyftsubset\
+  NotoSansItalic-VF.ttf \
+  --output-file="NotoSansItalic-VF.woff" \
+  --flavor=woff \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
+
+pyftsubset\
+  NotoSans-VF.ttf \
+  --output-file="NotoSans-VF.woff2" \
+  --flavor=woff2 \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
+
+pyftsubset\
+  NotoSansItalic-VF.ttf \
+  --output-file="NotoSansItalic-VF.woff2" \
+  --flavor=woff2 \
+  --layout-features="kern,liga,clig,tnum"\
+  --unicodes="U+0000-00FF"
