3204c93eac490dde696916a8905c42d82a706bff
Minghua Sun <clairesunstudio@gmail.com>
2020-12-14 13:29:05 -0500

    wip

--- packages/core/stories/util/renderCode.js
+++ packages/core/stories/util/renderCode.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { Story, DocsContext } from '@storybook/addon-docs/blocks';
 import prettier from 'prettier/standalone';
 import parserHtml from 'prettier/parser-html';
 
@@ -20,31 +18,25 @@ export const renderReactMarkup = (component, notes) => {
       plugins: [parserHtml]
     });
   // Replaces the path to the state seal with the latest assets CDN.
-  return prettyMarkup.replace(/static\/media\/stateseal?([a-z|\-]*)\.(.*)\.png/g, `${STORYBOOK_CDN_PATH}/static/images/logo/stateseal$1.png`);
+  return prettyMarkup.replace(/static\/media\/stateseal?([a-z|-]*)\.(.*)\.png/g, `${STORYBOOK_CDN_PATH}/static/images/logo/stateseal$1.png`);
 };
 
-// can't pass DocsContext from .mdx
-// export const StoryCode = ({ children }) => (
-//   <Story
-//     name="test"
-//     parameters={{ docs: { transformSource: (src) => renderReactMarkup(children) } }}
-//   >
-//     {children}
-//   </Story>
-// )
-
-export const attachCSS = (story, css) => story.parameters = {
-  docs: {
-    source: {
-      code: css
+export function attachCSS(story, css) {
+  story.parameters = {
+    docs: {
+      source: {
+        code: css
+      }
     }
-  }
-};
+  };
+}
 
-export const attachHTML = (story, Component, notes) => story.parameters = {
-  docs: {
-    source: {
-      code: renderReactMarkup(Component, notes)
+export function attachHTML(story, Component, notes) {
+  story.parameters = {
+    docs: {
+      source: {
+        code: renderReactMarkup(Component, notes)
+      }
     }
-  }
-};
+  };
+}
