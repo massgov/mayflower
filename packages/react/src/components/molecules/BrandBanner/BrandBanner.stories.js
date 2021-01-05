df62ec3e005f8507c483894534442ad70b7598b5
Minghua Sun <clairesunstudio@gmail.com>
2021-01-05 17:06:26 -0500

    initial commit

---
+++ packages/react/src/components/molecules/BrandBanner/BrandBanner.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StoryPage } from 'StorybookConfig/preview';
+import { action } from '@storybook/addon-actions';
+
+import BrandBanner from './index';
+
+export const BrandBannerExample = (args) => <BrandBanner {...args} />;
+
+BrandBannerExample.storyName = 'Default';
+BrandBannerExample.args = {
+  hasSeal: true,
+  text: 'An official website of the Commonwealth of Massachusetts'
+};
+BrandBannerExample.argTypes = {
+  hasSeal: {
+    control: {
+      type: 'boolean'
+    }
+  }
+};
+
+export default {
+  title: 'molecules/BrandBanner',
+  component: BrandBanner,
+  parameters: {
+    docs: {
+      page: () => <StoryPage />
+    }
+  }
+};
---
+++ packages/react/src/components/molecules/BrandBanner/index.js
@@ -0,0 +1,38 @@
+/**
+ * BrandBanner module.
+ * @module @massds/mayflower-react/BrandBanner
+ * @requires module:@massds/mayflower-assets/scss/02-molecules/arrow-nav
+ * @requires module:@massds/mayflower-assets/scss/01-atoms/arrow-button
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Image from 'MayflowerReactMedia/Image';
+import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
+
+const BrandBanner = ({ 
+  hasSeal,
+  text='An official website of the Commonwealth of Massachusetts'
+}) => {
+  const sectionClasses = classNames({
+    'js-clickable-link': true,
+    'ma__arrow-nav': true
+  });
+  return(
+    <div className="ma__brand-banner">
+      {hasSeal && <Image className="ma__brand-banner-logo" src={logo}/>}
+      {text}
+    </div>
+  );
+};
+
+BrandBanner.propTypes = {
+  /** Banner text */
+  text: PropTypes.string,
+  /** Whether to include seal */
+  hasSeal: PropTypes.bool
+};
+
+
+export default BrandBanner;
