0ce0a940d184c84c14392dc1e2cbfd8b79b539e5
Minghua Sun <clairesunstudio@gmail.com>
2021-01-06 18:18:30 -0500

    cleanup

--- packages/react/src/components/molecules/BrandBanner/BrandBanner.stories.js
+++ packages/react/src/components/molecules/BrandBanner/BrandBanner.stories.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { StoryPage } from 'StorybookConfig/preview';
-import { action } from '@storybook/addon-actions';
 
 import BrandBanner from './index';
 
 export const BrandBannerExample = (args) => <BrandBanner {...args} />;
 
 BrandBannerExample.storyName = 'Default';
-BrandBannerExample.args = {
-  hasSeal: true,
-  hasToggle: true,
-  text: 'An official website of the Commonwealth of Massachusetts'
-};
 
 export default {
   title: 'molecules/BrandBanner',
