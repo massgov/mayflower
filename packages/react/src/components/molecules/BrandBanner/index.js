3117a44a03df00243431880bfeb59a958fdaf03e
Minghua Sun <clairesunstudio@gmail.com>
2021-01-06 16:47:52 -0500

    add color themes

--- packages/react/src/components/molecules/BrandBanner/index.js
+++ packages/react/src/components/molecules/BrandBanner/index.js
@@ -6,6 +6,7 @@
  */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import Image from 'MayflowerReactMedia/Image';
 import ButtonWithIcon from 'MayflowerReactButtons/ButtonWithIcon';
@@ -17,15 +18,20 @@ import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
 const BrandBanner = ({
   hasSeal = true,
   hasToggle = true,
+  bgTheme= 'light',
+  bgColor= 'c-primary',
   text = 'An official website of the Commonwealth of Massachusetts'
 }) => {
+  const brandBannerClasses = classNames('ma__brand-banner', {
+    [`ma__brand-banner--${bgColor}-bg-${bgTheme}`]: bgColor && bgTheme
+  })
   const [expanded, setExpanded] = useState(false);
 
   const handleOnToggle = () => {
     setExpanded(!expanded);
   };
   return(
-    <div className="ma__brand-banner">
+    <div className={brandBannerClasses}>
       <div className="ma__brand-banner-container">
         {hasSeal && <Image className="ma__brand-banner-logo" src={logo} />}
         <span>
@@ -85,7 +91,11 @@ BrandBanner.propTypes = {
   /** Whether to include seal */
   hasSeal: PropTypes.bool,
   /** Whether to render the toggle button and content */
-  hasToggle: PropTypes.bool
+  hasToggle: PropTypes.bool,
+  /** Background color option */
+  bgColor: PropTypes.oneOf(['', 'c-primary', 'c-primary-alt', 'c-highlight', 'c-gray-dark']),
+  /** Background color theme */
+  bgTheme: PropTypes.oneOf(['', 'light', 'dark'])
 };
 
 export default BrandBanner;
