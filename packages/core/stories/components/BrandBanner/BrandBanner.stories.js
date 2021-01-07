903f505f33ce62aae4692845031d1058649bfac1
Minghua Sun <clairesunstudio@gmail.com>
2021-01-07 15:41:55 -0500

    add stories

--- packages/core/stories/components/BrandBanner/BrandBanner.stories.js
+++ packages/core/stories/components/BrandBanner/BrandBanner.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BrandBanner from '@massds/mayflower-react/dist/BrandBanner';
 import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
+import logoWhite from '@massds/mayflower-assets/static/images/logo/stateseal-white.png';
 
 import { attachHTML } from '../../util/renderCode';
 
@@ -10,4 +11,25 @@ const brandBannerBasic = (
   <BrandBanner seal={logo} hasToggle={false} />
 );
 
+const brandBannerLightTheme = (
+    <>
+        <BrandBanner seal={logo} hasToggle={false} />
+        <BrandBanner seal={logo} hasToggle={false} bgTheme="light" bgColor="c-primary-alt" />
+        <BrandBanner seal={logo} hasToggle={false} bgTheme="light" bgColor="c-gray" />
+    </>
+  );
+
+
+const brandBannerDarkTheme = (
+<>
+    <BrandBanner seal={logoWhite} hasToggle={false} bgTheme="dark" />
+    <BrandBanner seal={logoWhite} hasToggle={false} bgTheme="dark" bgColor="c-primary-alt" />
+    <BrandBanner seal={logoWhite} hasToggle={false} bgTheme="dark" bgColor="c-gray" />
+</>
+);
+
 export const brandBannerExample = () => brandBannerBasic;
+
+export const brandBannerLight= () => brandBannerLightTheme;
+
+export const brandBannerDark= () => brandBannerDarkTheme;
