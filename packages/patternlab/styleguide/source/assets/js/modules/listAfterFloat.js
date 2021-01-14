2d5fd0ebeadfb83eb6c6217b70e1d676f1e2310d
Yukiko Gannett <yukikogannett@gmail.com>
2021-01-14 13:13:51 -0500

    override the right margin of a left float figure compoenent

--- packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
+++ packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
@@ -1,14 +1,21 @@
-// Adjust the position of bullets and numbers of lists after a floated element to avoid overwrapping in rich text.
+// Adjust the position of bullets and numbers of lists after a floated figure
+// component to avoid overwrapping in rich text.
+//
+// Note:  Left floated elements overwrap their followed list items, specifically
+//        their bullets. The issue only occurs with left float.  The issue is not
+//        fixable by adjusting left side spacing of those lists since the floated
+//        element's right margin defines their positioning.
+//        Solving the issue by adding extra right margin to left float figure
+//        components without affecting the wrapping of following elements.
+
 const figureElements = document.querySelectorAll(".ma__rich-text .ma__figure");
-// const orderedLists = document.querySelectorAll(".ma__rich-text ol");
-// const unorderedLists = document.querySelectorAll(".ma__rich-text ul");
 
 figureElements.forEach(function (figure) {
-  let floatValue = window.getComputedStyle(figure, null).float;
+  if (!figure.previousElementSibling.classList.contains("ma__figure")) {
+    let floatDirection = window.getComputedStyle(figure, null).float;
 
-  if(floatValue === "left" && !figure.nextElementSibling.classList.contains(".ma__figure")) {
-  //   // figure.style.
-    console.log(figure.nextElementSibling.tagName);
+    if(floatDirection === "left" && !figure.nextElementSibling.classList.contains("ma__figure")) {
+      figure.style.cssText = "margin-right: 50px;";
+    }
   }
-  // console.log(figure.nextElementSibling.tagName);
 });
