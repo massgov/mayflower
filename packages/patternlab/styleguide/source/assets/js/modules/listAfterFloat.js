f61196244e1cd514e4a37c8daba6820d8d34c807
Yukiko Gannett <yukikogannett@gmail.com>
2021-01-27 17:11:30 -0500

    add left floated image adjustment

--- packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
+++ packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
@@ -1,14 +1,15 @@
 // Adjust the position of bullets and numbers of lists after a floated figure
-// component to avoid overwrapping in rich text.
+// component or image to avoid overwrapping in rich text.
 //
 // Note:  Left floated elements overwrap their followed list items, specifically
 //        their bullets. The issue only occurs with left float.  The issue is not
 //        fixable by adjusting left side spacing of those lists since the floated
 //        element's right margin defines their positioning.
-//        Solving the issue by adding extra right margin to left float figure
-//        components without affecting the wrapping of following elements.
+//        Solving the issue by adding extra right margin to left float components
+//        without affecting the wrapping feature of their following elements.
 
 const leftFloatFigures = document.querySelectorAll(".ma__rich-text .ma__figure--left");
+const leftFloatImages = document.querySelectorAll(".ma__rich-text article.align-left");
 
 if (window.screen.width > 620) {
   addInlineMargin ();
@@ -19,33 +20,32 @@ window.addEventListener('resize', () => {
     addInlineMargin ();
   } else {
     // Remove the inline style with no float with screen width under 620px.
+
     leftFloatFigures.forEach( (figure) => {
       if (figure.hasAttribute("style")) {
         figure.removeAttribute("style");
+      }
+    });
+
+    leftFloatImages.forEach( (image) => {
+      if (image.hasAttribute("style")) {
+        image.removeAttribute("style");
       }
     });
   }
 });
 
 function addInlineMargin () {
+  // Figure components
   leftFloatFigures.forEach( (figure) => {
     //  Check if the previous sibling is not figure component.
-
-    // In Drupal, rich text elements are spreaded into multiple .ma__rich-text
-    // containers. Figure components are in their own .ma__rich-text containers.
-    // They are never be with other components like lists and paragraphs in a
-    // same container.
     if (!figure.parentElement.previousElementSibling.classList.contains("ma__figure")) {
       figure.style.cssText = "margin-right: 50px;";
     }
+  });
 
-    // This is for Mayflower samples.
-    // In Mayflower samples, one .ma__rich-text container contains all rich
-    // text components in a section.
-    if (!figure.previousElementSibling.classList.contains("ma__figure")) {
-      if(!figure.nextElementSibling.classList.contains("ma__figure")) {
-        figure.style.cssText = "margin-right: 50px;";
-      }
-    }
+  // article image embed
+  leftFloatImages.forEach( (image) => {
+    image.style.cssText = "margin-right: 50px;";
   });
 }
