b5eb5cfbaf093c98cfd6426553476cc31cf4b15e
Minghua Sun <clairesunstudio@gmail.com>
2021-02-25 17:45:39 -0500

    toggle button style and cleanup

--- packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
+++ packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
@@ -1,7 +1,8 @@
-export default (function (window,document) {
-    var button = document.getElementsByClassName('ma__brand-banner-button')[0];
+export default (function (document) {
+    const button = document.getElementsByClassName('ma__brand-banner-button')[0];
     button.onclick = function() {
+        this.classList.toggle("ma__button-icon--expanded");
         const expansionDiv = document.getElementsByClassName('ma__brand-banner-expansion')[0];
         expansionDiv.classList.toggle("ma__brand-banner-expansion--expanded");
     }
-  })(window,document);
+  })(document);
