4988a4c00eba8b41d8a3947492c41d112bcd8d7f
Minghua Sun <clairesunstudio@gmail.com>
2021-02-26 16:55:02 -0500

    bug fix

--- packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
+++ packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
@@ -1,8 +1,10 @@
 export default (function (document) {
     const button = document.getElementsByClassName('ma__brand-banner-button')[0];
-    button.onclick = function() {
-        this.classList.toggle("ma__button-icon--expanded");
-        const expansionDiv = document.getElementsByClassName('ma__brand-banner-expansion')[0];
-        expansionDiv.classList.toggle("ma__brand-banner-expansion--expanded");
+    if(button) {
+        button.onclick = function() {
+            this.classList.toggle("ma__button-icon--expanded");
+            const expansionDiv = document.getElementsByClassName('ma__brand-banner-expansion')[0];
+            expansionDiv.classList.toggle("ma__brand-banner-expansion--expanded");
+        }
     }
   })(document);
