6fc75e502852fc63aa5c9bba260abf80dfc6d25e
Minghua Sun <clairesunstudio@gmail.com>
2021-03-08 19:47:24 -0500

    convert TypeAheadDropdown into a functional component

--- packages/react/src/components/forms/TypeAheadDropdown/index.js
+++ packages/react/src/components/forms/TypeAheadDropdown/index.js
@@ -7,151 +7,147 @@
  * @requires module:@massds/mayflower-assets/scss/01-atoms/svg-icons
  * @requires module:@massds/mayflower-assets/scss/01-atoms/svg-loc-icons
  */
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ButtonWithIcon from 'MayflowerReactButtons/ButtonWithIcon';
 import InputTextFuzzy from 'MayflowerReactForms/InputTextFuzzy';
 // eslint-disable-next-line import/no-unresolved
 import IconChevron from 'MayflowerReactBase/Icon/IconChevron';
 
-class TypeAheadDropdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      buttonText: this.props.dropdownButton.text,
-      buttonExpand: false
-    };
+const TypeAheadDropdown = (props) => {
+    const [buttonExpand, setButtonExpand] = useState(false);
+    const [buttonText, setButtonText] = useState(props.dropdownButton.text);
+    const [buttonClicked, setButtonClicked] = useState();
+    const buttonRef = useRef();
+    const wrapperRef = useRef();
 
-    this.wrapperRef = React.createRef();
-    this.setDropDownButtonRef = this.setDropDownButtonRef.bind(this);
-    this.handleRefMouseDown = this.handleRefMouseDown.bind(this);
+    // this.wrapperRef = React.createRef();
+    // this.setDropDownButtonRef = this.setDropDownButtonRef.bind(this);
+    // this.handleRefMouseDown = this.handleRefMouseDown.bind(this);
 
-    this.closeDropdown = this.closeDropdown.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-  }
+    // closeDropdown = closeDropdown.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+    // this.handleKeyDown = this.handleKeyDown.bind(this);
+    // this.handleInputBlur = this.handleInputBlur.bind(this);
+    // this.handleSelect = this.handleSelect.bind(this);
+    // handleClickOutside = handleClickOutside.bind(this);
+  
 
-  componentDidMount() {
-    this.buttonClicked = false;
-
-    document.addEventListener('mousedown', this.handleClickOutside);
-    this.dropDownButtonRef.addEventListener('mousedown', this.handleRefMouseDown);
-  }
+  // componentDidMount() {
+  //   this.buttonClicked = false;
+  // }
 
   // eslint-disable-next-line camelcase
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const selectedValue = nextProps.inputText.selected;
-    if (selectedValue !== undefined) {
-      this.setState({
-        buttonText: selectedValue,
-        buttonExpand: false
-      });
-    }
+  // UNSAFE_componentWillReceiveProps(nextProps) {
+  //   const selectedValue = nextProps.inputText.selected;
+  //   if (selectedValue !== undefined) {
+  //     setButtonText(selectedValue);
+  //     setButtonExpand(false);
+  //   }
+  // }
+  const handleRefMouseDown = () => {
+    setButtonClicked(true);
   }
 
-  componentDidUpdate() {
-    this.buttonClicked = false;
+  const handleClick = () => {
+    setButtonExpand(!buttonExpand);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-    this.dropDownButtonRef.removeEventListener('mousedown', this.handleRefMouseDown);
-  }
-
-  handleRefMouseDown() {
-    this.buttonClicked = true;
-  }
-
-  handleClick() {
-    this.setState((prevState) => ({ buttonExpand: !prevState.buttonExpand }));
-  }
-
-  handleKeyDown(event) {
+  const handleKeyDown = (event) => {
     // If the user pressed escape, or pressed enter with nothing selected close
     // the panel.
     if ((event.key === 'Escape')
         || (event.key === 'Enter' && event.target.value === '')) {
-      this.closeDropdown();
+      closeDropdown();
     }
-    if (event.key === 'Escape' && this.dropDownButtonRef) {
-      this.dropDownButtonRef.focus();
+    if (event.key === 'Escape' && buttonRef) {
+      buttonRef.focus();
     }
-    if (typeof this.props.onKeyDown === 'function') {
-      this.props.onKeyDown(event);
-    }
-  }
-
-  handleInputBlur() {
-    if (!this.buttonClicked) {
-      this.closeDropdown();
+    if (typeof props.onKeyDown === 'function') {
+      props.onKeyDown(event);
     }
   }
 
-  handleSelect(event, { suggestion }) {
+  const handleInputBlur = () => {
+    if (buttonClicked) {
+      closeDropdown();
+    }
+  }
+
+  const handleSelect = (event, { suggestion }) => {
     // Stop the filters form submission if enter is pressed in the selector.
     event.preventDefault();
     // Update this component state and pass the event out to the calling code.
     const { text } = suggestion.item;
     if (text.length > 0) {
-      this.setState({
-        buttonText: text,
-        buttonExpand: false
-      });
-      if (typeof this.props.inputText.onSuggestionClick === 'function') {
-        this.props.inputText.onSuggestionClick(event, { suggestion });
+      setButtonText(text);
+      setButtonExpand(false);
+      if (typeof props.inputText.onSuggestionClick === 'function') {
+        props.inputText.onSuggestionClick(event, { suggestion });
       }
     }
   }
 
-  handleClickOutside(event) {
+  const handleClickOutside = (event) => {
     // Close the panel if the user clicks outside the component.
-    const node = this.wrapperRef.current;
+    const node = wrapperRef.current;
     if ((node && !node.contains(event.target))) {
-      if (this.state.buttonExpand) {
-        this.setState({ buttonExpand: false });
+      if (buttonExpand) {
+        setButtonExpand(false);
       }
     }
   }
 
-  setDropDownButtonRef(node) {
-    this.dropDownButtonRef = node;
+  const closeDropdown = () => {
+    setButtonExpand(false);
   }
 
-  closeDropdown() {
-    this.setState({ buttonExpand: false });
-  }
+  useEffect(() => {
+    const selectedValue = props.inputText.selected;
+    if (selectedValue !== undefined) {
+      setButtonText(selectedValue);
+      setButtonExpand(false);
+    }
+  }, [props.inputText.selected]);
 
-  render() {
-    const dropdownButton = {
-      onClick: this.handleClick,
-      setButtonRef: this.setDropDownButtonRef,
-      expanded: this.state.buttonExpand,
-      icon: <IconChevron height={20} width={20} />,
-      size: '',
-      ...this.props.dropdownButton
+  useEffect(() => {
+    setButtonClicked(false);
+    document.addEventListener('mousedown', handleClickOutside);
+    buttonRef.current.addEventListener('mousedown', handleRefMouseDown);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      buttonRef.current.removeEventListener('mousedown', handleRefMouseDown);
     };
-    dropdownButton.text = this.state.buttonText || dropdownButton.text;
+  });
 
-    const inputTextFuzzyProps = {
-      ...this.props.inputText,
-      onKeyDown: this.handleKeyDown,
-      onSuggestionClick: this.handleSelect,
-      onBlur: this.handleInputBlur,
-      autoFocusInput: true
-    };
 
-    return(
-      <div ref={this.wrapperRef}>
-        <ButtonWithIcon {...dropdownButton} />
-        {this.state.buttonExpand && (
-          <InputTextFuzzy {...inputTextFuzzyProps} />
-        )}
-      </div>
-    );
-  }
+
+  const dropdownButton = {
+    onClick: handleClick,
+    setButtonRef: buttonRef,
+    expanded: buttonExpand,
+    icon: <IconChevron height={20} width={20} />,
+    size: '',
+    ...props.dropdownButton
+  };
+  dropdownButton.text = buttonText || dropdownButton.text;
+
+  const inputTextFuzzyProps = {
+    ...props.inputText,
+    onKeyDown: handleKeyDown,
+    onSuggestionClick: handleSelect,
+    onBlur: handleInputBlur,
+    autoFocusInput: true
+  };
+
+  return(
+    <div ref={wrapperRef}>
+      <ButtonWithIcon {...dropdownButton} />
+      {buttonExpand && (
+        <InputTextFuzzy {...inputTextFuzzyProps} />
+      )}
+    </div>
+  );
 }
 
 TypeAheadDropdown.propTypes = {
