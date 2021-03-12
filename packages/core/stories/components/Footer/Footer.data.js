311c9fdfdd6a66c2ee4f5c6af8b449889000273e
Minghua Sun <clairesunstudio@gmail.com>
2021-03-12 13:26:00 -0500

    address comment

--- packages/core/stories/components/Footer/Footer.data.js
+++ packages/core/stories/components/Footer/Footer.data.js
@@ -5,23 +5,23 @@ export default {
       id: 'FooterLinks1',
       links: [
         {
-          href: 'https://mass.gov/topics/living',
+          href: 'https://www.mass.gov/topics/living',
           text: 'Living'
         },
         {
-          href: 'https://mass.gov/topics/working',
+          href: 'https://www.mass.gov/topics/working',
           text: 'Working'
         },
         {
-          href: 'https://mass.gov/topics/learning',
+          href: 'https://www.mass.gov/topics/learning',
           text: 'Learning'
         },
         {
-          href: 'https://mass.gov/topics/exploring-massachusetts',
+          href: 'https://www.mass.gov/topics/exploring-massachusetts',
           text: 'Visiting & Exploring'
         },
         {
-          href: 'https://mass.gov/topics/your-government',
+          href: 'https://www.mass.gov/topics/your-government',
           text: 'Your Government'
         }
       ]
@@ -31,11 +31,11 @@ export default {
       id: 'FooterLinks2',
       links: [
         {
-          href: 'https://mass.gov/site-policies',
+          href: 'https://www.mass.gov/site-policies',
           text: 'Site Policies'
         },
         {
-          href: 'https://mass.gov/topics/public-records-requests',
+          href: 'https://www.mass.gov/topics/public-records-requests',
           text: 'Public Records Requests'
         }
       ]
