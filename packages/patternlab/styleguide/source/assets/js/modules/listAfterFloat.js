4e7152dda0d51a25109ff397a10213c74dbfec5a
Yukiko Gannett <yukikogannett@gmail.com>
2021-03-15 17:34:50 -0400

    test edit1

--- packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
+++ packages/patternlab/styleguide/source/assets/js/modules/listAfterFloat.js
@@ -9,66 +9,87 @@
 //        without affecting the wrapping feature of their following elements.
 
 const leftFloatFigures = document.querySelectorAll(".ma__rich-text .ma__figure--left");
-const leftFloatImageFigures = document.querySelectorAll(".ma__rich-text .ma__figure--left.ma__image.ma__figure--x-small");
-const rightFloatFigures = document.querySelectorAll(".ma__rich-text .ma__figure--right");
+// const leftFloatImageFigures = document.querySelectorAll(".ma__rich-text .ma__figure--left.ma__image.ma__figure--x-small");
+// const rightFloatFigures = document.querySelectorAll(".ma__rich-text .ma__figure--right");
 const leftFloatImages = document.querySelectorAll(".ma__rich-text article.align-left");
 
 if (window.screen.width > 620) {
-  addInlineImageMargin (leftFloatFigures, "right");
-  addInlineImageMargin (leftFloatImages, "right");
+  addInlineMargin ();
+  // addInlineImageMargin (leftFloatFigures, "right");
+  // addInlineImageMargin (leftFloatImages, "right");
 }
 
-if (window.screen.width > 480) {
-  addInlineImageMargin (leftFloatImageFigures, "right");
-  addInlineImageMargin (rightFloatFigures, "left");
-}
+// if (window.screen.width > 480) {
+//   addInlineImageMargin (leftFloatImageFigures, "right");
+//   addInlineImageMargin (rightFloatFigures, "left");
+// }
 
 window.addEventListener('resize', () => {
   if (window.screen.width > 620) {
-    addInlineImageMargin (leftFloatFigures, "right");
-    addInlineImageMargin (leftFloatImages, "right");
+    addInlineMargin ();
+    // addInlineImageMargin (leftFloatFigures, "right");
+    // addInlineImageMargin (leftFloatImages, "right");
   } else {
     // Remove the inline style with no float with screen width under 620px.
-    removeInlineImageMargin(leftFloatFigures);
-    removeInlineImageMargin(leftFloatImages);
+    leftFloatFigures.forEach( (figure) => {
+      if (figure.hasAttribute("style")) {
+        figure.removeAttribute("style");
+      }
+    });
+
+    leftFloatImages.forEach( (image) => {
+      if (image.hasAttribute("style")) {
+        image.removeAttribute("style");
+      }
+    });
+    // removeInlineImageMargin(leftFloatFigures);
+    // removeInlineImageMargin(leftFloatImages);
   }
 
-  if (window.screen.width > 480) {
-    addInlineImageMargin(leftFloatImageFigures, "right");
-    addInlineImageMargin(rightFloatFigures, "left");
-  } else {
-    // Remove the inline style with no float with screen width under 481px.
-    removeInlineImageMargin(leftFloatImageFigures);
-    removeInlineImageMargin(rightFloatFigures);
+  // if (window.screen.width > 480) {
+  //   addInlineMargin ();
+  //   addInlineImageMargin(leftFloatImageFigures, "right");
+  //   addInlineImageMargin(rightFloatFigures, "left");
+  // } else {
+  //   // Remove the inline style with no float with screen width under 481px.
+  //   removeInlineImageMargin(leftFloatImageFigures);
+  //   removeInlineImageMargin(rightFloatFigures);
 
-  }
+  // }
 });
 
-function addInlineImageMargin (el, direction) {
+function addInlineImageMargin () {
+// function addInlineImageMargin (el, direction) {
   // Figure components
-  el.forEach( (figure) => {
-    //  Check if the previous sibling is not figure component.
-    if (figure.parentElement.previousElementSibling !== null && !figure.parentElement.previousElementSibling.classList.contains("ma__figure")) {
-      figure.style.cssText = "margin-" + direction + ": 50px;";
-    }
-  });
+  if (!figure.parentElement.previousElementSibling.classList.contains("ma__figure")) {
+    figure.style.cssText = "margin-right: 50px;";
+  }
+  // el.forEach( (figure) => {
+  //   //  Check if the previous sibling is not figure component.
+  //   if (figure.parentElement.previousElementSibling !== null && !figure.parentElement.previousElementSibling.classList.contains("ma__figure")) {
+  //     figure.style.cssText = "margin-" + direction + ": 50px;";
+  //   }
+  // });
 
   // article image embed
-  el.forEach( (image) => {
-    image.style.cssText = "margin-" + direction + ": 50px;";
+  leftFloatImages.forEach( (image) => {
+    image.style.cssText = "margin-right: 50px;";
   });
+  // el.forEach( (image) => {
+  //   image.style.cssText = "margin-" + direction + ": 50px;";
+  // });
 }
 
-function removeInlineImageMargin (el) {
-  el.forEach( (figure) => {
-    if (figure.hasAttribute("style")) {
-      figure.removeAttribute("style");
-    }
-  });
+// function removeInlineImageMargin (el) {
+//   el.forEach( (figure) => {
+//     if (figure.hasAttribute("style")) {
+//       figure.removeAttribute("style");
+//     }
+//   });
 
-  el.forEach( (image) => {
-    if (image.hasAttribute("style")) {
-      image.removeAttribute("style");
-    }
-  });
-}
+//   el.forEach( (image) => {
+//     if (image.hasAttribute("style")) {
+//       image.removeAttribute("style");
+//     }
+//   });
+// }
