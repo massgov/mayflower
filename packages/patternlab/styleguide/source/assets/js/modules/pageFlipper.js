11e3f6718af55ea8a77ceacd9f386c518c839e59
Yukiko Gannett <yukikogannett@gmail.com>
2021-06-30 12:39:41 +0900

    ensure the visibility on load with screen width 620 and less

--- packages/patternlab/styleguide/source/assets/js/modules/pageFlipper.js
+++ packages/patternlab/styleguide/source/assets/js/modules/pageFlipper.js
@@ -1,24 +1,33 @@
-let arrowButtons = document.querySelectorAll(".ma__arrow-button");
+let arrowNavItems = document.querySelectorAll(".ma__arrow-nav");
 
 function switchVisibility() {
   let width = document.querySelector("body").clientWidth;
   // Make arrow buttons visible to keyboard and screen readers.
   if (width < 621) {
-    arrowButtons.forEach(arrowButton => {
-      if (arrowButton.getAttribute("tabindex") == "-1") {
-        arrowButton.setAttribute("tabindex", "0");
+    arrowNavItems.forEach(item => {
+      if(item.querySelector("span").hasAttribute("aria-hidden")) {
+        item.querySelector("span").removeAttribute("aria-hidden");
+      }
+
+      if (item.querySelector(".ma__arrow-button").getAttribute("tabindex") == "-1") {
+        item.querySelector(".ma__arrow-button").setAttribute("tabindex", "0");
       }
     });
   }
 
   // Hide arrow buttons to keyboard and screen readers.
   if (width > 620) {
-    arrowButtons.forEach(arrowButton => {
-      if (arrowButton.getAttribute("tabindex") == "0") {
-        arrowButton.setAttribute("tabindex", "-1");
+    arrowNavItems.forEach(item => {
+      if(!item.querySelector("span").hasAttribute("aria-hidden")) {
+        item.querySelector("span").setAttribute("aria-hidden", "true");
+      }
+
+      if (item.querySelector(".ma__arrow-button").getAttribute("tabindex") == "0") {
+        item.querySelector(".ma__arrow-button").setAttribute("tabindex", "-1");
       }
     });
   }
 }
 
+switchVisibility();
 window.addEventListener("resize", switchVisibility);
