165aed444016c13561c9f914e2fc09b0c1fe040b
Diego Diestra <ddiestras@gmail.com>
2021-07-02 13:21:43 -0500

    Removing directions click event

--- packages/patternlab/styleguide/source/assets/js/modules/locationListingLeaflet.js
+++ packages/patternlab/styleguide/source/assets/js/modules/locationListingLeaflet.js
@@ -7,19 +7,6 @@ export default (function (window, document, $) {
       let $el = $(this),
       $mapCol = $el.find(".js-location-listing-map");
       sticky.init($mapCol);
-
-      $el.find(".ma__image-promo__directions a").on('click', function(e){
-        e.preventDefault();
-        let parent = $(this).parents('.ma__image-promo');
-        let index = parent.index();
-        $('html, body').animate({
-          scrollTop: $mapCol.offset().top,
-        });
-
-        let maker = window.leafletMarkers[index];
-        window.leafletMap.flyTo(maker.getLatLng());
-        maker.openPopup();
-      });
     });
   }
 
