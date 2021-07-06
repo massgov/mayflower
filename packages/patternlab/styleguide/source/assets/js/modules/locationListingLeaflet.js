4744561b010d3d21fd482a09f1f14ee20f299708
Diego Diestra <ddiestras@gmail.com>
2021-07-06 13:02:36 -0500

    Add hover events

--- packages/patternlab/styleguide/source/assets/js/modules/locationListingLeaflet.js
+++ packages/patternlab/styleguide/source/assets/js/modules/locationListingLeaflet.js
@@ -7,6 +7,32 @@ export default (function (window, document, $) {
       let $el = $(this),
       $mapCol = $el.find(".js-location-listing-map");
       sticky.init($mapCol);
+
+      $el.find(".ma__image-promo").on('click', function(e){
+        if (e.target.href) {
+          e.stopImmediatePropagation();
+          return;
+        }
+
+        e.preventDefault();
+        let index = $(this).index();
+        $('html, body').animate({
+          scrollTop: $mapCol.offset().top,
+        });
+
+        let maker = window.leafletMarkers[index];
+        window.leafletMap.flyTo(maker.getLatLng());
+        maker.openPopup();
+      }).on("mouseenter focusin", function (e) {
+        let index = $(this).index();
+        let maker = window.leafletMarkers[index];
+        $(maker._icon).addClass('ma-highlighted');
+
+      }).on("mouseleave", function (e) {
+        let index = $(this).index();
+        let maker = window.leafletMarkers[index];
+        $(maker._icon).removeClass('ma-highlighted');
+      });
     });
   }
 
