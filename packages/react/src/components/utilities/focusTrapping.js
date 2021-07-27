303848d1444351eba576e92e2b7318e30387ac60
Minghua Sun <clairesunstudio@gmail.com>
2021-07-27 13:38:59 -0400

    cleanup console

--- packages/react/src/components/utilities/focusTrapping.js
+++ packages/react/src/components/utilities/focusTrapping.js
@@ -4,7 +4,6 @@ const isDisplayed = (el) => {
 }
 
 const filterDisplayedElements = (elements) => {
-    console.log(elements)
     if (elements.length > 0) {
         return elements.filter((el) => isDisplayed(el))
     }
@@ -25,7 +24,6 @@ export default ({
     const firstFocusableElement = focusableContent[0]; // get first element to be focused inside modal
     const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
-    console.log(lastFocusableElement)
     let isTabPressed = e.key === 'Tab';
 
     if (!isTabPressed) {
@@ -33,15 +31,11 @@ export default ({
     }
 
     if (e.shiftKey) { // if shift key pressed for shift + tab combination
-        console.log('shift tab')
         if (document.activeElement === firstFocusableElement) {
             lastFocusableElement.focus(); // add focus for the last focusable element
             e.preventDefault();
         }
     } else { // if tab key is pressed
-        console.log('tab')
-        console.log(document.activeElement)
-        console.log(lastFocusableElement)
         if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
             firstFocusableElement.focus(); // add focus for the first focusable element
             e.preventDefault();
