2780153f54f07d9a99d83236cc1874346a596b80
Diego Diestra <ddiestras@gmail.com>
2021-09-29 15:57:36 +0200

    DP-22028 - Updating max bounds

--- packages/patternlab/styleguide/source/assets/js/modules/mapLeaflet.js
+++ packages/patternlab/styleguide/source/assets/js/modules/mapLeaflet.js
@@ -21,8 +21,8 @@ export default (function (window,document) {
     const { map, markers, isStatic=0, hideAttribution=0 } = ma.leafletMapData[i]; // Data object created in @molecules/leaflet-map.twig
 
     // max bounds
-    const corner1 = L.latLng(43.12916191721289, -67.40279674530031); //northEast
-    const corner2 = L.latLng(41.09188542307055, -76.28524303436281); //southWest
+    const corner1 = L.latLng(46.694667307773116, -64.05029296875001); //northEast
+    const corner2 = L.latLng(38.634036452919226, -80.39794921875001); //southWest
     const maxBounds = L.latLngBounds(corner1, corner2);
 
     const mymap = L
@@ -32,10 +32,10 @@ export default (function (window,document) {
         zoomControl: false,
         minZoom: 7,
         scrollWheelZoom: false,
-        dragging: false /*,
+        dragging: false ,
         // maxBounds is disabled due to unexpected shifting when popups are hitting the boundaries.
         // This can be turned back on when the tiles are large enough so that the min zoom level shows the whole state and have enough tiles padded for popups.
-        maxBounds */
+        maxBounds
       });
       
     // if map is not static, add zoom control with custom position
