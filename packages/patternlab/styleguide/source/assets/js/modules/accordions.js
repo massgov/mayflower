7f641b7637622785c184c0488f8cdb868804f172
Javi-er <javier.reartes@lullabot.com>
2021-10-04 12:36:21 -0300

    DP-22657 "What would you like to do" usability adjustments (#1502)
    * DP-22657 usability adjustments
    
    * adjusted changelog.
    
    * adding aria labels to accordion
    
    * Update accordions.js
    
    * Adjusted accesiblity.
    
    * adding aria-expanded to collapsible content

--- packages/patternlab/styleguide/source/assets/js/modules/accordions.js
+++ packages/patternlab/styleguide/source/assets/js/modules/accordions.js
@@ -1,27 +1,49 @@
 import checkActive from "../helpers/cssControlCode.js";
 
 export default (function (window,document,$,undefined) {
-  let $jsAccordion = $('.js-accordion');
+  let $jsAccordion = $('.ma__collapsible-content .js-accordion');
+  var $toggleLink = $('.ma__collapsible-content__toggle-all');
+  let accordionIds = "";
 
+  // Get or generates an ID for an element.
+  function getId($el, $fallbackId) {
+    return $el.attr('id') || 'accordion' + $fallbackId;
+  }
 
   $jsAccordion.each(function(index){
     init.apply(this, [index]);
   });
 
-  // In case that all paragraphs are expanded by default, change the "expand all" link to "collapse all".
-  if($(".ma__collapsible-content--extended").length) {
-    let allExpanded = true;
+  // Gather the ids for all the accordions.
+  $('.ma__collapsible-content--extended .js-accordion').each(function(index){
+    accordionIds += getId($(this), index + 1) + ' ';
+  });
+  // Add aria labels to toggle all
+  $toggleLink.attr("aria-controls", accordionIds);
 
-    $('.ma__collapsible-content--extended .js-accordion').each(function(index){
-      if(! $(this).hasClass('is-open')) {
-        allExpanded = false;
-      }
-    });
-
-    if(allExpanded) {
-      toggleAll();
+  function checkAccordionsSameStatus(){
+    let isOpen = $('.ma__collapsible-content--extended .js-accordion.is-open').length;
+    if (isOpen === 0) {
+      collapseLinkToggle('expand');
+      return "open";
+    } else if (isOpen == $(".ma__collapsible-content--extended .js-accordion").length) {
+      collapseLinkToggle('collapse');
+      return "close";
     }
   }
+
+  function collapseLinkToggle(status){
+    if(status == "collapse") {
+      $toggleLink.addClass('ma__collapsible-content__toggle-all--expanded');
+      $toggleLink.removeClass('ma__collapsible-content__toggle-all--collapsed');
+    } else {
+      $toggleLink.addClass('ma__collapsible-content__toggle-all--collapsed');
+      $toggleLink.removeClass('ma__collapsible-content__toggle-all--expanded');
+    }
+  }
+
+  // In case that all paragraphs are expanded by default, change the "expand all" link to "collapse all".
+  checkAccordionsSameStatus();
 
   $(document).on('ma:AjaxPattern:Render', function(e,data){
     let $context = data.el;
@@ -35,31 +57,26 @@ export default (function (window,document,$,undefined) {
     }
   });
 
-  $('.ma__collapsible-content__toggle-all').on("click",function(e){
-    toggleAll();
+  // Toggle button.
+  $toggleLink.on("click",function(e){
+    let status = $toggleLink.hasClass('ma__collapsible-content__toggle-all--expanded')?"close":"open";
 
-    e.preventDefault();
-  });
-
-  function toggleAll(toggleStatus = 'open') {
-    let $toggleLink = $('.ma__collapsible-content__toggle-all');
-
-    if($toggleLink.hasClass('ma__collapsible-content__toggle-all--expanded')) {
-      $toggleLink.addClass('ma__collapsible-content__toggle-all--collapsed');
-      $toggleLink.removeClass('ma__collapsible-content__toggle-all--expanded');
-      toggleStatus =  'close';
-
+    if (status == 'close') {
+      $toggleLink.attr('aria-expanded', false);
     } else {
-      $toggleLink.addClass('ma__collapsible-content__toggle-all--expanded');
-      $toggleLink.removeClass('ma__collapsible-content__toggle-all--collapsed');
+      $toggleLink.attr('aria-expanded', true);
     }
 
     $('.ma__collapsible-content--extended .js-accordion').each(function(index){
-      accordionToggle($(this), toggleStatus);
+      accordionToggle($(this), status);
     });
-  }
 
-  function accordionToggle($el, toggleStatus = 'default') {
+    checkAccordionsSameStatus();
+    e.preventDefault();
+  });
+
+  // Toggle each individual accordion.
+  function accordionToggle($el, toggleStatus = 'default'){
     let ind = '';
 
     if ($el.hasClass('ma__header-alerts')) {
@@ -84,10 +101,24 @@ export default (function (window,document,$,undefined) {
       $status.attr('aria-label', 'click to hide info');
       $el.addClass('is-open');
     }
-    $link.attr('aria-expanded',!open);
+    accordionAriaToggle($link, !open);
+
   }
 
-  function init(index) {
+  // Set and update aria labels depending on the accordion status.
+  function accordionAriaToggle($link, open) {
+    let statusText = "Expand ";
+    $link.attr('aria-expanded',open);
+
+    if(open) {
+      statusText = "Collapse ";
+    }
+
+    $link.attr('aria-label', statusText + $link.children('h2').text());
+  }
+
+  // Initialize each accordion item.
+  function init(index){
     let $el = $(this);
     let ind = '';
     const isExtended = $el.parents('.ma__collapsible-content--extended').length;
@@ -98,16 +129,17 @@ export default (function (window,document,$,undefined) {
 
     let $link = $el.find(`${ind} .js-accordion-link`),
         $content = $el.find(`${ind} .js-accordion-content`),
-        id = $content.attr('id') || 'accordion' + (index + 1),
+        id = getId($el, index + 1),
         active = checkActive($el),
         open = $el.hasClass('is-open');
 
     $content.attr('id', id);
-    $link.attr('aria-expanded',open).attr('aria-controls', id);
+    $link.attr('aria-controls', id);
+    accordionAriaToggle($link, open);
 
     if(isExtended) {
       let childs = $el.find('.ma__collapsible-content__body-item a').length;
-      $el.find('.ma__collapsible-header__title').append( `<div class="header__title__counter">(${childs})</div>`);
+      $el.find('.ma__collapsible-header__button').append( `<div class="header__title__counter">(${childs})</div>`);
     }
 
 
@@ -116,10 +148,12 @@ export default (function (window,document,$,undefined) {
       $content.stop(true,true).slideDown();
     }
 
+    // + and - clicks.
     $link.on('click',function(e){
       if(active) {
         e.preventDefault();
         accordionToggle($el);
+        checkAccordionsSameStatus();
       }
     });
 
