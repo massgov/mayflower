469b51d01f9e8f1f69ea0667293543e84aa8be0d
Minghua Sun <clairesunstudio@gmail.com>
2021-11-30 17:23:29 -0500

    stype wip

--- packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
+++ packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
@@ -1,21 +1,14 @@
 export default (function (document) {
     const button = document.getElementsByClassName('ma__brand-banner-container')[0];
     const toggle = button.getElementsByClassName('ma__brand-banner-button')[0];
-    console.log('test')
-    console.log(button)
-    console.log(toggle)
     if(button && toggle) {
-        console.log('true')
         button.onmouseover = function() {
-            console.log('hover')
             toggle.classList.add("active");
         }
         button.onmouseout = function() {
-            console.log('blur')
             toggle.classList.remove("active");
         }
         button.onclick = function() {
-            console.log('clicked')
             toggle.classList.toggle("ma__button-icon--expanded");
             const expansionDiv = document.getElementsByClassName('ma__brand-banner-expansion')[0];
             expansionDiv.classList.toggle("ma__brand-banner-expansion--expanded");
