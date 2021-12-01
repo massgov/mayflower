b589ac61f9c7d7b010c81ddfd782bf1a1535b326
Minghua Sun <clairesunstudio@gmail.com>
2021-12-01 14:03:25 -0500

    add aria patternlab

--- packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
+++ packages/patternlab/styleguide/source/assets/js/modules/brandBanner.js
@@ -2,6 +2,8 @@ export default (function (document) {
     const button = document.getElementsByClassName('ma__brand-banner-container')[0];
     const toggle = button && button.getElementsByClassName('ma__brand-banner-button')[0];
     if(toggle) {
+        button.setAttribute('aria-controls', 'ma__brand-banner-content');
+        button.setAttribute('aria-expanded', false);
         button.onmouseover = function() {
             toggle.classList.add("active");
         }
@@ -12,6 +14,8 @@ export default (function (document) {
             toggle.classList.toggle("ma__button-icon--expanded");
             const expansionDiv = document.getElementsByClassName('ma__brand-banner-expansion')[0];
             expansionDiv.classList.toggle("ma__brand-banner-expansion--expanded");
+            const expanded = button.getAttribute("aria-expanded") == 'true';
+            button.setAttribute('aria-expanded', !expanded);
         }
     }
   })(document);
