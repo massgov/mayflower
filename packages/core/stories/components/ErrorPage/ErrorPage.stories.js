88e1f7a136a2bba33f9403555a3be2be0d1b9573
Minghua Sun <clairesunstudio@gmail.com>
2021-12-07 18:17:34 -0500

    cleanup

--- packages/core/stories/components/ErrorPage/ErrorPage.stories.js
+++ packages/core/stories/components/ErrorPage/ErrorPage.stories.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import BrandBanner from '@massds/mayflower-react/dist/BrandBanner';
+import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
 import SiteLogo from '@massds/mayflower-react/dist/SiteLogo';
 import Error404 from '@massds/mayflower-react/dist/Error404';
-import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
-import logoWhite from '@massds/mayflower-assets/static/images/logo/stateseal-white.png';
-import { headerBasic } from '../Header/Header.stories';
+
 
 import { attachHTML } from '../../util/renderCode';
 
 const { STORYBOOK_CDN_PATH } = process.env;
 
-const brandBannerBasic = (
-  <Error404 sitelogo={() => <SiteLogo/>} />
+const err404 = (
+  <Error404 
+    siteLogo={() => (
+      <SiteLogo
+        url={{
+          domain: 'https://www.mass.gov/'
+        }}
+        image={{
+          src: logo,
+          alt: 'Massachusetts state seal',
+          width: 45,
+          height: 45
+        }}
+        siteName="Mass.gov"
+        title="Mass.gov homepage"
+      />
+    )} 
+  />
 );
 
-const notesBrandBanner = `
+const notesErrorPage = `
   // Link to CSS: 
   <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/general.css">
   <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/error-page.css">
 `;
 
-export const brandBannerExample = () => brandBannerBasic;
-attachHTML(brandBannerExample, brandBannerBasic, notesBrandBanner);
+export const errorPage404 = () => err404;
+attachHTML(errorPage404, err404, notesErrorPage);
