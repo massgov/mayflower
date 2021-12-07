e7b7a61d60ef85fc20c67ac3b883b58f8fc1d4ee
Minghua Sun <clairesunstudio@gmail.com>
2021-12-07 18:22:59 -0500

    radd all variants

--- packages/core/stories/components/ErrorPage/ErrorPage.stories.js
+++ packages/core/stories/components/ErrorPage/ErrorPage.stories.js
@@ -2,32 +2,41 @@ import React from 'react';
 import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
 import SiteLogo from '@massds/mayflower-react/dist/SiteLogo';
 import Error404 from '@massds/mayflower-react/dist/Error404';
+import Error403 from '@massds/mayflower-react/dist/Error403';
+import Error500 from '@massds/mayflower-react/dist/Error500';
 
 
 import { attachHTML } from '../../util/renderCode';
 
 const { STORYBOOK_CDN_PATH } = process.env;
 
-const err404 = (
-  <Error404 
-    siteLogo={() => (
-      <SiteLogo
-        url={{
-          domain: 'https://www.mass.gov/'
-        }}
-        image={{
-          src: logo,
-          alt: 'Massachusetts state seal',
-          width: 45,
-          height: 45
-        }}
-        siteName="Mass.gov"
-        title="Mass.gov homepage"
-      />
-    )} 
+const siteLogo = () => (
+  <SiteLogo
+    url={{
+      domain: 'https://www.mass.gov/'
+    }}
+    image={{
+      src: logo,
+      alt: 'Massachusetts state seal',
+      width: 45,
+      height: 45
+    }}
+    siteName="Mass.gov"
+    title="Mass.gov homepage"
   />
+)
+
+const err404 = (
+  <Error404 siteLogo={siteLogo} />
 );
 
+const err403 = (
+  <Error403 siteLogo={siteLogo} />
+);
+
+const err500 = (
+  <Error500 siteLogo={siteLogo} />
+);
 const notesErrorPage = `
   // Link to CSS: 
   <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/general.css">
@@ -36,3 +45,9 @@ const notesErrorPage = `
 
 export const errorPage404 = () => err404;
 attachHTML(errorPage404, err404, notesErrorPage);
+
+export const errorPage403 = () => err403;
+attachHTML(errorPage403, err403, notesErrorPage);
+
+export const errorPage500 = () => err500;
+attachHTML(errorPage500, err500, notesErrorPage);
