5d01b4d78c3d03bfb9042fda8eee42acac356761
Javi-er <javier.reartes@lullabot.com>
2022-01-10 18:44:04 -0300

    DP-23521 Front end enhancement for Breadcrumbs & Offered By, medium & small (#1564)
    * Adjusting for mobile
    
    * Adjusting for mobile
    
    * Adjusting gray color, reducing timeout.
    
    * Adjusting wobbly relationship indicators and making breadcrumbs on mobile to be scrolled all the way to the right.
    
    * Adjusting breadcrumbs shakiness, indicators floating and details. Making breadcrumbs to be right scrolled.
    
    * small styling and position adjustments.
    
    * Adjusting right border
    
    * Adjusted aria labels, minor alignment details, cleaning up.
    
    * adjusting
    
    * Adjusting
    
    * Adjusting details
    
    * clean up, PL adjustments

--- packages/patternlab/styleguide/source/assets/js/modules/breadcrumbs.js
+++ packages/patternlab/styleguide/source/assets/js/modules/breadcrumbs.js
@@ -1,38 +1,98 @@
-export default (function(window, document, $) {
-  let $breadcrumbsIndicator = $('.ma__breadcrumbs__item__expand-indicators');
-  let $button = $('.js-breadcrumbs-button');
-  let $breadcrumbItems = $( ".ma__breadcrumbs__item" );
-  let $breadcrumbItemsWrapper = $( ".ma__breadcrumbs__items" );
+export default (function (window, document, $) {
+  let $breadcrumbsIndicator = $(".ma__breadcrumbs__item__expand-indicators");
+  let $button = $(".js-breadcrumbs-button");
+  let $breadcrumbItems = $(".ma__breadcrumbs__item");
+  let $breadcrumbsContainer = $(".ma__breadcrumbs__container");
+  let $breadcrumbs = $(".ma__breadcrumbs");
   let $breadcrumbItemsSlice = null;
-  if($breadcrumbItems.length) {
-    $breadcrumbItemsSlice = $breadcrumbItems.slice( 2, $breadcrumbItems.length -3 );
 
-    if($breadcrumbItemsSlice.length) {
-      $breadcrumbsIndicator.css('display', 'inline-block');
-      $breadcrumbsIndicator.insertAfter($breadcrumbItems[1]);
-      $breadcrumbItemsSlice.hide();
+  function drawBreadcrumbs() {
+    let windowWidth = $(window).width();
 
-      let ariaControls = "";
-      $breadcrumbItemsSlice.map( function() {
-        ariaControls += $(this).attr('id') + " ";
+    // Mobile display.
+    if (windowWidth < 910 && !$breadcrumbs.hasClass('ma__breadcrumbs--mobile')) {
+      $breadcrumbs.addClass('ma__breadcrumbs--mobile');
+      $breadcrumbs.removeClass('ma__breadcrumbs--desktop');
+
+      // Start by scrolling right if there are more items than the viewport allows.
+      if ($breadcrumbsContainer[0].scrollWidth > $breadcrumbs.width() + 40) {
+        $breadcrumbsContainer.scrollLeft($breadcrumbsContainer[0].scrollWidth);
+        $breadcrumbs.addClass('ma__breadcrumbs--mobile-scroll');
+      }
+
+      // On breadcrumb scrolling, add or remove the left / right fading.
+      $breadcrumbsContainer.on("scroll", function () {
+        // Left fading
+        if ($breadcrumbsContainer.scrollLeft() > 0) {
+          $breadcrumbs.addClass("ma__breadcrumbs--fade-left");
+        } else {
+          $breadcrumbs.removeClass("ma__breadcrumbs--fade-left");
+        }
+
+        // Right fading
+        var newScrollLeft = $breadcrumbsContainer.scrollLeft(),
+          width = $breadcrumbsContainer.outerWidth(),
+          scrollWidth = $breadcrumbsContainer.get(0).scrollWidth;
+        if (scrollWidth - newScrollLeft == width) {
+          $breadcrumbs.removeClass("ma__breadcrumbs--fade-right");
+        } else {
+          $breadcrumbs.addClass("ma__breadcrumbs--fade-right");
+        }
       });
 
-      $breadcrumbItemsSlice.attr("aria-hidden", "true");
-      $button.attr('aria-expanded', 'false');
-      $button.attr('aria-controls', ariaControls);
+    //Desktop display.
+    } else if (windowWidth >= 910 && !$breadcrumbs.hasClass('ma__breadcrumbs--desktop')){
+      $breadcrumbsIndicator.removeAttr('hidden');
+      $breadcrumbs.removeClass('ma__breadcrumbs--mobile');
+      $breadcrumbs.addClass('ma__breadcrumbs--desktop');
 
-      $button.on('click', function() {
-        $breadcrumbsIndicator.hide();
-        $breadcrumbItemsSlice.show();
+      $breadcrumbItemsSlice = $breadcrumbItems.slice(2, $breadcrumbItems.length - 3);
 
-        $breadcrumbsIndicator.attr("aria-hidden", "true");
-        $button.attr("aria-expanded", "true");
-        $breadcrumbItemsSlice.attr("aria-hidden", "false");
+      if ($breadcrumbItemsSlice.length) {
+        $breadcrumbsIndicator.css("display", "inline-block");
+        $breadcrumbItemsSlice.hide();
 
-      });
+        let ariaControls = "";
+        $breadcrumbItemsSlice.map(function () {
+          ariaControls += $(this).attr("id") + " ";
+        });
 
-    } else {
-      $breadcrumbsIndicator.remove();
+        $breadcrumbItemsSlice.attr("hidden", "");
+        $button.attr("aria-expanded", "false");
+        $button.attr("aria-pressed", "false");
+        $button.attr("aria-controls", ariaControls);
+
+        $button.on("click", function () {
+          $breadcrumbsIndicator.hide();
+          $breadcrumbsIndicator.attr("hidden", "");
+
+          $breadcrumbItemsSlice.show();
+          $breadcrumbItemsSlice.removeAttr("hidden");
+
+          $button.attr("aria-expanded", "true");
+          $button.attr("aria-pressed", "true");
+          $button.attr("hidden", "");
+
+
+        });
+
+      } else {
+        $breadcrumbsIndicator.remove();
+      }
     }
   }
-})(window,document,jQuery);
+
+  if ($breadcrumbItems.length) {
+    // In either case (mobile or desktop), move the indicator button to the right position to prevent CSS issues.
+    $breadcrumbsIndicator.insertAfter($breadcrumbItems[1]);
+
+    drawBreadcrumbs();
+
+    $(window).resize(function () {
+      setTimeout(function(){
+        drawBreadcrumbs();
+      }, 50);
+    });
+  }
+
+})(window, document, jQuery);
--- packages/patternlab/styleguide/source/assets/js/modules/headerTags.js
+++ packages/patternlab/styleguide/source/assets/js/modules/headerTags.js
@@ -1,112 +1,143 @@
-export default (function (window,document,$,undefined) {
+export default (function (window, document, $, undefined) {
   "use strict";
 
-  $('.js-header-tag-link').each(function(index) {
+  let $relationshipIndicators = $(".ma__relationship-indicators");
+
+  $(".js-header-tag-link").each(function (index) {
 
     let $el = $(this),
-        $showHideButton = $('.js-header-tag-button', $el),
-        $dynamicItems = $('a:nth-child(n+4)', $el),
-        $focusItem = $('a:nth-child(4)', $el),
-        id = $el.attr('id') || 'headerTags' + (index + 1),
-        open = $el.hasClass('is-open');
+      $showHideButton = $(".js-header-tag-button", $el),
+      $dynamicItems = $("a:nth-child(n+4)", $el),
+      $focusItem = $("a:nth-child(4)", $el),
+      id = $el.attr("id") || "headerTags" + (index + 1),
+      open = $el.hasClass("is-open");
 
-      // Set the id attribute (respects default if set).
-      $el.attr('id', id);
+    // Set the id attribute (respects default if set).
+    $el.attr("id", id);
 
-      if ($dynamicItems.length) {
-        // Show our see button if we have more than three items.
-        $showHideButton.attr('aria-expanded',open).attr('aria-controls', id);
-        $showHideButton.show();
+    if ($dynamicItems.length) {
+      // Show our see button if we have more than three items.
+      $showHideButton.attr("aria-expanded", open).attr("aria-controls", id);
+      $showHideButton.show();
+    }
+
+    $showHideButton.on("click", function (e) {
+      e.preventDefault();
+      $el.toggleClass("is-open");
+
+      if ($el.hasClass("is-open")) {
+        $showHideButton.attr("aria-expanded", "true");
+        $dynamicItems.show();
+        $focusItem.focus();
+      } else {
+        $showHideButton.attr("aria-expanded", "false");
+        $dynamicItems.hide();
+      }
+    });
+  });
+
+  /**
+   * Handles relationship indicators dropdown grouping for desktop and mobile.
+   *
+   * @param groupAfter optional, if it's not specified it will look at data-group-after on the section group.
+   *
+   */
+  function groupIndicators(groupAfter = null) {
+    // There could be more than one relation ship indicators component in the page.
+    $(".ma__relationship-indicators--terms").each(function (index) {
+      let $tagWrapper = $(this);
+      let $button = $tagWrapper.find(".js-relationship-indicator-button");
+      let $buttonCounter = $button.find(".tag-count");
+      let $hiddenTags = $tagWrapper.find(".js-term[hidden]");
+      let hiddenIds = "";
+      let groupId = "ma-rig" + index;
+
+      // Start by making visible all the relationship indicators.
+      $hiddenTags.show();
+      $hiddenTags.removeAttr("hidden");
+
+      // Determines the folding number of items, either as a parameter or and attribute.
+      let $group = $(".ma__relationship-indicators--section-group");
+      groupAfter = (groupAfter != null) ? groupAfter : parseInt($group.data("group-after")) - 1;
+      // Hide tags.
+      if ($group.length && !$hiddenTags.length) {
+        $hiddenTags = $tagWrapper.find(".js-term:gt(" + groupAfter + ")");
+        $hiddenTags.addClass("ma__relationship-indicators--term--fold");
+        $hiddenTags.hide();
+
+        // Generate ids for hidden items.
+        $hiddenTags.each(function (itemIndex) {
+          let $hiddenTagItem = $(this);
+          let itemId = groupId + itemIndex;
+          $hiddenTagItem.attr("id", itemId);
+          hiddenIds += itemId + " ";
+        });
+
+        // Aria handling.
+        ariaToggle($hiddenTags);
+        $button.attr("aria-controls", hiddenIds);
+      }
+      // Updates button text.
+      let tagCount = $hiddenTags.length;
+      let $tagState = $button.find(".tag-state");
+
+      // If hidden tags exist, show button.
+      if (tagCount) {
+        $button.show();
+        $tagWrapper.removeClass('ma__relationship-indicators--terms--without-fold');
+      } else {
+        $button.hide();
+        $tagWrapper.addClass('ma__relationship-indicators--terms--without-fold');
       }
 
-      $showHideButton.on('click', function(e) {
-        e.preventDefault();
-        $el.toggleClass('is-open');
+      // Use hidden tags to populate button label.
+      $buttonCounter.text(tagCount);
 
-        if ($el.hasClass('is-open')) {
-          $showHideButton.attr('aria-expanded', 'true');
-          $dynamicItems.show();
-          $focusItem.focus();
-        }
-        else {
-          $showHideButton.attr('aria-expanded', 'false');
-          $dynamicItems.hide();
-        }
+      $button.on("click", function () {
+        let $tagStateText = $tagState.text();
+
+        // Open hidden tags.
+        $tagWrapper.parent().toggleClass("tags-open");
+        $button.toggleClass("is-open");
+        $hiddenTags.toggle();
+
+        // Aria handling.
+        ariaToggle($hiddenTags);
+        $button.attr("aria-pressed", function (_, attr) {return !(attr == "true");});
+        $button.attr("aria-expanded", function (_, attr) {return !(attr == "true");});
+
+        // Change button text.
+        $tagState.text($tagStateText === "fewer" ? "more" : "fewer");
+
       });
-  });
-
-  $('.ma__relationship-indicators--terms').each(function() {
-
-    let $tagWrapper = $(this);
-    let $button = $tagWrapper.find('.js-relationship-indicator-button');
-    let $buttonCounter = $button.find('.tag-count');
-    let $hiddenTag = $tagWrapper.find('.js-term:hidden');
-
-
-    let $group = $('.ma__relationship-indicators--section-group');
-
-    let groupAfter = parseInt($group.data('group-after')) -1 ;
-    if ($group.length && !$hiddenTag.length) {
-      $hiddenTag = $tagWrapper.find('.js-term:gt('+ groupAfter +')');
-      $hiddenTag.addClass('ma__relationship-indicators--term--fold');
-    }
-
-    let $focusTag = $hiddenTag.first().find('a');
-    let tagCount = $hiddenTag.length;
-    let $tagState = $button.find('.tag-state');
-
-    // If hidden  tags exist, show button.
-    if (tagCount) {
-      $button.toggle();
-    }
-
-    // Use hidden tags to populate button label.
-    $buttonCounter.text(tagCount);
-
-    $button.on('click', function() {
-      let $tagStateText = $tagState.text();
-
-      // Open hidden tags.
-      $tagWrapper.parent().toggleClass('tags-open');
-      $button.toggleClass('is-open');
-      $hiddenTag.toggle();
-
-      // Change button text.
-      $tagState.text($tagStateText === 'fewer' ? 'more' : 'fewer');
-
-      $button.attr('aria-pressed', function(_, attr) { return !(attr == 'true') });
-
     });
+  }
 
-    $(window).resize(function () {
-      // Remove all the screen width specific styles.
-      $buttonCounter.removeAttr('style');
-      $hiddenTag.removeAttr('style');
+  function ariaToggle($hiddenTags) {
+    if ($hiddenTags.attr("hidden")) {
+      $hiddenTags.removeAttr("hidden");
+    } else {
+      $hiddenTags.attr("hidden", "");
+    }
+  }
 
-      setTimeout(function(){
-        let windowWidth = $(window).width();
+  $(window).resize(function () {
+    let windowWidth = $(window).width();
 
-        // recount the hidden tags.
-        $hiddenTag = $tagWrapper.find('.ma__relationship-indicators--term:hidden');
+    setTimeout(function () {
+      // Reset the button visibility.
+      if (windowWidth < 910 && !$relationshipIndicators.hasClass("ma__relationship-indicators--mobile")) {
+        $relationshipIndicators.addClass("ma__relationship-indicators--mobile");
+        $relationshipIndicators.removeClass("ma__relationship-indicators--desktop");
+        groupIndicators(0);
+      } else if (windowWidth >= 910 && !$relationshipIndicators.hasClass("ma__relationship-indicators--desktop")) {
+        $relationshipIndicators.removeClass("ma__relationship-indicators--mobile");
+        $relationshipIndicators.addClass("ma__relationship-indicators--desktop");
+        groupIndicators();
+      }
 
-        tagCount = $hiddenTag.length;
+    }, 50);
 
-        // Update button text.
-        $buttonCounter.text(tagCount);
-        $tagState.text('more');
+  }).resize();
 
-        // Reset the button visibility.
-        if(tagCount >= 1 && windowWidth < 910) {
-          $button.show();
-        }
-        else if (tagCount == 0) {
-          $button.hide();
-        }
-
-      }, 500);
-
-    });
-
-  });
-
-})(window,document,jQuery);
+})(window, document, jQuery);
