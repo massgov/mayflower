d8f45a1170a7864d5c3fa35b6bf088c44e618477
Minghua Sun <clairesunstudio@gmail.com>
2022-03-21 17:50:08 -0400

    upgrade svgr to import svgo config

---
+++ packages/react/.svgorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      name: 'addAttributesToSVGElement',
+      params: {
+        attributes: [
+          {
+            focusable: false
+          }
+        ]
+      }
+    }
+  ]
+};
