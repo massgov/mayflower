da3fa71b8aca6f7d1ac5b957bea7b525bd041345
Minghua Sun <clairesunstudio@gmail.com>
2022-07-08 17:24:41 -0400

    refactor logic wip

--- packages/patternlab/styleguide/source/assets/js/modules/relationshipIndicators.js
+++ packages/patternlab/styleguide/source/assets/js/modules/relationshipIndicators.js
@@ -6,84 +6,87 @@ export default (function (window, document, $, undefined) {
    * @param groupAfter optional, if it's not specified it will look at data-group-after on the section group.
    *
    */
+
   function groupIndicators(groupAfter) {
-    console.log(groupAfter)
+
     // There could be more than one relationship indicators component in the page.
     $(".ma__relationship-indicators--terms").each(function (index) {
       let $tagWrapper = $(this);
       let $button = $tagWrapper.find(".js-relationship-indicator-button");
-      let $buttonCounter = $button.find(".tag-count");
-      let $hiddenTags = $tagWrapper.find(".js-term[hidden]");
-      let hiddenIds = "";
-      let groupId = "ma-ri_" + index;
+      let $items = $tagWrapper.find("li.js-term")
+      let totalCount = $items.length;
+      let hiddenCount = totalCount - (groupAfter + 1);
+      console.log(totalCount)
+      console.log(groupAfter)
 
-      // Start by making visible all the relationship indicators.
-      // $hiddenTags.show();
-      // $hiddenTags.removeAttr("hidden");
+      // render toggle button
+      if (hiddenCount > 0) {
+        let hiddenIds = "";
+        let groupId = "ma-ri_" + index;
+        let $buttonCounter = $button.find(".tag-count");
+        let $tagState = $button.find(".tag-state");
+        let $hiddenItems = $tagWrapper.find(".js-term:gt(" + groupAfter + ")");
+        // Use hidden tags to populate button label.
+        $button.show();
+        $buttonCounter.text(hiddenCount);
+        // hide items
+        $hiddenItems.hide();
+        // $hiddenItems.each((itemIndex) => {
+        //   $(this).hide();
+        //   let $hiddenTagItem = $(this);
+        //   let itemId = groupId + itemIndex;
+        //   $hiddenTagItem.attr("id", itemId);
+        //   hiddenIds += itemId + " ";
+        // });
+
+        console.log($hiddenItems)
 
 
-      // Hide tags.
-      if ($group.length && !$hiddenTags.length) {
-        $hiddenTags = $tagWrapper.find(".js-term:gt(" + groupAfter + ")");
-        $hiddenTags.hide();
-
-        // Generate ids for hidden items.
-        $hiddenTags.each(function (itemIndex) {
-          let $hiddenTagItem = $(this);
-          let itemId = groupId + itemIndex;
-          $hiddenTagItem.attr("id", itemId);
-          hiddenIds += itemId + " ";
-        });
+      
 
         // Aria handling.
-        ariaToggle($hiddenTags);
-        $button.attr("aria-controls", hiddenIds);
-      }
+        // ariaToggle($hiddenItems);
+        // $button.attr("aria-controls", hiddenIds);
 
-      // Updates button text.
-      let tagCount = $hiddenTags.length;
-      let $tagState = $button.find(".tag-state");
+    
+        // Class headerTagClickEvent is a flag to avoid attaching this event
+        // multiple times when the window is resized.
+        // $button.on("click", function () {
 
-      // If hidden tags exist, show button.
-      if (tagCount) {
-        $button.show();
+        //   const hiddenItems = $('.ma__relationship-indicators--term', $tagWrapper).not(':visible');
+        //   const hiddenItemsCount = hiddenItems.length;
+        //   const hiddenItemsToggle = hiddenItemsCount > 0;
+        //   const $tagStateText = hiddenItemsToggle ? "less" : "more";
+        //   $tagWrapper.parent().toggleClass("tags-open", hiddenItemsToggle);
+        //   $button.toggleClass("is-open", hiddenItemsToggle);
+        //   $hiddenItems.toggle(hiddenItemsToggle);
+
+        //   // Aria handling.
+        //   ariaToggle($hiddenItems);
+        //   $button.attr("aria-pressed", function (_, attr) {return !(attr == "true");});
+        //   $button.attr("aria-expanded", function (_, attr) {return !(attr == "true");});
+
+        //   // Change button text.
+        //   $tagState.text($tagStateText);
+        // });
+      // don't render toggle button
       } else {
         $button.hide();
+        $items.show();
+      // Start by making visible all the relationship indicators.
+      // $hiddenItems.show();
+      // $hiddenItems.removeAttr("hidden");
       }
-
-      // Use hidden tags to populate button label.
-      $buttonCounter.text(tagCount);
-
-      // Class headerTagClickEvent is a flag to avoid attaching this event
-      // multiple times when the window is resized.
-      $button.on("click", function () {
-
-        const hiddenItems = $('.ma__relationship-indicators--term', $tagWrapper).not(':visible');
-        const hiddenItemsCount = hiddenItems.length;
-        const hiddenItemsToggle = hiddenItemsCount > 0;
-        const $tagStateText = hiddenItemsToggle ? "less" : "more";
-        $tagWrapper.parent().toggleClass("tags-open", hiddenItemsToggle);
-        $button.toggleClass("is-open", hiddenItemsToggle);
-        $hiddenTags.toggle(hiddenItemsToggle);
-
-        // Aria handling.
-        ariaToggle($hiddenTags);
-        $button.attr("aria-pressed", function (_, attr) {return !(attr == "true");});
-        $button.attr("aria-expanded", function (_, attr) {return !(attr == "true");});
-
-        // Change button text.
-        $tagState.text($tagStateText);
-      });
     });
   }
 
-  function ariaToggle($hiddenTags) {
-    if ($hiddenTags.attr("hidden")) {
-      $hiddenTags.removeAttr("hidden");
-    } else {
-      $hiddenTags.attr("hidden", "");
-    }
-  }
+  // function ariaToggle($hiddenItems) {
+  //   if ($hiddenItems.attr("hidden")) {
+  //     $hiddenItems.removeAttr("hidden");
+  //   } else {
+  //     $hiddenItems.attr("hidden", "");
+  //   }
+  // }
 
   // To debounce function calls.
   // @see https://www.freecodecamp.org/news/javascript-debounce-example/
@@ -97,12 +100,12 @@ export default (function (window, document, $, undefined) {
 
   // Determines the folding number of items, either by the parameter or by the data attribute.
   let $group = $(".ma__relationship-indicators--section-group");
-  var groupAfter = parseInt($group.data("group-after")) - 1 || 3;
+  var $groupAfter = parseInt($group.data("group-after")) - 1 || 3;
 
   // What we do when viewport is resized.
   function resizeResponse() {
     let windowWidth = $(window).width();
-    groupIndicators(windowWidth < 910 ? 0 : groupAfter);
+    groupIndicators(windowWidth < 910 ? 0 : $groupAfter);
   }
 
 
