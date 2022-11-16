b5cf8ef6854420febf5ad7c4cc15846ae921fb9f
Minghua Sun <clairesunstudio@gmail.com>
2022-11-16 16:36:21 -0500

    add toggle logic

--- packages/patternlab/styleguide/source/assets/js/modules/helptip.js
+++ packages/patternlab/styleguide/source/assets/js/modules/helptip.js
@@ -1,8 +1,24 @@
 export default (function (window,document) {
   const helptipTriggerClass = '.ma__help-tip__trigger';
-  const $helptipTrigger = document.querySelectorAll(helptipTriggerClass);
+  const helptipTriggers = document.querySelectorAll(helptipTriggerClass);
   
-  if($helptipTrigger) {
-    console.log($helptipTrigger)
+  if(helptipTriggers && helptipTriggers.length > 0) {
+    helptipTriggers.forEach((trigger) => {
+      let isExpanded = trigger.getAttribute('aria-expanded') === 'true' ? true : false;
+      const helpTextID = trigger.getAttribute('aria-controls');
+      const helpText = document.getElementById(helpTextID);
+
+      const toggleExpansion = (expanded) => {
+        trigger.setAttribute('aria-expanded', expanded);
+        trigger.classList.toggle('ma__help-tip__trigger--active');
+        helpText.classList.toggle('collapsed');
+      }
+
+      trigger.onclick = (() => {
+        isExpanded = !isExpanded;
+        toggleExpansion(isExpanded);
+      })
+
+    })
   }
 })(window,document);
