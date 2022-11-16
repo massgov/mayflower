c4006d7b9e483f9c9a3f179c002cdde818822a37
Minghua Sun <clairesunstudio@gmail.com>
2022-11-16 12:54:34 -0500

    add helptip component

---
+++ packages/patternlab/styleguide/source/assets/js/modules/helptip.js
@@ -0,0 +1,8 @@
+export default (function (window,document) {
+  const helptipTriggerClass = '.ma__help-tip__trigger';
+  const $helptipTrigger = document.querySelectorAll(helptipTriggerClass);
+  
+  if($helptipTrigger) {
+    console.log($helptipTrigger)
+  }
+})(window,document);
