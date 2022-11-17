8cc5923215e526615833dab0bc0d4eba018f21e9
Minghua Sun <clairesunstudio@gmail.com>
2022-11-17 14:07:53 -0500

    start adding validation

--- packages/patternlab/styleguide/source/assets/js/modules/feedbackForm.js
+++ packages/patternlab/styleguide/source/assets/js/modules/feedbackForm.js
@@ -6,19 +6,6 @@ export default (function(window, document, $) {
     const $loadRadios = $feedbackForm.find('.feedback-load input[type="radio"]');
     const $submitButton = $feedbackForm.find(".submitButton");
   
-    // Auto size the text boxes
-    $textArea.each(function() {
-      let $el = $(this);
-  
-      $el.on("keyup", function() {
-        if ($el.prop("scrollHeight") > $el.prop("clientHeight")) {
-          $el.css("height", $el.prop("scrollHeight") + "px");
-        } else {
-          $el.css("height", "100%");
-        }
-      });
-    });
-  
     const radioOnSelect = () => {
       let foundIndicator = $loadRadios.filter(":checked").val();
   
@@ -39,5 +26,59 @@ export default (function(window, document, $) {
     $loadRadios.on("change", function() {
       radioOnSelect();
     });
+
+    // Auto size the text boxes
+    $textArea.each(function() {
+      let $el = $(this);
+  
+      $el.on("keyup", function() {
+        if ($el.prop("scrollHeight") > $el.prop("clientHeight")) {
+          $el.css("height", $el.prop("scrollHeight") + "px");
+        } else {
+          $el.css("height", "100%");
+        }
+      });
+    });
+  
+    $feedbackForm.ajaxForm({
+      // Add jsonp parameter when using ajax submission.
+      data: {jsonp: 1},
+      // Interpret received data as a script (JSONP).
+      dataType: 'script',
+      // Validate prior to submission.
+      beforeSubmit: validateForm
+    });
+
+  // Validation Handler.
+  function validateForm(data, $form) {
+    var validates = true;
+    data.forEach(function (field) {
+      // Check for empty required fields. Required status is determined
+      // by the "required" attribute being set on an input.
+      if (field.required && !field.value.length > 0) {
+        // Add Error attribute for easy theming and accessibility.
+        $('[name="' + field.name + '"]', $form)
+          .addClass('error')
+          .closest('fieldset').addClass('error');
+
+        validates = false;
+      }
+    });
+
+    if (!validates) {
+      getMessaging($form).html('<p class="error">Please fill in a valid value for all required fields</p>');
+    }
+
+    return validates;
+  }
+  function getMessaging($form) {
+    var $messages = $('.messages', $form);
+    if (!$messages.length) {
+      $form.prepend('<div class="messages"/>');
+      $messages = $('.messages', $form);
+    }
+    return $messages;
+  }
+
   }
 })(window, document, jQuery);
