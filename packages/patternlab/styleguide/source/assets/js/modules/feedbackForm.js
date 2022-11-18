20002721932b7e855b44b95ed13e15e71f128108
Minghua Sun <clairesunstudio@gmail.com>
2022-11-18 10:10:36 -0500

    wip

--- packages/patternlab/styleguide/source/assets/js/modules/feedbackForm.js
+++ packages/patternlab/styleguide/source/assets/js/modules/feedbackForm.js
@@ -40,45 +40,50 @@ export default (function(window, document, $) {
       });
     });
   
-    $feedbackForm.ajaxForm({
-      // Add jsonp parameter when using ajax submission.
-      data: {jsonp: 1},
-      // Interpret received data as a script (JSONP).
-      dataType: 'script',
-      // Validate prior to submission.
-      beforeSubmit: validateForm
-    });
+  //   $feedbackForm.ajaxForm({
+  //     // Add jsonp parameter when using ajax submission.
+  //     data: {jsonp: 1},
+  //     // Interpret received data as a script (JSONP).
+  //     dataType: 'script',
+  //     // Validate prior to submission.
+  //     beforeSubmit: validateForm
+  //   });
 
-  // Validation Handler.
-  function validateForm(data, $form) {
-    var validates = true;
-    data.forEach(function (field) {
-      // Check for empty required fields. Required status is determined
-      // by the "required" attribute being set on an input.
-      if (field.required && !field.value.length > 0) {
-        // Add Error attribute for easy theming and accessibility.
-        $('[name="' + field.name + '"]', $form)
-          .addClass('error')
-          .closest('fieldset').addClass('error');
+  //   console.log('test test')
+  // // Validation Handler.
+  // function validateForm(data, $form) {
+  //   let validates = true;
+  //   console.log(data)
+  //   console.log($form)
+  //   data.forEach(function (field) {
+  //     // Check for empty required fields. Required status is determined
+  //     // by the "required" attribute being set on an input.
+  //     if (field.required && !field.value.length > 0) {
+  //       // Add Error attribute for easy theming and accessibility.
+  //       $('[name="' + field.name + '"]', $form)
+  //         .addClass('error')
+  //         .closest('fieldset').addClass('error');
 
-        validates = false;
-      }
-    });
+  //       validates = false;
+  //     }
+  //   });
 
-    if (!validates) {
-      getMessaging($form).html('<p class="error">Please fill in a valid value for all required fields</p>');
-    }
 
-    return validates;
-  }
-  function getMessaging($form) {
-    var $messages = $('.messages', $form);
-    if (!$messages.length) {
-      $form.prepend('<div class="messages"/>');
-      $messages = $('.messages', $form);
-    }
-    return $messages;
-  }
+
+  //   if (!validates) {
+  //     getMessaging($formm).html('<p class="error">Please fill in a valid value for all required fields</p>');
+  //   }
+
+  //   return validates;
+  // }
+  // function getMessaging($form) {
+  //   var $messages = $('.messages', $form);
+  //   if (!$messages.length) {
+  //     $form.prepend('<div class="messages"/>');
+  //     $messages = $('.messages', $form);
+  //   }
+  //   return $messages;
+  // }
 
   }
 })(window, document, jQuery);
