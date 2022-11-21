3676c10efeb3a6e8b5327b762991fcc10cb18cf0
DmitriiTartynovLCM <dmitrii@lastcallmedia.com>
2022-11-21 17:49:50 +0100

    DP-26366: Pass className to the root element.

--- packages/react/src/components/molecules/ResultsHeading/index.js
+++ packages/react/src/components/molecules/ResultsHeading/index.js
@@ -12,18 +12,23 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import ButtonToggle from 'MayflowerReactButtons/ButtonToggle';
 import SelectBox from 'MayflowerReactForms/SelectBox';
 import Tags from 'MayflowerReactMolecules/Tags';
 
 const ResultsHeading = (resultsHeading) => {
+  const classes = classNames(
+    'ma__results-heading js-results-heading',
+    resultsHeading.className
+  );
   const resultsHeadingTotal = resultsHeading.totalResults ? ` of ${resultsHeading.totalResults} for: ` : '';
   const resultsHeadingTitle = `Showing results ${resultsHeading.numResults}${resultsHeadingTotal}`;
   const { tags } = resultsHeading;
   const selectBoxProps = resultsHeading.selectBox;
   const buttonToggleProps = resultsHeading.buttonToggle;
   return(
-    <div className="ma__results-heading js-results-heading">
+    <div className={classes}>
       <div className="ma__results-heading__container">
         <div className="ma__results-heading__title">
           {resultsHeadingTitle}
@@ -47,6 +52,8 @@ const ResultsHeading = (resultsHeading) => {
 };
 
 ResultsHeading.propTypes = {
+  /** Custom class added to the root element. */
+  className: PropTypes.string,
   /** The range of results being displayed, e.g. 1-10 */
   numResults: PropTypes.string,
   /** The total count of results */
