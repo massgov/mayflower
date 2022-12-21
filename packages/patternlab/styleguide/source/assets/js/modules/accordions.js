7cacc015bd1d71d28042f6ff92d4e8ce179dea39
Yukiko Gannett <yukikogannett@gmail.com>
2022-12-21 15:55:55 -0500

    accordions.js adjustment test edit 2

--- packages/patternlab/styleguide/source/assets/js/modules/accordions.js
+++ packages/patternlab/styleguide/source/assets/js/modules/accordions.js
@@ -110,11 +110,13 @@ export default (function (window,document,$,undefined) {
       $status.attr('aria-label', 'click to show info');
       $el.removeClass('is-open');
       $statusIcon.text('+');
+      $link.attr('aria-expanded', false);
     } else {
       $content.stop(true,true).slideDown();
       $status.attr('aria-label', 'click to hide info');
       $el.addClass('is-open');
       $statusIcon.text('|');
+      $link.attr('aria-expanded', true);
     }
   }
 
@@ -143,7 +145,7 @@ export default (function (window,document,$,undefined) {
 
     $content.attr('id', id);
     $link.attr('aria-controls', id);
-    accordionAriaToggle($link, open);
+    // accordionAriaToggle($link, open);
 
     if(isExtended) {
       let childs = $el.find('.ma__collapsible-content__body-item a').length;
