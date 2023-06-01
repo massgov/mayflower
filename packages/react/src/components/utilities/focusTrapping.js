0ba41e1f574dbe42fc9c05b4e87efa22262ad96a
Minghua Sun <clairesunstudio@gmail.com>
2023-06-01 17:24:11 -0400

    refactor focusTrapping

--- packages/react/src/components/utilities/focusTrapping.js
+++ packages/react/src/components/utilities/focusTrapping.js
@@ -26,6 +26,18 @@ export default ({
   const modal = document.querySelector(modalSelector); // select the modal by it's id
   const e = keyEvent;
 
+  const action = {
+    tab: keyEvent === 'Tab', // tab
+    shift: keyEvent.shift, //shift
+    esc: keyEvent === 'Esc' || keyEvent === 'Escape', // esc
+    left: keyEvent === 'Left' || keyEvent === 'ArrowLeft', // left arrow
+    right: keyEvent === 'Right' || keyEvent === 'ArrowRight', // right arrow
+    up: keyEvent === 'Up' || keyEvent === 'ArrowUp', // up arrow
+    down: keyEvent === 'Down' || keyEvent === 'ArrowDown' // down arrow
+  };
+
+  console.log(e)
+
   if (modal) {
     const focusableNodeArray = Array.from(modal.querySelectorAll(focusableElements));
     let focusableContent = modal && filterDisplayedElements(focusableNodeArray);
@@ -36,18 +48,18 @@ export default ({
     const firstFocusableElement = focusableContent[0]; // get first element to be focused inside modal
     const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
-    const isTabPressed = e.key === 'Tab';
+    const isTabPressed = tab || right;
 
     if (!isTabPressed) {
       return;
     }
 
-    if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (shift || left) { // if shift keyEvent pressed for shift + tab combination
       if (document.activeElement === firstFocusableElement) {
         lastFocusableElement.focus(); // add focus for the last focusable element
         e.preventDefault();
       }
-    } else if (document.activeElement === lastFocusableElement) { // if tab key is pressed
+    } else if (document.activeElement === lastFocusableElement) { // if tab keyEvent is pressed
       // if focused has reached to last focusable element then focus first focusable element after pressing tab
       firstFocusableElement.focus(); // add focus for the first focusable element
       e.preventDefault();
