25c1ebdb3b9d785fe1cb565ee367461cd1108522
Minghua Sun <clairesunstudio@gmail.com>
2023-06-01 17:40:10 -0400

    wip

--- packages/react/src/components/utilities/focusTrapping.js
+++ packages/react/src/components/utilities/focusTrapping.js
@@ -3,7 +3,7 @@
  * @param {string} focusableSelectors(requied): css selectors for all the focusable elements inside of the modal
  * @param {string} modalSelector(requied): css selector for the modal container
  * @param {string} closeButtonSelector(optional): If the modal close button is outside of the modal container, add the css selector for it to be included as the first focusable element
- * @param {event} keyEvent(requied): keydown event
+ * @param {event} key(requied): keydown event
  */
 
 // check if an element is visibly displayed using computed style
@@ -24,19 +24,12 @@ export default ({
   // add all the elements inside modal which you want to make focusable
   const focusableElements = focusableSelectors;
   const modal = document.querySelector(modalSelector); // select the modal by it's id
-  const e = keyEvent;
+  const { key } = keyEvent;
 
   const action = {
-    tab: keyEvent === 'Tab', // tab
-    shift: keyEvent.shift, //shift
-    esc: keyEvent === 'Esc' || keyEvent === 'Escape', // esc
-    left: keyEvent === 'Left' || keyEvent === 'ArrowLeft', // left arrow
-    right: keyEvent === 'Right' || keyEvent === 'ArrowRight', // right arrow
-    up: keyEvent === 'Up' || keyEvent === 'ArrowUp', // up arrow
-    down: keyEvent === 'Down' || keyEvent === 'ArrowDown' // down arrow
+    tab: key === 'Tab', // tab
+    shift: key.shift //shift
   };
-
-  console.log(e)
 
   if (modal) {
     const focusableNodeArray = Array.from(modal.querySelectorAll(focusableElements));
@@ -45,21 +38,30 @@ export default ({
       const closeButton = document.querySelector(closeButtonSelector);
       focusableContent = [closeButton, ...focusableContent];
     }
+    const focusedElement = document.activeElement;
     const firstFocusableElement = focusableContent[0]; // get first element to be focused inside modal
     const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
 
-    const isTabPressed = tab || right;
+    const isTabPressed = action.tab;
 
     if (!isTabPressed) {
       return;
     }
 
-    if (shift || left) { // if shift keyEvent pressed for shift + tab combination
-      if (document.activeElement === firstFocusableElement) {
+    // if (action.left || action.right) {
+    //   let focusIndex = focusableNodeArray.findIndex((el) => el === focusedElement);
+    //   console.log(focusedElement)
+    //   console.log(focusableNodeArray)
+    //   focusIndex += (action.left ? -1 : 1);
+    //   focusableContent[focusIndex].focus();
+    // }
+
+    if (action.shift) { // if shift key pressed for shift + tab combination
+      if (focusedElement === firstFocusableElement) {
         lastFocusableElement.focus(); // add focus for the last focusable element
         e.preventDefault();
       }
-    } else if (document.activeElement === lastFocusableElement) { // if tab keyEvent is pressed
+    } else if (focusedElement === lastFocusableElement) { // if tab key is pressed
       // if focused has reached to last focusable element then focus first focusable element after pressing tab
       firstFocusableElement.focus(); // add focus for the first focusable element
       e.preventDefault();
