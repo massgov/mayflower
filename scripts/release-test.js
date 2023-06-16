407bbd971f0b6d0db62a0b66a890ddddee7646b4
Minghua Sun <clairesunstudio@gmail.com>
2023-06-16 14:28:48 -0400

    wip

---
+++ scripts/release-test.js
@@ -0,0 +1,39 @@
+const { Octokit, App } = require("octokit")
+const { createAppAuth } = require('@octokit/auth-app');
+require('dotenv').config();
+
+(async function() {
+  const octokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: 1,
+      privateKey: process.env.DANGER_GITHUB_PRIV_KEY,
+      installationId: 123,
+    },
+  });
+  
+  
+  // authenticates as app based on request URLs
+  const {
+    data: { slug }
+  } = await octokit.rest.apps.getAuthenticated();
+
+  console.log(data)
+  
+  // creates an installation access token as needed
+  // assumes that installationId 123 belongs to @octocat, otherwise the request will fail
+  octokit.rest.issues.create({
+    owner: "massgov",
+    repo: "mayflower",
+    title: "Hello world from " + slug,
+  });
+  
+  // const app = new App({ appId, privateKey });
+  // const { data: slug } = await app.octokit.rest.apps.getAuthenticated();
+  // const octokit = await app.getInstallationOctokit(123);
+  // await octokit.rest.issues.create({
+  //   owner: "massgov",
+  //   repo: "mayflower",
+  //   title: "Hello world from " + slug,
+  // });
+})
