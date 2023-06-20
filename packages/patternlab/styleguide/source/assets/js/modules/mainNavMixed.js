ea413f5b2215e3166880d407a0dd00e5051926e8
abaghdasaryan <arthur.baghdasar@gmail.com>
2023-06-20 16:15:21 +0400

    DP-28478: [a11y Mayflower Patternlab] duplicate 'main navigation' labels in screen reader

--- packages/patternlab/styleguide/source/assets/js/modules/mainNavMixed.js
+++ packages/patternlab/styleguide/source/assets/js/modules/mainNavMixed.js
@@ -51,11 +51,11 @@ function toggleGoogleTranslate() {
 // Set aria-label of nav based on the screen size.
 function navAriaLabel() {
   if (document.querySelector('header.ma__header__mixed')) {
-    if ($(window).width() > 840) {
-      $('nav.ma__header__hamburger__nav').attr('aria-label', 'utility options');
+    if (window.innerWidth > 840) {
+      document.querySelector('nav.ma__header__hamburger__nav').setAttribute('aria-label', 'utility options');
     }
     else {
-      $('nav.ma__header__hamburger__nav').attr('aria-label', 'main navigation');
+      document.querySelector('nav.ma__header__hamburger__nav').setAttribute('aria-label', 'main navigation');
     }
   }
 }
