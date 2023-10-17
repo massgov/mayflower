5e08070bf2dc8c991d8ca04c3dea60d1da710f21
Minghua Sun <clairesunstudio@gmail.com>
2023-10-17 10:13:40 -0400

    add javascrip and tweak layout

---
+++ packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
@@ -0,0 +1,22 @@
+export default (function (window, document,$) {
+    "use strict";
+
+    $('.ma__header-search__input').each(function(){
+        const $searchInput = $(this);
+        // Add an event listener to the input element
+        $searchInput.on('input', function() {
+            var inputValue = $(this).val();
+            if (inputValue) {
+                $(".ma__header-search-suggestions-option-input").each(function() {
+                    $(this).text(inputValue);
+                })
+                $searchInput.next(".ma__header-search-suggestions").removeClass("hidden");
+                console.log(inputValue);
+            } else {
+                $searchInput.next(".ma__header-search-suggestions").addClass("hidden");
+            }
+        });
+    });
+
+  })(window, document,jQuery);
+  
