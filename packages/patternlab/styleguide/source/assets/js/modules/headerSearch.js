253927fcca178146de680c7e8594fa30e69528fe
Minghua Sun <clairesunstudio@gmail.com>
2023-10-17 17:30:00 -0400

    add keyboard interactions

--- packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
+++ packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
@@ -1,9 +1,13 @@
 export default (function (window, document,$) {
     "use strict";
 
-    $('.ma__header-search__input').each(function(){
-        let $searchInput = $(this);
-        let options = document.querySelectorAll(".ma__header-search-suggestion-option");
+    $('.ma__header-search').each(function(){
+        let $searchInput = $(this).find('.ma__header-search__input');
+        let $options = $(this).find('.ma__header-search-suggestion-option');
+        console.log()
+        console.log($options)
+        let optionsCount = $options.length;
+        var focusIndex = 0;
         // Add an event listener to the input element
         $searchInput.on('input', function() {
             let inputValue = $(this).val();
@@ -17,47 +21,33 @@ export default (function (window, document,$) {
                 $searchInput.next(".ma__header-search-suggestions").addClass("hidden");
             }
         });
-        $searchInput.on('keyDown', function(e) {
-            console.log(options)
+        $searchInput.keydown(function(e) {
             if (e.key === "ArrowDown" || e.code === "ArrowDown") {
-                options[0].focus();
+                focusIndex = 0;
+                $options[focusIndex].focus();
+                $options[focusIndex].addClass('hover');
             }
             if (e.key === "ArrowUp" || e.code === "ArrowUp") { 
-                options[options.length() -1].focus()
+                focusIndex = [optionsCount -1];
+                $options[focusIndex].focus();
+                $options[focusIndex].addClass('hover');
             }
         });
 
-    //   options.forEach((option) => {
-    //     option.addEventListener("keydown", function (e) {
-    //       let targetParent = e.target.closest(".js-main-nav-hamburger__subitem");
-  
-    //       if (e.key === "ArrowDown" || e.code === "ArrowDown") {
-    //         if (targetParent.nextElementSibling) {
-    //           targetParent.nextElementSibling.querySelector("a").focus();
-    //         } else {
-    //           // Set focus on the first sibling submenu item option.
-    //           document
-    //             .querySelector(
-    //               ".js-main-nav-hamburger-content:not(.is-closed) .js-main-nav-hamburger__subitem:first-of-type a"
-    //             )
-    //             .focus();
-    //         }
-    //       }
-  
-    //       if (e.key === "ArrowUp" || e.code === "ArrowUp") {
-    //         if (targetParent.previousElementSibling) {
-    //           targetParent.previousElementSibling.querySelector("a").focus();
-    //         } else {
-    //           // Set focus on the last sibling submenu item option.
-    //           document
-    //             .querySelector(
-    //               ".js-main-nav-hamburger-content:not(.is-closed) .js-main-nav-hamburger__subitem:last-of-type a"
-    //             )
-    //             .focus();
-    //         }
-    //       }
-    //     });
-    //   });
+        Array.from($options).forEach(function(option) {
+            console.log(option)
+            option.onkeydown = function (e) {
+                if (e.key === "ArrowDown" || e.code === "ArrowDown") {
+                    focusIndex += 1;
+                    $options[focusIndex].focus();
+                }
+        
+                if (e.key === "ArrowUp" || e.code === "ArrowUp") {
+                    focusIndex -= 1;
+                    $options[focusIndex].focus();
+                }
+            }
+        });
     });
 
   })(window, document,jQuery);
