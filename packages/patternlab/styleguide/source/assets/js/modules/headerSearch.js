fb7e049271d8e72537f25d74b96d9df90dad4a47
Minghua Sun <clairesunstudio@gmail.com>
2023-10-18 11:29:01 -0400

    fix logic and mobile display

--- packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
+++ packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
@@ -8,8 +8,14 @@ export default (function (window, document,$) {
         let focusIndex = 0;
         let key = (e) => ({
             arrowDown: e.key === "ArrowDown" || e.code === "ArrowDown",
-            arrowUp: e.key === "ArrowUp" || e.code === "ArrowUp"
+            arrowUp: e.key === "ArrowUp" || e.code === "ArrowUp",
+            escape: e.key === "Escape" || e.code === "Escape",
         });
+        let setFocus = function(index) {
+            $options[index].focus();
+            $options.removeClass('hover');
+            $options[index].classList.add('hover');
+        }
         // Add an event listener to the input element
         $searchInput.on('input', function() {
             let inputValue = $(this).val();
@@ -17,31 +23,37 @@ export default (function (window, document,$) {
                 $(".ma__header-search-suggestion-option-input").each(function() {
                     $(this).text(inputValue);
                 })
+                $searchInput.attr("aria-expanded", true);
                 $searchInput.next(".ma__header-search-suggestions").removeClass("hidden");
             } else {
+                $searchInput.attr("aria-expanded", false);
                 $searchInput.next(".ma__header-search-suggestions").addClass("hidden");
             }
         });
         $searchInput.keydown(function(e) {
             if (key(e).arrowDown) {
                 focusIndex = 0;
-                $options[focusIndex].focus();
-                $options[focusIndex].addClass('hover');
+                setFocus(focusIndex);
             }
             if (key(e).arrowUp) { 
                 focusIndex = [optionsCount -1];
-                $options[focusIndex].focus();
-                $options[focusIndex].addClass('hover');
+                setFocus(focusIndex);
             }
         });
 
         Array.from($options).forEach(function(option) {
             option.onkeydown = function (e) {
                 if (key(e).arrowDown || key(e).arrowUp) {
+                    // prevent scrolling the page
                     e.preventDefault();
                     key(e).arrowDown ? focusIndex += 1 : focusIndex -= 1;
+                    // loop through indices between 0 and optionsCount -1
                     focusIndex = (focusIndex + optionsCount) % optionsCount;
-                    $options[focusIndex].focus();
+                    setFocus(focusIndex);
+                }
+                if (key(e).escape) {
+                    $searchInput.attr("aria-expanded", false);
+                    $searchInput.next(".ma__header-search-suggestions").addClass("hidden");
                 }
             }
         });
