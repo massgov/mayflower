10d830ba9ebe10321fd0ac5bdfee73feaf9f48e3
Yukiko Gannett <yukikogannett@gmail.com>
2023-11-24 18:19:05 -0500

    Revert "change title to aria-label"
    This reverts commit 91ccccea7d8b2186def16046c7e65203d4555a5d.

--- packages/patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ packages/patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -60,9 +60,11 @@ if (hamburgerMenuContainer) {
     selectTopClickableItems(body.clientWidth);
   });
 
-  // Add a label for the utility nav UL on non-home pages in desktopgssssg.
+  // Add a label for the utility nav UL on non-home pages in desktop.
   if (!headerClass.contains("ma__header__mixed") && utilNavWide) {
-    utilNavWide.querySelector(".ma__utility-nav__items").setAttribute("aria-label", "Language options and quick access links");
+      utilNavWide
+        .querySelector(".ma__utility-nav__items")
+        .setAttribute("title", "Language options and quick access links");
   }
 
   // Open and close the menu
