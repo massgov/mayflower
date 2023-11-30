0b4e0bb39e8092bb5cb67e0fa0599328d79093a7
Yukiko Gannett <yukikogannett@gmail.com>
2023-11-30 15:24:03 -0500

    test edit 10

--- packages/patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
+++ packages/patternlab/styleguide/source/assets/js/modules/mainNavHamburger.js
@@ -62,11 +62,11 @@ if (hamburgerMenuContainer) {
   });
 
   // Add a label for the utility nav UL on non-home pages in desktop.
-    if (!headerClass.contains("ma__header__mixed") && (width > 840)) {
-      utilNavWide
-        .querySelector(".ma__utility-nav__items")
-        .setAttribute("aria-label", "Language options and quick access links");
-    }
+    // if (!headerClass.contains("ma__header__mixed") && (width > 840)) {
+    //   utilNavWide
+    //     .querySelector(".ma__utility-nav__items")
+    //     .setAttribute("aria-label", "Language options and quick access links");
+    // }
 
   // Open and close the menu
   if (menuButton !== null) {
@@ -976,10 +976,10 @@ if (hamburgerMenuContainer) {
     );
 
     // Hide "+" symbol from screen readers.
-    // if (utilNarrowNav) {
-    //   let hamburgerGTSymbol = utilNarrowNav.querySelector(".ma__utility-nav__translate #google_translate_element a span:nth-of-type(3)");
-    //   hamburgerGTSymbol.setAttribute("aria-hidden", "true");
-    // }
+    if (utilNarrowNav) {
+      let hamburgerGTSymbol = utilNarrowNav.querySelector(".ma__utility-nav__translate #google_translate_element a span:nth-of-type(3)");
+      hamburgerGTSymbol.setAttribute("aria-hidden", "true");
+    }
 
     if (gTranslateOptionContainer) {
       gTranslateOptionContainer.contentWindow.document.addEventListener(
