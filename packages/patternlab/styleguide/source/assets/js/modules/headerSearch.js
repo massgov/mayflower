54f18666d89e4a5a0e1fa9bc88204cea6b31301f
Minghua Sun <clairesunstudio@gmail.com>
2023-12-05 09:50:46 -0500

    Patternlab/dp 30988 search suggestion dropdown improvement (#1851)
    * wip
    
    * wip
    
    * refactor
    
    * wip
    
    * cleanup and add doc
    
    * add docs
    
    * add changelog
    
    * fix logic
    
    ---------
    
    Co-authored-by: Tom Fleming <tom@lastcallmedia.com>

--- packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
+++ packages/patternlab/styleguide/source/assets/js/modules/headerSearch.js
@@ -19,18 +19,26 @@ export default (function (window, document,$) {
             $options[index].classList.add('hover');
         }
         let timer;
+
+        const openDropdown = () => {
+            $searchInput.attr("aria-expanded", true);
+            $suggestions.removeClass("hidden");
+        }
+        const closeDropdown = () => {
+            $searchInput.attr("aria-expanded", false);
+            $suggestions.addClass("hidden");
+        }
         // On input value change, unhide or hide the suggestions.
         $searchInput.on('input', function() {
             clearTimeout(timer);
-            let inputValue = $(this).val();
-            let helper = `${optionsCount} search scope suggestions are available for ${inputValue}; to navigate, use up and down arrow keys on desktop; to select, use space or enter keys`;
+            const inputValue = $(this).val();
+            const helper = `${optionsCount} search scope suggestions are available for ${inputValue}; to navigate, use up and down arrow keys on desktop; to select, use space or enter keys`;
             // when the input has value, show the suggestions
             if (inputValue) {
                 $(".ma__header-search-suggestion-option-input").each(function() {
                     $(this).text(inputValue);
                 })
-                $searchInput.attr("aria-expanded", true);
-                $suggestions.removeClass("hidden");
+                openDropdown();
 
                 timer = setTimeout(function() {
                     // add a 2 second delay to avoid the status announcement getting cut off. 
@@ -39,10 +47,34 @@ export default (function (window, document,$) {
                 
             } // else, hide the suggestions
             else {
-                $searchInput.attr("aria-expanded", false);
-                $suggestions.addClass("hidden");
+                closeDropdown();
             }
         });
+
+        // hide the suggestions when input and suggestions lose focus
+        // don't close the dropdown when the next element in focus is in the suggestions
+        const handleBlur = () => {
+            // Use a timeout to allow time for the suggestions to receive focus
+            setTimeout(() => {
+                if (!$suggestions[0].contains(document.activeElement)) {
+                    closeDropdown();
+                }
+            }, 200);
+        };
+        $searchInput.on('blur', function() {
+            handleBlur();
+        })
+        $options.on('blur', function() {
+            handleBlur();
+        })
+        // when the input is in focus and has a value, show the suggestions
+        $searchInput.on('focus', function() {
+            const inputValue = $(this).val();
+            if(inputValue) {
+                openDropdown();
+            }
+        })
+
         // keydown from input move focus into the suggestion options
         $searchInput.keydown(function(e) {
             if (key(e).arrowDown) {
@@ -54,6 +86,7 @@ export default (function (window, document,$) {
                 setFocus(focusIndex);
             }
         });
+              
 
         // transform org name to value
         const slugify = (string = '') => (
@@ -72,8 +105,7 @@ export default (function (window, document,$) {
                 }
                 // escape key hide the suggestions
                 if (key(e).escape) {
-                    $searchInput.attr("aria-expanded", false);
-                    $searchInput.next(".ma__header-search-suggestions").addClass("hidden");
+                    closeDropdown();
                     $searchInput.focus();
                 }
             }
