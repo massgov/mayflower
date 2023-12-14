dbf78b08695639f3f5a408001c45b35f97a6a131
Yukiko Gannett <yukikogannett@gmail.com>
2023-12-14 10:29:00 -0500

    js cleanup

--- packages/patternlab/styleguide/source/assets/js/modules/responsiveTable.js
+++ packages/patternlab/styleguide/source/assets/js/modules/responsiveTable.js
@@ -69,7 +69,7 @@ export default (function (window, document, $) {
         top: getAdditionalOffset(),
         opacity: 0,
         "z-index": 50,
-        height: theadHeight,
+        height: theadHeight
       });
       $stickyHeader[0].scrollLeft = 0;
     }
@@ -77,7 +77,7 @@ export default (function (window, document, $) {
     // Add class, remove margins, reset width and wrap table.
     $table.addClass("sticky-enabled").css({
       margin: 0,
-      width: "100%",
+      width: "100%"
     });
 
     $element.toggleClass("has-horizontal-scroll", canScroll);
@@ -97,17 +97,19 @@ export default (function (window, document, $) {
       theadHeight: theadHeight,
       headerStuck: false,
       scrollStuck: false,
-      canScroll,
+      canScroll
     };
     // Set the widths of the header.
     setWidths(rt);
+    // // Setup the scrollbar deminsions.
+    // recalcScrollbar(rt);
 
     // If we are reseting, replace the element. Otherwise, add it.
     if (reset) {
       responsiveTables[index] = rt;
-    }
-    else {
+    } else {
       responsiveTables.push(rt);
+      // rt.$root[0].addEventListener("scroll", handleTableScroll, true);
     }
     // Decide what should be showing or stuck.
     checkVisibility(rt);
@@ -116,26 +118,28 @@ export default (function (window, document, $) {
     let tableTitleCount = $table.find(".ma__table__caption__content").length;
     let scrollInfo = $table.find(".ma__table__caption__scroll-info");
 
-      if (rt.canScroll) {
-        const tableWrapper = element.getElementsByClassName("ma__table--responsive__wrapper")[0];
-        const caption = $(tableWrapper).find("caption");
-        const captionId = $(caption).attr("id");
+    if (rt.canScroll) {
+      const tableWrapper = element.getElementsByClassName(
+        "ma__table--responsive__wrapper"
+      )[0];
+      const caption = $(tableWrapper).find("caption");
+      const captionId = $(caption).attr("id");
 
-        $(tableWrapper).attr("tabindex", "0");
+      $(tableWrapper).attr("tabindex", "0");
 
-        // Table caption and screen reader instructions are separated.
-        // Caption is announced first, then the instrucions follow.
-        let srInfo = captionId + " sr-instructions";
-        $(tableWrapper).attr("aria-labelledby", srInfo);
-        $(scrollInfo).addClass("show");
+      // Table caption and screen reader instructions are separated.
+      // Caption is announced first, then the instrucions follow.
+      let srInfo = captionId + " sr-instructions";
+      $(tableWrapper).attr("aria-labelledby", srInfo);
+      $(scrollInfo).addClass("show");
 
-        if ($(caption).hasClass("hide")) {
-          $(caption).removeClass("hide");
-        }
+      if ($(caption).hasClass("hide")) {
+        $(caption).removeClass("hide");
       }
+    }
 
-    // Hide caption if no table title in no overflow.
-    // scrollInfo is hardcoded in caption, so don't remove caption.
+    // Hide caption if no table title when no overflow.
+    // scrollInfo is hardcoded in caption in the template. Don't remove caption.
     if (!rt.canScroll) {
       if (tableTitleCount == 0) {
         $table.find(".ma__table__caption").addClass("hide");
@@ -171,10 +175,10 @@ export default (function (window, document, $) {
   // or stick the header and scrollbar.
   function checkVisibility(rt) {
     const elementTop = rt.$root.offset().top;
-    const windowTop = $window.scrollTop();
-    const windowBottom = windowTop + $window.height();
+    // const windowTop = $window.scrollTop();
+    // const windowBottom = windowTop + $window.height();
     // + 50 to accommodate table padding for stickyNav
-    const elementBottom = (elementTop + (rt.$root.height() + 50));
+    // const elementBottom = (elementTop + (rt.$root.height() + 50));
     const tableBottom = elementTop + rt.$table.height();
 
     // Handle header visibility.
@@ -199,11 +203,11 @@ export default (function (window, document, $) {
   }
 
   // When scrolling the window, handle header / scrollbar visibility and position.
-  function handleScroll() {
-    responsiveTables.forEach((rt) => {
-      checkVisibility(rt);
-    });
-  }
+  // function handleScroll() {
+  //   responsiveTables.forEach((rt) => {
+  //     checkVisibility(rt);
+  //   });
+  // }
 
   // When the window is resized, reset the tables.
   function handleWindowResize() {
