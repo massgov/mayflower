65cbeaeddb9301e211be30cff0076eb5eabcf2b8
Yukiko Gannett <yukikogannett@gmail.com>
2023-12-18 18:02:44 -0500

    add video

--- packages/patternlab/styleguide/source/assets/js/modules/skipLinkTarget.js
+++ packages/patternlab/styleguide/source/assets/js/modules/skipLinkTarget.js
@@ -2,52 +2,65 @@
  * Control display of a skip link target component.
  */
 
-// For figure content skip links
-document.querySelectorAll(".ma__figure__skip-link").forEach(link => {
+skipLinkLoop(".ma__figure__skip-link");
 
-  link.addEventListener("click", (e) => {
-    // Find the matched linkTarget.
-    let targetId = e.target.getAttribute("href");
-    let skipTarget = document.querySelector(targetId);
-    skipTarget.setAttribute("tabindex", "0");
-    skipTarget.classList.add("visible");
-    // Offset timing to set focus from going to the link target.
-    setTimeout(function timeoutFunction() {
-      skipTarget.focus();
-    }, 200);
+skipLinkLoop(".ma__video__skip-link");
+
+// For non-TOC skip links.
+// Manage visibiliy of the link and its target.
+function skipLinkLoop (skipLinkClass) {
+  document.querySelectorAll(skipLinkClass).forEach((link) => {
+    link.addEventListener("click", (e) => {
+      // Find the matched linkTarget.
+      let targetId = e.target.getAttribute("href");
+      let skipTarget = document.querySelector(targetId);
+      skipTarget.setAttribute("tabindex", "0");
+      skipTarget.classList.add("visible");
+      // Offset timing to set focus from going to the link target.
+      setTimeout(function timeoutFunction() {
+        skipTarget.focus();
+      }, 200);
+    });
   });
-});
 
-// Hide the skipTargetAnchor/e.target as users move to next element.
-// Check a keypress action with tab or arrow keys,
-// which indicates that users attempts to leave the active anchor to another element.
-// No need to display the active anchor at this point until its corresponding link gets clicked again.
-document.querySelectorAll(".ma__figure__skip-link_target a").forEach(anchor => {
+  // Hide the skipTargetAnchor/e.target as users move to next element.
+  // Check a keypress action with tab or arrow keys,
+  // which indicates that users attempts to leave the active anchor to another element.
+  // No need to display the active anchor at this point until its corresponding link gets clicked again.
+  let targetClass = skipLinkClass + "_target";
+  let targetAnchor = document.querySelectorAll(targetClass + " > a");
 
-  anchor.addEventListener("keydown", (e) => {
-
-    if (e.target.getAttribute("tabindex") === "0") {
-      if (e.key === "Tab" || e.key === "ArrowUp" || e.key === "ArrowRight" || e.key === "ArrowDown" || e.key === "ArrowLeft" ) {
-        if (e.target === document.activeElement) {
+  targetAnchor.forEach((anchor) => {
+    anchor.addEventListener("keydown", (e) => {
+      if (e.target.getAttribute("tabindex") === "0") {
+        if (
+          e.key === "Tab" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowDown" ||
+          e.key === "ArrowLeft"
+        ) {
           e.target.classList.remove("visible");
           e.target.setAttribute("tabindex", -1);
         }
       }
-    }
-  });
+    });
 
-  // For screen readers:
-  // When using arrow keys to move to another element, focus stays on the anchor.
-  // This results the anchor remains visible. Hide the anchor when another element gets focus to make it less distracting
-  // to screen reader users.
-  anchor.addEventListener("blur", (e) => {
-
-    if ((e.target !== document.activeElement) && (e.target.getAttribute("tabindex") === "0")) {
-      e.target.classList.remove("visible");
-      e.target.setAttribute("tabindex", -1);
-    }
+  //   // For screen readers:
+  //   // When using arrow keys to move to another element, focus stays on the anchor.
+  //   // This results the anchor remains visible. Hide the anchor when another element gets focus to make it less distracting
+  //   // to screen reader users.
+  //   anchor.addEventListener("blur", (e) => {
+  //     if (
+  //       e.target !== document.activeElement &&
+  //       e.target.getAttribute("tabindex") === "0"
+  //     ) {
+  //       e.target.classList.remove("visible");
+  //       e.target.setAttribute("tabindex", -1);
+  //     }
+  //   });
   });
-});
+}
 
 
 // For TOC skip link target
