46e64704f10a8e5cf92b9123ae28fb46753186f4
Tom Fleming <tom@lastcallmedia.com>
2024-02-01 16:28:42 -0500

    DP-28856 - Update React footer (#1858)
    * Added FooterNew
    
    * Added footer new use
    
    * Added styles for FooterNew
    
    * Renamed original Footer to FooterWithColumns
    
    * Updated FooterWithColumns docs, renamed FooterNew to Footer
    
    * Updated footer, added feedback button
    
    * Added changelog
    
    * Fixed feedback button import
    
    * Fixed dropdown
    
    * Fixed errors
    
    * lint fixes
    
    * Updated footer reference images, added new reference images
    
    * approve react backstop
    
    * rename style file names
    
    * update naming
    
    * docs wip
    
    * switch footer component
    
    * update notes
    
    * update docs
    
    * update docs
    
    * update comments
    
    * fix broken links
    
    * lint fix
    
    ---------
    
    Co-authored-by: Minghua Sun <clairesunstudio@gmail.com>

--- packages/core/stories/components/Footer/Footer.stories.js
+++ packages/core/stories/components/Footer/Footer.stories.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import FooterSlim from '@massds/mayflower-react/dist/FooterSlim';
 import Footer from '@massds/mayflower-react/dist/Footer';
+import FooterSlim from '@massds/mayflower-react/dist/FooterSlim';
+import FooterWithColumns from '@massds/mayflower-react/dist/FooterWithColumns';
 import SiteLogo from '@massds/mayflower-react/dist/SiteLogo';
 import IconMarker from '@massds/mayflower-react/dist/Icon/IconMarker';
-import IconPhone from '@massds/mayflower-react/dist/Icon/IconPhone';
 import IconLaptop from '@massds/mayflower-react/dist/Icon/IconLaptop';
 import IconMail from '@massds/mayflower-react/dist/Icon/IconMail';
 import logo from '@massds/mayflower-assets/static/images/logo/stateseal.png';
 import { attachHTML } from '../../util/renderCode';
 
 import footerLinks from './Footer.data';
+import footerWithColsLinks from './FooterWithCols.data';
 
 const { STORYBOOK_CDN_PATH } = process.env;
 
 const footerBasic = (
+  <Footer
+    footerLinks={footerLinks}
+    footerLogo={{
+      src: logo
+    }}
+    footerText={{}}
+  />
+);
+
+const footerDuelLogo = (
   <FooterSlim
     contact={[
       {
@@ -31,66 +42,12 @@ const footerBasic = (
         component: (
           <span className="ma__email">
             <a href="mailto:edu@state.ma.us" className="ma__email__email">edu@state.ma.us</a>
-            <p className="ma__contact__details">Open Monday through Friday from 9:00 a.m. to 5:00 p.m.</p>
-          </span>
-        )
-      },
-      {
-        icon: <IconPhone />,
-        component: (
-          <span className="ma__phone-number">
-            <a href="tel:617-988-6600" className="ma__phone-number__number">(617) 988-6600</a>
             <p className="ma__contact__details">
-              Our Customer Service Representatives can answer your questions in
-              <b>English</b>
-              ,
-              <b>Español</b>
-              ,
-              <b>Português</b>
-              . Translation services for up to
-              <b>240+ languages</b>
-              {' '}
-              are also available to better serve employees, employers and medical providers.
+              Open Monday through Friday from
+              <b>9:00 a.m. to 5:00 p.m</b>
+              .
             </p>
           </span>
-        )
-      }
-    ]}
-    description="The Department of Early Education and Care's mission is to support the healthy growth and development of all children by providing high quality programs and resources for families"
-    links={[
-      {
-        href: '#',
-        title: 'Lead Agencies Policies'
-      },
-      {
-        href: '#',
-        title: 'Child Care Licensing Procedures'
-      }
-    ]}
-    siteLogo={(
-      <SiteLogo
-        image={{
-          alt: 'Massachusetts state seal', height: 45, src: logo, width: 45
-        }}
-        title="Mass.gov homepage"
-        url={{ domain: 'https://www.mass.gov/' }}
-      />
-)}
-    title="Massachusetts Executive Office of Eductation (EDU)"
-  />
-);
-
-const footerDuelLogo = (
-  <FooterSlim
-    contact={[
-      {
-        icon: <IconMarker />,
-        component: <span className="ma__address"><div className="ma__address__address">51 Sleeper St. 4th Floor, Boston, MA 02210</div></span>
-      },
-      {
-        icon: <IconLaptop />,
-        component: (
-          <a href="https://www.mass.gov/orgs/department-of-early-education-and-care">EEC Official Website</a>
         )
       }
     ]}
@@ -128,9 +85,9 @@ const footerDuelLogo = (
   />
 );
 
-const footer = (
-  <Footer
-    footerLinks={footerLinks}
+const footerWithCol = (
+  <FooterWithColumns
+    footerLinks={footerWithColsLinks}
     footerLogo={{
       src: logo
     }}
@@ -169,15 +126,15 @@ const footer = (
 
 const notesFooterSlim = `// Link to CSS inside the head tag: <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/footer-slim.css">`;
 
-const notesFooter = `// Link to CSS inside the head tag: <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/footer.css">`;
+const notesFooterWithCol = `// Link to CSS inside the head tag: <link rel="stylesheet" href="${STORYBOOK_CDN_PATH}/css/footer-with-columns.css">`;
 
 export const footerExample = () => footerBasic;
 
-export const footerSlim = () => footerBasic;
-attachHTML(footerSlim, footerBasic, notesFooterSlim);
+export const footer = () => footerBasic;
+attachHTML(footer, footerBasic, notesFooterSlim);
 
 export const footerSlimDuelLogo = () => footerDuelLogo;
 attachHTML(footerSlimDuelLogo, footerDuelLogo, notesFooterSlim);
 
-export const footerFullNav = () => footer;
-attachHTML(footerFullNav, footer, notesFooter);
+export const footerFullNav = () => footerWithCol;
+attachHTML(footerFullNav, footerWithCol, notesFooterWithCol);
