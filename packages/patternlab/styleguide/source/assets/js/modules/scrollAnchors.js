f2950f471215f4721c39e49f3a3656821fdb92e7
Minghua Sun <clairesunstudio@gmail.com>
2024-04-10 09:47:17 -0400

    cleanup

--- packages/patternlab/styleguide/source/assets/js/modules/scrollAnchors.js
+++ packages/patternlab/styleguide/source/assets/js/modules/scrollAnchors.js
@@ -201,7 +201,6 @@ export default (function (window,document,$,undefined) {
       }
     }
 
-    console.log(anchors)
     function activateLink(dir) {
       // do we have more than one anchor
       if(numAnchors < 2 || linkScrolling) {
