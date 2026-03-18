b792edc3d2c1a23ce7ca95372ef55612f2d37bbb
Thomas Fleming <thomas.r.fleming@mass.gov>
2026-03-18 15:48:09 -0400

    DP-44787: Footer iteration (#2052)
    * Updated footer links and removed privacy policy in Patternlab and React
    
    * Added changelog
    
    * Removed privacy policy from docs
    
    * Updated state seal to use black seal
    
    * Initial pass at footer layout and styling for Patternlab
    
    * Footer adjustments
    
    * More link adjustments
    
    * Footer fixes
    
    * More font fixes for footer
    
    * Removed text stroke width, weight will need to be adjusted if required
    
    * Fixed stylelint errors
    
    * Updated React template
    
    * Fixed linting errors
    
    * fixed icon import
    
    * padding mobile fix
    
    * role region is redundant
    
    * Updated Pattern Lab screenshots
    
    * Updated React screenshots
    
    * Updated screenshots
    
    * Updated screenshots for Pattern Lab
    
    * Fixed array to string conversion errors
    
    * Removed extra markup
    
    * Fixes for patternlab tests
    
    * Updated screenshots
    
    * Removed caspio from test info details page
    
    * Updated changelog
    
    * Updated screenshots
    
    ---------
    
    Co-authored-by: Arthur Baghdasaryan <62405127+arthurbaghdas@users.noreply.github.com>
    Co-authored-by: Arthur Baghdasaryan <arthur.baghdasar@gmail.com>
    Co-authored-by: Dima <dima@itech4web.com>

--- packages/react/src/components/organisms/Footer/index.js
+++ packages/react/src/components/organisms/Footer/index.js
@@ -6,31 +6,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonFixedFeedback from 'MayflowerReactButtons/ButtonFixedFeedback';
+import SiteLogo from 'MayflowerReactMedia/SiteLogo';
+import IconBuilding from 'MayflowerReactBase/Icon/IconBuilding';
 
 const today = new Date();
 
 const Footer = ({
   footerLinks,
   footerText: {
-    copyright = today.getFullYear(),
-    privacyPolicy = {
-      text: 'Mass.gov Privacy Policy',
-      url: 'https://www.mass.gov/privacypolicy'
-    }
+    copyright = today.getFullYear()
   },
   footerLogo: {
     domain = '/',
     title = 'Mass.gov home page',
+    siteName = 'Mass.gov',
     src
   },
+  stateOrgsHref = '/',
   floatingAction
 }) => (
   <footer data-nosnippet="true" className="ma__footer-new" id="footer">
     <div className="ma__footer-new__container">
-      <div className="ma__footer-new__logo">
-        <a href={domain} title={title}>
-          <img src={src} alt="Massachusetts State Seal" width="100" height="100" />
-        </a>
+      <div className="ma__footer-new__content-top">
+        <div className="ma__footer-new__logo">
+          <SiteLogo
+            url={{
+              domain
+            }}
+            image={{
+              src,
+              width: 56,
+              height: 56
+            }}
+            siteName={siteName}
+            title={title}
+          />
+        </div>
+        <div className="ma__footer-new__state-organizations">
+          <a href={stateOrgsHref} className="direct-link">
+            <IconBuilding height={18} width={20} />
+            <span>State Organizations</span>
+          </a>
+        </div>
       </div>
       <div className="ma__footer-new__content">
         <nav className="ma__footer-new__navlinks" aria-label="site navigation">
@@ -46,7 +63,6 @@ const Footer = ({
             {` ${copyright} Commonwealth of Massachusetts.`}
           </div>
           <span>Mass.gov&#x00AE; is a registered service mark of the Commonwealth of Massachusetts. </span>
-          <a href={privacyPolicy.url}>{privacyPolicy.text}</a>
         </div>
       </div>
     </div>
@@ -63,27 +79,25 @@ Footer.propTypes = {
   // showNavHeading: PropTypes.bool,
   /** Adds footer logo. (Defaults to matching Mass.gov) <br />
    * `src:` logo image source url <br />
-   * `domain:` The URL for the site root <br />
-   * `title:` The title of the logo link
+   * `url:` The URL for the site root <br />
+   * `title:` The title of the logo link <br />
+   * `siteName:` The name of the site
   */
   footerLogo: PropTypes.shape({
     src: PropTypes.string.isRequired,
     domain: PropTypes.string,
-    title: PropTypes.string
+    title: PropTypes.string,
+    siteName: PropTypes.string
   }),
   /** Adds footer info section. (Defaults to matching Mass.gov) <br />
    * `copyright:` Bolded copyright info starting with © <br />
    * `description:` Descriptive info below copyright <br />
-   * `privacyPolicy:` A link to the privacy policy page of the site
   */
   footerText: PropTypes.shape({
     copyright: PropTypes.string,
-    description: PropTypes.string,
-    privacyPolicy: PropTypes.shape({
-      text: PropTypes.string,
-      url: PropTypes.string
-    })
+    description: PropTypes.string
   }),
+  stateOrgsHref: PropTypes.string,
   /** Adds feedback button. */
   floatingAction: PropTypes.bool
 };
